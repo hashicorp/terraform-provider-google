f0cc9af4bcbd0d5bd62dd946220a3a7e1a54e27a
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 11:58:56 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-google/google"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: google.Provider})
 }
