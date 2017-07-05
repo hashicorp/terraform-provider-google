db01252832578f4a3d6aa08d7a4642deb74c146b
Stefan Schmidt <stschmidt@google.com>
2017-07-05 16:24:19 +0200

    Add acceptance test for unlinking the billing account.

--- google/resource_google_project_test.go
+++ google/resource_google_project_test.go
@@ -115,6 +115,13 @@ func TestAccGoogleProject_updateBilling(t *testing.T) {
 					testAccCheckGoogleProjectHasBillingAccount("google_project.acceptance", pid, billingId2),
 				),
 			},
+			// Unlink the billing account
+			resource.TestStep{
+				Config: testAccGoogleProject_createBilling(pid, pname, org, ""),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectHasBillingAccount("google_project.acceptance", pid, ""),
+				),
+			},
 		},
 	})
 }
