236c0f5d24b27165acc878281f16d2a92b3757f4
Joe Selman <jsselman@google.com>
2017-07-17 16:28:51 -0700

    Add support for google_compute_project_metadata_item (#176)
    * Add support for google_compute_project_metadata_item
    
    This allows terraform users to manage single key/value items within the
    project metadata map, rather than the entire map itself.
    
    * Update CHANGELOG.md
    
    * Add details about import

--- google/provider.go
+++ google/provider.go
@@ -86,6 +86,7 @@ func Provider() terraform.ResourceProvider {
 			"google_compute_instance_template":      resourceComputeInstanceTemplate(),
 			"google_compute_network":                resourceComputeNetwork(),
 			"google_compute_project_metadata":       resourceComputeProjectMetadata(),
+			"google_compute_project_metadata_item":  resourceComputeProjectMetadataItem(),
 			"google_compute_region_backend_service": resourceComputeRegionBackendService(),
 			"google_compute_route":                  resourceComputeRoute(),
 			"google_compute_router":                 resourceComputeRouter(),
