7ad09a897688a978d14768440d118f015cfb1c3c
Vincent Roseberry <rosbo@google.com>
2017-07-28 09:43:32 -0700

    Add a comment for provenance of project regex

--- google/validation.go
+++ google/validation.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 )
 
+// Copied from the official Google Cloud auto-generated client.
 const ProjectRegex = "(?:(?:[-a-z0-9]{1,63}\\.)*(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?):)?(?:[0-9]{1,19}|(?:[a-z0-9](?:[-a-z0-9]{0,61}[a-z0-9])?))"
 
 func validateGCPName(v interface{}, k string) (ws []string, errors []error) {
