80c5922a4ec3d37db7d5ced423984ddae0739ffc
Riley Karson <rileykarson@google.com>
2017-07-28 10:28:48 -0700

    Add versioned Beta support to google_compute_global_forwarding_rule.

--- google/resource_compute_global_forwarding_rule.go
+++ google/resource_compute_global_forwarding_rule.go
@@ -5,8 +5,13 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
+
+	computeBeta "google.golang.org/api/compute/v0.beta"
 	"google.golang.org/api/compute/v1"
 )
+
+var GlobalForwardingRuleBaseApiVersion = v1
+var GlobalForwardingRuleVersionedFeatures = []Feature{}
 
 func resourceComputeGlobalForwardingRule() *schema.Resource {
 	return &schema.Resource{
@@ -75,6 +80,7 @@ func resourceComputeGlobalForwardingRule() *schema.Resource {
 }
 
 func resourceComputeGlobalForwardingRuleCreate(d *schema.ResourceData, meta interface{}) error {
+	computeApiVersion := getComputeApiVersion(d, GlobalForwardingRuleBaseApiVersion, GlobalForwardingRuleVersionedFeatures)
 	config := meta.(*Config)
 
 	project, err := getProject(d, config)
@@ -82,7 +88,7 @@ func resourceComputeGlobalForwardingRuleCreate(d *schema.ResourceData, meta inte
 		return err
 	}
 
-	frule := &compute.ForwardingRule{
+	frule := &computeBeta.ForwardingRule{
 		IPAddress:   d.Get("ip_address").(string),
 		IPProtocol:  d.Get("ip_protocol").(string),
 		Description: d.Get("description").(string),
@@ -91,16 +97,36 @@ func resourceComputeGlobalForwardingRuleCreate(d *schema.ResourceData, meta inte
 		Target:      d.Get("target").(string),
 	}
 
-	op, err := config.clientCompute.GlobalForwardingRules.Insert(
-		project, frule).Do()
-	if err != nil {
-		return fmt.Errorf("Error creating Global Forwarding Rule: %s", err)
+	var op interface{}
+	switch computeApiVersion {
+	case v1:
+		v1Frule := &compute.ForwardingRule{}
+		err = Convert(frule, v1Frule)
+		if err != nil {
+			return err
+		}
+
+		op, err = config.clientCompute.GlobalForwardingRules.Insert(project, v1Frule).Do()
+		if err != nil {
+			return fmt.Errorf("Error creating Global Forwarding Rule: %s", err)
+		}
+	case v0beta:
+		v0BetaFrule := &computeBeta.ForwardingRule{}
+		err = Convert(frule, v0BetaFrule)
+		if err != nil {
+			return err
+		}
+
+		op, err = config.clientComputeBeta.GlobalForwardingRules.Insert(project, v0BetaFrule).Do()
+		if err != nil {
+			return fmt.Errorf("Error creating Global Forwarding Rule: %s", err)
+		}
 	}
 
 	// It probably maybe worked, so store the ID now
 	d.SetId(frule.Name)
 
-	err = computeOperationWait(config, op, project, "Creating Global Fowarding Rule")
+	err = computeSharedOperationWait(config, op, project, "Creating Global Fowarding Rule")
 	if err != nil {
 		return err
 	}
@@ -109,6 +135,7 @@ func resourceComputeGlobalForwardingRuleCreate(d *schema.ResourceData, meta inte
 }
 
 func resourceComputeGlobalForwardingRuleUpdate(d *schema.ResourceData, meta interface{}) error {
+	computeApiVersion := getComputeApiVersionUpdate(d, GlobalForwardingRuleBaseApiVersion, GlobalForwardingRuleVersionedFeatures, []Feature{})
 	config := meta.(*Config)
 
 	project, err := getProject(d, config)
@@ -119,15 +146,38 @@ func resourceComputeGlobalForwardingRuleUpdate(d *schema.ResourceData, meta inte
 	d.Partial(true)
 
 	if d.HasChange("target") {
-		target_name := d.Get("target").(string)
-		target_ref := &compute.TargetReference{Target: target_name}
-		op, err := config.clientCompute.GlobalForwardingRules.SetTarget(
-			project, d.Id(), target_ref).Do()
-		if err != nil {
-			return fmt.Errorf("Error updating target: %s", err)
+		target := d.Get("target").(string)
+		targetRef := &computeBeta.TargetReference{Target: target}
+
+		var op interface{}
+		switch computeApiVersion {
+		case v1:
+			v1TargetRef := &compute.TargetReference{}
+			err = Convert(targetRef, v1TargetRef)
+			if err != nil {
+				return err
+			}
+
+			op, err = config.clientCompute.GlobalForwardingRules.SetTarget(
+				project, d.Id(), v1TargetRef).Do()
+			if err != nil {
+				return fmt.Errorf("Error updating target: %s", err)
+			}
+		case v0beta:
+			v0BetaTargetRef := &compute.TargetReference{}
+			err = Convert(targetRef, v0BetaTargetRef)
+			if err != nil {
+				return err
+			}
+
+			op, err = config.clientCompute.GlobalForwardingRules.SetTarget(
+				project, d.Id(), v0BetaTargetRef).Do()
+			if err != nil {
+				return fmt.Errorf("Error updating target: %s", err)
+			}
 		}
 
-		err = computeOperationWait(config, op, project, "Updating Global Forwarding Rule")
+		err = computeSharedOperationWait(config, op, project, "Updating Global Forwarding Rule")
 		if err != nil {
 			return err
 		}
@@ -141,6 +191,7 @@ func resourceComputeGlobalForwardingRuleUpdate(d *schema.ResourceData, meta inte
 }
 
 func resourceComputeGlobalForwardingRuleRead(d *schema.ResourceData, meta interface{}) error {
+	computeApiVersion := getComputeApiVersion(d, GlobalForwardingRuleBaseApiVersion, GlobalForwardingRuleVersionedFeatures)
 	config := meta.(*Config)
 
 	project, err := getProject(d, config)
@@ -148,10 +199,28 @@ func resourceComputeGlobalForwardingRuleRead(d *schema.ResourceData, meta interf
 		return err
 	}
 
-	frule, err := config.clientCompute.GlobalForwardingRules.Get(
-		project, d.Id()).Do()
-	if err != nil {
-		return handleNotFoundError(err, d, fmt.Sprintf("Global Forwarding Rule %q", d.Get("name").(string)))
+	frule := &computeBeta.ForwardingRule{}
+	switch computeApiVersion {
+	case v1:
+		v1Frule, err := config.clientCompute.GlobalForwardingRules.Get(project, d.Id()).Do()
+		if err != nil {
+			return handleNotFoundError(err, d, fmt.Sprintf("Global Forwarding Rule %q", d.Get("name").(string)))
+		}
+
+		err = Convert(v1Frule, frule)
+		if err != nil {
+			return err
+		}
+	case v0beta:
+		v0BetaFrule, err := config.clientComputeBeta.GlobalForwardingRules.Get(project, d.Id()).Do()
+		if err != nil {
+			return handleNotFoundError(err, d, fmt.Sprintf("Global Forwarding Rule %q", d.Get("name").(string)))
+		}
+
+		err = Convert(v0BetaFrule, frule)
+		if err != nil {
+			return err
+		}
 	}
 
 	d.Set("ip_address", frule.IPAddress)
@@ -162,6 +231,7 @@ func resourceComputeGlobalForwardingRuleRead(d *schema.ResourceData, meta interf
 }
 
 func resourceComputeGlobalForwardingRuleDelete(d *schema.ResourceData, meta interface{}) error {
+	computeApiVersion := getComputeApiVersion(d, GlobalForwardingRuleBaseApiVersion, GlobalForwardingRuleVersionedFeatures)
 	config := meta.(*Config)
 
 	project, err := getProject(d, config)
@@ -171,13 +241,21 @@ func resourceComputeGlobalForwardingRuleDelete(d *schema.ResourceData, meta inte
 
 	// Delete the GlobalForwardingRule
 	log.Printf("[DEBUG] GlobalForwardingRule delete request")
-	op, err := config.clientCompute.GlobalForwardingRules.Delete(
-		project, d.Id()).Do()
-	if err != nil {
-		return fmt.Errorf("Error deleting GlobalForwardingRule: %s", err)
+	var op interface{}
+	switch computeApiVersion {
+	case v1:
+		op, err = config.clientCompute.GlobalForwardingRules.Delete(project, d.Id()).Do()
+		if err != nil {
+			return fmt.Errorf("Error deleting GlobalForwardingRule: %s", err)
+		}
+	case v0beta:
+		op, err = config.clientComputeBeta.GlobalForwardingRules.Delete(project, d.Id()).Do()
+		if err != nil {
+			return fmt.Errorf("Error deleting GlobalForwardingRule: %s", err)
+		}
 	}
 
-	err = computeOperationWait(config, op, project, "Deleting GlobalForwarding Rule")
+	err = computeSharedOperationWait(config, op, project, "Deleting GlobalForwarding Rule")
 	if err != nil {
 		return err
 	}
