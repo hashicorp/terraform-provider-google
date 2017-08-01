2c900a61230ceb08614cf993041a9e50f19ae34b
Nicki Watt <nicki.watt@opencredo.com>
2017-08-01 06:48:10 +0100

    Refactored / simplified wait mechanism

--- google/dataproc_cluster_operation.go
+++ google/dataproc_cluster_operation.go
@@ -16,26 +16,23 @@ type DataprocClusterOperationWaiter struct {
 
 func (w *DataprocClusterOperationWaiter) Conf() *resource.StateChangeConf {
 	return &resource.StateChangeConf{
-		Pending: []string{"NOT_DONE"},
-		Target:  []string{"DONE"},
+		Pending: []string{"false"},
+		Target:  []string{"true"},
 		Refresh: w.RefreshFunc(),
 	}
 }
 
 func (w *DataprocClusterOperationWaiter) RefreshFunc() resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		resp, err := w.Service.Projects.Regions.Operations.Get(w.Op.Name).Do()
+		op, err := w.Service.Projects.Regions.Operations.Get(w.Op.Name).Do()
 
 		if err != nil {
 			return nil, "", err
 		}
 
-		log.Printf("[DEBUG] Progress of operation %s: is done -> %t", w.Op.Name, resp.Done)
+		log.Printf("[DEBUG] Got %v while polling for operation %s's 'done' status", op.Done, w.Op.Name)
 
-		if resp.Done {
-			return resp, "DONE", err
-		}
-		return resp, "NOT_DONE", err
+		return op, fmt.Sprint(op.Done), nil
 	}
 }
 
@@ -48,9 +45,14 @@ func dataprocClusterOperationWait(config *Config, op *dataproc.Operation, activi
 	state := w.Conf()
 	state.Timeout = time.Duration(timeoutMinutes) * time.Minute
 	state.MinTimeout = time.Duration(minTimeoutSeconds) * time.Second
-	_, err := state.WaitForState()
+	opRaw, err := state.WaitForState()
 	if err != nil {
 		return fmt.Errorf("Error waiting for %s: %s", activity, err)
+	}
+
+	op = opRaw.(*dataproc.Operation)
+	if op.Error != nil {
+		return fmt.Errorf("Error code %v, message: %s", op.Error.Code, op.Error.Message)
 	}
 
 	return nil
