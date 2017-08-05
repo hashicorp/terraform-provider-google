77265f492f83f26fd1292e3dec033907a8b8e7f9
Roberto Jung Drebes <drebes@google.com>
2017-08-05 13:22:33 +0200

    allow configuring node_config of node_pools specified in container_cluster

--- google/node_config.go
+++ google/node_config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"google.golang.org/api/container/v1"
 )
 
 var schemaNodeConfig = &schema.Schema{
@@ -102,4 +103,69 @@ var schemaNodeConfig = &schema.Schema{
 			},
 		},
 	},
+}
+
+func expandNodeConfig(v interface{}) *container.NodeConfig {
+	nodeConfigs := v.([]interface{})
+	nodeConfig := nodeConfigs[0].(map[string]interface{})
+
+	nc := &container.NodeConfig{}
+
+	if v, ok := nodeConfig["machine_type"]; ok {
+		nc.MachineType = v.(string)
+	}
+
+	if v, ok := nodeConfig["disk_size_gb"]; ok {
+		nc.DiskSizeGb = int64(v.(int))
+	}
+
+	if v, ok := nodeConfig["local_ssd_count"]; ok {
+		nc.LocalSsdCount = int64(v.(int))
+	}
+
+	if v, ok := nodeConfig["oauth_scopes"]; ok {
+		scopesList := v.([]interface{})
+		scopes := []string{}
+		for _, v := range scopesList {
+			scopes = append(scopes, canonicalizeServiceScope(v.(string)))
+		}
+
+		nc.OauthScopes = scopes
+	}
+
+	if v, ok := nodeConfig["service_account"]; ok {
+		nc.ServiceAccount = v.(string)
+	}
+
+	if v, ok := nodeConfig["metadata"]; ok {
+		m := make(map[string]string)
+		for k, val := range v.(map[string]interface{}) {
+			m[k] = val.(string)
+		}
+		nc.Metadata = m
+	}
+
+	if v, ok := nodeConfig["image_type"]; ok {
+		nc.ImageType = v.(string)
+	}
+
+	if v, ok := nodeConfig["labels"]; ok {
+		m := make(map[string]string)
+		for k, val := range v.(map[string]interface{}) {
+			m[k] = val.(string)
+		}
+		nc.Labels = m
+	}
+
+	if v, ok := nodeConfig["tags"]; ok {
+		tagsList := v.([]interface{})
+		tags := []string{}
+		for _, v := range tagsList {
+			tags = append(tags, v.(string))
+		}
+		nc.Tags = tags
+	}
+
+	return nc
+
 }
