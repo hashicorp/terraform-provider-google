ff8bdc9b52f379b7cb42a2c2fe6dcb49e0c130c3
Riley Karson <rileykarson@users.noreply.github.com>
2017-08-10 13:01:56 -0700

    Remove Beta operation waiting code and just wait at v1 instead. (#319)

--- google/compute_shared_operation.go
+++ google/compute_shared_operation.go
@@ -23,20 +23,3 @@ func computeSharedOperationWaitTime(config *Config, op interface{}, project stri
 		panic("Attempted to wait on an Operation of unknown type.")
 	}
 }
-
-func computeSharedOperationWaitZone(config *Config, op interface{}, project string, zone, activity string) error {
-	return computeSharedOperationWaitZoneTime(config, op, project, zone, 4, activity)
-}
-
-func computeSharedOperationWaitZoneTime(config *Config, op interface{}, project string, zone string, minutes int, activity string) error {
-	switch op.(type) {
-	case *compute.Operation:
-		return computeOperationWaitTime(config, op.(*compute.Operation), project, activity, minutes)
-	case *computeBeta.Operation:
-		return computeBetaOperationWaitZoneTime(config, op.(*computeBeta.Operation), project, zone, minutes, activity)
-	case nil:
-		panic("Attempted to wait on an Operation that was nil.")
-	default:
-		panic("Attempted to wait on an Operation of unknown type.")
-	}
-}
