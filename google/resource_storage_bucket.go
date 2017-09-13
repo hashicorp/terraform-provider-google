13da282c70891bd7291bb4ab66ea1332a1b33a5f
Dana Hoffman <danahoffman@google.com>
2017-09-13 17:15:09 +0800

    use new retry wrapper fn in resource_storage_bucket.go

--- google/resource_storage_bucket.go
+++ google/resource_storage_bucket.go
@@ -244,20 +244,13 @@ func resourceStorageBucketCreate(d *schema.ResourceData, meta interface{}) error
 
 	var res *storage.Bucket
 
-	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+	err = retry(func() error {
 		call := config.clientStorage.Buckets.Insert(project, sb)
 		if v, ok := d.GetOk("predefined_acl"); ok {
 			call = call.PredefinedAcl(v.(string))
 		}
-
 		res, err = call.Do()
-		if err == nil {
-			return nil
-		}
-		if gerr, ok := err.(*googleapi.Error); ok && gerr.Code == 429 {
-			return resource.RetryableError(gerr)
-		}
-		return resource.NonRetryableError(err)
+		return err
 	})
 
 	if err != nil {
