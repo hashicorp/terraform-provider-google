eee011fc993b5279c9ab8d3b33df1286428d36e1
Raúl Naveiras <me@raulnaveiras.com>
2017-09-14 18:39:21 +0100

    Add support for setting labels to google_project (#383)

--- google/resource_google_project_test.go
+++ google/resource_google_project_test.go
@@ -3,9 +3,12 @@ package google
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
@@ -61,6 +64,23 @@ func TestAccGoogleProject_createBilling(t *testing.T) {
 				Config: testAccGoogleProject_createBilling(pid, pname, org, billingId),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGoogleProjectHasBillingAccount("google_project.acceptance", pid, billingId),
+				),
+			},
+		},
+	})
+}
+
+// Test that a Project resource can be created with labels
+func TestAccGoogleProject_createLabels(t *testing.T) {
+	pid := "terraform-" + acctest.RandString(10)
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGoogleProject_createLabels(pid, pname, org, "test", "that"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectHasLabels("google_project.acceptance", pid, map[string]string{"test": "that"}),
 				),
 			},
 		},
@@ -145,6 +165,48 @@ func TestAccGoogleProject_merge(t *testing.T) {
 	})
 }
 
+// Test that a Project resource can be updated with labels
+func TestAccGoogleProject_updateLabels(t *testing.T) {
+	pid := "terraform-" + acctest.RandString(10)
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			// create project without labels
+			{
+				Config: testAccGoogleProject_create(pid, pname, org),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectExists("google_project.acceptance", pid),
+				),
+			},
+			// update project with labels
+			{
+				Config: testAccGoogleProject_updateLabels(pid, pname, org, map[string]string{"label": "label-value"}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectExists("google_project.acceptance", pid),
+					testAccCheckGoogleProjectHasLabels("google_project.acceptance", pid, map[string]string{"label": "label-value"}),
+				),
+			},
+			// update project with other labels
+			{
+				Config: testAccGoogleProject_updateLabels(pid, pname, org, map[string]string{"empty-label": ""}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectExists("google_project.acceptance", pid),
+					testAccCheckGoogleProjectHasLabels("google_project.acceptance", pid, map[string]string{"empty-label": ""}),
+				),
+			},
+			// update project delete labels
+			{
+				Config: testAccGoogleProject_create(pid, pname, org),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleProjectExists("google_project.acceptance", pid),
+					testAccCheckGoogleProjectHasNoLabels("google_project.acceptance", pid),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckGoogleProjectExists(r, pid string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[r]
@@ -203,6 +265,77 @@ func testAccCheckGoogleProjectHasMoreBindingsThan(pid string, count int) resourc
 	}
 }
 
+func testAccCheckGoogleProjectHasLabels(r, pid string, expected map[string]string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[r]
+		if !ok {
+			return fmt.Errorf("Not found: %s", r)
+		}
+
+		// State should have the same number of labels
+		if rs.Primary.Attributes["labels.%"] != strconv.Itoa(len(expected)) {
+			return fmt.Errorf("Expected %d labels, got %s", len(expected), rs.Primary.Attributes["labels.%"])
+		}
+
+		// Actual value in API should match state and expected
+		config := testAccProvider.Meta().(*Config)
+
+		found, err := config.clientResourceManager.Projects.Get(pid).Do()
+		if err != nil {
+			return err
+		}
+
+		actual := found.Labels
+		if !reflect.DeepEqual(actual, expected) {
+			// Determine only the different attributes
+			for k, v := range expected {
+				if av, ok := actual[k]; ok && v == av {
+					delete(expected, k)
+					delete(actual, k)
+				}
+			}
+
+			spewConf := spew.NewDefaultConfig()
+			spewConf.SortKeys = true
+			return fmt.Errorf(
+				"Labels not equivalent. Difference is shown below. Top is actual, bottom is expected."+
+					"\n\n%s\n\n%s",
+				spewConf.Sdump(actual), spewConf.Sdump(expected),
+			)
+		}
+		return nil
+	}
+}
+
+func testAccCheckGoogleProjectHasNoLabels(r, pid string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[r]
+		if !ok {
+			return fmt.Errorf("Not found: %s", r)
+		}
+
+		// State should have zero labels
+		if rs.Primary.Attributes["labels.%"] != "0" {
+			return fmt.Errorf("Expected 0 labels, got %s", rs.Primary.Attributes["labels.%"])
+		}
+
+		// Actual value in API should match state and expected
+		config := testAccProvider.Meta().(*Config)
+
+		found, err := config.clientResourceManager.Projects.Get(pid).Do()
+		if err != nil {
+			return err
+		}
+
+		spewConf := spew.NewDefaultConfig()
+		spewConf.SortKeys = true
+		if found.Labels != nil {
+			return fmt.Errorf("Labels should be empty. Actual \n%s", spewConf.Sdump(found.Labels))
+		}
+		return nil
+	}
+}
+
 func testAccGoogleProject_toMerge(pid, name, org string) string {
 	return fmt.Sprintf(`
 resource "google_project" "acceptance" {
@@ -233,6 +366,35 @@ resource "google_project" "acceptance" {
     name = "%s"
     org_id = "%s"
 }`, pid, name, org)
+}
+
+func testAccGoogleProject_createLabels(pid, name, org, key, value string) string {
+	return fmt.Sprintf(`
+resource "google_project" "acceptance" {
+    project_id = "%s"
+    name = "%s"
+    org_id = "%s"
+	labels {
+		"%s" = "%s"
+	}
+}`, pid, name, org, key, value)
+}
+
+func testAccGoogleProject_updateLabels(pid, name, org string, labels map[string]string) string {
+	r := fmt.Sprintf(`
+resource "google_project" "acceptance" {
+    project_id = "%s"
+    name = "%s"
+    org_id = "%s"
+	labels {`, pid, name, org)
+
+	l := ""
+	for key, value := range labels {
+		l += fmt.Sprintf("%q = %q\n", key, value)
+	}
+
+	l += fmt.Sprintf("}\n}")
+	return r + l
 }
 
 func skipIfEnvNotSet(t *testing.T, envs ...string) {
