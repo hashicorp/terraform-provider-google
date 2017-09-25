20929eb468feada228f197c6308d6dd2a481804e
Vincent Roseberry <roseberryv@gmail.com>
2017-09-25 11:30:45 -0700

    Assign IAM policy to folder. (#447)
    * Assign IAM policy to folder.
    * Add documentation for google_folder_iam_policy

--- google/provider.go
+++ google/provider.go
@@ -107,6 +107,7 @@ func Provider() terraform.ResourceProvider {
 			"google_dns_managed_zone":                      resourceDnsManagedZone(),
 			"google_dns_record_set":                        resourceDnsRecordSet(),
 			"google_folder":                                resourceGoogleFolder(),
+			"google_folder_iam_policy":                     resourceGoogleFolderIamPolicy(),
 			"google_logging_project_sink":                  resourceLoggingProjectSink(),
 			"google_sourcerepo_repository":                 resourceSourceRepoRepository(),
 			"google_spanner_instance":                      resourceSpannerInstance(),
