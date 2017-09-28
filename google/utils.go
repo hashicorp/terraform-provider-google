08b228e65dc0861952d4174486d6d74b23b8386d
Dana Hoffman <danahoffman@google.com>
2017-09-28 14:04:12 -0700

    Merge the schemas and logic for the node pool resource and the node pool field in the cluster to aid in maintainability

--- google/utils.go
+++ google/utils.go
@@ -325,3 +325,17 @@ func convertArrToMap(ifaceArr []interface{}) map[string]struct{} {
 	}
 	return sm
 }
+
+func mergeSchemas(a, b map[string]*schema.Schema) map[string]*schema.Schema {
+	merged := make(map[string]*schema.Schema)
+
+	for k, v := range a {
+		merged[k] = v
+	}
+
+	for k, v := range b {
+		merged[k] = v
+	}
+
+	return merged
+}
