c2106a9b63d6b6e429663e521c3f0d3643778263
Mrparkers <michael@parker.gg>
2017-10-25 19:13:14 -0500

    Update import test to use the same config as the basic test

--- google/import_kms_key_ring_test.go
+++ google/import_kms_key_ring_test.go
@@ -6,20 +6,30 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
+	"os"
 )
 
 func TestAccGoogleKmsKeyRing_importBasic(t *testing.T) {
-	t.Parallel()
+	skipIfEnvNotSet(t,
+		[]string{
+			"GOOGLE_ORG",
+			"GOOGLE_BILLING_ACCOUNT",
+		}...,
+	)
 
 	resourceName := "google_kms_key_ring.key_ring"
-	name := acctest.RandString(10)
+
+	projectId := "terraform-" + acctest.RandString(10)
+	projectOrg := os.Getenv("GOOGLE_ORG")
+	projectBillingAccount := os.Getenv("GOOGLE_BILLING_ACCOUNT")
+	keyRingName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testGoogleKmsKeyRing_import(name),
+				Config: testGoogleKmsKeyRing_basic(projectId, projectOrg, projectBillingAccount, keyRingName),
 			},
 
 			resource.TestStep{
@@ -29,13 +39,4 @@ func TestAccGoogleKmsKeyRing_importBasic(t *testing.T) {
 			},
 		},
 	})
-}
-
-func testGoogleKmsKeyRing_import(keyRingName string) string {
-	return fmt.Sprintf(`
-resource "google_kms_key_ring" "key_ring" {
-	name     = "%s"
-	location = "us-central1"
-}
-	`, keyRingName)
 }
