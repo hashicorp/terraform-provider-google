3d8a005efea4b7a140f1930aa12b95afa3af2f7c
Nic Cope <negz@planet.com>
2017-10-27 14:41:39 -0700

    Add a test for importing internal compute addresses

--- google/import_compute_address_test.go
+++ google/import_compute_address_test.go
@@ -28,3 +28,26 @@ func TestAccComputeAddress_importBasic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccComputeAddress_importInternal(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "google_compute_address.internal_with_subnet"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeAddressDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeAddress_internal,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
