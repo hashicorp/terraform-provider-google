0b4973007ddf3904533f8dbe531d3e19914ea5ec
James Turley <jamesturley@gocardless.com>
2017-11-09 15:42:55 +0000

    Run test CryptoKey configs through terraform fmt

--- google/resource_kms_crypto_key_test.go
+++ google/resource_kms_crypto_key_test.go
@@ -297,28 +297,29 @@ func testAccCheckGoogleKmsCryptoKeyVersionsDestroyed(projectId, location, keyRin
 func testGoogleKmsCryptoKey_basic(projectId, projectOrg, projectBillingAccount, keyRingName, cryptoKeyName string) string {
 	return fmt.Sprintf(`
 resource "google_project" "acceptance" {
-	name			= "%s"
-	project_id		= "%s"
-	org_id			= "%s"
-	billing_account	= "%s"
+  name            = "%s"
+  project_id      = "%s"
+  org_id          = "%s"
+  billing_account = "%s"
 }
 
 resource "google_project_services" "acceptance" {
-	project  = "${google_project.acceptance.project_id}"
-	services = [
-		"cloudkms.googleapis.com"
-	]
+  project = "${google_project.acceptance.project_id}"
+
+  services = [
+    "cloudkms.googleapis.com",
+  ]
 }
 
 resource "google_kms_key_ring" "key_ring" {
-	project  = "${google_project_services.acceptance.project}"
-	name     = "%s"
-	location = "us-central1"
+  project  = "${google_project_services.acceptance.project}"
+  name     = "%s"
+  location = "us-central1"
 }
 
 resource "google_kms_crypto_key" "crypto_key" {
-	name     = "%s"
-    key_ring = "${google_kms_key_ring.key_ring.id}"
+  name     = "%s"
+  key_ring = "${google_kms_key_ring.key_ring.id}"
 }
 	`, projectId, projectId, projectOrg, projectBillingAccount, keyRingName, cryptoKeyName)
 }
@@ -326,29 +327,30 @@ resource "google_kms_crypto_key" "crypto_key" {
 func testGoogleKmsCryptoKey_rotation(projectId, projectOrg, projectBillingAccount, keyRingName, cryptoKeyName, rotationPeriod string) string {
 	return fmt.Sprintf(`
 resource "google_project" "acceptance" {
-	name			= "%s"
-	project_id		= "%s"
-	org_id			= "%s"
-	billing_account	= "%s"
+  name            = "%s"
+  project_id      = "%s"
+  org_id          = "%s"
+  billing_account = "%s"
 }
 
 resource "google_project_services" "acceptance" {
-	project  = "${google_project.acceptance.project_id}"
-	services = [
-	    "cloudkms.googleapis.com"
-	]
+  project = "${google_project.acceptance.project_id}"
+
+  services = [
+    "cloudkms.googleapis.com",
+  ]
 }
 
 resource "google_kms_key_ring" "key_ring" {
-	project  = "${google_project.acceptance.project_id}"
-	name     = "%s"
-	location = "us-central1"
+  project  = "${google_project.acceptance.project_id}"
+  name     = "%s"
+  location = "us-central1"
 }
 
 resource "google_kms_crypto_key" "crypto_key" {
-	name     = "%s"
-    key_ring = "${google_kms_key_ring.key_ring.id}"
-    rotation_period = "%s"
+  name            = "%s"
+  key_ring        = "${google_kms_key_ring.key_ring.id}"
+  rotation_period = "%s"
 }
 	`, projectId, projectId, projectOrg, projectBillingAccount, keyRingName, cryptoKeyName, rotationPeriod)
 }
@@ -356,23 +358,24 @@ resource "google_kms_crypto_key" "crypto_key" {
 func testGoogleKmsCryptoKey_removed(projectId, projectOrg, projectBillingAccount, keyRingName string) string {
 	return fmt.Sprintf(`
 resource "google_project" "acceptance" {
-	name 			= "%s"
-	project_id		= "%s"
-	org_id			= "%s"
-	billing_account	= "%s"
+  name            = "%s"
+  project_id      = "%s"
+  org_id          = "%s"
+  billing_account = "%s"
 }
 
 resource "google_project_services" "acceptance" {
-	project  = "${google_project.acceptance.project_id}"
-	services = [
-		"cloudkms.googleapis.com"
-	]
+  project = "${google_project.acceptance.project_id}"
+
+  services = [
+    "cloudkms.googleapis.com",
+  ]
 }
 
 resource "google_kms_key_ring" "key_ring" {
-	project  = "${google_project.acceptance.project_id}"
-	name     = "%s"
-	location = "us-central1"
+  project  = "${google_project.acceptance.project_id}"
+  name     = "%s"
+  location = "us-central1"
 }
 	`, projectId, projectId, projectOrg, projectBillingAccount, keyRingName)
 }
