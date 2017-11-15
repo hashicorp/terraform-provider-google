d4a7ef516f40c4687d73b4a7cb161c495fb5e45a
Vincent Roseberry <rosbo@google.com>
2017-11-15 14:12:40 -0800

    Add ForceNew to org_id field

--- google/resource_google_organization_policy.go
+++ google/resource_google_organization_policy.go
@@ -18,6 +18,7 @@ func resourceGoogleOrganizationPolicy() *schema.Resource {
 			"org_id": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"constraint": {
 				Type:             schema.TypeString,
