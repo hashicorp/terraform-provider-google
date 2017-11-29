8e837f3341400b5e07ad7a6b2ecf4ca391a6cb62
Vincent Roseberry <rosbo@google.com>
2017-11-29 09:59:51 -0800

    Add comment about why the detect_md5hash field is optional and not computed

--- google/resource_storage_bucket_object.go
+++ google/resource_storage_bucket_object.go
@@ -100,6 +100,7 @@ func resourceStorageBucketObject() *schema.Resource {
 			// Detect changes to local file or changes made outside of Terraform to the file stored on the server.
 			"detect_md5hash": &schema.Schema{
 				Type:     schema.TypeString,
+				// This field is not Computed because it needs to trigger a diff.
 				Optional: true,
 				ForceNew: true,
 				// Makes the diff message nicer:
