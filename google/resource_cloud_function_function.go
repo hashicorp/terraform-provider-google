5c394fa6b06a62f04332ab3b64a255177c2e97da
Sarunas Krisciukaitis <sarunas.krisciukaitis@gmail.com>
2017-12-14 11:42:55 +0000

    Adding google_cloudfunction_function resource

--- google/resource_cloud_function_function.go
+++ google/resource_cloud_function_function.go
@@ -1,2 +1,175 @@
 package google
 
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+
+	"log"
+)
+
+const DEFAULT_FUNCTION_TIMEOUT_IN_SEC = 60
+//Min is 1 second, max is 9 minutes 540 sec
+const FUNCTION_TIMEOUT_MAX = 540
+const FUNCTION_TIMEOUT_MIN = 1
+
+func resourceCloudFunctionsFunction() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudFunctionsCreate,
+		Read:   resourceCloudFunctionsRead,
+		Delete: resourceCloudFunctionsDestroy,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"entry_point": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"memory": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"remove_labels": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"retry": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"timeout": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew: 	  true,
+				Default: 	  DEFAULT_FUNCTION_TIMEOUT_IN_SEC,
+				ValidateFunc: validation.IntBetween(FUNCTION_TIMEOUT_MIN, FUNCTION_TIMEOUT_MAX),
+			},
+
+			"update_labels": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"key": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+							ForceNew: true,
+						},
+						"value": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+					},
+				},
+			},
+
+			"source": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"stage_bucket": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"trigger_bucket": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"trigger_http": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"trigger_topic": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"project": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceCloudFunctionsCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Create project name %s", project)
+	//TODO: Add Cloud Function creation code here
+
+	return resourceCloudFunctionsRead(d, meta)
+}
+
+func resourceCloudFunctionsRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Read project name %s", project)
+
+	//TODO: Add Cloud Function read code here
+
+	return nil
+}
+
+func resourceCloudFunctionsDestroy(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Destroy project name %s", project)
+
+	//TODO: Add Cloud Function destroy code here
+
+	return nil
+}
