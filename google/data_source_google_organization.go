ecb382fb4be4915421a5231011b9375c0a02ee71
Kit Ewbank <Kit_Ewbank@hotmail.com>
2017-12-21 14:26:36 -0500

    Use 'ConflictsWith' in schema.

--- google/data_source_google_organization.go
+++ google/data_source_google_organization.go
@@ -15,14 +15,16 @@ func dataSourceGoogleOrganization() *schema.Resource {
 		Read: dataSourceOrganizationRead,
 		Schema: map[string]*schema.Schema{
 			"domain": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"name"},
 			},
 			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"domain"},
 			},
 			"directory_customer_id": {
 				Type:     schema.TypeString,
@@ -43,15 +45,9 @@ func dataSourceGoogleOrganization() *schema.Resource {
 func dataSourceOrganizationRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
-	domain, domainOk := d.GetOk("domain")
-	name, nameOk := d.GetOk("name")
-	if domainOk == nameOk {
-		return fmt.Errorf("One of ['domain', 'name'] must be set to read organizations")
-	}
-
 	var organization *cloudresourcemanager.Organization
-	if domainOk {
-		filter := fmt.Sprintf("domain=%s", domain.(string))
+	if v, ok := d.GetOk("domain"); ok {
+		filter := fmt.Sprintf("domain=%s", v.(string))
 		resp, err := config.clientResourceManager.Organizations.Search(&cloudresourcemanager.SearchOrganizationsRequest{
 			Filter: filter,
 		}).Do()
@@ -60,24 +56,26 @@ func dataSourceOrganizationRead(d *schema.ResourceData, meta interface{}) error 
 		}
 
 		if len(resp.Organizations) == 0 {
-			return fmt.Errorf("Organization not found: %s", domain)
+			return fmt.Errorf("Organization not found: %s", v)
 		}
 		if len(resp.Organizations) > 1 {
 			return fmt.Errorf("More than one matching organization found")
 		}
 
 		organization = resp.Organizations[0]
-	} else {
-		resp, err := config.clientResourceManager.Organizations.Get(name.(string)).Do()
+	} else if v, ok := d.GetOk("name"); ok {
+		resp, err := config.clientResourceManager.Organizations.Get(v.(string)).Do()
 		if err != nil {
 			if gerr, ok := err.(*googleapi.Error); ok && gerr.Code == http.StatusNotFound {
-				return fmt.Errorf("Organization not found: %s", name)
+				return fmt.Errorf("Organization not found: %s", v)
 			}
 
 			return fmt.Errorf("Error reading organization: %s", err)
 		}
 
 		organization = resp
+	} else {
+		return fmt.Errorf("one of domain or name must be set")
 	}
 
 	d.SetId(GetResourceNameFromSelfLink(organization.Name))
