e478e97a608b1a087c3ca28cc9c0eb8f951d6711
Jimmy <jimmy@usebutton.com>
2018-01-08 14:01:08 -0500

    Ensure Elem is always either a *Schema or *Resource

--- google/resource_compute_instance.go
+++ google/resource_compute_instance.go
@@ -271,7 +271,7 @@ func resourceComputeInstance() *schema.Resource {
 			"metadata": &schema.Schema{
 				Type:     schema.TypeMap,
 				Optional: true,
-				Elem:     schema.TypeString,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 
 			"metadata_startup_script": &schema.Schema{
