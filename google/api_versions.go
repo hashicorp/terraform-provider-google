b57b2ee2a0bf16eb34131523dd08d1691ab56c23
Dana Hoffman <danahoffman@google.com>
2018-02-16 17:10:51 -0500

    add json omitted fields back when converting

--- google/api_versions.go
+++ google/api_versions.go
@@ -3,6 +3,7 @@ package google
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -26,7 +27,7 @@ var OrderedContainerApiVersions = []ApiVersion{
 
 // Convert between two types by converting to/from JSON. Intended to switch
 // between multiple API versions, as they are strict supersets of one another.
-// Convert loses information about ForceSendFields and NullFields.
+// item and out are pointers to structs
 func Convert(item, out interface{}) error {
 	bytes, err := json.Marshal(item)
 	if err != nil {
@@ -38,7 +39,57 @@ func Convert(item, out interface{}) error {
 		return err
 	}
 
+	setOmittedFields(item, out)
+
 	return nil
+}
+
+func setOmittedFields(item, out interface{}) {
+	// Both inputs must be pointers, see https://blog.golang.org/laws-of-reflection:
+	// "To modify a reflection object, the value must be settable."
+	iVal := reflect.ValueOf(item).Elem()
+	oVal := reflect.ValueOf(out).Elem()
+
+	// Loop through all the fields of the struct to look for omitted fields and nested fields
+	for i := 0; i < iVal.NumField(); i++ {
+		iField := iVal.Field(i)
+		if isEmptyValue(iField) {
+			continue
+		}
+
+		fieldInfo := iVal.Type().Field(i)
+		oField := oVal.FieldByName(fieldInfo.Name)
+
+		// Only look at fields that exist in the output struct
+		if !oField.IsValid() {
+			continue
+		}
+
+		// If the field contains a 'json:"="' tag, then it was omitted from the Marshal/Unmarshal
+		// call and needs to be added back in.
+		if fieldInfo.Tag.Get("json") == "-" {
+			oField.Set(iField)
+		}
+
+		// If this field is a struct, *struct, []struct, or []*struct, recurse.
+		if iField.Kind() == reflect.Struct {
+			setOmittedFields(iField.Addr().Interface(), oField.Addr().Interface())
+		}
+		if iField.Kind() == reflect.Ptr && iField.Type().Elem().Kind() == reflect.Struct {
+			setOmittedFields(iField.Interface(), oField.Interface())
+		}
+		if iField.Kind() == reflect.Slice && iField.Type().Elem().Kind() == reflect.Struct {
+			for j := 0; j < iField.Len(); j++ {
+				setOmittedFields(iField.Index(j).Addr().Interface(), oField.Index(j).Addr().Interface())
+			}
+		}
+		if iField.Kind() == reflect.Slice && iField.Type().Elem().Kind() == reflect.Ptr &&
+			iField.Type().Elem().Elem().Kind() == reflect.Struct {
+			for j := 0; j < iField.Len(); j++ {
+				setOmittedFields(iField.Index(j).Interface(), oField.Index(j).Interface())
+			}
+		}
+	}
 }
 
 type TerraformResourceData interface {
