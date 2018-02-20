304c0aa6cbbb3d061ce24ce34d50b0def7f1e700
Vincent Roseberry <roseberryv@gmail.com>
2018-02-20 12:40:02 -0800

    Move method out of resource file to prepare for autogen (#1102)

--- google/resource_compute_address.go
+++ google/resource_compute_address.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"regexp"
-	"strings"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
@@ -250,64 +249,4 @@ func resourceComputeAddressImportState(d *schema.ResourceData, meta interface{})
 	d.SetId(addressId.canonicalId())
 
 	return []*schema.ResourceData{d}, nil
-}
-
-type computeAddressId struct {
-	Project string
-	Region  string
-	Name    string
-}
-
-func (s computeAddressId) canonicalId() string {
-	return fmt.Sprintf(computeAddressIdTemplate, s.Project, s.Region, s.Name)
-}
-
-func parseComputeAddressId(id string, config *Config) (*computeAddressId, error) {
-	var parts []string
-	if computeAddressLinkRegex.MatchString(id) {
-		parts = computeAddressLinkRegex.FindStringSubmatch(id)
-
-		return &computeAddressId{
-			Project: parts[1],
-			Region:  parts[2],
-			Name:    parts[3],
-		}, nil
-	} else {
-		parts = strings.Split(id, "/")
-	}
-
-	if len(parts) == 3 {
-		return &computeAddressId{
-			Project: parts[0],
-			Region:  parts[1],
-			Name:    parts[2],
-		}, nil
-	} else if len(parts) == 2 {
-		// Project is optional.
-		if config.Project == "" {
-			return nil, fmt.Errorf("The default project for the provider must be set when using the `{region}/{name}` id format.")
-		}
-
-		return &computeAddressId{
-			Project: config.Project,
-			Region:  parts[0],
-			Name:    parts[1],
-		}, nil
-	} else if len(parts) == 1 {
-		// Project and region is optional
-		if config.Project == "" {
-			return nil, fmt.Errorf("The default project for the provider must be set when using the `{name}` id format.")
-		}
-		if config.Region == "" {
-			return nil, fmt.Errorf("The default region for the provider must be set when using the `{name}` id format.")
-		}
-
-		return &computeAddressId{
-			Project: config.Project,
-			Region:  config.Region,
-			Name:    parts[0],
-		}, nil
-	}
-
-	return nil, fmt.Errorf("Invalid compute address id. Expecting resource link, `{project}/{region}/{name}`, `{region}/{name}` or `{name}` format.")
 }
