67ecf5345e85b310dd302520e51a96eafdfac23f
ishashchuk <irene.shashchuk@gmail.com>
2018-02-28 15:37:24 -0500

    Data source for retrieving project information (#1111)
    * Project data source
    
    * Mirroring project datasource from project resource (#14)

---
+++ google/data_source_google_project.go
@@ -0,0 +1,34 @@
+package google
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceGoogleProject() *schema.Resource {
+	// Generate datasource schema from resource
+	dsSchema := datasourceSchemaFromResourceSchema(resourceGoogleProject().Schema)
+
+	addOptionalFieldsToSchema(dsSchema, "project_id")
+
+	return &schema.Resource{
+		Read:   datasourceGoogleProjectRead,
+		Schema: dsSchema,
+	}
+}
+
+func datasourceGoogleProjectRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	if v, ok := d.GetOk("project_id"); ok {
+		project := v.(string)
+		d.SetId(project)
+	} else {
+		project, err := getProject(d, config)
+		if err != nil {
+			return err
+		}
+		d.SetId(project)
+	}
+
+	return resourceGoogleProjectRead(d, meta)
+}
