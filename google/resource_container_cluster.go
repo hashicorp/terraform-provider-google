46fb949c1f21d45a9eb4877a3a46256382521fc4
Dana Hoffman <danahoffman@google.com>
2018-03-07 11:06:00 -0800

    Set ID for GKE resources before waiting (#1158)

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -567,6 +567,8 @@ func resourceContainerClusterCreate(d *schema.ResourceData, meta interface{}) er
 		}
 	}
 
+	d.SetId(clusterName)
+
 	// Wait until it's created
 	waitErr := containerSharedOperationWait(config, op, project, zoneName, "creating GKE cluster", timeoutInMinutes, 3)
 	if waitErr != nil {
@@ -576,8 +578,6 @@ func resourceContainerClusterCreate(d *schema.ResourceData, meta interface{}) er
 	}
 
 	log.Printf("[INFO] GKE cluster %s has been created", clusterName)
-
-	d.SetId(clusterName)
 
 	return resourceContainerClusterRead(d, meta)
 }
