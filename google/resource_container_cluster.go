6f3b0a61f11a0204e3bab92ee0ad69e67053ee02
Dana Hoffman <danahoffman@google.com>
2018-03-14 14:12:10 -0700

    add update support for pod security policy

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -341,13 +341,11 @@ func resourceContainerCluster() *schema.Resource {
 				Type:     schema.TypeList,
 				Optional: true,
 				MaxItems: 1,
-				ForceNew: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"enabled": {
 							Type:     schema.TypeBool,
 							Required: true,
-							ForceNew: true,
 						},
 					},
 				},
@@ -1087,6 +1085,31 @@ func resourceContainerClusterUpdate(d *schema.ResourceData, meta interface{}) er
 		log.Printf("[INFO] GKE cluster %s: logging service has been updated to %s", d.Id(),
 			logging)
 		d.SetPartial("logging_service")
+	}
+
+	if d.HasChange("pod_security_policy_config") {
+		c := d.Get("pod_security_policy_config")
+		req := &containerBeta.UpdateClusterRequest{
+			Update: &containerBeta.ClusterUpdate{
+				DesiredPodSecurityPolicyConfig: expandPodSecurityPolicyConfig(c),
+			},
+		}
+
+		updateF := func() error {
+			op, err := config.clientContainerBeta.Projects.Zones.Clusters.Update(
+				project, zoneName, clusterName, req).Do()
+			if err != nil {
+				return err
+			}
+			// Wait until it's updated
+			return containerSharedOperationWait(config, op, project, zoneName, "updating GKE cluster pod security policy config", timeoutInMinutes, 2)
+		}
+		if err := lockedCall(lockKey, updateF); err != nil {
+			return err
+		}
+		log.Printf("[INFO] GKE cluster %s pod security policy config has been updated", d.Id())
+
+		d.SetPartial("pod_security_policy_config")
 	}
 
 	d.Partial(false)
--- google/resource_container_cluster_test.go
+++ google/resource_container_cluster_test.go
@@ -738,6 +738,13 @@ func TestAccContainerCluster_withPodSecurityPolicy(t *testing.T) {
 						"pod_security_policy_config.0.enabled", "true"),
 				),
 			},
+			{
+				Config: testAccContainerCluster_withPodSecurityPolicy(clusterName, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerCluster(
+						"google_container_cluster.with_pod_security_policy"),
+				),
+			},
 		},
 	})
 }
