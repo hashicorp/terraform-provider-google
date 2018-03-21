fde96ca9d9ba69f3a53035f489ed292c7b9cd5bf
Dana Hoffman <danahoffman@google.com>
2018-03-21 16:52:23 -0700

    Add new `google_compute_security_policy` resource (#1242)
    * revendor compute/v0.beta
    
    * add new `google_compute_security_policy` resource
    
    * docs updates for security policy
    
    * Add link to official docs

---
+++ google/resource_compute_security_policy_test.go
@@ -0,0 +1,202 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccComputeSecurityPolicy_basic(t *testing.T) {
+	t.Parallel()
+
+	spName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeSecurityPolicyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeSecurityPolicy_basic(spName),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccComputeSecurityPolicy_withRule(t *testing.T) {
+	t.Parallel()
+
+	spName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeSecurityPolicyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeSecurityPolicy_withRule(spName),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccComputeSecurityPolicy_update(t *testing.T) {
+	t.Parallel()
+
+	spName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeSecurityPolicyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeSecurityPolicy_withRule(spName),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+
+			resource.TestStep{
+				Config: testAccComputeSecurityPolicy_update(spName),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+
+			resource.TestStep{
+				Config: testAccComputeSecurityPolicy_withRule(spName),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckComputeSecurityPolicyDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "google_compute_security_policy" {
+			continue
+		}
+
+		pol := rs.Primary.ID
+
+		_, err := config.clientComputeBeta.SecurityPolicies.Get(config.Project, pol).Do()
+		if err == nil {
+			return fmt.Errorf("Security policy %q still exists", pol)
+		}
+	}
+
+	return nil
+}
+
+func testAccComputeSecurityPolicy_basic(spName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_security_policy" "policy" {
+	name        = "%s"
+	description = "basic security policy"
+}
+`, spName)
+}
+
+func testAccComputeSecurityPolicy_withRule(spName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_security_policy" "policy" {
+	name = "%s"
+
+	rule {
+		action   = "allow"
+		priority = "2147483647"
+		match {
+			versioned_expr = "SRC_IPS_V1"
+			config {
+				src_ip_ranges = ["*"]
+			}
+		}
+		description = "default rule"
+	}
+
+	rule {
+		action   = "allow"
+		priority = "2000"
+		match {
+			versioned_expr = "SRC_IPS_V1"
+			config {
+				src_ip_ranges = ["10.0.0.0/24"]
+			}
+		}
+		preview = true
+	}
+}
+`, spName)
+}
+
+func testAccComputeSecurityPolicy_update(spName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_security_policy" "policy" {
+	name        = "%s"
+	description = "updated description"
+
+	// keep this
+	rule {
+		action   = "allow"
+		priority = "2147483647"
+		match {
+			versioned_expr = "SRC_IPS_V1"
+			config {
+				src_ip_ranges = ["*"]
+			}
+		}
+		description = "default rule"
+	}
+
+	// add this
+	rule {
+		action   = "deny(403)"
+		priority = "1000"
+		match {
+			versioned_expr = "SRC_IPS_V1"
+			config {
+				src_ip_ranges = ["10.0.1.0/24"]
+			}
+		}
+	}
+
+	// update this
+	rule {
+		action   = "allow"
+		priority = "2000"
+		match {
+			versioned_expr = "SRC_IPS_V1"
+			config {
+				src_ip_ranges = ["10.0.0.0/24"]
+			}
+		}
+		description = "updated description"
+		preview     = false
+	}
+}
+`, spName)
+}
---
+++ website/docs/r/compute_security_policy.html.markdown
@@ -0,0 +1,112 @@
+---
+layout: "google"
+page_title: "Google: google_compute_security_policy"
+sidebar_current: "docs-google-compute-security-policy"
+description: |-
+  Creates a Security Policy resource for Google Compute Engine.
+---
+
+# google\_compute\_security\_policy
+
+A Security Policy defines an IP blacklist or whitelist that protects load balanced Google Cloud services by denying or permitting traffic from specified IP ranges. For more information
+see the [official documentation](https://cloud.google.com/armor/docs/configure-security-policies)
+and the [API](https://cloud.google.com/compute/docs/reference/rest/beta/securityPolicies).
+
+~> **Note:** This entire resource is in [Beta](/docs/providers/google/index.html#beta-features)
+
+## Example Usage
+
+```hcl
+resource "google_compute_security_policy" "policy" {
+  name = "my-policy"
+
+  rule {
+    action   = "deny(403)"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["9.9.9.9/32"]
+      }
+    }
+    description = "Deny access to IPs in 9.9.9.0/24"
+  }
+
+  rule {
+    action   = "allow"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The name of the security policy.
+
+- - -
+
+* `description` - (Optional) An optional description of this security policy. Max size is 2048.
+
+* `project` - (Optional) The project in which the resource belongs. If it
+    is not provided, the provider project is used.
+
+* `rule` - (Optional) The set of rules that belong to this policy. There must always be a default
+    rule (rule with priority 2147483647 and match "\*"). If no rules are provided when creating a
+    security policy, a default rule with action "allow" will be added. Structure is documented below.
+
+The `rule` block supports:
+
+* `action` - (Required) Action to take when `match` matches the request. Valid values:
+  * "allow" : allow access to target
+  * "deny(status)" : deny access to target, returns the  HTTP response code specified (valid values are 403, 404 and 502)
+
+* `priority` - (Required) An unique positive integer indicating the priority of evaluation for a rule.
+    Rules are evaluated from highest priority (lowest numerically) to lowest priority (highest numerically) in order.
+
+* `match` - (Required) A match condition that incoming traffic is evaluated against.
+    If it evaluates to true, the corresponding `action` is enforced. Structure is documented below.
+
+* `description` - (Optional) An optional description of this rule. Max size is 64.
+
+* `preview` - (Optional) When set to true, the `action` specified above is not enforced.
+    Stackdriver logs for requests that trigger a preview action are annotated as such.
+
+The `match` block supports:
+
+* `config` - (Required) The configuration options available when specifying `versioned_expr`.
+    Structure is documented below.
+
+* `versioned_expr` - (Required) Predefined rule expression. Available options:
+    * SRC_IPS_V1: Must specify the corresponding `src_ip_ranges` field in `config`.
+
+The `config` block supports:
+
+* `src_ip_ranges` - (Required) Set of IP addresses or ranges (IPV4 or IPV6) in CIDR notation
+    to match against inbound traffic. There is a limit of 5 IP ranges per rule. A value of '\*' matches all IPs
+    (can be used to override the default behavior).
+
+## Attributes Reference
+
+In addition to the arguments listed above, the following computed attributes are
+exported:
+
+* `fingerprint` - Fingerprint of this resource.
+
+* `self_link` - The URI of the created resource.
+
+## Import
+
+Security policies can be imported using the `name`, e.g.
+
+```
+$ terraform import google_compute_security_policy.policy my-policy
+```
