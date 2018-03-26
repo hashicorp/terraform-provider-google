650553aa82c7ab094248c7b98fdf8be898569dd6
Nick Jacques <nickjacques@gmail.com>
2018-03-26 15:22:31 -0500

    Fix update logic for custom_features and add enabled_features

--- google/resource_compute_ssl_policy.go
+++ google/resource_compute_ssl_policy.go
@@ -70,6 +70,13 @@ func resourceComputeSslPolicy() *schema.Resource {
 				ForceNew: true,
 			},
 
+			"enabled_features": &schema.Schema{
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+
 			"fingerprint": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
@@ -158,10 +165,8 @@ func resourceComputeSslPolicyRead(d *schema.ResourceData, meta interface{}) erro
 	d.Set("fingerprint", sslPolicy.Fingerprint)
 	d.Set("project", project)
 	d.Set("self_link", ConvertSelfLinkToV1(sslPolicy.SelfLink))
-
-	if sslPolicy.CustomFeatures != nil {
-		d.Set("custom_features", convertStringArrToInterface(sslPolicy.CustomFeatures))
-	}
+	d.Set("enabled_features", convertStringArrToInterface(sslPolicy.EnabledFeatures))
+	d.Set("custom_features", convertStringArrToInterface(sslPolicy.CustomFeatures))
 
 	d.SetId(sslPolicy.Name)
 
@@ -180,10 +185,23 @@ func resourceComputeSslPolicyUpdate(d *schema.ResourceData, meta interface{}) er
 	name := d.Get("name").(string)
 
 	sslPolicy := &computeBeta.SslPolicy{
-		Fingerprint:    d.Get("fingerprint").(string),
-		Profile:        d.Get("profile").(string),
-		MinTlsVersion:  d.Get("min_tls_version").(string),
-		CustomFeatures: convertStringSet(d.Get("custom_features").(*schema.Set)),
+		Fingerprint: d.Get("fingerprint").(string),
+	}
+
+	if v, ok := d.GetOk("profile"); ok {
+		sslPolicy.Profile = v.(string)
+	}
+
+	if v, ok := d.GetOk("min_tls_version"); ok {
+		sslPolicy.MinTlsVersion = v.(string)
+	}
+
+	if v, ok := d.Get("custom_features").(*schema.Set); ok {
+		if v.Len() > 0 {
+			sslPolicy.CustomFeatures = convertStringSet(v)
+		} else {
+			sslPolicy.NullFields = append(sslPolicy.NullFields, "CustomFeatures")
+		}
 	}
 
 	op, err := config.clientComputeBeta.SslPolicies.Patch(project, name, sslPolicy).Do()
@@ -191,7 +209,7 @@ func resourceComputeSslPolicyUpdate(d *schema.ResourceData, meta interface{}) er
 		return fmt.Errorf("Error updating SSL Policy: %s", err)
 	}
 
-	err = computeSharedOperationWaitTime(config.clientCompute, op, project, int(d.Timeout(schema.TimeoutCreate).Minutes()), "Updating SSL Policy")
+	err = computeSharedOperationWaitTime(config.clientCompute, op, project, int(d.Timeout(schema.TimeoutUpdate).Minutes()), "Updating SSL Policy")
 	if err != nil {
 		return err
 	}
