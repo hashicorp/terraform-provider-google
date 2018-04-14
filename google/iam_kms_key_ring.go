793c296dccce71b41707554dba7834ab561dcbbb
Dana Hoffman <danahoffman@google.com>
2018-04-13 18:41:13 -0700

    try a different kms id

--- google/iam_kms_key_ring.go
+++ google/iam_kms_key_ring.go
@@ -31,7 +31,7 @@ func NewKmsKeyRingIamUpdater(d *schema.ResourceData, config *Config) (ResourceIa
 	}
 
 	return &KmsKeyRingIamUpdater{
-		resourceId: keyRingId.keyRingId(),
+		resourceId: keyRingId.terraformId(),
 		Config:     config,
 	}, nil
 }
@@ -42,7 +42,7 @@ func KeyRingIdParseFunc(d *schema.ResourceData, config *Config) error {
 		return err
 	}
 
-	d.Set("key_ring_id", keyRingId.keyRingId())
+	d.Set("key_ring_id", keyRingId.terraformId())
 	d.SetId(keyRingId.keyRingId())
 	return nil
 }
