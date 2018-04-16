49191c5d910e4238341f69d9b9af6f4de59a0ad2
Vincent Roseberry <roseberryv@gmail.com>
2018-04-16 10:25:49 -0700

    Fix auto-delete default network in google_project. (#1336)
    * Use projectId and enable required compute API
    
    * Delete firewall rules before deleting default network

--- google/resource_google_project.go
+++ google/resource_google_project.go
@@ -151,15 +151,13 @@ func resourceGoogleProjectCreate(d *schema.ResourceData, meta interface{}) error
 	// people if we don't have to.  The GCP Console is doing the same thing - creating
 	// a network and deleting it in the background.
 	if !d.Get("auto_create_network").(bool) {
-		op, err := config.clientCompute.Networks.Delete(
-			project.Name, "default").Do()
-		if err != nil {
-			return fmt.Errorf("Error deleting network: %s", err)
+		// The compute API has to be enabled before we can delete a network.
+		if err = enableService("compute.googleapis.com", project.ProjectId, config); err != nil {
+			return fmt.Errorf("Error enabling the Compute Engine API required to delete the default network: %s", err)
 		}
 
-		err = computeOperationWaitTime(config.clientCompute, op, project.Name, "Deleting Network", 10)
-		if err != nil {
-			return err
+		if err = forceDeleteComputeNetwork(project.ProjectId, "default", config); err != nil {
+			return fmt.Errorf("Error deleting default network in project %s: %s", project.ProjectId, err)
 		}
 	}
 	return nil
@@ -342,4 +340,36 @@ func resourceProjectImportState(d *schema.ResourceData, meta interface{}) ([]*sc
 	// don't see a diff immediately after import.
 	d.Set("auto_create_network", true)
 	return []*schema.ResourceData{d}, nil
+}
+
+// Delete a compute network along with the firewall rules inside it.
+func forceDeleteComputeNetwork(projectId, networkName string, config *Config) error {
+	networkLink := fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/global/networks/%s", projectId, networkName)
+
+	token := ""
+	for paginate := true; paginate; {
+		filter := fmt.Sprintf("network eq %s", networkLink)
+		resp, err := config.clientCompute.Firewalls.List(projectId).Filter(filter).Do()
+		if err != nil {
+			return fmt.Errorf("Error listing firewall rules in proj: %s", err)
+		}
+
+		log.Printf("[DEBUG] Found %d firewall rules in %q network", len(resp.Items), networkName)
+
+		for _, firewall := range resp.Items {
+			op, err := config.clientCompute.Firewalls.Delete(projectId, firewall.Name).Do()
+			if err != nil {
+				return fmt.Errorf("Error deleting firewall: %s", err)
+			}
+			err = computeSharedOperationWait(config.clientCompute, op, projectId, "Deleting Firewall")
+			if err != nil {
+				return err
+			}
+		}
+
+		token = resp.NextPageToken
+		paginate = token != ""
+	}
+
+	return deleteComputeNetwork(projectId, networkName, config)
 }
