135f5c414473a9ce8497f3c16af8c798f4b7b4b7
Vincent Roseberry <roseberryv@gmail.com>
2018-05-03 09:53:22 -0700

    Allow setting folder_id to empty string on google_project (#1425)
    * Allow setting folder_id to empty string on google_project
    
    * Simplify logic
    
    * Update Changelog

--- google/resource_google_project_test.go
+++ google/resource_google_project_test.go
@@ -170,6 +170,23 @@ func TestAccProject_deleteDefaultNetwork(t *testing.T) {
 	})
 }
 
+func TestAccProject_parentFolder(t *testing.T) {
+	t.Parallel()
+
+	org := getTestOrgFromEnv(t)
+	pid := "terraform-" + acctest.RandString(10)
+	folderDisplayName := "tf-test-" + acctest.RandString(10)
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProject_parentFolder(pid, pname, folderDisplayName, org),
+			},
+		},
+	})
+}
+
 func testAccCheckGoogleProjectExists(r, pid string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[r]
@@ -312,6 +329,24 @@ resource "google_project" "acceptance" {
 	billing_account = "%s" # requires billing to enable compute API
 	auto_create_network = false
 }`, pid, name, org, billing)
+}
+
+func testAccProject_parentFolder(pid, projectName, folderName, org string) string {
+	return fmt.Sprintf(`
+resource "google_project" "acceptance" {
+  project_id = "%s"
+  name       = "%s"
+  # ensures we can set both org_id and folder_id as long as only one is not empty.
+  org_id     = ""                            
+  folder_id  = "${google_folder.folder1.id}"
+}
+
+resource "google_folder" "folder1" {
+  display_name = "%s"
+  parent       = "organizations/%s"
+}
+
+`, pid, projectName, folderName, org)
 }
 
 func skipIfEnvNotSet(t *testing.T, envs ...string) {
