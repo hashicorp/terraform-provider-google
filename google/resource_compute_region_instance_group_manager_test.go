b3ceef76ee6903ad5e74ecfe28ec7d51be23c6ce
Nathan McKinley <nmckinley@google.com>
2018-05-08 11:23:35 -0700

    Get region from provider in case of import (or in case of not-set-in-resource) (#1447)

--- google/resource_compute_region_instance_group_manager_test.go
+++ google/resource_compute_region_instance_group_manager_test.go
@@ -113,6 +113,12 @@ func TestAccRegionInstanceGroupManager_update(t *testing.T) {
 						&manager),
 				),
 			},
+			resource.TestStep{
+				ResourceName:            "google_compute_region_instance_group_manager.igm-update",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"update_strategy"},
+			},
 		},
 	})
 }
