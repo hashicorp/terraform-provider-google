38f1aea2875af17c95f048b24b79cc5a1aaf9424
josemanuelt <josemanuelt@google.com>
2018-05-22 11:26:01 -0500

    Added name and project_id regex validations (#1498)

--- google/resource_google_project.go
+++ google/resource_google_project.go
@@ -32,9 +32,10 @@ func resourceGoogleProject() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"project_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validateProjectID(),
 			},
 			"skip_delete": &schema.Schema{
 				Type:     schema.TypeBool,
@@ -47,8 +48,9 @@ func resourceGoogleProject() *schema.Resource {
 				Default:  true,
 			},
 			"name": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validateProjectName(),
 			},
 			"org_id": &schema.Schema{
 				Type:     schema.TypeString,
