ef23dc9f48698f4ea18c9f6cb79943c6e4141801
Jamie Lennox <jamielennox@gmail.com>
2018-05-30 16:20:26 +1000

    Acceptance testing for cloudbuild trigger with filename
    Ensure that when a cloudbuild repo trigger is created with a filename,
    that filename is what actually ends up in the cloud.

--- google/resource_cloudbuild_build_trigger_test.go
+++ google/resource_cloudbuild_build_trigger_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
+	cloudbuild "google.golang.org/api/cloudbuild/v1"
 )
 
 func TestAccCloudBuildTrigger_basic(t *testing.T) {
@@ -37,24 +38,80 @@ func TestAccCloudBuildTrigger_basic(t *testing.T) {
 	})
 }
 
+func TestAccCloudBuildTrigger_filename(t *testing.T) {
+	t.Parallel()
+
+	projectID := "terraform-" + acctest.RandString(10)
+	projectOrg := getTestOrgFromEnv(t)
+	projectBillingAccount := getTestBillingAccountFromEnv(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGoogleCloudBuildTriggerVersionsDestroyed,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testGoogleCloudBuildTrigger_filename(projectID, projectOrg, projectBillingAccount),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleCloudFilenameConfig("google_cloudbuild_trigger.filename_build_trigger"),
+				),
+			},
+			resource.TestStep{
+				Config: testGoogleCloudBuildTrigger_removed(projectID, projectOrg, projectBillingAccount),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleCloudBuildTriggerWasRemovedFromState("google_cloudbuild_trigger.filename_build_trigger"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccGetBuildTrigger(s *terraform.State, resourceName string) (*cloudbuild.BuildTrigger, error) {
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return nil, fmt.Errorf("Resource not found: %s", resourceName)
+	}
+
+	if rs.Primary.ID == "" {
+		return nil, fmt.Errorf("No ID is set")
+	}
+
+	config := testAccProvider.Meta().(*Config)
+	project := rs.Primary.Attributes["project"]
+
+	trigger, err := config.clientBuild.Projects.Triggers.Get(project, rs.Primary.ID).Do()
+	if err != nil {
+		return nil, fmt.Errorf("Trigger does not exist")
+	}
+
+	return trigger, nil
+}
+
 func testAccCheckGoogleCloudBuildTriggerExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+		_, err := testAccGetBuildTrigger(s, resourceName)
 
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("Resource not found: %s", resourceName)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No ID is set")
-		}
-		config := testAccProvider.Meta().(*Config)
-		project := rs.Primary.Attributes["project"]
-
-		_, err := config.clientBuild.Projects.Triggers.Get(project, rs.Primary.ID).Do()
 		if err != nil {
 			return fmt.Errorf("Trigger does not exist")
 		}
+
+		return nil
+	}
+}
+
+func testAccCheckGoogleCloudFilenameConfig(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		trigger, err := testAccGetBuildTrigger(s, resourceName)
+
+		if err != nil {
+			return fmt.Errorf("Trigger does not exist")
+		}
+
+		if trigger.Filename != "cloudbuild.yaml" {
+			return fmt.Errorf("Config filename mismatch: %s", trigger.Filename)
+		}
+
 		return nil
 	}
 }
@@ -143,6 +200,40 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       args = "build -t gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA -f Dockerfile ."
     }
   }
+}
+  `, projectID, projectID, projectOrg, projectBillingAccount)
+}
+
+func testGoogleCloudBuildTrigger_filename(projectID, projectOrg, projectBillingAccount string) string {
+	return fmt.Sprintf(`
+resource "google_project" "acceptance" {
+  name            = "%s"
+  project_id      = "%s"
+  org_id          = "%s"
+  billing_account = "%s"
+}
+
+resource "google_project_services" "acceptance" {
+  project = "${google_project.acceptance.project_id}"
+
+  services = [
+    "cloudbuild.googleapis.com",
+    "containerregistry.googleapis.com",
+    "logging.googleapis.com",
+    "pubsub.googleapis.com",
+    "storage-api.googleapis.com",
+  ]
+}
+
+resource "google_cloudbuild_trigger" "filename_build_trigger" {
+  project  = "${google_project_services.acceptance.project}"
+  description = "acceptance test build trigger"
+  trigger_template {
+    branch_name = "master"
+    project     = "${google_project_services.acceptance.project}"
+    repo_name   = "some-repo"
+  }
+  filename = "cloudbuild.yaml"
 }
   `, projectID, projectID, projectOrg, projectBillingAccount)
 }
