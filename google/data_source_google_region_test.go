fbf2f1f0363646d8748c64fbb9a616aaec051c12
Walter Dolce <walterdolce@gmail.com>
2018-07-23 17:26:38 +0100

    Data source to return same region name as set in GCLOUD_REGION env var

--- google/data_source_google_region_test.go
+++ google/data_source_google_region_test.go
@@ -43,6 +43,28 @@ func TestAccRegion_fromGoogleRegionEnvVar(t *testing.T) {
 	})
 }
 
+func TestAccRegion_fromGCloudRegionEnvVar(t *testing.T) {
+	oldGCloudRegionVar := os.Getenv("GCLOUD_REGION")
+	os.Setenv("GCLOUD_REGION", "us-central1")
+	defer os.Setenv("GCLOUD_REGION", oldGCloudRegionVar)
+
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckProviderRegionConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceGoogleRegionCheck("data.google_region.current"),
+					testAccItReturnsTheRegionSetByTheGCloudRegionEnvVar("data.google_region.current"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceGoogleRegionCheck(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		_, ok := s.RootModule().Resources[name]
@@ -65,6 +87,31 @@ func testAccItReturnsTheRegionSetByTheGoogleRegionEnvVar(name string) resource.T
 
 		if currentRegion == "" {
 			return fmt.Errorf("the environment variable GOOGLE_REGION must be set to something")
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		if rs.Primary.ID != currentRegion {
+			return fmt.Errorf("resource ID was meant to be %s but was %s instead", currentRegion, rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+func testAccItReturnsTheRegionSetByTheGCloudRegionEnvVar(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		currentRegion := os.Getenv("GCLOUD_REGION")
+
+		if !ok {
+			return fmt.Errorf("root module has no resource called %s", name)
+		}
+
+		if currentRegion == "" {
+			return fmt.Errorf("the environment variable GCLOUD_REGION must be set to something")
 		}
 
 		if rs.Primary.ID == "" {
