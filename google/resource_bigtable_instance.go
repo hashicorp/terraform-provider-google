37de4312e3b58c24632dd309403f52d580552490
Riley Karson <rileykarson@users.noreply.github.com>
2018-09-10 15:07:48 -0700

    go tool fix google (#2019)

--- google/resource_bigtable_instance.go
+++ google/resource_bigtable_instance.go
@@ -1,6 +1,7 @@
 package google
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -8,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform/helper/validation"
 
 	"cloud.google.com/go/bigtable"
-	"golang.org/x/net/context"
 )
 
 func resourceBigtableInstance() *schema.Resource {
