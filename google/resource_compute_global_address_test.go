2393a9d721c8f0c185a62d80b9f0e4bcb26f287a
Chris Stephens <chrisst@google.com>
2018-09-20 23:35:42 +0000

    Supporting IP range reservation for global address

--- google/resource_compute_global_address_test.go
+++ google/resource_compute_global_address_test.go
@@ -67,6 +67,26 @@ func TestAccComputeGlobalAddress_ipv6(t *testing.T) {
 	})
 }
 
+func TestAccComputeGlobalAddress_internal(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeGlobalAddressDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeGlobalAddress_internal(),
+			},
+			resource.TestStep{
+				ResourceName:      "google_compute_global_address.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckComputeGlobalAddressDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
 
@@ -155,4 +175,20 @@ resource "google_compute_global_address" "foobar" {
 	description = "Created for Terraform acceptance testing"
 	ip_version = "IPV6"
 }`, acctest.RandString(10))
+}
+
+func testAccComputeGlobalAddress_internal() string {
+	return fmt.Sprintf(`
+resource "google_compute_network" "foobar" {
+  name = "address-test-%s"
+}
+
+
+resource "google_compute_global_address" "foobar" {
+  name = "address-test-%s"
+  address_type = "INTERNAL"
+  purpose = "VPC_PEERING"
+  prefix_length = 24
+  network = "${google_compute_network.foobar.self_link}"
+}`, acctest.RandString(10), acctest.RandString(10))
 }
