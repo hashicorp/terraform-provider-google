fe8b2207fe5f64f0c071f6bb106a005f991056ad
The Magician <magic-modules@google.com>
2018-10-05 13:05:58 -0700

    Add an address example with a VM instance (#2141)
    <!-- This change is generated by MagicModules. -->
    /cc @rileykarson

--- google/resource_compute_address_generated_test.go
+++ google/resource_compute_address_generated_test.go
@@ -94,3 +94,56 @@ resource "google_compute_address" "internal_with_subnet_and_address" {
 `, val, val, val,
 	)
 }
+
+func TestAccComputeAddress_instanceWithIpExample(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeAddressDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeAddress_instanceWithIpExample(acctest.RandString(10)),
+			},
+			{
+				ResourceName:      "google_compute_address.static",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccComputeAddress_instanceWithIpExample(val string) string {
+	return fmt.Sprintf(`
+resource "google_compute_address" "static" {
+  name = "ipv4-address-%s"
+}
+
+data "google_compute_image" "debian_image" {
+	family  = "debian-9"
+	project = "debian-cloud"
+}
+
+resource "google_compute_instance" "instance_with_ip" {
+	name         = "vm-instance-%s"
+	machine_type = "f1-micro"
+	zone         = "us-central1-a"
+
+	boot_disk {
+		initialize_params{
+			image = "${data.google_compute_image.debian_image.self_link}"
+		}
+	}
+
+	network_interface {
+		network = "default"
+		access_config {
+			nat_ip = "${google_compute_address.static.address}"
+		}
+	}
+}
+`, val, val,
+	)
+}
