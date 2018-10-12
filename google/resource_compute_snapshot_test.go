c4db896d390c4a34e21d0d01898fe0aee7739938
Chris Stephens <chrisst@google.com>
2018-10-12 08:57:09 -0700

    Test failures (#2250)
    fix disk encrypt tests
    fxing compute instance data source tests
    fixing the compute instance migration tests

--- google/resource_compute_snapshot_test.go
+++ google/resource_compute_snapshot_test.go
@@ -254,11 +254,7 @@ resource "google_compute_snapshot" "foobar" {
 	name = "%s"
 	source_disk = "${google_compute_disk.foobar.name}"
 	zone = "us-central1-a"
-	source_disk_encryption_key {
-		raw_key = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
-	}
-	snapshot_encryption_key {
-		raw_key = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
-	}
+	source_disk_encryption_key_raw = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
+	snapshot_encryption_key_raw = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
 }`, diskName, snapshotName)
 }
