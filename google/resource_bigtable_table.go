0081132e4309177535bdd016eb3ccf8850a70674
Eraac <kevin@labesse.me>
2018-10-12 10:13:41 +0200

    family is now required

--- google/resource_bigtable_table.go
+++ google/resource_bigtable_table.go
@@ -29,7 +29,7 @@ func resourceBigtableTable() *schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"family": {
 							Type:     schema.TypeString,
-							Optional: true,
+							Required: true,
 						},
 					},
 				},
