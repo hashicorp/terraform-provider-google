4570cd119a087a219777cc181538ffa14912c137
The Magician <magic-modules@google.com>
2018-11-02 09:21:18 -0700

    Regional Clusters are GA followup (#2387)
    <!-- This change is generated by MagicModules. -->
    /cc @rileykarson

--- google/resource_container_node_pool.go
+++ google/resource_container_node_pool.go
@@ -54,10 +54,9 @@ func resourceContainerNodePool() *schema.Resource {
 					ForceNew: true,
 				},
 				"region": &schema.Schema{
-					Deprecated: "This field is in beta and will be removed from this provider. Use it in the the google-beta provider instead. See https://terraform.io/docs/providers/google/provider_versions.html for more details.",
-					Type:       schema.TypeString,
-					Optional:   true,
-					ForceNew:   true,
+					Type:     schema.TypeString,
+					Optional: true,
+					ForceNew: true,
 				},
 			}),
 	}
