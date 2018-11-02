6606c134e288346d7aeb6beda49b1042e03a25a2
Riley Karson <rileykarson@users.noreply.github.com>
2018-11-02 21:14:45 +0000

    Remove service_account_id from data.google_service_account_key

--- google/data_source_google_service_account_key_test.go
+++ google/data_source_google_service_account_key_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
-	"strings"
 )
 
 func TestAccDatasourceGoogleServiceAccountKey_basic(t *testing.T) {
@@ -36,49 +35,6 @@ func TestAccDatasourceGoogleServiceAccountKey_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "public_key"),
 				),
 			},
-			{
-				Config: testAccDatasourceGoogleServiceAccountKey_deprecated(account),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGoogleServiceAccountKeyExists(resourceName),
-					// Check that the 'name' starts with the service account name
-					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile(serviceAccountName)),
-					resource.TestCheckResourceAttrSet(resourceName, "key_algorithm"),
-					resource.TestCheckResourceAttrSet(resourceName, "public_key"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDatasourceGoogleServiceAccountKey_errors(t *testing.T) {
-	t.Parallel()
-
-	account := acctest.RandomWithPrefix("tf-test")
-	serviceAccountName := fmt.Sprintf(
-		"projects/%s/serviceAccounts/%s@%s.iam.gserviceaccount.com",
-		getTestProjectFromEnv(),
-		account,
-		getTestProjectFromEnv(),
-	)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDatasourceGoogleServiceAccountKey_error(
-					account,
-					`name = "${google_service_account.acceptance.name}"`),
-				ExpectError: regexp.MustCompile(
-					fmt.Sprintf("invalid key name %q", serviceAccountName)),
-			},
-			{
-				Config: testAccDatasourceGoogleServiceAccountKey_error(
-					account,
-					`service_account_id = "${google_service_account.acceptance.id}"`),
-				ExpectError: regexp.MustCompile(
-					fmt.Sprintf("invalid key name %q", serviceAccountName)),
-			},
 		},
 	})
 }
@@ -97,36 +53,4 @@ resource "google_service_account_key" "acceptance" {
 data "google_service_account_key" "acceptance" {
 	name = "${google_service_account_key.acceptance.name}"
 }`, account)
-}
-
-func testAccDatasourceGoogleServiceAccountKey_deprecated(account string) string {
-	return fmt.Sprintf(`
-resource "google_service_account" "acceptance" {
-	account_id = "%s"
-}
-
-resource "google_service_account_key" "acceptance" {
-	service_account_id = "${google_service_account.acceptance.name}"
-	public_key_type = "TYPE_X509_PEM_FILE"
-}
-
-data "google_service_account_key" "acceptance" {
-	service_account_id = "${google_service_account_key.acceptance.name}"
-}`, account)
-}
-
-func testAccDatasourceGoogleServiceAccountKey_error(account string, incorrectDataFields ...string) string {
-	return fmt.Sprintf(`
-resource "google_service_account" "acceptance" {
-	account_id = "%s"
-}
-
-resource "google_service_account_key" "acceptance" {
-	service_account_id = "${google_service_account.acceptance.name}"
-	public_key_type = "TYPE_X509_PEM_FILE"
-}
-
-data "google_service_account_key" "acceptance" {
-%s
-}`, account, strings.Join(incorrectDataFields, "\n\t"))
 }
