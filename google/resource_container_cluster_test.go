795be8e07fd02d31fb23c0eb9724cefdcdff981d
The Magician <magic-modules@google.com>
2018-12-06 13:19:22 -0800

    Remove beta fields from GA Container resources (#2601)
    <!-- This change is generated by MagicModules. -->
    /cc @rileykarson

--- google/resource_container_cluster_test.go
+++ google/resource_container_cluster_test.go
@@ -658,52 +658,6 @@ func TestAccContainerCluster_withNodeConfigScopeAlias(t *testing.T) {
 	})
 }
 
-func TestAccContainerCluster_withNodeConfigTaints(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckContainerClusterDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccContainerCluster_withNodeConfigTaints(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("google_container_cluster.with_node_config", "node_config.0.taint.#", "2"),
-				),
-			},
-			// Don't include an import step because beta features can't yet be imported.
-			// Once taints are in GA, consider merging this test with the _withNodeConfig test.
-		},
-	})
-}
-
-func TestAccContainerCluster_withWorkloadMetadataConfig(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckContainerClusterDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccContainerCluster_withWorkloadMetadataConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("google_container_cluster.with_workload_metadata_config",
-						"node_config.0.workload_metadata_config.0.node_metadata", "SECURE"),
-				),
-			},
-			{
-				ResourceName:            "google_container_cluster.with_workload_metadata_config",
-				ImportStateIdPrefix:     "us-central1-a/",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"min_master_version"},
-			},
-		},
-	})
-}
-
 func TestAccContainerCluster_network(t *testing.T) {
 	t.Parallel()
 
@@ -1758,55 +1712,6 @@ resource "google_container_cluster" "with_node_config_scope_alias" {
 		oauth_scopes = [ "compute-rw", "storage-ro", "logging-write", "monitoring" ]
 	}
 }`, acctest.RandString(10))
-}
-
-func testAccContainerCluster_withNodeConfigTaints() string {
-	return fmt.Sprintf(`
-resource "google_container_cluster" "with_node_config" {
-	name = "cluster-test-%s"
-	zone = "us-central1-f"
-	initial_node_count = 1
-
-	node_config {
-		taint {
-			key = "taint_key"
-			value = "taint_value"
-			effect = "PREFER_NO_SCHEDULE"
-		}
-		taint {
-			key = "taint_key2"
-			value = "taint_value2"
-			effect = "NO_EXECUTE"
-		}
-	}
-}`, acctest.RandString(10))
-}
-
-func testAccContainerCluster_withWorkloadMetadataConfig() string {
-	return fmt.Sprintf(`
-data "google_container_engine_versions" "central1a" {
-  zone = "us-central1-a"
-}
-
-resource "google_container_cluster" "with_workload_metadata_config" {
-  name               = "cluster-test-%s"
-  zone               = "us-central1-a"
-  initial_node_count = 1
-  min_master_version = "${data.google_container_engine_versions.central1a.latest_master_version}"
-  node_version       = "${data.google_container_engine_versions.central1a.latest_node_version}"
-
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
-    ]
-
-    workload_metadata_config {
-      node_metadata = "SECURE"
-    }
-  }
-}
-`, acctest.RandString(10))
 }
 
 func testAccContainerCluster_networkRef() string {
