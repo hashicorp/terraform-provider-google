a348e35263edeba0e96a119fa1c2bf99abd5aeb9
The Magician <magic-modules@google.com>
2018-12-07 11:30:50 -0800

    Fix spanner database iam binding import (#2603)

--- google/iam_spanner_database.go
+++ google/iam_spanner_database.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/terraform/helper/schema"
 	"google.golang.org/api/cloudresourcemanager/v1"
-	spanner "google.golang.org/api/spanner/v1"
+	"google.golang.org/api/spanner/v1"
 )
 
 var IamSpannerDatabaseSchema = map[string]*schema.Schema{
@@ -51,17 +51,8 @@ func NewSpannerDatabaseIamUpdater(d *schema.ResourceData, config *Config) (Resou
 }
 
 func SpannerDatabaseIdParseFunc(d *schema.ResourceData, config *Config) error {
-	id, err := extractSpannerDatabaseId(d.Id())
-	if err != nil {
-		return err
-	}
-	d.Set("instance", id.Instance)
-	d.Set("project", id.Project)
-	d.Set("database", id.Database)
-
-	// Explicitly set the id so imported resources have the same ID format as non-imported ones.
-	d.SetId(id.terraformId())
-	return nil
+	_, err := resourceSpannerDatabaseImport("database")(d, config)
+	return err
 }
 
 func (u *SpannerDatabaseIamUpdater) GetResourceIamPolicy() (*cloudresourcemanager.Policy, error) {
