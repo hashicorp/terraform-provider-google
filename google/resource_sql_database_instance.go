411e1067c39d60a78239e8e242f4eee3c1ddeffa
The Magician <magic-modules@google.com>
2018-12-12 09:23:30 -0800

    mark region as computed in sql db instance since we use getregion (#2635)

--- google/resource_sql_database_instance.go
+++ google/resource_sql_database_instance.go
@@ -56,6 +56,7 @@ func resourceSqlDatabaseInstance() *schema.Resource {
 			"region": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
 				ForceNew: true,
 			},
 
