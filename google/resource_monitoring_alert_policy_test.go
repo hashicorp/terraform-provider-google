013a3f591e5c0dd6a90cea31b4f942e58ede8f8e
Nathan McKinley <nmckinley@google.com>
2019-01-02 19:04:04 +0000

    Homogenize spacing (trivial no-op change)

--- google/resource_monitoring_alert_policy_test.go
+++ google/resource_monitoring_alert_policy_test.go
@@ -129,9 +129,9 @@ resource "google_monitoring_alert_policy" "basic" {
         per_series_aligner = "%s"
       }
 
-      duration       = "60s"
-      comparison     = "COMPARISON_GT"
-      filter         = "%s"
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      filter          = "%s"
       threshold_value = "0.5"
     }
   }
