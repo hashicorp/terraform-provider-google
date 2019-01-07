f1c8a6d4ff6250cecd25cf3986d9bd71118ef018
Chris Stephens <chrisst@google.com>
2019-01-07 14:48:40 -0800

    Add a method that will bootstrap a KMS keyring
    It will either grab a well-known key or create a new one in the project
    that is running the tests. This will allow reuse of KMS keys without needing
    to create/delete them for all of the new resources that are starting to
    support KMS integration.

--- google/provider_test.go
+++ google/provider_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/terraform-providers/terraform-provider-random/random"
+	"google.golang.org/api/cloudkms/v1"
 )
 
 var testAccProviders map[string]terraform.ResourceProvider
@@ -178,6 +179,10 @@ func getTestRegionFromEnv() string {
 	return multiEnvSearch(regionEnvVars)
 }
 
+func getTestZoneFromEnv() string {
+	return multiEnvSearch(zoneEnvVars)
+}
+
 func getTestOrgFromEnv(t *testing.T) string {
 	skipIfEnvNotSet(t, orgEnvVars...)
 	return multiEnvSearch(orgEnvVars)
@@ -205,4 +210,94 @@ func multiEnvSearch(ks []string) string {
 		}
 	}
 	return ""
+}
+
+/**
+* bootstrapKMSkey will return a KMS key that can be used in tests that are
+* testing KMS integration with other resources.
+*
+* This will either return an existing key or create one if it hasn't been created
+* in the project yet. The motivation is because keyrings don't get deleted and we
+* don't want a linear growth of disabled keyrings in a project but we also don't want
+* to incur the overhead of creating a new project for each test that needs to use
+* a KMS key.
+**/
+var SharedKeyRing = "tftest-shared-keyring-1"
+var SharedCyptoKey = "tftest-shared-key-1"
+
+type bootstrappedKMS struct {
+	*cloudkms.KeyRing
+	*cloudkms.CryptoKey
+}
+
+func bootstrapKMSKey(t *testing.T) bootstrappedKMS {
+	projectID := getTestProjectFromEnv()
+	locationID := "global"
+	keyRingParent := fmt.Sprintf("projects/%s/locations/%s", projectID, locationID)
+	keyRingName := fmt.Sprintf("%s/keyRings/%s", keyRingParent, SharedKeyRing)
+	keyParent := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", projectID, locationID, SharedKeyRing)
+	keyName := fmt.Sprintf("%s/cryptoKeys/%s", keyParent, SharedCyptoKey)
+
+	config := Config{
+		Credentials: getTestCredsFromEnv(),
+		Project:     getTestProjectFromEnv(),
+		Region:      getTestRegionFromEnv(),
+		Zone:        getTestZoneFromEnv(),
+	}
+
+	if err := config.loadAndValidate(); err != nil {
+		t.Errorf("Unable to bootstrap KMS key: %s", err)
+	}
+
+	// Get or Create the hard coded shared keyring for testing
+	kmsClient := config.clientKms
+	keyRing, err := kmsClient.Projects.Locations.KeyRings.Get(keyRingName).Do()
+	if err != nil {
+		if isGoogleApiErrorWithCode(err, 404) {
+			keyRing, err = kmsClient.Projects.Locations.KeyRings.Create(keyRingParent, &cloudkms.KeyRing{}).
+				KeyRingId(SharedKeyRing).Do()
+			if err != nil {
+				t.Errorf("Unable to bootstrap KMS key. Cannot create keyRing: %s", err)
+			}
+		} else {
+			t.Errorf("Unable to bootstrap KMS key. Cannot retrieve keyRing: %s", err)
+		}
+	}
+
+	if keyRing == nil {
+		t.Errorf("Unable to bootstrap KMS key. keyRing is nil!")
+		t.FailNow()
+	}
+
+	// Get or Create the hard coded, shared crypto key for testing
+	cryptoKey, err := kmsClient.Projects.Locations.KeyRings.CryptoKeys.Get(keyName).Do()
+	if err != nil {
+		if isGoogleApiErrorWithCode(err, 404) {
+			newKey := cloudkms.CryptoKey{
+				Purpose: "ENCRYPT_DECRYPT",
+			}
+
+			cryptoKey, err = kmsClient.Projects.Locations.KeyRings.CryptoKeys.Create(keyParent, &newKey).
+				CryptoKeyId(SharedCyptoKey).Do()
+			if err != nil {
+				t.Errorf("Unable to bootstrap KMS key. Cannot create new CryptoKey: %s", err)
+			}
+
+		} else {
+			t.Errorf("Unable to bootstrap KMS key. Cannot call CryptoKey service: %s", err)
+		}
+	}
+
+	if cryptoKey == nil {
+		t.Errorf("Unable to bootstrap KMS key. CryptoKey is nil!")
+		t.FailNow()
+	}
+
+	// TODO chrisst - Set up permissions?
+	// TODO chrisst - Check if tests are failing due to inactive keys and build activation logic.
+
+	return bootstrappedKMS{
+		keyRing,
+		cryptoKey,
+	}
 }
