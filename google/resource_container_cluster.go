506e267f45c3ac08592e975fcad97226b629fa93
emily <emilyye@google.com>
2019-01-11 18:34:27 +0000

    Increase update timeout for clusters
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -57,7 +57,7 @@ func resourceContainerCluster() *schema.Resource {
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
 
