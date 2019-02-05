2ad0e745dccb7bfc3ee0071c76c7fc9976944d6d
Riley Karson <rileykarson@google.com>
2019-02-05 18:50:19 +0000

    Update the googlesource cookie for the Terraform providers.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -6,5 +6,5 @@ chmod 0600 ~/.gitcookies
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-rileykarson.google.com=1/rOwTyPQnsZnGgNtlqMhkqM63-n0W68pQ7GfhAKGIy4E
 __END__
