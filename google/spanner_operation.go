0b1d338d3189d3b35f003a0e18eb74da82aa737e
The Magician <magic-modules@google.com>
2019-02-15 16:13:30 -0800

    Autogen Spanner operations on TF (#3068)
    <!-- This change is generated by MagicModules. -->
    /cc @rambleraptor

--- google/spanner_operation.go
+++ google/spanner_operation.go
@@ -17,26 +17,26 @@ import (
 	"fmt"
 )
 
-type RedisOperationWaiter struct {
+type SpannerOperationWaiter struct {
 	Config *Config
 	CommonOperationWaiter
 }
 
-func (w *RedisOperationWaiter) QueryOp() (interface{}, error) {
+func (w *SpannerOperationWaiter) QueryOp() (interface{}, error) {
 	if w == nil {
 		return nil, fmt.Errorf("Cannot query operation, it's unset or nil.")
 	}
 	// Returns the proper get.
-	url := fmt.Sprintf("https://redis.googleapis.com/v1/%s", w.CommonOperationWaiter.Op.Name)
+	url := fmt.Sprintf("https://spanner.googleapis.com/v1/%s", w.CommonOperationWaiter.Op.Name)
 	return sendRequest(w.Config, "GET", url, nil)
 }
 
-func redisOperationWaitTime(config *Config, op map[string]interface{}, project, activity string, timeoutMinutes int) error {
+func spannerOperationWaitTime(config *Config, op map[string]interface{}, project, activity string, timeoutMinutes int) error {
 	if val, ok := op["name"]; !ok || val == "" {
 		// This was a synchronous call - there is no operation to wait for.
 		return nil
 	}
-	w := &RedisOperationWaiter{
+	w := &SpannerOperationWaiter{
 		Config: config,
 	}
 	if err := w.CommonOperationWaiter.SetOp(op); err != nil {
