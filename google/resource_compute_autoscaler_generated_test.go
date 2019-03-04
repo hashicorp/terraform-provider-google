8106530963b0a09f44346aff2b8ba8f2f19593ca
Riley Karson <rileykarson@google.com>
2019-03-04 22:15:42 +0000

    Versioned OiCS Examples / Generated tests
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_autoscaler_generated_test.go
+++ google/resource_compute_autoscaler_generated_test.go
@@ -99,6 +99,7 @@ resource "google_compute_instance_group_manager" "foobar" {
   zone = "us-central1-f"
 
   instance_template  = "${google_compute_instance_template.foobar.self_link}"
+
   target_pools       = ["${google_compute_target_pool.foobar.self_link}"]
   base_instance_name = "foobar"
 }
