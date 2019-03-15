6ff19d39cddb1af4f616d5b3830bf0714242313a
The Magician <magic-modules@google.com>
2019-03-15 16:35:30 -0700

    [terraform-conversions] Manually add storage_bucket to conversions library (#3256)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_sql_database_instance_test.go
+++ google/resource_sql_database_instance_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-
 	sqladmin "google.golang.org/api/sqladmin/v1beta4"
 )
 
