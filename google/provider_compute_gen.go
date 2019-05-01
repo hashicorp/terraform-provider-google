6c2307d4a9d90d5fa2d850d7a92a7aceeeb35037
emily <emilyye@google.com>
2019-05-01 20:03:19 +0000

    Add node groups (sole-tenant nodes)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider_compute_gen.go
+++ google/provider_compute_gen.go
@@ -34,6 +34,7 @@ var GeneratedComputeResourcesMap = map[string]*schema.Resource{
 	"google_compute_image":                          resourceComputeImage(),
 	"google_compute_interconnect_attachment":        resourceComputeInterconnectAttachment(),
 	"google_compute_network":                        resourceComputeNetwork(),
+	"google_compute_node_group":                     resourceComputeNodeGroup(),
 	"google_compute_node_template":                  resourceComputeNodeTemplate(),
 	"google_compute_region_autoscaler":              resourceComputeRegionAutoscaler(),
 	"google_compute_region_disk":                    resourceComputeRegionDisk(),
