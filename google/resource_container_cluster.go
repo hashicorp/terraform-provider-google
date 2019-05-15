5300821dfd09c6f7df7c207c0f901adf38b6b7e1
tinkerborg <15373049+tinkerborg@users.noreply.github.com>
2019-05-15 12:35:27 -0400

    remove unnecessary computed flag

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -614,7 +614,6 @@ func resourceContainerCluster() *schema.Resource {
 				Type:     schema.TypeList,
 				MaxItems: 1,
 				Optional: true,
-				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"enabled": {
