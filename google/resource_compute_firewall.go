8defec0947e78bb780a3b7a5118381d0a137d61b
Dana Hoffman <danahoffman@google.com>
2019-05-22 21:30:09 +0000

    allow setting firewall priority to 0
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_firewall.go
+++ google/resource_compute_firewall.go
@@ -282,7 +282,7 @@ func resourceComputeFirewallCreate(d *schema.ResourceData, meta interface{}) err
 	priorityProp, err := expandComputeFirewallPriority(d.Get("priority"), d, config)
 	if err != nil {
 		return err
-	} else if v, ok := d.GetOkExists("priority"); !isEmptyValue(reflect.ValueOf(priorityProp)) && (ok || !reflect.DeepEqual(v, priorityProp)) {
+	} else if v, ok := d.GetOkExists("priority"); ok || !reflect.DeepEqual(v, priorityProp) {
 		obj["priority"] = priorityProp
 	}
 	sourceRangesProp, err := expandComputeFirewallSourceRanges(d.Get("source_ranges"), d, config)
@@ -475,7 +475,7 @@ func resourceComputeFirewallUpdate(d *schema.ResourceData, meta interface{}) err
 	priorityProp, err := expandComputeFirewallPriority(d.Get("priority"), d, config)
 	if err != nil {
 		return err
-	} else if v, ok := d.GetOkExists("priority"); !isEmptyValue(reflect.ValueOf(v)) && (ok || !reflect.DeepEqual(v, priorityProp)) {
+	} else if v, ok := d.GetOkExists("priority"); ok || !reflect.DeepEqual(v, priorityProp) {
 		obj["priority"] = priorityProp
 	}
 	sourceRangesProp, err := expandComputeFirewallSourceRanges(d.Get("source_ranges"), d, config)
