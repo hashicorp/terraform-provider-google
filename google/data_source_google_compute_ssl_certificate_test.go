8566bcee72939a80f5f73a5b5a75b1845f52c9ff
The Magician <magic-modules@google.com>
2019-05-23 09:57:09 -0700

    New data source: Compute SSL certificate (#3683)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_compute_ssl_certificate_test.go
@@ -0,0 +1,47 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceComputeSslCertificate(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceComputeSslCertificateConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceStateWithIgnores(
+						"data.google_compute_ssl_certificate.cert",
+						"google_compute_ssl_certificate.foobar",
+						map[string]struct{}{
+							"private_key": {},
+						},
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceComputeSslCertificateConfig() string {
+	return fmt.Sprintf(`
+resource "google_compute_ssl_certificate" "foobar" {
+	name		= "cert-test-%s"
+	description = "really descriptive"
+	private_key = "${file("test-fixtures/ssl_cert/test.key")}"
+	certificate = "${file("test-fixtures/ssl_cert/test.crt")}"
+}
+
+data "google_compute_ssl_certificate" "cert" {
+	name     = "${google_compute_ssl_certificate.foobar.name}"
+}
+`, acctest.RandString(10))
+}
