95ce360790e95978f883086798e9833811189e64
The Magician <magic-modules@google.com>
2019-06-19 10:23:15 -0700

    Refactor node auto-provisioning, clarify docs (#3876)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -211,9 +211,9 @@ func resourceContainerCluster() *schema.Resource {
 
 			"cluster_autoscaling": {
 				Type:     schema.TypeList,
-				Computed: true,
 				MaxItems: 1,
 				Removed:  "This field is in beta. Use it in the the google-beta provider instead. See https://terraform.io/docs/providers/google/provider_versions.html for more details.",
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"enabled": {
