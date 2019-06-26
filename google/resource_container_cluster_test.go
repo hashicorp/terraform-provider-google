fad6dbd71eade84f85941afd928cdd55a76fa3e8
Dana Hoffman <danahoffman@google.com>
2019-06-26 01:20:24 +0000

    Remove a few redundant GKE tests
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_container_cluster_test.go
+++ google/resource_container_cluster_test.go
@@ -106,27 +106,6 @@ func TestAccContainerCluster_basic(t *testing.T) {
 	})
 }
 
-func TestAccContainerCluster_withTimeout(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckContainerClusterDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccContainerCluster_withTimeout(),
-			},
-			{
-				ResourceName:        "google_container_cluster.primary",
-				ImportStateIdPrefix: "us-central1-a/",
-				ImportState:         true,
-				ImportStateVerify:   true,
-			},
-		},
-	})
-}
-
 func TestAccContainerCluster_withAddons(t *testing.T) {
 	t.Parallel()
 
@@ -139,10 +118,6 @@ func TestAccContainerCluster_withAddons(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccContainerCluster_withAddons(clusterName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("google_container_cluster.primary", "addons_config.0.http_load_balancing.0.disabled", "true"),
-					resource.TestCheckResourceAttr("google_container_cluster.primary", "addons_config.0.kubernetes_dashboard.0.disabled", "true"),
-				),
 			},
 			{
 				ResourceName:        "google_container_cluster.primary",
@@ -152,11 +127,6 @@ func TestAccContainerCluster_withAddons(t *testing.T) {
 			},
 			{
 				Config: testAccContainerCluster_updateAddons(clusterName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("google_container_cluster.primary", "addons_config.0.horizontal_pod_autoscaling.0.disabled", "true"),
-					resource.TestCheckResourceAttr("google_container_cluster.primary", "addons_config.0.http_load_balancing.0.disabled", "false"),
-					resource.TestCheckResourceAttr("google_container_cluster.primary", "addons_config.0.kubernetes_dashboard.0.disabled", "true"),
-				),
 			},
 			{
 				ResourceName:        "google_container_cluster.primary",
@@ -1488,35 +1458,18 @@ resource "google_container_cluster" "primary" {
 }`, name)
 }
 
-func testAccContainerCluster_withTimeout() string {
-	return fmt.Sprintf(`
-resource "google_container_cluster" "primary" {
-	name = "cluster-test-%s"
-	zone = "us-central1-a"
-	initial_node_count = 3
-
-	ip_allocation_policy {
-    use_ip_aliases = false
-	}
-
-	timeouts {
-		create = "30m"
-		delete = "30m"
-		update = "30m"
-	}
-}`, acctest.RandString(10))
-}
-
 func testAccContainerCluster_withAddons(clusterName string) string {
 	return fmt.Sprintf(`
 resource "google_container_cluster" "primary" {
 	name = "%s"
-	zone = "us-central1-a"
-	initial_node_count = 3
+	location = "us-central1-a"
+	initial_node_count = 1
 
 	addons_config {
 		http_load_balancing { disabled = true }
+		horizontal_pod_autoscaling { disabled = true }
 		kubernetes_dashboard { disabled = true }
+		network_policy_config { disabled = true }
 	}
 }`, clusterName)
 }
@@ -1525,13 +1478,14 @@ func testAccContainerCluster_updateAddons(clusterName string) string {
 	return fmt.Sprintf(`
 resource "google_container_cluster" "primary" {
 	name = "%s"
-	zone = "us-central1-a"
-	initial_node_count = 3
+	location = "us-central1-a"
+	initial_node_count = 1
 
 	addons_config {
 		http_load_balancing { disabled = false }
-		kubernetes_dashboard { disabled = true }
-		horizontal_pod_autoscaling { disabled = true }
+		kubernetes_dashboard { disabled = false }
+		horizontal_pod_autoscaling { disabled = false }
+		network_policy_config { disabled = false }
 	}
 }`, clusterName)
 }
