057f5d473058d8edfa9b76ff53a861b16f71b2b5
The Magician <magic-modules@google.com>
2019-06-25 17:16:18 -0700

    fix service perimeter tests (#3921)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_access_context_manager_service_perimeter_test.go
+++ google/resource_access_context_manager_service_perimeter_test.go
@@ -39,7 +39,7 @@ func testAccAccessContextManagerServicePerimeter_updateTest(t *testing.T) {
 		CheckDestroy: testAccCheckAccessContextManagerServicePerimeterDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAccessContextManagerServicePerimeter_update(org, "my policy", "level", "perimeter"),
+				Config: testAccAccessContextManagerServicePerimeter_basic(org, "my policy", "level", "perimeter"),
 			},
 			{
 				ResourceName:      "google_access_context_manager_service_perimeter.test-access",
@@ -47,7 +47,7 @@ func testAccAccessContextManagerServicePerimeter_updateTest(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccAccessContextManagerServicePerimeter_update2(org, "my policy", "level", "perimeter"),
+				Config: testAccAccessContextManagerServicePerimeter_update(org, "my policy", "level", "perimeter"),
 			},
 			{
 				ResourceName:      "google_access_context_manager_service_perimeter.test-access",
@@ -104,7 +104,10 @@ resource "google_access_context_manager_service_perimeter" "test-access" {
   parent         = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}"
   name           = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}/servicePerimeters/%s"
   title          = "%s"
-  perimeter_type = "PERIMETER_TYPE_BRIDGE"
+  perimeter_type = "PERIMETER_TYPE_REGULAR"
+  status {
+    restricted_services = ["storage.googleapis.com"]
+  }
 }
 `, org, policyTitle, levelTitleName, levelTitleName, perimeterTitleName, perimeterTitleName)
 }
@@ -135,37 +138,9 @@ resource "google_access_context_manager_service_perimeter" "test-access" {
   title          = "%s"
   perimeter_type = "PERIMETER_TYPE_REGULAR"
   status {
-    restricted_services = ["storage.googleapis.com"]
+    restricted_services = ["bigquery.googleapis.com"]
+    access_levels = ["${google_access_context_manager_access_level.test-access.name}"]
   }
-}
-`, org, policyTitle, levelTitleName, levelTitleName, perimeterTitleName, perimeterTitleName)
-}
-
-func testAccAccessContextManagerServicePerimeter_update2(org, policyTitle, levelTitleName, perimeterTitleName string) string {
-	return fmt.Sprintf(`
-resource "google_access_context_manager_access_policy" "test-access" {
-  parent = "organizations/%s"
-  title  = "%s"
-}
-
-resource "google_access_context_manager_access_level" "test-access" {
-  parent      = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}"
-  name        = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}/accessLevels/%s"
-  title       = "%s"
-  description = "hello"
-  basic {
-    combining_function = "AND"
-    conditions {
-      ip_subnetworks = ["192.0.4.0/24"]
-    }
-  }
-}
-
-resource "google_access_context_manager_service_perimeter" "test-access" {
-  parent         = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}"
-  name           = "accessPolicies/${google_access_context_manager_access_policy.test-access.name}/servicePerimeters/%s"
-  title          = "%s"
-  perimeter_type = "PERIMETER_TYPE_REGULAR"
 }
 `, org, policyTitle, levelTitleName, levelTitleName, perimeterTitleName, perimeterTitleName)
 }
