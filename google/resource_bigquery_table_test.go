5d2bd830c1fc12824db4fdf34b87574a62da7b86
Rickard von Essen <rickard.von.essen@gmail.com>
2019-07-03 19:42:25 +0000

    WIP: BQ - Support for External Data Tables
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_bigquery_table_test.go
+++ google/resource_bigquery_table_test.go
@@ -94,6 +94,60 @@ func TestAccBigQueryTable_updateView(t *testing.T) {
 	})
 }
 
+func TestAccBigQueryExternalDataTable_CSV(t *testing.T) {
+	t.Parallel()
+
+	bucketName := testBucketName()
+	objectName := fmt.Sprintf("tf_test_%s.csv", acctest.RandString(10))
+
+	datasetID := fmt.Sprintf("tf_test_%s", acctest.RandString(10))
+	tableID := fmt.Sprintf("tf_test_%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBigQueryTableDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigQueryTableFromGCS(datasetID, tableID, bucketName, objectName, TEST_CSV, "CSV", "\\\""),
+				Check:  testAccCheckBigQueryExtData("\""),
+			},
+			{
+				Config: testAccBigQueryTableFromGCS(datasetID, tableID, bucketName, objectName, TEST_CSV, "CSV", ""),
+				Check:  testAccCheckBigQueryExtData(""),
+			},
+		},
+	})
+}
+
+func testAccCheckBigQueryExtData(expectedQuoteChar string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "google_bigquery_table" {
+				continue
+			}
+
+			config := testAccProvider.Meta().(*Config)
+			dataset := rs.Primary.Attributes["dataset_id"]
+			table := rs.Primary.Attributes["table_id"]
+			res, err := config.clientBigQuery.Tables.Get(config.Project, dataset, table).Do()
+			if err != nil {
+				return err
+			}
+
+			if res.Type != "EXTERNAL" {
+				return fmt.Errorf("Table \"%s.%s\" is of type \"%s\", expecterd EXTERNAL.", dataset, table, res.Type)
+			}
+			edc := res.ExternalDataConfiguration
+			cvsOpts := edc.CsvOptions
+			if cvsOpts == nil || *cvsOpts.Quote != expectedQuoteChar {
+				return fmt.Errorf("Table \"%s.%s\" quote should be '%s' but was '%s'", dataset, table, expectedQuoteChar, *cvsOpts.Quote)
+			}
+		}
+		return nil
+	}
+}
+
 func testAccCheckBigQueryTableDestroy(s *terraform.State) error {
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "google_bigquery_table" {
@@ -257,3 +311,51 @@ resource "google_bigquery_table" "test" {
 EOH
 }`, datasetID, tableID)
 }
+
+func testAccBigQueryTableFromGCS(datasetID, tableID, bucketName, objectName, content, format, quoteChar string) string {
+	return fmt.Sprintf(`
+resource "google_bigquery_dataset" "test" {
+  dataset_id = "%s"
+}
+
+resource "google_storage_bucket" "test" {
+  name     = "%s"
+  force_destroy = true
+}
+
+resource "google_storage_bucket_object" "test" {
+  name    = "%s"
+  content = <<EOF
+%s
+EOF
+  bucket  = "${google_storage_bucket.test.name}"
+}
+
+resource "google_bigquery_table" "test" {
+  table_id   = "%s"
+  dataset_id = "${google_bigquery_dataset.test.dataset_id}"
+  external_data_configuration {
+    autodetect    = true
+    source_format = "%s"
+    csv_options {
+      encoding = "UTF-8"
+      quote    = "%s"
+    }
+
+    source_uris = [
+      "gs://${google_storage_bucket.test.name}/${google_storage_bucket_object.test.name}"
+    ]
+  }
+}`, datasetID, bucketName, objectName, content, tableID, format, quoteChar)
+}
+
+var TEST_CSV = `lifelock,LifeLock,,web,Tempe,AZ,1-May-07,6850000,USD,b
+lifelock,LifeLock,,web,Tempe,AZ,1-Oct-06,6000000,USD,a
+lifelock,LifeLock,,web,Tempe,AZ,1-Jan-08,25000000,USD,c
+mycityfaces,MyCityFaces,7,web,Scottsdale,AZ,1-Jan-08,50000,USD,seed
+flypaper,Flypaper,,web,Phoenix,AZ,1-Feb-08,3000000,USD,a
+infusionsoft,Infusionsoft,105,software,Gilbert,AZ,1-Oct-07,9000000,USD,a
+gauto,gAuto,4,web,Scottsdale,AZ,1-Jan-08,250000,USD,seed
+chosenlist-com,ChosenList.com,5,web,Scottsdale,AZ,1-Oct-06,140000,USD,seed
+chosenlist-com,ChosenList.com,5,web,Scottsdale,AZ,25-Jan-08,233750,USD,angel
+`
