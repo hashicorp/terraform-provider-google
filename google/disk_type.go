f201d94d640b7749a81e9e25bb291a870f7a2fab
Dana Hoffman <danahoffman@google.com>
2019-07-18 00:01:41 +0000

    remove network calls to determine disk type self link
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/disk_type.go
+++ google/disk_type.go
@@ -1,26 +1,11 @@
 package google
 
-import (
-	computeBeta "google.golang.org/api/compute/v0.beta"
-	"google.golang.org/api/compute/v1"
-)
-
 // readDiskType finds the disk type with the given name.
-func readDiskType(c *Config, zone *compute.Zone, project, name string) (*compute.DiskType, error) {
-	diskType, err := c.clientCompute.DiskTypes.Get(project, zone.Name, name).Do()
-	if err == nil && diskType != nil && diskType.SelfLink != "" {
-		return diskType, nil
-	} else {
-		return nil, err
-	}
+func readDiskType(c *Config, d TerraformResourceData, name string) (*ZonalFieldValue, error) {
+	return parseZonalFieldValue("diskTypes", name, "project", "zone", d, c, false)
 }
 
 // readRegionDiskType finds the disk type with the given name.
-func readRegionDiskType(c *Config, region *compute.Region, project, name string) (*computeBeta.DiskType, error) {
-	diskType, err := c.clientComputeBeta.RegionDiskTypes.Get(project, region.Name, name).Do()
-	if err == nil && diskType != nil && diskType.SelfLink != "" {
-		return diskType, nil
-	} else {
-		return nil, err
-	}
+func readRegionDiskType(c *Config, d TerraformResourceData, name string) (*RegionalFieldValue, error) {
+	return parseRegionalFieldValue("diskTypes", name, "project", "region", "zone", d, c, false)
 }
