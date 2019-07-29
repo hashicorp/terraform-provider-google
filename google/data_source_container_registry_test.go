3934a0f8297de23f77b0fea2870035a9ac66efdf
Kevin Davis <kmdavis@google.com>
2019-07-29 16:16:14 -0700

    Replace the ':' in project IDs with '/' when constructing gcr.io URLs. This is necessary to work correctly with domain-scoped projects in GCR: https://cloud.google.com/container-registry/docs/overview#domain-scoped_projects

--- google/data_source_container_registry_test.go
+++ google/data_source_container_registry_test.go
@@ -9,7 +9,7 @@ import (
 func TestDataSourceGoogleContainerRegistryRepository(t *testing.T) {
 	t.Parallel()
 
-	resourceName := "data.google_container_registry_repository.default"
+	resourceName := "data.google_container_registry_repository.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -21,6 +21,8 @@ func TestDataSourceGoogleContainerRegistryRepository(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "project"),
 					resource.TestCheckResourceAttrSet(resourceName, "region"),
 					resource.TestCheckResourceAttr(resourceName, "repository_url", "bar.gcr.io/foo"),
+					resource.TestCheckResourceAttrSet(resourceName+"Scoped", "project"),
+					resource.TestCheckResourceAttr(resourceName+"Scoped", "repository_url", "bar.gcr.io/example.com/foo"),
 				),
 			},
 		},
@@ -28,8 +30,12 @@ func TestDataSourceGoogleContainerRegistryRepository(t *testing.T) {
 }
 
 const testAccCheckGoogleContainerRegistryRepo_basic = `
-data "google_container_registry_repository" "default" {
+data "google_container_registry_repository" "test" {
 	project = "foo"
+	region = "bar"
+}
+data "google_container_registry_repository" "testScoped" {
+	project = "example.com:foo"
 	region = "bar"
 }
 `
@@ -51,6 +57,8 @@ func TestDataSourceGoogleContainerRegistryImage(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "image_url", "bar.gcr.io/foo/baz"),
 					resource.TestCheckResourceAttr(resourceName+"2", "image_url", "bar.gcr.io/foo/baz:qux"),
 					resource.TestCheckResourceAttr(resourceName+"3", "image_url", "bar.gcr.io/foo/baz@1234"),
+					resource.TestCheckResourceAttrSet(resourceName+"Scoped", "project"),
+					resource.TestCheckResourceAttr(resourceName+"Scoped", "image_url", "bar.gcr.io/example.com/foo/baz:qux"),
 				),
 			},
 		},
@@ -74,5 +82,11 @@ data "google_container_registry_image" "test3" {
 	region = "bar"
 	name = "baz"
 	digest = "1234"
+}
+data "google_container_registry_image" "testScoped" {
+	project = "example.com:foo"
+	region = "bar"
+	name = "baz"
+	tag = "qux"
 }
 `
