c28f840c43f8c45d28aa2a044aa8f40f454f9467
Riley Karson <rileykarson@google.com>
2019-08-19 21:52:25 +0000

    add ability to update Bigtable instance cluster node count
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_bigtable_instance.go
+++ google/resource_bigtable_instance.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/hashicorp/terraform/helper/customdiff"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 
@@ -15,7 +16,13 @@ func resourceBigtableInstance() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceBigtableInstanceCreate,
 		Read:   resourceBigtableInstanceRead,
+		Update: resourceBigtableInstanceUpdate,
 		Delete: resourceBigtableInstanceDestroy,
+
+		CustomizeDiff: customdiff.All(
+			resourceBigtableInstanceValidateDevelopment,
+			resourceBigtableInstanceClusterReorderTypeList,
+		),
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -25,10 +32,9 @@ func resourceBigtableInstance() *schema.Resource {
 			},
 
 			"cluster": {
-				Type:     schema.TypeSet,
-				Required: true,
-				ForceNew: true,
-				MaxItems: 4,
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"cluster_id": {
@@ -44,7 +50,6 @@ func resourceBigtableInstance() *schema.Resource {
 						"num_nodes": {
 							Type:         schema.TypeInt,
 							Optional:     true,
-							ForceNew:     true,
 							ValidateFunc: validation.IntAtLeast(3),
 						},
 						"storage_type": {
@@ -57,7 +62,6 @@ func resourceBigtableInstance() *schema.Resource {
 					},
 				},
 			},
-
 			"display_name": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -137,7 +141,7 @@ func resourceBigtableInstanceCreate(d *schema.ResourceData, meta interface{}) er
 		conf.InstanceType = bigtable.PRODUCTION
 	}
 
-	conf.Clusters = expandBigtableClusters(d.Get("cluster").(*schema.Set).List(), conf.InstanceID)
+	conf.Clusters = expandBigtableClusters(d.Get("cluster").([]interface{}), conf.InstanceID)
 
 	c, err := config.bigtableClientFactory.NewInstanceAdminClient(project)
 	if err != nil {
@@ -181,7 +185,7 @@ func resourceBigtableInstanceRead(d *schema.ResourceData, meta interface{}) erro
 
 	d.Set("project", project)
 
-	clusters := d.Get("cluster").(*schema.Set).List()
+	clusters := d.Get("cluster").([]interface{})
 	clusterState := []map[string]interface{}{}
 	for _, cl := range clusters {
 		cluster := cl.(map[string]interface{})
@@ -205,6 +209,47 @@ func resourceBigtableInstanceRead(d *schema.ResourceData, meta interface{}) erro
 	d.Set("display_name", instance.DisplayName)
 
 	return nil
+}
+
+func resourceBigtableInstanceUpdate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	ctx := context.Background()
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	c, err := config.bigtableClientFactory.NewInstanceAdminClient(project)
+	if err != nil {
+		return fmt.Errorf("Error starting instance admin client. %s", err)
+	}
+	defer c.Close()
+
+	clusters, err := c.Clusters(ctx, d.Get("name").(string))
+	if err != nil {
+		return fmt.Errorf("Error retrieving clusters for instance %s", err.Error())
+	}
+
+	clusterMap := make(map[string]*bigtable.ClusterInfo, len(clusters))
+	for _, cluster := range clusters {
+		clusterMap[cluster.Name] = cluster
+	}
+
+	for _, cluster := range d.Get("cluster").([]interface{}) {
+		config := cluster.(map[string]interface{})
+		cluster_id := config["cluster_id"].(string)
+		if cluster, ok := clusterMap[cluster_id]; ok {
+			if cluster.ServeNodes != config["num_nodes"].(int) {
+				err = c.UpdateCluster(ctx, d.Get("name").(string), cluster.Name, int32(config["num_nodes"].(int)))
+				if err != nil {
+					return fmt.Errorf("Error updating cluster %s for instance %s", cluster.Name, d.Get("name").(string))
+				}
+			}
+		}
+	}
+
+	return resourceBigtableInstanceRead(d, meta)
 }
 
 func resourceBigtableInstanceDestroy(d *schema.ResourceData, meta interface{}) error {
@@ -264,4 +309,60 @@ func expandBigtableClusters(clusters []interface{}, instanceID string) []bigtabl
 		})
 	}
 	return results
+}
+
+func resourceBigtableInstanceValidateDevelopment(diff *schema.ResourceDiff, meta interface{}) error {
+	if diff.Get("instance_type").(string) != "DEVELOPMENT" {
+		return nil
+	}
+	if diff.Get("cluster.#").(int) != 1 {
+		return fmt.Errorf("config is invalid: instance with instance_type=\"DEVELOPMENT\" should have exactly one \"cluster\" block")
+	}
+	if diff.Get("cluster.0.num_nodes").(int) != 0 {
+		return fmt.Errorf("config is invalid: num_nodes cannot be set for instance_type=\"DEVELOPMENT\"")
+	}
+	return nil
+}
+
+func resourceBigtableInstanceClusterReorderTypeList(diff *schema.ResourceDiff, meta interface{}) error {
+	old_count, new_count := diff.GetChange("cluster.#")
+
+	// simulate Required:true, MinItems:1, MaxItems:4 for "cluster"
+	if new_count.(int) < 1 {
+		return fmt.Errorf("config is invalid: Too few cluster blocks: Should have at least 1 \"cluster\" block")
+	}
+	if new_count.(int) > 4 {
+		return fmt.Errorf("config is invalid: Too many cluster blocks: No more than 4 \"cluster\" blocks are allowed")
+	}
+
+	if old_count.(int) != new_count.(int) {
+		return nil
+	}
+
+	var old_ids []string
+	clusters := make(map[string]interface{}, new_count.(int))
+
+	for i := 0; i < new_count.(int); i++ {
+		old_id, new_id := diff.GetChange(fmt.Sprintf("cluster.%d.cluster_id", i))
+		if old_id != nil && old_id != "" {
+			old_ids = append(old_ids, old_id.(string))
+		}
+		_, c := diff.GetChange(fmt.Sprintf("cluster.%d", i))
+		clusters[new_id.(string)] = c
+	}
+
+	// reorder clusters according to the old cluster order
+	var old_cluster_order []interface{}
+	for _, id := range old_ids {
+		if c, ok := clusters[id]; ok {
+			old_cluster_order = append(old_cluster_order, c)
+		}
+	}
+
+	err := diff.SetNew("cluster", old_cluster_order)
+	if err != nil {
+		return fmt.Errorf("Error setting cluster diff: %s", err)
+	}
+
+	return nil
 }
