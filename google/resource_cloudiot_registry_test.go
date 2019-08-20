b02ae137b1e79d3dc3b8e10c61dd96293f177b4b
emily <emilyye@google.com>
2019-08-20 00:39:02 +0000

    Add log_level, plural event_notification_configs to IoT registry
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloudiot_registry_test.go
+++ google/resource_cloudiot_registry_test.go
@@ -2,12 +2,37 @@ package google
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
+
+func TestValidateCloudIoTID(t *testing.T) {
+	x := []StringValidationTestCase{
+		// No errors
+		{TestName: "basic", Value: "foobar"},
+		{TestName: "with numbers", Value: "foobar123"},
+		{TestName: "short", Value: "foo"},
+		{TestName: "long", Value: "foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoo"},
+		{TestName: "has a hyphen", Value: "foo-bar"},
+
+		// With errors
+		{TestName: "empty", Value: "", ExpectError: true},
+		{TestName: "starts with a goog", Value: "googfoobar", ExpectError: true},
+		{TestName: "starts with a number", Value: "1foobar", ExpectError: true},
+		{TestName: "has an slash", Value: "foo/bar", ExpectError: true},
+		{TestName: "has an backslash", Value: "foo\bar", ExpectError: true},
+		{TestName: "too long", Value: strings.Repeat("f", 260), ExpectError: true},
+	}
+
+	es := testStringValidationCases(x, validateCloudIotID)
+	if len(es) > 0 {
+		t.Errorf("Failed to validate CloudIoT ID names: %v", es)
+	}
+}
 
 func TestAccCloudIoTRegistry_basic(t *testing.T) {
 	t.Parallel()
@@ -93,6 +118,64 @@ func TestAccCloudIoTRegistry_update(t *testing.T) {
 	})
 }
 
+func TestAccCloudIoTRegistry_eventNotificationConfigDeprecatedSingleToPlural(t *testing.T) {
+	t.Parallel()
+
+	registryName := fmt.Sprintf("tf-registry-test-%s", acctest.RandString(10))
+	topic := fmt.Sprintf("tf-registry-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudIoTRegistryDestroy,
+		Steps: []resource.TestStep{
+			{
+				// Use deprecated field (event_notification_config) to create
+				Config: testAccCloudIoTRegistry_singleEventNotificationConfig(topic, registryName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_cloudiot_registry.foobar", "event_notification_configs.#", "1"),
+				),
+			},
+			{
+				// Use new field (event_notification_configs) to see if plan changed
+				Config:             testAccCloudIoTRegistry_pluralEventNotificationConfigs(topic, registryName),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
+		},
+	})
+}
+
+func TestAccCloudIoTRegistry_eventNotificationConfigPluralToDeprecatedSingle(t *testing.T) {
+	t.Parallel()
+
+	registryName := fmt.Sprintf("tf-registry-test-%s", acctest.RandString(10))
+	topic := fmt.Sprintf("tf-registry-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudIoTRegistryDestroy,
+		Steps: []resource.TestStep{
+			{
+				// Use new field (event_notification_config) to create
+				Config: testAccCloudIoTRegistry_pluralEventNotificationConfigs(topic, registryName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_cloudiot_registry.foobar", "event_notification_configs.#", "1"),
+				),
+			},
+			{
+				// Use new field (event_notification_configs) to see if plan changed
+				Config:             testAccCloudIoTRegistry_singleEventNotificationConfig(topic, registryName),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
+		},
+	})
+}
+
 func testAccCheckCloudIoTRegistryDestroy(s *terraform.State) error {
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "google_cloudiot_registry" {
@@ -167,6 +250,8 @@ resource "google_cloudiot_registry" "foobar" {
   mqtt_config = {
     mqtt_enabled_state = "MQTT_DISABLED"
   }
+	
+  log_level = "INFO"
 
   credentials {
     public_key_certificate = {
@@ -176,4 +261,50 @@ resource "google_cloudiot_registry" "foobar" {
   }
 }
 `, acctest.RandString(10), acctest.RandString(10), registryName)
+}
+
+func testAccCloudIoTRegistry_singleEventNotificationConfig(topic, registryName string) string {
+	return fmt.Sprintf(`
+resource "google_project_iam_binding" "cloud-iot-iam-binding" {
+  members = ["serviceAccount:cloud-iot@system.gserviceaccount.com"]
+  role    = "roles/pubsub.publisher"
+}
+
+resource "google_pubsub_topic" "event-topic" {
+  name = "%s"
+}
+
+resource "google_cloudiot_registry" "foobar" {
+  depends_on = ["google_project_iam_binding.cloud-iot-iam-binding"]
+
+  name = "%s"
+
+  event_notification_config = {
+    pubsub_topic_name = "${google_pubsub_topic.event-topic.id}"
+  }
+}
+`, topic, registryName)
+}
+
+func testAccCloudIoTRegistry_pluralEventNotificationConfigs(topic, registryName string) string {
+	return fmt.Sprintf(`
+resource "google_project_iam_binding" "cloud-iot-iam-binding" {
+  members = ["serviceAccount:cloud-iot@system.gserviceaccount.com"]
+  role    = "roles/pubsub.publisher"
+}
+
+resource "google_pubsub_topic" "event-topic" {
+  name = "%s"
+}
+
+resource "google_cloudiot_registry" "foobar" {
+  depends_on = ["google_project_iam_binding.cloud-iot-iam-binding"]
+
+  name = "%s"
+
+  event_notification_config = {
+    pubsub_topic_name = "${google_pubsub_topic.event-topic.id}"
+  }
+}
+`, topic, registryName)
 }
