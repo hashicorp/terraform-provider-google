eac037e3cb3ef2efafea35ae9acd7615cb206bee
Kevin Labesse <kevin@labesse.me>
2019-09-30 12:02:39 +0200

    update import to new terraform sdk
    Signed-off-by: Kevin Labesse <kevin@labesse.me>

--- google/resource_bigtable_gc_policy.go
+++ google/resource_bigtable_gc_policy.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"cloud.google.com/go/bigtable"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 const (
