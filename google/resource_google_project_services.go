9119bf02ee087cacb6a4cb6f57eaf21d6f26f878
Nathan McKinley <nmckinley@google.com>
2019-10-01 19:39:22 +0000

    Deprecate project_services in 2.x.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_google_project_services.go
+++ google/resource_google_project_services.go
@@ -28,6 +28,10 @@ func resourceGoogleProjectServices() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
+		DeprecationMessage: "google_project_services is deprecated - many users reported " +
+			"issues with dependent services that were not resolvable.  Please use google_project_service or the " +
+			"https://github.com/terraform-google-modules/terraform-google-project-factory/tree/master/modules/project_services" +
+			" module.  This resource will be removed in version 3.0.0.",
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(20 * time.Minute),
