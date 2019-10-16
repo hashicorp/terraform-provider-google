7c4fae129dff14b42a42dcd3ae7fcc723915b50c
Maggie Templeton <mtempleton@pendo.io>
2019-10-16 14:24:02 -0400

    remove unneeded tests

--- google/resource_task_queue_test.go
+++ google/resource_task_queue_test.go
@@ -2,23 +2,19 @@ package google
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 
 	"golang.org/x/net/context"
-	"google.golang.org/api/cloudtasks/v2"
 )
 
 func TestAccTaskQueue_basic(t *testing.T) {
 	t.Parallel()
 
-	var queue cloudtasks.Queue
 	queueName := fmt.Sprintf("tf-test-task-queue-%d", acctest.RandInt())
-	location := "us-central1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -27,12 +23,6 @@ func TestAccTaskQueue_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccTaskQueue_basic(queueName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTaskQueueExists(
-						"google_task_queue.fizzbuzz", location, queueName, &queue),
-					testAccCheckTaskQueueEquals(
-						"google_task_queue.fizzbuzz", &queue),
-				),
 			},
 			{
 				ResourceName:      "google_task_queue.fizzbuzz",
@@ -46,9 +36,7 @@ func TestAccTaskQueue_basic(t *testing.T) {
 func TestAccTaskQueue_withParams(t *testing.T) {
 	t.Parallel()
 
-	var queue cloudtasks.Queue
 	queueName := fmt.Sprintf("tf-test-task-queue-%d", acctest.RandInt())
-	location := "us-central1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -57,12 +45,6 @@ func TestAccTaskQueue_withParams(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccTaskQueue_withParams(queueName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTaskQueueExists(
-						"google_task_queue.fizzbuzz", location, queueName, &queue),
-					testAccCheckTaskQueueEquals(
-						"google_task_queue.fizzbuzz", &queue),
-				),
 			},
 			{
 				ResourceName:            "google_task_queue.fizzbuzz",
@@ -77,9 +59,7 @@ func TestAccTaskQueue_withParams(t *testing.T) {
 func TestAccTaskQueue_update(t *testing.T) {
 	t.Parallel()
 
-	var queue cloudtasks.Queue
 	queueName := fmt.Sprintf("tf-test-task-queue-%d", acctest.RandInt())
-	location := "us-central1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -88,21 +68,9 @@ func TestAccTaskQueue_update(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccTaskQueue_basic(queueName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTaskQueueExists(
-						"google_task_queue.fizzbuzz", location, queueName, &queue),
-					testAccCheckTaskQueueEquals(
-						"google_task_queue.fizzbuzz", &queue),
-				),
 			},
 			{
 				Config: testAccTaskQueue_withParams(queueName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTaskQueueExists(
-						"google_task_queue.fizzbuzz", location, queueName, &queue),
-					testAccCheckTaskQueueEquals(
-						"google_task_queue.fizzbuzz", &queue),
-				),
 			},
 		},
 	})
@@ -111,9 +79,7 @@ func TestAccTaskQueue_update(t *testing.T) {
 func TestAccTaskQueue_forceDestroy(t *testing.T) {
 	t.Parallel()
 
-	var queue cloudtasks.Queue
 	queueName := fmt.Sprintf("tf-test-task-queue-%d", acctest.RandInt())
-	location := "us-central1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -122,10 +88,6 @@ func TestAccTaskQueue_forceDestroy(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccTaskQueue_basic(queueName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckTaskQueueExists(
-						"google_task_queue.fizzbuzz", location, queueName, &queue),
-				),
 			},
 		},
 	})
@@ -163,95 +125,6 @@ resource "google_task_queue" "fizzbuzz" {
     max_doublings = 16
   }
 }`, name)
-}
-
-func testAccCheckTaskQueueExists(n string, location, queueName string, queue *cloudtasks.Queue) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Project_ID is set")
-		}
-
-		config := testAccProvider.Meta().(*Config)
-
-		expectedName := fmt.Sprintf("projects/%s/locations/%s/queues/%s", config.Project, location, queueName)
-
-		found, err := config.clientCloudTasks.Projects.Locations.Queues.Get(expectedName).Context(context.Background()).Do()
-		if err != nil {
-			return err
-		}
-
-		if found.Name != expectedName {
-			return fmt.Errorf("expected name %s, got %s", expectedName, found.Name)
-		}
-
-		*queue = *found
-		return nil
-	}
-}
-
-func testAccCheckTaskQueueEquals(n string, queue *cloudtasks.Queue) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		config := testAccProvider.Meta().(*Config)
-
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		name := rs.Primary.Attributes["name"]
-		location := rs.Primary.Attributes["location"]
-		expectedName := fmt.Sprintf("projects/%s/locations/%s/queues/%s", config.Project, location, name)
-
-		maxBurstSize, _ := strconv.ParseInt(rs.Primary.Attributes["rate_limits.0.max_burst_size"], 10, 64)
-		maxConcurrentDispatches, _ := strconv.ParseInt(rs.Primary.Attributes["rate_limits.0.max_concurrent_dispatches"], 10, 64)
-		maxDispatchesPerSecond, _ := strconv.ParseFloat(rs.Primary.Attributes["rate_limits.0.max_dispatches_per_second"], 64)
-
-		maxBackoff := rs.Primary.Attributes["retry.0.max_backoff"]
-		maxAttempts, _ := strconv.ParseInt(rs.Primary.Attributes["retry.0.max_attempts"], 10, 64)
-		minBackoff := rs.Primary.Attributes["retry.0.min_backoff"]
-		maxDoublings, _ := strconv.ParseInt(rs.Primary.Attributes["retry.0.max_doublings"], 10, 64)
-
-		if expectedName != queue.Name {
-			return fmt.Errorf("Error name mismatch, (%s, %s)", expectedName, queue.Name)
-		}
-
-		// AppEngineRoutingOverride comparison
-		if len(rs.Primary.Attributes["app_engine_routing_override.0"]) > 0 && queue.AppEngineRoutingOverride == nil {
-			return fmt.Errorf("Error app_engine_routing_override mismatch, (%v, %v)", rs.Primary.Attributes["app_engine_routing_override.0"], queue.AppEngineRoutingOverride)
-		}
-
-		// RateLimits comparison
-		if maxBurstSize != queue.RateLimits.MaxBurstSize {
-			return fmt.Errorf("Error max_burst_size mismatch, (%v, %v)", maxBurstSize, queue.RateLimits.MaxBurstSize)
-		}
-		if maxConcurrentDispatches != queue.RateLimits.MaxConcurrentDispatches {
-			return fmt.Errorf("Error max_concurrent_dispatches mismatch, (%v, %v)", maxConcurrentDispatches, queue.RateLimits.MaxConcurrentDispatches)
-		}
-		if maxDispatchesPerSecond != queue.RateLimits.MaxDispatchesPerSecond {
-			return fmt.Errorf("Error max_dispatches_per_second mismatch, (%v, %v)", maxDispatchesPerSecond, queue.RateLimits.MaxDispatchesPerSecond)
-		}
-
-		// RetryConfig comparison
-		if maxBackoff != queue.RetryConfig.MaxBackoff {
-			return fmt.Errorf("Error max_backoff mismatch, (%s, %s)", maxBackoff, queue.RetryConfig.MaxBackoff)
-		}
-		if maxAttempts != queue.RetryConfig.MaxAttempts {
-			return fmt.Errorf("Error max_attempts mismatch, (%v, %v)", maxAttempts, queue.RetryConfig.MaxAttempts)
-		}
-		if minBackoff != queue.RetryConfig.MinBackoff {
-			return fmt.Errorf("Error min_backoff mismatch, (%s, %s)", minBackoff, queue.RetryConfig.MinBackoff)
-		}
-		if maxDoublings != queue.RetryConfig.MaxDoublings {
-			return fmt.Errorf("Error max_doublings mismatch, (%v, %v)", maxDoublings, queue.RetryConfig.MaxDoublings)
-		}
-
-		return nil
-	}
 }
 
 func testAccTaskQueueDestroy(s *terraform.State) error {
