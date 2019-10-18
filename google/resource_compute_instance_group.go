968f02b64d5709be64baf8b1878bc3a39475d78a
Riley Karson <rileykarson@google.com>
2019-10-18 16:20:25 +0000

    Compare relpath and not by version in compute ig
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_instance_group.go
+++ google/resource_compute_instance_group.go
@@ -49,7 +49,7 @@ func resourceComputeInstanceGroup() *schema.Resource {
 				Optional: true,
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
-				Set:      schema.HashString,
+				Set:      selfLinkRelativePathHash,
 			},
 
 			"named_port": {
