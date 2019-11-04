375dff52a9b51e6350949d2339bf3a507642a0dc
Megan Bang <mbang@hashicorp.com>
2019-11-04 12:50:02 -0600

    update examples from 0.11 syntax to 0.12 syntax

--- examples/shared-vpc/main.tf
+++ examples/shared-vpc/main.tf
@@ -1,11 +1,12 @@
 # https://cloud.google.com/vpc/docs/shared-vpc
 
 provider "google" {
-  region      = "${var.region}"
-  credentials = "${file("${var.credentials_file_path}")}"
+  region      = var.region
+  credentials = file(var.credentials_file_path)
 }
 
-provider "random" {}
+provider "random" {
+}
 
 resource "random_id" "host_project_name" {
   byte_length = 8
@@ -27,79 +28,81 @@ resource "random_id" "standalone_project_name" {
 resource "google_project" "host_project" {
   name            = "Host Project"
   project_id      = "tf-vpc-${random_id.host_project_name.hex}"
-  org_id          = "${var.org_id}"
-  billing_account = "${var.billing_account_id}"
+  org_id          = var.org_id
+  billing_account = var.billing_account_id
 }
 
 # One project which will use the VPC.
 resource "google_project" "service_project_1" {
   name            = "Service Project 1"
   project_id      = "tf-vpc-${random_id.service_project_1_name.hex}"
-  org_id          = "${var.org_id}"
-  billing_account = "${var.billing_account_id}"
+  org_id          = var.org_id
+  billing_account = var.billing_account_id
 }
 
 # The other project which will use the VPC.
 resource "google_project" "service_project_2" {
   name            = "Service Project 2"
   project_id      = "tf-vpc-${random_id.service_project_2_name.hex}"
-  org_id          = "${var.org_id}"
-  billing_account = "${var.billing_account_id}"
+  org_id          = var.org_id
+  billing_account = var.billing_account_id
 }
 
 # A project which will not use the VPC, for the sake of demonstration.
 resource "google_project" "standalone_project" {
   name            = "Standalone Project"
   project_id      = "tf-vpc-${random_id.standalone_project_name.hex}"
-  org_id          = "${var.org_id}"
-  billing_account = "${var.billing_account_id}"
+  org_id          = var.org_id
+  billing_account = var.billing_account_id
 }
 
 # Compute service needs to be enabled for all four new projects.
 resource "google_project_service" "host_project" {
-  project = "${google_project.host_project.project_id}"
+  project = google_project.host_project.project_id
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "service_project_1" {
-  project = "${google_project.service_project_1.project_id}"
+  project = google_project.service_project_1.project_id
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "service_project_2" {
-  project = "${google_project.service_project_2.project_id}"
+  project = google_project.service_project_2.project_id
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "standalone_project" {
-  project = "${google_project.standalone_project.project_id}"
+  project = google_project.standalone_project.project_id
   service = "compute.googleapis.com"
 }
 
 # Enable shared VPC hosting in the host project.
 resource "google_compute_shared_vpc_host_project" "host_project" {
-  project    = "${google_project.host_project.project_id}"
-  depends_on = ["google_project_service.host_project"]
+  project    = google_project.host_project.project_id
+  depends_on = [google_project_service.host_project]
 }
 
 # Enable shared VPC in the two service projects - explicitly depend on the host
 # project enabling it, because enabling shared VPC will fail if the host project
 # is not yet hosting.
 resource "google_compute_shared_vpc_service_project" "service_project_1" {
-  host_project    = "${google_project.host_project.project_id}"
-  service_project = "${google_project.service_project_1.project_id}"
+  host_project    = google_project.host_project.project_id
+  service_project = google_project.service_project_1.project_id
 
-  depends_on = ["google_compute_shared_vpc_host_project.host_project",
-    "google_project_service.service_project_1",
+  depends_on = [
+    google_compute_shared_vpc_host_project.host_project,
+    google_project_service.service_project_1,
   ]
 }
 
 resource "google_compute_shared_vpc_service_project" "service_project_2" {
-  host_project    = "${google_project.host_project.project_id}"
-  service_project = "${google_project.service_project_2.project_id}"
+  host_project    = google_project.host_project.project_id
+  service_project = google_project.service_project_2.project_id
 
-  depends_on = ["google_compute_shared_vpc_host_project.host_project",
-    "google_project_service.service_project_2",
+  depends_on = [
+    google_compute_shared_vpc_host_project.host_project,
+    google_project_service.service_project_2,
   ]
 }
 
@@ -107,18 +110,19 @@ resource "google_compute_shared_vpc_service_project" "service_project_2" {
 resource "google_compute_network" "shared_network" {
   name                    = "shared-network"
   auto_create_subnetworks = "true"
-  project                 = "${google_compute_shared_vpc_host_project.host_project.project}"
+  project                 = google_compute_shared_vpc_host_project.host_project.project
 
-  depends_on = ["google_compute_shared_vpc_service_project.service_project_1",
-    "google_compute_shared_vpc_service_project.service_project_2",
+  depends_on = [
+    google_compute_shared_vpc_service_project.service_project_1,
+    google_compute_shared_vpc_service_project.service_project_2,
   ]
 }
 
 # Allow the hosted network to be hit over ICMP, SSH, and HTTP.
 resource "google_compute_firewall" "shared_network" {
   name    = "allow-ssh-and-icmp"
-  network = "${google_compute_network.shared_network.self_link}"
-  project = "${google_compute_network.shared_network.project}"
+  network = google_compute_network.shared_network.self_link
+  project = google_compute_network.shared_network.project
 
   allow {
     protocol = "icmp"
@@ -134,13 +138,13 @@ resource "google_compute_firewall" "shared_network" {
 resource "google_compute_network" "standalone_network" {
   name                    = "standalone-network"
   auto_create_subnetworks = "true"
-  project                 = "${google_project.standalone_project.project_id}"
-  depends_on              = ["google_project_service.standalone_project"]
+  project                 = google_project.standalone_project.project_id
+  depends_on              = [google_project_service.standalone_project]
 }
 
 resource "google_compute_firewall" "standalone_network" {
   name    = "allow-ssh-and-icmp"
-  network = "${google_compute_network.standalone_network.self_link}"
+  network = google_compute_network.standalone_network.self_link
 
   allow {
     protocol = "icmp"
@@ -151,15 +155,15 @@ resource "google_compute_firewall" "standalone_network" {
     ports    = ["22", "80"]
   }
 
-  project = "${google_project.standalone_project.project_id}"
+  project = google_project.standalone_project.project_id
 }
 
 # Create a VM which hosts a web page stating its identity ("VM1")
 resource "google_compute_instance" "project_1_vm" {
   name         = "tf-project-1-vm"
-  project      = "${google_project.service_project_1.project_id}"
+  project      = google_project.service_project_1.project_id
   machine_type = "f1-micro"
-  zone         = "${var.region_zone}"
+  zone         = var.region_zone
 
   boot_disk {
     initialize_params {
@@ -170,7 +174,7 @@ resource "google_compute_instance" "project_1_vm" {
   metadata_startup_script = "VM_NAME=VM1\n${file("scripts/install-vm.sh")}"
 
   network_interface {
-    network = "${google_compute_network.shared_network.self_link}"
+    network = google_compute_network.shared_network.self_link
 
     access_config {
       // Ephemeral IP
@@ -181,15 +185,15 @@ resource "google_compute_instance" "project_1_vm" {
     scopes = ["https://www.googleapis.com/auth/compute.readonly"]
   }
 
-  depends_on = ["google_project_service.service_project_1"]
+  depends_on = [google_project_service.service_project_1]
 }
 
 # Create a VM which hosts a web page demonstrating the example networking.
 resource "google_compute_instance" "project_2_vm" {
   name         = "tf-project-2-vm"
   machine_type = "f1-micro"
-  project      = "${google_project.service_project_2.project_id}"
-  zone         = "${var.region_zone}"
+  project      = google_project.service_project_2.project_id
+  zone         = var.region_zone
 
   boot_disk {
     initialize_params {
@@ -198,15 +202,16 @@ resource "google_compute_instance" "project_2_vm" {
   }
 
   metadata_startup_script = <<EOF
-VM1_EXT_IP=${google_compute_instance.project_1_vm.network_interface.0.access_config.0.nat_ip}
-ST_VM_EXT_IP=${google_compute_instance.standalone_project_vm.network_interface.0.access_config.0.nat_ip}
-VM1_INT_IP=${google_compute_instance.project_1_vm.network_interface.0.address}
-ST_VM_INT_IP=${google_compute_instance.standalone_project_vm.network_interface.0.address}
+VM1_EXT_IP=${google_compute_instance.project_1_vm.network_interface[0].access_config[0].nat_ip}
+ST_VM_EXT_IP=${google_compute_instance.standalone_project_vm.network_interface[0].access_config[0].nat_ip}
+VM1_INT_IP=${google_compute_instance.project_1_vm.network_interface[0].address}
+ST_VM_INT_IP=${google_compute_instance.standalone_project_vm.network_interface[0].address}
 ${file("scripts/install-network-page.sh")}
 EOF
 
+
   network_interface {
-    network = "${google_compute_network.shared_network.self_link}"
+    network = google_compute_network.shared_network.self_link
 
     access_config {
       // Ephemeral IP
@@ -217,15 +222,15 @@ EOF
     scopes = ["https://www.googleapis.com/auth/compute.readonly"]
   }
 
-  depends_on = ["google_project_service.service_project_2"]
+  depends_on = [google_project_service.service_project_2]
 }
 
 # Create a VM which hosts a web page stating its identity ("standalone").
 resource "google_compute_instance" "standalone_project_vm" {
   name         = "tf-standalone-vm"
   machine_type = "f1-micro"
-  project      = "${google_project.standalone_project.project_id}"
-  zone         = "${var.region_zone}"
+  project      = google_project.standalone_project.project_id
+  zone         = var.region_zone
 
   boot_disk {
     initialize_params {
@@ -236,7 +241,7 @@ resource "google_compute_instance" "standalone_project_vm" {
   metadata_startup_script = "VM_NAME=standalone\n${file("scripts/install-vm.sh")}"
 
   network_interface {
-    network = "${google_compute_network.standalone_network.self_link}"
+    network = google_compute_network.standalone_network.self_link
 
     access_config {
       // Ephemeral IP
@@ -247,5 +252,5 @@ resource "google_compute_instance" "standalone_project_vm" {
     scopes = ["https://www.googleapis.com/auth/compute.readonly"]
   }
 
-  depends_on = ["google_project_service.standalone_project"]
+  depends_on = [google_project_service.standalone_project]
 }
