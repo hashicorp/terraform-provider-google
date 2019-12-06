3d8c9b59e93e7dcc70f8213a1dcae333c067d53f
Chris Stephens <chrisst@google.com>
2019-12-06 01:12:00 +0000

    Move cloudrun sql test to examples
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloud_run_service_generated_test.go
+++ google/resource_cloud_run_service_generated_test.go
@@ -28,7 +28,7 @@ func TestAccCloudRunService_cloudRunServiceBasicExample(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"namespace":     getTestProjectFromEnv(),
+		"project":       getTestProjectFromEnv(),
 		"random_suffix": acctest.RandString(10),
 	}
 
@@ -56,7 +56,7 @@ resource "google_cloud_run_service" "default" {
   location = "us-central1"
 
   metadata {
-    namespace = "%{namespace}"
+    namespace = ""
   }
 
   template {
@@ -71,6 +71,131 @@ resource "google_cloud_run_service" "default" {
     percent         = 100
     latest_revision = true
   }
+}
+`, context)
+}
+
+func TestAccCloudRunService_cloudRunServiceSqlExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":       getTestProjectFromEnv(),
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudRunServiceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceSqlExample(context),
+			},
+			{
+				ResourceName:      "google_cloud_run_service.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceSqlExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_service" "default" {
+  name     = "tftest-cloudrun%{random_suffix}"
+  location = "us-central1"
+
+  metadata {
+    namespace = "%{project}"
+  }
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "1000"
+        "run.googleapis.com/cloudsql-instances" = "%{project}:us-central1:${google_sql_database_instance.instance.name}"
+        "run.googleapis.com/client-name"        = "cloud-console"
+      }
+    }
+  }
+}
+
+resource "google_sql_database_instance" "instance" {
+  name   = "cloudrun-sql%{random_suffix}"
+  region = "us-east1"
+  settings {
+    tier = "D0"
+  }
+}
+`, context)
+}
+
+func TestAccCloudRunService_cloudRunServiceNoauthExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":       getTestProjectFromEnv(),
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudRunServiceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceNoauthExample(context),
+			},
+			{
+				ResourceName:      "google_cloud_run_service.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceNoauthExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_service" "default" {
+  name     = "tftest-cloudrun%{random_suffix}"
+  location = "us-central1"
+
+  metadata {
+    namespace = "%{project}"
+  }
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
 `, context)
 }
