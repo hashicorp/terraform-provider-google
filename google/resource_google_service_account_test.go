4a449a184fef8a0349f7801a55b4376cf8e11baa
Alex Stephen <alexstephen@google.com>
2019-12-11 21:28:34 +0000

    Changing Ansible Repo on Concourse
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_google_service_account_test.go
+++ google/resource_google_service_account_test.go
@@ -18,7 +18,7 @@ func TestAccServiceAccount_basic(t *testing.T) {
 	displayName := "Terraform Test"
 	displayName2 := "Terraform Test Update"
 	desc := "test description"
-	desc2 := "test description update"
+	desc2 := ""
 	project := getTestProjectFromEnv()
 	expectedEmail := fmt.Sprintf("%s@%s.iam.gserviceaccount.com", accountId, project)
 	resource.Test(t, resource.TestCase{
