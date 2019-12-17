b153307c1ef799d90eb1f5636dc923790155a769
The Magician <magic-modules@google.com>
2019-12-17 13:33:34 -0800

    Add error retry predicate for monitoring 409 error (#5204)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/error_retry_predicates.go
+++ google/error_retry_predicates.go
@@ -41,3 +41,12 @@ func isSqlOperationInProgressError(err error) (bool, string) {
 	}
 	return false, ""
 }
+
+func isMonitoringRetryableError(err error) (bool, string) {
+	if gerr, ok := err.(*googleapi.Error); ok {
+		if gerr.Code == 409 && strings.Contains(strings.ToLower(gerr.Body), "too many concurrent edits") {
+			return true, "Waiting for other Monitoring changes to finish"
+		}
+	}
+	return false, ""
+}
