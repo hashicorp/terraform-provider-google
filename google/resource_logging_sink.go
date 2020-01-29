364af2f2c53ff34aec3ab156ab1264d37c87c680
Modular Magician <magic-modules@google.com>
2020-01-29 19:16:26 +0000

    update bigquery_options to be O+C (#3035)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_logging_sink.go
+++ google/resource_logging_sink.go
@@ -34,6 +34,7 @@ func resourceLoggingSinkSchema() map[string]*schema.Schema {
 		"bigquery_options": {
 			Type:     schema.TypeList,
 			Optional: true,
+			Computed: true,
 			MaxItems: 1,
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
