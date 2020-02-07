406a128fdd2cffafcee4dfd0837ef8940cafa233
Modular Magician <magic-modules@google.com>
2020-02-07 21:08:27 +0000

    Implement new-style resource state handling for GKE cluster (#3077)
    * Implement new-style resource state handling, add post-create taint and post-update await.
    
    * Review comments
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/state_util.go
@@ -0,0 +1,19 @@
+package google
+
+// A StateType represents the specific type of resting state that a state value
+// is.
+type StateType int
+
+const (
+	UndefinedState StateType = iota
+	// A special resting state, that generally requires special consideration
+	// Interactive states like PENDING_PARTNER in interconnects are an example
+	RestingState
+	// An error state is a state that indicates that a resource is not working
+	// correctly. If this is Create, it should be tainted by returning an error
+	ErrorState
+	// A ready resource is fully provisioned, and ready to accept traffic/work
+	ReadyState
+)
+
+type RestingStates map[string]StateType
