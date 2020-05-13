134038a1bd301933f962b74e159ad82cb36503e9
Modular Magician <magic-modules@google.com>
2020-05-13 17:25:49 +0000

    configConnector GKE addon config added to clsuter resource (#3498)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_container_cluster_test.go
+++ google/resource_container_cluster_test.go
@@ -134,6 +134,7 @@ func TestAccContainerCluster_withAddons(t *testing.T) {
 	t.Parallel()
 
 	clusterName := fmt.Sprintf("tf-test-cluster-%s", randString(t, 10))
+	pid := getTestProjectFromEnv()
 
 	vcrTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -141,7 +142,7 @@ func TestAccContainerCluster_withAddons(t *testing.T) {
 		CheckDestroy: testAccCheckContainerClusterDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccContainerCluster_withAddons(clusterName),
+				Config: testAccContainerCluster_withAddons(pid, clusterName),
 			},
 			{
 				ResourceName:            "google_container_cluster.primary",
@@ -150,7 +151,7 @@ func TestAccContainerCluster_withAddons(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"min_master_version"},
 			},
 			{
-				Config: testAccContainerCluster_updateAddons(clusterName),
+				Config: testAccContainerCluster_updateAddons(pid, clusterName),
 			},
 			{
 				ResourceName:            "google_container_cluster.primary",
@@ -1531,14 +1532,19 @@ resource "google_container_cluster" "primary" {
 `, name)
 }
 
-func testAccContainerCluster_withAddons(clusterName string) string {
+func testAccContainerCluster_withAddons(projectID string, clusterName string) string {
 	return fmt.Sprintf(`
+data "google_project" "project" {
+  project_id = "%s"
+}
+
 resource "google_container_cluster" "primary" {
   name               = "%s"
   location           = "us-central1-a"
   initial_node_count = 1
 
   min_master_version = "latest"
+
 
   addons_config {
     http_load_balancing {
@@ -1555,17 +1561,22 @@ resource "google_container_cluster" "primary" {
     }
   }
 }
-`, clusterName)
+`, projectID, clusterName)
 }
 
-func testAccContainerCluster_updateAddons(clusterName string) string {
+func testAccContainerCluster_updateAddons(projectID string, clusterName string) string {
 	return fmt.Sprintf(`
+data "google_project" "project" {
+  project_id = "%s"
+}	
+
 resource "google_container_cluster" "primary" {
   name               = "%s"
   location           = "us-central1-a"
   initial_node_count = 1
 
   min_master_version = "latest"
+
 
   addons_config {
     http_load_balancing {
@@ -1582,7 +1593,7 @@ resource "google_container_cluster" "primary" {
     }
   }
 }
-`, clusterName)
+`, projectID, clusterName)
 }
 
 func testAccContainerCluster_withMasterAuth(clusterName string) string {
