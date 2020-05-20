c74d12abbf276d1d74307948e5c5a809a34ceb81
Modular Magician <magic-modules@google.com>
2020-05-20 23:34:22 +0000

    Data Catalog EntryGroup (#3485)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/resource_data_catalog_entry_group_test.go
@@ -0,0 +1,50 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataCatalogEntryGroup_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDataCatalogEntryGroupDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataCatalogEntryGroup_dataCatalogEntryGroupBasicExample(context),
+			},
+			{
+				ResourceName:            "google_data_catalog_entry_group.basic_entry_group",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "entry_group_id"},
+			},
+			{
+				Config: testAccDataCatalogEntryGroup_dataCatalogEntryGroupFullExample(context),
+			},
+			{
+				ResourceName:            "google_data_catalog_entry_group.basic_entry_group",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "entry_group_id"},
+			},
+			{
+				Config: testAccDataCatalogEntryGroup_dataCatalogEntryGroupBasicExample(context),
+			},
+			{
+				ResourceName:            "google_data_catalog_entry_group.basic_entry_group",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "entry_group_id"},
+			},
+		},
+	})
+}
