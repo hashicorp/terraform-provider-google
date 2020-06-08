92160eafaf7e056a8725c014f4d3f2397762adea
Mounir Karoud <mounir.karoud@ev-box.com>
2020-06-08 10:43:36 +0200

    Removed wrong import in test

--- google/data_source_google_redis_instance_test.go
+++ google/data_source_google_redis_instance_test.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccRedisInstanceDatasource_basic(t *testing.T) {
