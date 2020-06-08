ad1c1ceedb470fe58d51481636b4a727f6b735b7
Mounir Karoud <mounir.karoud@ev-box.com>
2020-06-08 10:59:11 +0200

    Fixed test

--- google/data_source_google_redis_instance_test.go
+++ google/data_source_google_redis_instance_test.go
@@ -15,60 +15,25 @@ func TestAccRedisInstanceDatasource_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccRedisInstanceDatasourceConfig,
+				Config: testAccRedisInstanceDatasourceConfig(randString(t, 10)),
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourceGoogleRedisInstanceCheck("data.google_redis_instance.redis", "google_redis_instance.redis"),
+					checkDataSourceStateMatchesResourceState("data.google_redis_instance.redis", "google_redis_instance.redis"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceGoogleRedisInstanceCheck(dataSourceName string, resourceName string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		ds, ok := s.RootModule().Resources[dataSourceName]
-		if !ok {
-			return fmt.Errorf("root module has no resource called %s", dataSourceName)
-		}
-
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("can't find %s in state", resourceName)
-		}
-
-		dsAttr := ds.Primary.Attributes
-		rsAttr := rs.Primary.Attributes
-
-		clusterAttrToCheck := []string{
-			"name",
-			"region",
-			"host",
-			"port",
-		}
-
-		for _, attr := range clusterAttrToCheck {
-			if dsAttr[attr] != rsAttr[attr] {
-				return fmt.Errorf(
-					"%s is %s; want %s",
-					attr,
-					dsAttr[attr],
-					rsAttr[attr],
-				)
-			}
-		}
-
-		return nil
-	}
+func testAccRedisInstanceDatasourceConfig(suffix string) string {
+	return fmt.Sprintf(`
+resource "google_redis_instance" "redis" {
+	name               = "redis-test-%s"
+  memory_size_gb     = 1
+  region             = "europe-west1"
 }
 
-var testAccRedisInstanceDatasourceConfig = fmt.Sprintf(`
-	resource "google_redis_instance" "redis" {
-		name               = "redis-test-%s"
-    memory_size_gb     = 1
-    region             = "europe-west1"
-	}
-
-	data "google_redis_instance" "redis" {
-		name = "${google_redis_instance.redis.name}"
-	}
-`, acctest.RandString(10))
+data "google_redis_instance" "redis" {
+	name = "${google_redis_instance.redis.name}"
+}
+`, suffix)
+}
