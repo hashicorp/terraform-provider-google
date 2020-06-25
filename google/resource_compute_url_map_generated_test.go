f6849ba716f9866d868ba9714021c5bc0439ad39
The Magician <magic-modules@google.com>
2020-06-25 11:13:49 -0700

    add examples for L7-XLB Header & Parameter Based Routing (#3703) (#6699)
    * add examples for L7-XLB Header & Parameter Based Routing
    
    * fixed formatting
    
    Co-authored-by: Edward Sun <sunedward@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: Edward Sun <sunedward@google.com>

--- google/resource_compute_url_map_generated_test.go
+++ google/resource_compute_url_map_generated_test.go
@@ -563,6 +563,212 @@ resource "google_compute_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeUrlMap_urlMapHeaderBasedRoutingExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeUrlMapDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeUrlMap_urlMapHeaderBasedRoutingExample(context),
+			},
+			{
+				ResourceName:      "google_compute_url_map.urlmap",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccComputeUrlMap_urlMapHeaderBasedRoutingExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_url_map" "urlmap" {
+  name        = "urlmap%{random_suffix}"
+  description = "header-based routing example"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts = ["*"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    route_rules {
+      priority = 1
+      service = google_compute_backend_service.service-a.id
+      match_rules {
+        prefix_match = "/"
+        ignore_case = true
+        header_matches {
+          header_name = "abtest"
+          exact_match = "a"
+        }
+      }
+    }
+    route_rules {
+      priority = 2
+      service = google_compute_backend_service.service-b.id
+      match_rules {
+        ignore_case = true
+        prefix_match = "/"
+        header_matches {
+          header_name = "abtest"
+          exact_match = "b"
+        }
+      }
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "default%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_backend_service" "service-a" {
+  name        = "tf-test-service-a%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_backend_service" "service-b" {
+  name        = "tf-test-service-b%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "tf-test-health-check%{random_suffix}"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+`, context)
+}
+
+func TestAccComputeUrlMap_urlMapParameterBasedRoutingExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeUrlMapDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeUrlMap_urlMapParameterBasedRoutingExample(context),
+			},
+			{
+				ResourceName:      "google_compute_url_map.urlmap",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccComputeUrlMap_urlMapParameterBasedRoutingExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_url_map" "urlmap" {
+  name        = "urlmap%{random_suffix}"
+  description = "parameter-based routing example"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts = ["*"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    route_rules {
+      priority = 1
+      service = google_compute_backend_service.service-a.id
+      match_rules {
+        prefix_match = "/"
+        ignore_case = true
+        query_parameter_matches {
+          name = "abtest"
+          exact_match = "a"
+        }
+      }
+    }
+    route_rules {
+      priority = 2
+      service = google_compute_backend_service.service-b.id
+      match_rules {
+        ignore_case = true
+        prefix_match = "/"
+        query_parameter_matches {
+          name = "abtest"
+          exact_match = "b"
+        }
+      }
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "default%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_backend_service" "service-a" {
+  name        = "tf-test-service-a%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_backend_service" "service-b" {
+  name        = "tf-test-service-b%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "tf-test-health-check%{random_suffix}"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+`, context)
+}
+
 func testAccCheckComputeUrlMapDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
