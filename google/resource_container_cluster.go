518e5efecbb431a3f2c90c3b492ddf2175ea709b
Modular Magician <magic-modules@google.com>
2020-08-03 21:09:16 +0000

    Fix description for ip_allocation_policy.cluster_secondary_range_name… (#3810)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_container_cluster.go
+++ google/resource_container_cluster.go
@@ -745,7 +745,7 @@ func resourceContainerCluster() *schema.Resource {
 							Computed:      true,
 							ForceNew:      true,
 							ConflictsWith: ipAllocationCidrBlockFields,
-							Description:   `The IP address range of the services IPs in this cluster. Set to blank to have a range chosen with the default size. Set to /netmask (e.g. /14) to have a range chosen with a specific netmask. Set to a CIDR notation (e.g. 10.96.0.0/14) from the RFC-1918 private networks (e.g. 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16) to pick a specific range to use.`,
+							Description:   `The name of the existing secondary range in the cluster's subnetwork to use for pod IP addresses. Alternatively, cluster_ipv4_cidr_block can be used to automatically create a GKE-managed one.`,
 						},
 
 						"services_secondary_range_name": {
