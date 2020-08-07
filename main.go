807e0799d2a3cd941cbf690e29d187a82282bd76
Megan Bang <mbang@hashicorp.com>
2020-08-07 16:30:12 -0500

    update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/terraform-provider-google/google"
+	"github.com/hashicorp/terraform-provider-google/google"
 )
 
 func main() {
