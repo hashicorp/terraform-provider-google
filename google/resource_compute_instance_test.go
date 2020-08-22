3f27d59e39a99a99e6108e1889ccfa2b4002c503
Megan Bang <mbang@hashicorp.com>
2020-08-21 22:29:03 -0500

    fix TestAccComputeInstance_scratchDisk test

--- google/resource_compute_instance_test.go
+++ google/resource_compute_instance_test.go
@@ -656,7 +656,7 @@ func TestAccComputeInstance_scratchDisk(t *testing.T) {
 				Config: testAccComputeInstance_scratchDisk(instanceName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckComputeInstanceExists(
-						t, "google_compute_instance.scratch", &instance),
+						t, "google_compute_instance.foobar", &instance),
 					testAccCheckComputeInstanceScratchDisk(&instance, []string{"NVME", "SCSI"}),
 				),
 			},
@@ -3531,7 +3531,7 @@ data "google_compute_image" "my_image" {
   project = "debian-cloud"
 }
 
-resource "google_compute_instance" "scratch" {
+resource "google_compute_instance" "foobar" {
   name         = "%s"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
