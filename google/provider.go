9c90d9d56a603bbf550d39e529af391dd23a9c9f
The Magician <magic-modules@google.com>
2020-08-25 19:40:39 -0700

    add IAM resources for disk & regionDisk (#3888) (#7110)
    Co-authored-by: Edward Sun <sunedward@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: Edward Sun <sunedward@google.com>

--- google/provider.go
+++ google/provider.go
@@ -616,8 +616,8 @@ func Provider() terraform.ResourceProvider {
 }
 
 // Generated resources: 155
-// Generated IAM resources: 63
-// Total generated resources: 218
+// Generated IAM resources: 69
+// Total generated resources: 224
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -682,6 +682,9 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_region_disk_resource_policy_attachment":        resourceComputeRegionDiskResourcePolicyAttachment(),
 			"google_compute_disk_resource_policy_attachment":               resourceComputeDiskResourcePolicyAttachment(),
 			"google_compute_disk":                                          resourceComputeDisk(),
+			"google_compute_disk_iam_binding":                              ResourceIamBinding(ComputeDiskIamSchema, ComputeDiskIamUpdaterProducer, ComputeDiskIdParseFunc),
+			"google_compute_disk_iam_member":                               ResourceIamMember(ComputeDiskIamSchema, ComputeDiskIamUpdaterProducer, ComputeDiskIdParseFunc),
+			"google_compute_disk_iam_policy":                               ResourceIamPolicy(ComputeDiskIamSchema, ComputeDiskIamUpdaterProducer, ComputeDiskIdParseFunc),
 			"google_compute_firewall":                                      resourceComputeFirewall(),
 			"google_compute_forwarding_rule":                               resourceComputeForwardingRule(),
 			"google_compute_global_address":                                resourceComputeGlobalAddress(),
@@ -708,6 +711,9 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_node_template":                                 resourceComputeNodeTemplate(),
 			"google_compute_region_autoscaler":                             resourceComputeRegionAutoscaler(),
 			"google_compute_region_disk":                                   resourceComputeRegionDisk(),
+			"google_compute_region_disk_iam_binding":                       ResourceIamBinding(ComputeRegionDiskIamSchema, ComputeRegionDiskIamUpdaterProducer, ComputeRegionDiskIdParseFunc),
+			"google_compute_region_disk_iam_member":                        ResourceIamMember(ComputeRegionDiskIamSchema, ComputeRegionDiskIamUpdaterProducer, ComputeRegionDiskIdParseFunc),
+			"google_compute_region_disk_iam_policy":                        ResourceIamPolicy(ComputeRegionDiskIamSchema, ComputeRegionDiskIamUpdaterProducer, ComputeRegionDiskIdParseFunc),
 			"google_compute_region_url_map":                                resourceComputeRegionUrlMap(),
 			"google_compute_region_health_check":                           resourceComputeRegionHealthCheck(),
 			"google_compute_resource_policy":                               resourceComputeResourcePolicy(),
