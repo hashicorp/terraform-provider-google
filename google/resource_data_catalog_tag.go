6167fcedb21cc288159528dde1824e5b4c41aaa5
The Magician <magic-modules@google.com>
2020-09-21 11:28:15 -0700

    Revert "fixed Error when trying to set boolean field to `false` on google_data_catalog_tag (#3910)" (#3998) (#7315)
    This reverts commit 96ce577810a713390d312d5974ca81765533d2c7.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_data_catalog_tag.go
+++ google/resource_data_catalog_tag.go
@@ -475,7 +475,7 @@ func expandNestedDataCatalogTagFields(v interface{}, d TerraformResourceData, co
 		transformedBoolValue, err := expandNestedDataCatalogTagFieldsBoolValue(original["bool_value"], d, config)
 		if err != nil {
 			return nil, err
-		} else {
+		} else if val := reflect.ValueOf(transformedBoolValue); val.IsValid() && !isEmptyValue(val) {
 			transformed["boolValue"] = transformedBoolValue
 		}
 
