5d93cfb204f67c70a13a2776b60597b2907482d6
The Magician <magic-modules@google.com>
2020-10-05 13:35:35 -0700

    Add Global forwarding rule datasource (#4052) (#7434)
    * add global forwarding rule datasource
    
    * fix broken test
    
    * fix typos
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_global_compute_forwarding_rule.go
@@ -0,0 +1,37 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleComputeGlobalForwardingRule() *schema.Resource {
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeGlobalForwardingRule().Schema)
+
+	// Set 'Required' schema elements
+	addRequiredFieldsToSchema(dsSchema, "name")
+
+	// Set 'Optional' schema elements
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleComputeGlobalForwardingRuleRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleComputeGlobalForwardingRuleRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	name := d.Get("name").(string)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("projects/%s/global/forwardingRules/%s", project, name))
+
+	return resourceComputeGlobalForwardingRuleRead(d, meta)
+}
