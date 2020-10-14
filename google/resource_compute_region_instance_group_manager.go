d6f17dbfcc52508b5b4527651aa5045c98458c93
Modular Magician <magic-modules@google.com>
2020-10-14 21:54:24 +0000

    Fix a typo in "detatch" word, add a "do". (#4107)
    Co-authored-by: Pavel Skuratovich <pavel.skuratovich@apalon.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_region_instance_group_manager.go
+++ google/resource_compute_region_instance_group_manager.go
@@ -299,7 +299,7 @@ func resourceComputeRegionInstanceGroupManager() *schema.Resource {
 							Type:         schema.TypeString,
 							Default:      "NEVER",
 							Optional:     true,
-							Description:  `A value that prescribes what should happen to the stateful disk when the VM instance is deleted. The available options are NEVER and ON_PERMANENT_INSTANCE_DELETION. NEVER detatch the disk when the VM is deleted, but not delete the disk. ON_PERMANENT_INSTANCE_DELETION will delete the stateful disk when the VM is permanently deleted from the instance group. The default is NEVER.`,
+							Description:  `A value that prescribes what should happen to the stateful disk when the VM instance is deleted. The available options are NEVER and ON_PERMANENT_INSTANCE_DELETION. NEVER - detach the disk when the VM is deleted, but do not delete the disk. ON_PERMANENT_INSTANCE_DELETION will delete the stateful disk when the VM is permanently deleted from the instance group. The default is NEVER.`,
 							ValidateFunc: validation.StringInSlice([]string{"NEVER", "ON_PERMANENT_INSTANCE_DELETION"}, true),
 						},
 					},
--- website/docs/r/compute_region_per_instance_config.html.markdown
+++ website/docs/r/compute_region_per_instance_config.html.markdown
@@ -181,7 +181,7 @@ The `disk` block supports:
   (Optional)
   A value that prescribes what should happen to the stateful disk when the VM instance is deleted.
   The available options are `NEVER` and `ON_PERMANENT_INSTANCE_DELETION`.
-  `NEVER` detatch the disk when the VM is deleted, but not delete the disk.
+  `NEVER` - detach the disk when the VM is deleted, but do not delete the disk.
   `ON_PERMANENT_INSTANCE_DELETION` will delete the stateful disk when the VM is permanently
   deleted from the instance group.
   Default value is `NEVER`.
