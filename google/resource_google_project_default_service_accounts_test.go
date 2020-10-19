21cab11b9a33fce48670003482122cc0e9848621
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-10-19 11:04:04 -0300

    Adds basic test for project_default_service_accounts resource

--- google/resource_google_project_default_service_accounts_test.go
+++ google/resource_google_project_default_service_accounts_test.go
@@ -1,4 +1,38 @@
 package google
 
-// IDK if I should do TDD for terraform providers development
-// Do you guys do?
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceGoogleProjectServiceAccountsBasic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "google_project_service_accounts.acceptance"
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckGoogleProjectServiceAccountBasic(getTestProjectFromEnv()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						resourceName, "id", getTestProjectFromEnv()),
+					resource.TestCheckResourceAttrSet(resourceName, "project"),
+					resource.TestCheckResourceAttrSet(resourceName, "action"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGoogleProjectServiceAccountBasic(project string) string {
+	return fmt.Sprintf(`
+resource "google_project_default_service_accounts" "acceptance" {
+	project = "%s"
+}
+`, project)
+}
