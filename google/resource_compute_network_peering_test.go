50797f5ab074fe34574b24d2ef38861061415cb2
Modular Magician <magic-modules@google.com>
2020-10-23 20:31:15 +0000

    update network peering custom routes (#4138)
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Co-authored-by: angie pinilla <angelinepinilla@gmail.com>
    
    Co-authored-by: angie pinilla <angelinepinilla@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_network_peering_test.go
+++ google/resource_compute_network_peering_test.go
@@ -59,6 +59,41 @@ func TestAccComputeNetworkPeering_subnetRoutes(t *testing.T) {
 	})
 }
 
+func TestAccComputeNetworkPeering_customRoutesUpdate(t *testing.T) {
+	t.Parallel()
+
+	primaryNetworkName := fmt.Sprintf("network-test-1-%d", randInt(t))
+	peeringName := fmt.Sprintf("peering-test-%d", randInt(t))
+	importId := fmt.Sprintf("%s/%s/%s", getTestProjectFromEnv(), primaryNetworkName, peeringName)
+	suffix := randString(t, 10)
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccComputeNetworkPeeringDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeNetworkPeeringDefaultCustomRoutes(primaryNetworkName, peeringName, suffix),
+			},
+			{
+				ResourceName:      "google_compute_network_peering.bar",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateId:     importId,
+			},
+			{
+				Config: testAccComputeNetworkPeering_basic(primaryNetworkName, peeringName, suffix),
+			},
+			{
+				ResourceName:      "google_compute_network_peering.bar",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateId:     importId,
+			},
+		},
+	})
+}
+
 func testAccComputeNetworkPeeringDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		config := googleProviderConfig(t)
@@ -127,4 +162,30 @@ resource "google_compute_network_peering" "bar" {
   export_subnet_routes_with_public_ip = false
 }
 `, primaryNetworkName, suffix, peeringName)
+}
+
+func testAccComputeNetworkPeeringDefaultCustomRoutes(primaryNetworkName, peeringName, suffix string) string {
+	s := `
+resource "google_compute_network" "network1" {
+  name                    = "%s"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_network_peering" "foo" {
+  name         = "%s"
+  network      = google_compute_network.network1.self_link
+  peer_network = google_compute_network.network2.self_link
+}
+
+resource "google_compute_network" "network2" {
+  name                    = "network-test-2-%s"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_network_peering" "bar" {
+  network      = google_compute_network.network2.self_link
+  peer_network = google_compute_network.network1.self_link
+  name         = "peering-test-2-%s"
+}`
+	return fmt.Sprintf(s, primaryNetworkName, peeringName, suffix, suffix)
 }
