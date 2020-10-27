65b15d0779459e48379aa0d5e9a7c2655e21bebb
Modular Magician <magic-modules@google.com>
2020-10-27 22:04:39 +0000

    Remove Logging bucket location from the Docs (#4154)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_logging_bucket_config.go
+++ google/resource_logging_bucket_config.go
@@ -19,7 +19,7 @@ var loggingBucketConfigSchema = map[string]*schema.Schema{
 		Type:        schema.TypeString,
 		Required:    true,
 		ForceNew:    true,
-		Description: `The location of the bucket. The supported locations are: "global" "us-central1"`,
+		Description: `The location of the bucket.`,
 	},
 	"bucket_id": {
 		Type:        schema.TypeString,
