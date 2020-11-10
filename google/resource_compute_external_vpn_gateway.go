e808891d2f7f22ff181fda13e4697bdc341205b3
Modular Magician <magic-modules@google.com>
2020-11-10 23:32:40 +0000

    fix typo (#4218)
    Co-authored-by: Tony Laidig <laidig@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_external_vpn_gateway.go
+++ google/resource_compute_external_vpn_gateway.go
@@ -70,7 +70,7 @@ character, which cannot be a dash.`,
 							Type:     schema.TypeInt,
 							Optional: true,
 							ForceNew: true,
-							Description: `The numberic ID for this interface. Allowed values are based on the redundancy type
+							Description: `The numeric ID for this interface. Allowed values are based on the redundancy type
 of this external VPN gateway
 * '0 - SINGLE_IP_INTERNALLY_REDUNDANT'
 * '0, 1 - TWO_IPS_REDUNDANCY'
