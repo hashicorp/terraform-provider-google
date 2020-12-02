ec11e2b3e7a797fc0b208241788d170b19853bbf
The Magician <magic-modules@google.com>
2020-12-01 16:07:38 -0800

    some APIs treat 412s as the conflict code. (#4277) (#7915)
    * some APIs treat 412s as the conflict code.
    
    it stands for 'invalid etag' in that context.
    
    * add unit test.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/utils.go
+++ google/utils.go
@@ -139,11 +139,11 @@ func isFailedPreconditionError(err error) bool {
 }
 
 func isConflictError(err error) bool {
-	if e, ok := err.(*googleapi.Error); ok && e.Code == 409 {
+	if e, ok := err.(*googleapi.Error); ok && (e.Code == 409 || e.Code == 412) {
 		return true
 	} else if !ok && errwrap.ContainsType(err, &googleapi.Error{}) {
 		e := errwrap.GetType(err, &googleapi.Error{}).(*googleapi.Error)
-		if e.Code == 409 {
+		if e.Code == 409 || e.Code == 412 {
 			return true
 		}
 	}
