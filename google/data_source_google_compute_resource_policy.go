df1d597a540e5123bac9c1620dfea49bce487e22
Modular Magician <magic-modules@google.com>
2020-12-02 00:39:05 +0000

    Promote resource policy to GA (#4265)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/data_source_google_compute_resource_policy.go
+++ google/data_source_google_compute_resource_policy.go
@@ -1,1 +1,39 @@
 package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleComputeResourcePolicy() *schema.Resource {
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeResourcePolicy().Schema)
+
+	addRequiredFieldsToSchema(dsSchema, "name")
+	addOptionalFieldsToSchema(dsSchema, "region")
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleComputeResourcePolicyRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleComputeResourcePolicyRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	name := d.Get("name").(string)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+	region, err := getRegion(d, config)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("projects/%s/regions/%s/resourcePolicies/%s", project, region, name))
+
+	return resourceComputeResourcePolicyRead(d, meta)
+}
