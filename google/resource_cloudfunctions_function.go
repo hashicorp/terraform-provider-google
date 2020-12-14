e87f7a95acee4f9a095462bc9f55195657fe29bb
Modular Magician <magic-modules@google.com>
2020-12-14 17:42:31 +0000

    Zero out upload url before update (#4292)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloudfunctions_function.go
+++ google/resource_cloudfunctions_function.go
@@ -586,6 +586,10 @@ func resourceCloudFunctionsUpdate(d *schema.ResourceData, meta interface{}) erro
 		return handleNotFoundError(err, d, fmt.Sprintf("Target CloudFunctions Function %q", cloudFuncId.Name))
 	}
 
+	// The full function may contain a reference to manually uploaded code if the function was imported from gcloud
+	// This does not work with Terraform, so zero it out from the function if it exists. See https://github.com/hashicorp/terraform-provider-google/issues/7921
+	function.SourceUploadUrl = ""
+
 	d.Partial(true)
 
 	var updateMaskArr []string
