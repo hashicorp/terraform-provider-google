cf310c67ea8a95a9e20bc7bd533f762d187ddbed
Modular Magician <magic-modules@google.com>
2020-12-15 16:53:22 +0000

    added data source for storage bucket object content (#4254)
    * init commit bucket object content
    
    * Update provider.go.erb
    
    Format space to tab
    
    * run gofmt for a file
    
    * Update third_party/terraform/website/docs/d/storage_bucket_object_content.html.markdown
    
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>
    
    Co-authored-by: Edward Sun <sunedward@google.com>
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_storage_bucket_object_content.go
@@ -0,0 +1,61 @@
+package google
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"google.golang.org/api/storage/v1"
+)
+
+func dataSourceGoogleStorageBucketObjectContent() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceStorageBucketObject().Schema)
+
+	addRequiredFieldsToSchema(dsSchema, "bucket")
+	addRequiredFieldsToSchema(dsSchema, "name")
+	addOptionalFieldsToSchema(dsSchema, "content")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleStorageBucketObjectContentRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleStorageBucketObjectContentRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.userAgent)
+	if err != nil {
+		return err
+	}
+
+	bucket := d.Get("bucket").(string)
+	name := d.Get("name").(string)
+
+	objectsService := storage.NewObjectsService(config.NewStorageClient(userAgent))
+	getCall := objectsService.Get(bucket, name)
+
+	res, err := getCall.Download()
+	if err != nil {
+		return fmt.Errorf("Error downloading storage bucket object: %s", err)
+	}
+
+	defer res.Body.Close()
+	var bodyString string
+
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return fmt.Errorf("Error reading all  from res.Body: %s", err)
+		}
+		bodyString = string(bodyBytes)
+	}
+
+	if err := d.Set("content", bodyString); err != nil {
+		return fmt.Errorf("Error setting content: %s", err)
+	}
+
+	d.SetId(bucket + "-" + name)
+	return nil
+}
