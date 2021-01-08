58763fe01fd549370eaa55b2472c47307a86f1a8
Modular Magician <magic-modules@google.com>
2021-01-08 20:18:21 +0000

    Sweep vpc connector (#4367)
    * Sweep vpc connector
    
    * Update products/vpcaccess/terraform.yaml
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_vpc_access_connector_generated_test.go
+++ google/resource_vpc_access_connector_generated_test.go
@@ -54,7 +54,7 @@ func TestAccVPCAccessConnector_vpcAccessConnectorExample(t *testing.T) {
 func testAccVPCAccessConnector_vpcAccessConnectorExample(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_vpc_access_connector" "connector" {
-  name          = "vpcconn%{random_suffix}"
+  name          = "tf-test-vpc-con%{random_suffix}"
   region        = "us-central1"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
