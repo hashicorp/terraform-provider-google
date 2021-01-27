356b9c799b898a78f4ff6612ec4376abe8560d27
Modular Magician <magic-modules@google.com>
2021-01-27 20:08:52 +0000

    availability sli for SLO monitoring resource (#4344)
    * inital check-in for review
    
    * custom_flattner added
    
    * extra line added at EOF
    
    * PR comments implemented
    
    * custom_expander logic is placed in encoder as it more appropriate, validation func added
    
    * empty lines added at EOF
    
    * PR comments implemented, Test cases added
    
    * fixed encoder for the case where basicsli is not used in the config
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_monitoring_slo_test.go
+++ google/resource_monitoring_slo_test.go
@@ -104,6 +104,40 @@ func TestAccMonitoringSlo_basic(t *testing.T) {
 	})
 }
 
+func TestAccMonitoringSlo_availabilitySli(t *testing.T) {
+	t.Parallel()
+
+	var generatedId string
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMonitoringSloDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMonitoringSlo_availabilitySli(),
+				Check:  setTestCheckMonitoringSloId("google_monitoring_slo.primary", &generatedId),
+			},
+			{
+				ResourceName:      "google_monitoring_slo.primary",
+				ImportState:       true,
+				ImportStateVerify: true,
+				// Ignore input-only field for import
+				ImportStateVerifyIgnore: []string{"service"},
+			},
+			{
+				Config: testAccMonitoringSlo_basicUpdate(),
+				Check:  testCheckMonitoringSloIdAfterUpdate("google_monitoring_slo.primary", &generatedId),
+			},
+			{
+				ResourceName:      "google_monitoring_slo.primary",
+				ImportState:       true,
+				ImportStateVerify: true,
+				// Ignore input-only field for import
+				ImportStateVerifyIgnore: []string{"service"},
+			},
+		},
+	})
+}
 func TestAccMonitoringSlo_requestBased(t *testing.T) {
 	t.Parallel()
 
@@ -404,6 +438,26 @@ resource "google_monitoring_slo" "primary" {
     latency {
       threshold = "2s"
     }
+  }
+}
+`
+}
+
+func testAccMonitoringSlo_availabilitySli() string {
+	return `
+data "google_monitoring_app_engine_service" "ae" {
+  module_id = "default"
+}
+
+resource "google_monitoring_slo" "primary" {
+  service = data.google_monitoring_app_engine_service.ae.service_id
+
+  goal = 0.9
+  rolling_period_days = 1
+
+  basic_sli {
+	availability {
+	}
   }
 }
 `
