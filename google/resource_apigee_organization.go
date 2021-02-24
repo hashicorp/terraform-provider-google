43743561006bd8ff1c795b7fcc414970afa1de7e
The Magician <magic-modules@google.com>
2021-02-24 15:28:44 -0800

    Increased timeouts for Apigee Organization/Instance (#4529) (#8548)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_apigee_organization.go
+++ google/resource_apigee_organization.go
@@ -37,9 +37,9 @@ func resourceApigeeOrganization() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(4 * time.Minute),
+			Create: schema.DefaultTimeout(10 * time.Minute),
 			Update: schema.DefaultTimeout(4 * time.Minute),
-			Delete: schema.DefaultTimeout(4 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
