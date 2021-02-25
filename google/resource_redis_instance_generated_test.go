2804291859adf20776ca192e54bafe0b6f537569
Modular Magician <magic-modules@google.com>
2021-02-25 01:09:01 +0000

    Use bootstrapped service networking networks for redis, memcache tests (#4513)
    * Use bootstrapped service networking networks for redis, memcache tests
    
    * Fix sprintf args
    
    * Correct address name
    
    * Ignore other memcache tests
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_redis_instance_generated_test.go
+++ google/resource_redis_instance_generated_test.go
@@ -126,9 +126,11 @@ data "google_compute_network" "redis-network" {
 }
 
 func TestAccRedisInstance_redisInstancePrivateServiceExample(t *testing.T) {
+	skipIfVcr(t)
 	t.Parallel()
 
 	context := map[string]interface{}{
+		"network_name":  BootstrapSharedTestNetwork(t, "redis-private"),
 		"random_suffix": randString(t, 10),
 	}
 
@@ -155,33 +157,41 @@ func TestAccRedisInstance_redisInstancePrivateServiceExample(t *testing.T) {
 
 func testAccRedisInstance_redisInstancePrivateServiceExample(context map[string]interface{}) string {
 	return Nprintf(`
-resource "google_compute_network" "network" {
-  name = "tf-test%{random_suffix}"
+// This example assumes this network already exists.
+// The API creates a tenant network per network authorized for a
+// Redis instance and that network is not deleted when the user-created
+// network (authorized_network) is deleted, so this prevents issues
+// with tenant network quota.
+// If this network hasn't been created and you are using this example in your
+// config, add an additional network resource or change
+// this from "data"to "resource"
+data "google_compute_network" "redis-network" {
+  name = "%{network_name}"
 }
 
 resource "google_compute_global_address" "service_range" {
-  name          = "tf-test%{random_suffix}"
+  name          = "address%{random_suffix}"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.network.id
+  network       = data.google_compute_network.redis-network.id
 }
 
 resource "google_service_networking_connection" "private_service_connection" {
-  network                 = google_compute_network.network.id
+  network                 = data.google_compute_network.redis-network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.service_range.name]
 }
 
 resource "google_redis_instance" "cache" {
-  name           = "tf-test%{random_suffix}"
+  name           = "tf-test-private-cache%{random_suffix}"
   tier           = "STANDARD_HA"
   memory_size_gb = 1
 
   location_id             = "us-central1-a"
   alternative_location_id = "us-central1-f"
 
-  authorized_network = google_compute_network.network.id
+  authorized_network = data.google_compute_network.redis-network.id
   connect_mode       = "PRIVATE_SERVICE_ACCESS"
 
   redis_version     = "REDIS_4_0"
--- website/docs/r/redis_instance.html.markdown
+++ website/docs/r/redis_instance.html.markdown
@@ -95,33 +95,41 @@ data "google_compute_network" "redis-network" {
 
 
 ```hcl
-resource "google_compute_network" "network" {
-  name = "tf-test%{random_suffix}"
+// This example assumes this network already exists.
+// The API creates a tenant network per network authorized for a
+// Redis instance and that network is not deleted when the user-created
+// network (authorized_network) is deleted, so this prevents issues
+// with tenant network quota.
+// If this network hasn't been created and you are using this example in your
+// config, add an additional network resource or change
+// this from "data"to "resource"
+data "google_compute_network" "redis-network" {
+  name = "redis-test-network"
 }
 
 resource "google_compute_global_address" "service_range" {
-  name          = "tf-test%{random_suffix}"
+  name          = "address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.network.id
+  network       = data.google_compute_network.redis-network.id
 }
 
 resource "google_service_networking_connection" "private_service_connection" {
-  network                 = google_compute_network.network.id
+  network                 = data.google_compute_network.redis-network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.service_range.name]
 }
 
 resource "google_redis_instance" "cache" {
-  name           = "tf-test%{random_suffix}"
+  name           = "private-cache"
   tier           = "STANDARD_HA"
   memory_size_gb = 1
 
   location_id             = "us-central1-a"
   alternative_location_id = "us-central1-f"
 
-  authorized_network = google_compute_network.network.id
+  authorized_network = data.google_compute_network.redis-network.id
   connect_mode       = "PRIVATE_SERVICE_ACCESS"
 
   redis_version     = "REDIS_4_0"
