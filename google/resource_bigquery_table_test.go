3511196d59bd659146157c5a5b68b8111b2c30cd
Alvaro Ferreira <alvaro.ferreira@pubnative.net>
2021-03-26 10:27:53 +0100

    Add require_partition_filter

--- google/resource_bigquery_table_test.go
+++ google/resource_bigquery_table_test.go
@@ -973,6 +973,7 @@ resource "google_bigquery_table" "test" {
     hive_partitioning_options {
       mode = "AUTO"
       source_uri_prefix = "gs://${google_storage_bucket.test.name}/"
+	  require_partition_filter = true
     }
 
   }
@@ -1011,6 +1012,7 @@ resource "google_bigquery_table" "test" {
     hive_partitioning_options {
       mode = "CUSTOM"
       source_uri_prefix = "gs://${google_storage_bucket.test.name}/{key1:STRING}"
+	  require_partition_filter = true
     }
 
     schema = <<EOH
