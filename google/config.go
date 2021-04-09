844f4801d9849ca8b8374add4047a10c278cd4a0
Modular Magician <magic-modules@google.com>
2021-04-09 15:47:43 +0000

    Added data source google kms secret asymmetric (#4664)
    * Added data source google_kms_secret_asymmetric
    
    * typo in the name
    
    * added missing reference to google_kms_crypto_key_version
    
    * processed lint errors
    
    * remove superfluous brackets
    
    * make it explicit that the crc32 is calculated using castagnoli
    
    * Removed duplicative beta-only imports
    
    * implemented using google.golang.org/api/cloudkms/v1
    
    * Marked kms_secret_asymmetric as beta-only
    
    * added comment on crc32 parameter
    
    Co-authored-by: Mark van Holsteijn <mark.van.holsteijn@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/config.go
+++ google/config.go
@@ -404,10 +404,10 @@ func (c *Config) NewDnsClient(userAgent string) *dns.Service {
 	return clientDns
 }
 
-func (c *Config) NewKmsClient(userAgent string) *cloudkms.Service {
+func (c *Config) NewKmsClientWithCtx(ctx context.Context, userAgent string) *cloudkms.Service {
 	kmsClientBasePath := removeBasePathVersion(c.KMSBasePath)
 	log.Printf("[INFO] Instantiating Google Cloud KMS client for path %s", kmsClientBasePath)
-	clientKms, err := cloudkms.NewService(c.context, option.WithHTTPClient(c.client))
+	clientKms, err := cloudkms.NewService(ctx, option.WithHTTPClient(c.client))
 	if err != nil {
 		log.Printf("[WARN] Error creating client kms: %s", err)
 		return nil
@@ -416,6 +416,10 @@ func (c *Config) NewKmsClient(userAgent string) *cloudkms.Service {
 	clientKms.BasePath = kmsClientBasePath
 
 	return clientKms
+}
+
+func (c *Config) NewKmsClient(userAgent string) *cloudkms.Service {
+	return c.NewKmsClientWithCtx(c.context, userAgent)
 }
 
 func (c *Config) NewLoggingClient(userAgent string) *cloudlogging.Service {
