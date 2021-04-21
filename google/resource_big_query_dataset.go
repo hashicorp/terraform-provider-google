025dc40640f24626f64c8f9a00bcbf36f1919056
The Magician <magic-modules@google.com>
2021-04-21 14:58:31 -0700

    make bigquery dataset dataset_id forcenew (#4726) (#8973)
    * make bigquery dataset dataset_id forcenew
    
    * make project_id and datasetReference input true as well
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_big_query_dataset.go
+++ google/resource_big_query_dataset.go
@@ -73,6 +73,7 @@ func resourceBigQueryDataset() *schema.Resource {
 			"dataset_id": {
 				Type:         schema.TypeString,
 				Required:     true,
+				ForceNew:     true,
 				ValidateFunc: validateDatasetId,
 				Description: `A unique ID for this dataset, without the project name. The ID
 must contain only letters (a-z, A-Z), numbers (0-9), or
