93feb028d40fad6c3045a92788387a1c1a460fa6
The Magician <magic-modules@google.com>
2021-04-23 09:13:43 -0700

    Made project_service.service validation reject invalid service domains (#4722) (#8987)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_google_project_service_test.go
+++ google/resource_google_project_service_test.go
@@ -10,6 +10,44 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+func TestProjectServiceServiceValidateFunc(t *testing.T) {
+	cases := map[string]struct {
+		val                   interface{}
+		ExpectValidationError bool
+	}{
+		"ignoredProjectService": {
+			val:                   "dataproc-control.googleapis.com",
+			ExpectValidationError: true,
+		},
+		"bannedProjectService": {
+			val:                   "bigquery-json.googleapis.com",
+			ExpectValidationError: true,
+		},
+		"third party API": {
+			val:                   "whatever.example.com",
+			ExpectValidationError: false,
+		},
+		"not a domain": {
+			val:                   "monitoring",
+			ExpectValidationError: true,
+		},
+		"not a string": {
+			val:                   5,
+			ExpectValidationError: true,
+		},
+	}
+
+	for tn, tc := range cases {
+		_, errs := validateProjectServiceService(tc.val, "service")
+		if tc.ExpectValidationError && len(errs) == 0 {
+			t.Errorf("bad: %s, %q passed validation but was expected to fail", tn, tc.val)
+		}
+		if !tc.ExpectValidationError && len(errs) > 0 {
+			t.Errorf("bad: %s, %q failed validation but was expected to pass. errs: %q", tn, tc.val, errs)
+		}
+	}
+}
+
 // Test that services can be enabled and disabled on a project
 func TestAccProjectService_basic(t *testing.T) {
 	t.Parallel()
