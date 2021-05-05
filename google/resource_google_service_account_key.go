401ea0318883cf0c3e1fa3e22493aff4c3378370
Modular Magician <magic-modules@google.com>
2021-05-05 18:46:02 +0000

    add details to google_service_account_key documentation (#4764)
    Co-authored-by: Mathis Raguin <mathis.raguin@blablacar.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_google_service_account_key.go
+++ google/resource_google_service_account_key.go
@@ -21,7 +21,7 @@ func resourceGoogleServiceAccountKey() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Description: `The ID of the parent service account of the key. This can be a string in the format {ACCOUNT} or projects/{PROJECT_ID}/serviceAccounts/{ACCOUNT}, where {ACCOUNT} is the email address or unique id of the service account. If the {ACCOUNT} syntax is used, the project will be inferred from the account.`,
+				Description: `The ID of the parent service account of the key. This can be a string in the format {ACCOUNT} or projects/{PROJECT_ID}/serviceAccounts/{ACCOUNT}, where {ACCOUNT} is the email address or unique id of the service account. If the {ACCOUNT} syntax is used, the project will be inferred from the provider's configuration.`,
 			},
 			// Optional
 			"key_algorithm": {
