0d11cc9e35d2b56fdfc2211bcd228f8b9fd3df9c
The Magician <magic-modules@google.com>
2021-06-30 14:46:04 -0500

    upstream functions docs (#4932) (#9471)
    * Docs: Update description for cloud function region
    
    Cloud Functions can now be deployed in multiple regions
    
    * Update cloudfunctions_function.html.markdown
    
    Co-authored-by: Matt Simmons <52540630+matt-sparq@users.noreply.github.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: Matt Simmons <52540630+matt-sparq@users.noreply.github.com>

--- google/resource_cloudfunctions_function.go
+++ google/resource_cloudfunctions_function.go
@@ -306,7 +306,7 @@ func resourceCloudFunctionsFunction() *schema.Resource {
 				Optional:    true,
 				Computed:    true,
 				ForceNew:    true,
-				Description: `Region of function. Currently can be only "us-central1". If it is not provided, the provider region is used.`,
+				Description: `Region of function. If it is not provided, the provider region is used.`,
 			},
 		},
 		UseJSONNumber: true,
