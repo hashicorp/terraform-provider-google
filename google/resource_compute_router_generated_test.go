6f88f50f42d64a5d4db75934faba8cb73195c6d0
The Magician <magic-modules@google.com>
2021-07-20 16:18:33 -0500

    GA ipsec interconnect tests (#4983) (#9603)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_router_generated_test.go
+++ google/resource_compute_router_generated_test.go
@@ -73,6 +73,49 @@ resource "google_compute_network" "foobar" {
 `, context)
 }
 
+func TestAccComputeRouter_computeRouterEncryptedInterconnectExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeRouterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRouter_computeRouterEncryptedInterconnectExample(context),
+			},
+			{
+				ResourceName:            "google_compute_router.encrypted-interconnect-router",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"network", "region"},
+			},
+		},
+	})
+}
+
+func testAccComputeRouter_computeRouterEncryptedInterconnectExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_router" "encrypted-interconnect-router" {
+  name                          = "tf-test-test-router%{random_suffix}"
+  network                       = google_compute_network.network.name
+  encrypted_interconnect_router = true
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_network" "network" {
+  name                    = "tf-test-test-network%{random_suffix}"
+  auto_create_subnetworks = false
+}
+`, context)
+}
+
 func testAccCheckComputeRouterDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
