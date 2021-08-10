ee5ce173ac3b015f2a51050b2fc3da76392c3155
Modular Magician <magic-modules@google.com>
2021-08-10 00:17:46 +0000

    Remove versions from redis_version field. (#5062)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_redis_instance.go
+++ google/resource_redis_instance.go
@@ -163,11 +163,8 @@ https://cloud.google.com/memorystore/docs/redis/reference/rest/v1/projects.locat
 				Computed: true,
 				Optional: true,
 				Description: `The version of Redis software. If not provided, latest supported
-version will be used. Currently, the supported values are:
-
-- REDIS_5_0 for Redis 5.0 compatibility
-- REDIS_4_0 for Redis 4.0 compatibility
-- REDIS_3_2 for Redis 3.2 compatibility`,
+version will be used. Please check the API documentation linked 
+at the top for the latest valid values.`,
 			},
 			"region": {
 				Type:        schema.TypeString,
