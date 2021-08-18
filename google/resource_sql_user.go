e8327e3780503033816602549e624966a8df6ff9
Josh Angolano <jangolano@hy-vee.com>
2021-08-18 09:36:20 -0500

    Fix:9723 google_sql_user - "Root resource was present, but now absent"

--- google/resource_sql_user.go
+++ google/resource_sql_user.go
@@ -186,9 +186,12 @@ func resourceSqlUserRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	var user *sqladmin.User
-	for _, currentUser := range users.Items {
+	var databaseInstance *sqladmin.DatabaseInstance
 
-		name = strings.Split(name, "@")[0]
+	for _, currentUser := range users.Items {
+		if !strings.Contains(databaseInstance.DatabaseVersion, "POSTGRES") {
+			name = strings.Split(name, "@")[0]
+		}
 
 		if currentUser.Name == name {
 			// Host can only be empty for postgres instances,
