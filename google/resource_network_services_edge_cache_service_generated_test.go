b72600acfb51e3e278eb2558c98608c7d16fbf2c
The Magician <magic-modules@google.com>
2021-09-23 17:08:10 -0500

    add dashes to networkservices edge_cache_origin names (#5232) (#10149)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_network_services_edge_cache_service_generated_test.go
+++ google/resource_network_services_edge_cache_service_generated_test.go
@@ -136,7 +136,7 @@ resource "google_storage_bucket" "dest" {
 }
 
 resource "google_network_services_edge_cache_origin" "google" {
-  name                 = "google%{random_suffix}"
+  name                 = "tf-test-origin-google%{random_suffix}"
   origin_address       = "google.com"
   description          = "The default bucket for media edge test"
   max_attempts         = 2
