94f78b504c83084493b249e62289a0474b3b4350
Modular Magician <magic-modules@google.com>
2021-10-04 23:52:24 +0000

    Add differ script to TPG and TPGB, along with helper shell script. (#5268)
    * Add differ script to TPG and TPGB, along with helper shell script.
    
    * we actually need that whitespace!
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ scripts/run_diff.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+set -x
+if [ -z "$1" ]; then
+  echo "Must provide 1 argument - name of resource to diff, e.g. 'google_compute_forwarding_rule'"
+  exit 1
+fi
+
+function cleanup() {
+  go mod edit -dropreplace=github.com/hashicorp/terraform-provider-clean-google
+  go mod edit -droprequire=github.com/hashicorp/terraform-provider-clean-google
+}
+
+trap cleanup EXIT
+if [[ -d ~/go/src/github.com/hashicorp/terraform-provider-clean-google ]]; then
+  pushd ~/go/src/github.com/hashicorp/terraform-provider-clean-google
+  git clean -fdx
+  git reset --hard
+  git checkout master
+  git pull
+  popd
+else
+  mkdir -p ~/go/src/github.com/hashicorp
+  git clone https://github.com/hashicorp/terraform-provider-google ~/go/src/github.com/hashicorp/terraform-provider-clean-google
+fi
+
+
+go mod edit -require=github.com/hashicorp/terraform-provider-clean-google@v0.0.0
+go mod edit -replace github.com/hashicorp/terraform-provider-clean-google=$(realpath ~/go/src/github.com/hashicorp/terraform-provider-clean-google)
+go run scripts/diff.go --resource $1
