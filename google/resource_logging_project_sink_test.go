32f6e6570a1c1134109a0b18b139dd0842c9a4b0
Modular Magician <magic-modules@google.com>
2021-11-03 22:06:33 +0000

    update description (#5416)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_logging_project_sink_test.go
+++ google/resource_logging_project_sink_test.go
@@ -54,6 +54,37 @@ func TestAccLoggingProjectSink_described(t *testing.T) {
 	})
 }
 
+func TestAccLoggingProjectSink_described_update(t *testing.T) {
+	t.Parallel()
+
+	sinkName := "tf-test-sink-" + randString(t, 10)
+	bucketName := "tf-test-sink-bucket-" + randString(t, 10)
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLoggingProjectSinkDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLoggingProjectSink_described(sinkName, getTestProjectFromEnv(), bucketName),
+			},
+			{
+				ResourceName:      "google_logging_project_sink.described",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccLoggingProjectSink_described_update(sinkName, getTestProjectFromEnv(), bucketName),
+			},
+			{
+				ResourceName:      "google_logging_project_sink.described",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccLoggingProjectSink_disabled(t *testing.T) {
 	t.Parallel()
 
@@ -336,6 +367,25 @@ resource "google_logging_project_sink" "described" {
   destination = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
   filter      = "logName=\"projects/%s/logs/compute.googleapis.com%%2Factivity_log\" AND severity>=ERROR"
   description = "this is a description for a project level logging sink"
+
+  unique_writer_identity = false
+}
+
+resource "google_storage_bucket" "log-bucket" {
+  name     = "%s"
+  location = "US"
+}
+`, name, project, project, bucketName)
+}
+
+func testAccLoggingProjectSink_described_update(name, project, bucketName string) string {
+	return fmt.Sprintf(`
+resource "google_logging_project_sink" "described" {
+  name        = "%s"
+  project     = "%s"
+  destination = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
+  filter      = "logName=\"projects/%s/logs/compute.googleapis.com%%2Factivity_log\" AND severity>=ERROR"
+  description = "description updated"
 
   unique_writer_identity = false
 }
