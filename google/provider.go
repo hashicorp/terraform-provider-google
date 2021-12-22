483fc251425c54d11ded88498d13f0cf8baf6321
Modular Magician <magic-modules@google.com>
2021-12-22 21:31:04 +0000

    Apigee Nat Address Resource (#5018)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -812,9 +812,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 215
+// Generated resources: 216
 // Generated IAM resources: 93
-// Total generated resources: 308
+// Total generated resources: 309
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -845,6 +845,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_apigee_envgroup":                                       resourceApigeeEnvgroup(),
 			"google_apigee_instance_attachment":                            resourceApigeeInstanceAttachment(),
 			"google_apigee_envgroup_attachment":                            resourceApigeeEnvgroupAttachment(),
+			"google_apigee_nat_address":                                    resourceApigeeNatAddress(),
 			"google_app_engine_domain_mapping":                             resourceAppEngineDomainMapping(),
 			"google_app_engine_firewall_rule":                              resourceAppEngineFirewallRule(),
 			"google_app_engine_standard_app_version":                       resourceAppEngineStandardAppVersion(),
