c6220ca0b60c71b911c9cda2b28f14e512203a02
The Magician <magic-modules@google.com>
2022-02-24 11:00:28 -0800

    Quantify version import statement in error (#5743) (#11169)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_secret_manager_secret_version.go
+++ google/resource_secret_manager_secret_version.go
@@ -266,7 +266,7 @@ func resourceSecretManagerSecretVersionImport(d *schema.ResourceData, meta inter
 
 	parts := secretRegex.FindStringSubmatch(name)
 	if len(parts) != 2 {
-		panic(fmt.Sprintf("Version name doesn not fit the format `projects/{{project}}/secrets/{{secret}}/versions{{version}}`"))
+		panic(fmt.Sprintf("Version name doesn not fit the format `projects/{{project}}/secrets/{{secret}}/versions/{{version}}`"))
 	}
 	if err := d.Set("secret", parts[1]); err != nil {
 		return nil, fmt.Errorf("Error setting secret: %s", err)
