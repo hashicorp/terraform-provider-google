0690bc101a30e03b81353dd0f53abdb2d05a6c86
Modular Magician <magic-modules@google.com>
2022-03-28 16:18:12 +0000

    Make allAuthenticatedUsers case sensitive in iam members (#5853)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/resource_privateca_ca_pool_iam_test.go
@@ -0,0 +1,95 @@
+package google
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccPrivatecaCaPoolIamMemberAllAuthenticatedUsersCasing(t *testing.T) {
+	t.Parallel()
+
+	capool := "tf-test-pool-iam-" + randString(t, 10)
+	project := getTestProjectFromEnv()
+	region := getTestRegionFromEnv()
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPrivatecaCaPoolIamMember_allAuthenticatedUsers(capool, region, project),
+				Check: testAccCheckPrivatecaCaPoolIam(t, capool, region, project, "roles/privateca.certificateManager", []string{
+					fmt.Sprintf("group:%s.svc.id.goog:/allAuthenticatedUsers/", project),
+				}),
+			},
+		},
+	})
+}
+
+func testAccCheckPrivatecaCaPoolIam(t *testing.T, capool, region, project, role string, members []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		d := &ResourceDataMock{
+			FieldsInSchema: map[string]interface{}{
+				"ca_pool": capool,
+				"role":    role,
+				"member":  "",
+			},
+		}
+		u := &PrivatecaCaPoolIamUpdater{
+			project:  project,
+			location: region,
+			caPool:   capool,
+			d:        d,
+			Config:   googleProviderConfig(t),
+		}
+		p, err := u.GetResourceIamPolicy()
+		if err != nil {
+			return err
+		}
+
+		for _, binding := range p.Bindings {
+			if binding.Role == role {
+				sort.Strings(members)
+				sort.Strings(binding.Members)
+
+				if reflect.DeepEqual(members, binding.Members) {
+					return nil
+				}
+
+				return fmt.Errorf("Binding found but expected members is %v, got %v", members, binding.Members)
+			}
+		}
+
+		return fmt.Errorf("No binding for role %q", role)
+	}
+}
+
+func testAccPrivatecaCaPoolIamMember_allAuthenticatedUsers(capool, region, project string) string {
+	return fmt.Sprintf(`
+resource "google_privateca_ca_pool" "default" {
+  name     = "%s"
+  location = "%s"
+  tier     = "ENTERPRISE"
+  publishing_options {
+    publish_ca_cert = true
+    publish_crl     = true
+  }
+  labels = {
+    foo = "bar"
+  }
+}
+
+resource "google_privateca_ca_pool_iam_member" "member" {
+  ca_pool  = google_privateca_ca_pool.default.id
+  role     = "roles/privateca.certificateManager"
+  member   = "group:%s.svc.id.goog:/allAuthenticatedUsers/"
+}
+  
+`, capool, region, project)
+}
