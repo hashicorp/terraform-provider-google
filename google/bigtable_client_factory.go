8f0857e75efd54a8ca063dd60c1cda3b1bd0f92c
The Magician <magic-modules@google.com>
2022-03-31 16:03:50 -0500

    Add support to verify data loss in Cloud Bigtable tests (#5850) (#11386)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/bigtable_client_factory.go
+++ google/bigtable_client_factory.go
@@ -48,3 +48,19 @@ func (s BigtableClientFactory) NewAdminClient(project, instance string) (*bigtab
 
 	return bigtable.NewAdminClient(context.Background(), project, instance, opts...)
 }
+
+func (s BigtableClientFactory) NewClient(project, instance string) (*bigtable.Client, error) {
+	var opts []option.ClientOption
+	if requestReason := os.Getenv("CLOUDSDK_CORE_REQUEST_REASON"); requestReason != "" {
+		opts = append(opts, option.WithRequestReason(requestReason))
+	}
+
+	if s.UserProjectOverride && s.BillingProject != "" {
+		opts = append(opts, option.WithQuotaProject(s.BillingProject))
+	}
+
+	opts = append(opts, option.WithTokenSource(s.TokenSource), option.WithUserAgent(s.UserAgent))
+	opts = append(opts, s.gRPCLoggingOptions...)
+
+	return bigtable.NewClient(context.Background(), project, instance, opts...)
+}
