b7e1b1ae5443baf634ce88d2a25c4f549998eeee
The Magician <magic-modules@google.com>
2022-04-01 15:10:48 -0500

    Update examples for Cloud Bigtable AppProfile. (#5871) (#11394)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_bigtable_app_profile_generated_test.go
+++ google/resource_bigtable_app_profile_generated_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(t *testing.T) {
+func TestAccBigtableAppProfile_bigtableAppProfileAnyclusterExample(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -37,7 +37,7 @@ func TestAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(t *testing.
 		CheckDestroy: testAccCheckBigtableAppProfileDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(context),
+				Config: testAccBigtableAppProfile_bigtableAppProfileAnyclusterExample(context),
 			},
 			{
 				ResourceName:            "google_bigtable_app_profile.ap",
@@ -49,16 +49,28 @@ func TestAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(t *testing.
 	})
 }
 
-func testAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(context map[string]interface{}) string {
+func testAccBigtableAppProfile_bigtableAppProfileAnyclusterExample(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_bigtable_instance" "instance" {
   name = "tf-test-bt-instance%{random_suffix}"
   cluster {
-    cluster_id   = "tf-test-bt-instance%{random_suffix}"
+    cluster_id   = "cluster-1"
+    zone         = "us-central1-a"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }
+  cluster {
+    cluster_id   = "cluster-2"
     zone         = "us-central1-b"
     num_nodes    = 3
     storage_type = "HDD"
   }
+  cluster {
+    cluster_id   = "cluster-3"
+    zone         = "us-central1-c"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }  
 
   deletion_protection  = "%{deletion_protection}"
 }
@@ -67,7 +79,9 @@ resource "google_bigtable_app_profile" "ap" {
   instance       = google_bigtable_instance.instance.name
   app_profile_id = "tf-test-bt-profile%{random_suffix}"
 
+  // Requests will be routed to any of the 3 clusters.
   multi_cluster_routing_use_any = true
+
   ignore_warnings               = true
 }
 `, context)
@@ -104,7 +118,7 @@ func testAccBigtableAppProfile_bigtableAppProfileSingleclusterExample(context ma
 resource "google_bigtable_instance" "instance" {
   name = "tf-test-bt-instance%{random_suffix}"
   cluster {
-    cluster_id   = "tf-test-bt-instance%{random_suffix}"
+    cluster_id   = "cluster-1"
     zone         = "us-central1-b"
     num_nodes    = 3
     storage_type = "HDD"
@@ -117,12 +131,78 @@ resource "google_bigtable_app_profile" "ap" {
   instance       = google_bigtable_instance.instance.name
   app_profile_id = "tf-test-bt-profile%{random_suffix}"
 
+  // Requests will be routed to the following cluster.
   single_cluster_routing {
-    cluster_id                 = "tf-test-bt-instance%{random_suffix}"
+    cluster_id                 = "cluster-1"
     allow_transactional_writes = true
   }
 
   ignore_warnings = true
+}
+`, context)
+}
+
+func TestAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"deletion_protection": false,
+		"random_suffix":       randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBigtableAppProfileDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(context),
+			},
+			{
+				ResourceName:            "google_bigtable_app_profile.ap",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"app_profile_id", "instance", "ignore_warnings", "ignore_warnings"},
+			},
+		},
+	})
+}
+
+func testAccBigtableAppProfile_bigtableAppProfileMulticlusterExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_bigtable_instance" "instance" {
+  name = "tf-test-bt-instance%{random_suffix}"
+  cluster {
+    cluster_id   = "cluster-1"
+    zone         = "us-central1-a"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }
+  cluster {
+    cluster_id   = "cluster-2"
+    zone         = "us-central1-b"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }
+  cluster {
+    cluster_id   = "cluster-3"
+    zone         = "us-central1-c"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }
+
+  deletion_protection  = "%{deletion_protection}"
+}
+
+resource "google_bigtable_app_profile" "ap" {
+  instance       = google_bigtable_instance.instance.name
+  app_profile_id = "tf-test-bt-profile%{random_suffix}"
+
+  // Requests will be routed to the following 2 clusters.
+  multi_cluster_routing_use_any = true
+  multi_cluster_routing_cluster_ids = ["cluster-1", "cluster-2"]
+
+  ignore_warnings               = true
 }
 `, context)
 }
