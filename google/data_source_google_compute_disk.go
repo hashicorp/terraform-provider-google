3b19d0def6419efb446b8e0211d08a66000054b8
Modular Magician <magic-modules@google.com>
2022-04-25 22:50:11 +0000

    datasource google_compute_disk (#5978)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_compute_disk.go
@@ -0,0 +1,31 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleComputeDisk() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeDisk().Schema)
+	addRequiredFieldsToSchema(dsSchema, "name")
+	addOptionalFieldsToSchema(dsSchema, "project")
+	addOptionalFieldsToSchema(dsSchema, "zone")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleComputeDiskRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleComputeDiskRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	id, err := replaceVars(d, config, "projects/{{project}}/zones/{{zone}}/disks/{{name}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+	return resourceComputeDiskRead(d, meta)
+}
