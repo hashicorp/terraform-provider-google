6023ea2618a950edb18d525953e257d02b258f04
Modular Magician <magic-modules@google.com>
2022-05-01 17:49:12 +0000

    fix doc (#5956)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloud_run_service.go
+++ google/resource_cloud_run_service.go
@@ -822,14 +822,11 @@ version or an integer for a specific version.`,
 									"name": {
 										Type:     schema.TypeString,
 										Required: true,
-										Description: `The name of the secret in Cloud Secret Manager. By default, the secret
-is assumed to be in the same project.
-If the secret is in another project, you must define an alias.
-You set the <alias> in this field, and create an annotation with the
-following structure
-"run.googleapis.com/secrets" = "<alias>:projects/<project-id|project-number>/secrets/<secret-name>".
-If multiple alias definitions are needed, they must be separated by
-commas in the annotation field.`,
+										Description: `The name of the secret in Cloud Secret Manager. By default, the secret is assumed to be in the same project. 
+If the secret is in another project, you must define an alias. 
+An alias definition has the form: :projects/<project-id|project-number>/secrets/. 
+If multiple alias definitions are needed, they must be separated by commas. 
+The alias definitions must be set on the run.googleapis.com/secrets annotation.`,
 									},
 								},
 							},
