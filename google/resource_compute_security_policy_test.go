df0b95ee11303d2841797316623406264415c916
Modular Magician <magic-modules@google.com>
2022-06-02 16:35:18 +0000

    Add `advanced_options_config` for `compute_security_policy` (#5851)
    This adds support for advanced_options_config, supporting enabling JSON
    body parsing of payloads for Cloud Armor policies, as well as well as
    providing the option of enabling verbose logging.
    
    Fixes hashicorp/terraform-provider-google#9939
    Fixes hashicorp/terraform-provider-google#10497
    
    https://cloud.google.com/compute/docs/reference/rest/v1/securityPolicies
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_security_policy_test.go
+++ google/resource_compute_security_policy_test.go
@@ -98,6 +98,44 @@ func TestAccComputeSecurityPolicy_update(t *testing.T) {
 	})
 }
 
+func TestAccComputeSecurityPolicy_withAdvancedOptionsConfig(t *testing.T) {
+	t.Parallel()
+
+	spName := fmt.Sprintf("tf-test-%s", randString(t, 10))
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeSecurityPolicyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeSecurityPolicy_basic(spName),
+			},
+			{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccComputeSecurityPolicy_withAdvancedOptionsConfig(spName),
+			},
+			{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccComputeSecurityPolicy_basic(spName),
+			},
+			{
+				ResourceName:      "google_compute_security_policy.policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckComputeSecurityPolicyDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		config := googleProviderConfig(t)
@@ -124,7 +162,7 @@ func testAccComputeSecurityPolicy_basic(spName string) string {
 resource "google_compute_security_policy" "policy" {
   name        = "%s"
   description = "basic security policy"
-  type        = "CLOUD_ARMOR_EDGE"
+  type        = "CLOUD_ARMOR"
 }
 `, spName)
 }
@@ -250,6 +288,20 @@ resource "google_compute_security_policy" "policy" {
     }
     description = "updated description"
     preview     = false
+  }
+}
+`, spName)
+}
+
+func testAccComputeSecurityPolicy_withAdvancedOptionsConfig(spName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_security_policy" "policy" {
+  name        = "%s"
+  description = "updated description"
+
+  advanced_options_config {
+    json_parsing = "STANDARD"
+    log_level    = "VERBOSE"
   }
 }
 `, spName)
