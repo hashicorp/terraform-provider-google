0baa871ae8f0cadef4feeeff730ee021ae114154
The Magician <magic-modules@google.com>
2022-06-17 14:58:34 -0700

    Corrected provider setup for a few cgc examples (#6145) (#11916)
    * Corrected provider setup for a few cgc examples
    
    * Cleaned up formatting
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_clouddeploy_delivery_pipeline_sweeper_test.go
+++ google/resource_clouddeploy_delivery_pipeline_sweeper_test.go
@@ -20,19 +20,19 @@ import (
 	"log"
 	"testing"
 
-	compute "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/compute"
+	clouddeploy "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/clouddeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func init() {
-	resource.AddTestSweepers("ComputeGlobalForwardingRule", &resource.Sweeper{
-		Name: "ComputeGlobalForwardingRule",
-		F:    testSweepComputeGlobalForwardingRule,
+	resource.AddTestSweepers("ClouddeployDeliveryPipeline", &resource.Sweeper{
+		Name: "ClouddeployDeliveryPipeline",
+		F:    testSweepClouddeployDeliveryPipeline,
 	})
 }
 
-func testSweepComputeGlobalForwardingRule(region string) error {
-	log.Print("[INFO][SWEEPER_LOG] Starting sweeper for ComputeGlobalForwardingRule")
+func testSweepClouddeployDeliveryPipeline(region string) error {
+	log.Print("[INFO][SWEEPER_LOG] Starting sweeper for ClouddeployDeliveryPipeline")
 
 	config, err := sharedConfigForRegion(region)
 	if err != nil {
@@ -58,14 +58,14 @@ func testSweepComputeGlobalForwardingRule(region string) error {
 		"billing_account": billingId,
 	}
 
-	client := NewDCLComputeClient(config, config.userAgent, "", 0)
-	err = client.DeleteAllForwardingRule(context.Background(), d["project"], d["location"], isDeletableComputeGlobalForwardingRule)
+	client := NewDCLClouddeployClient(config, config.userAgent, "", 0)
+	err = client.DeleteAllDeliveryPipeline(context.Background(), d["project"], d["location"], isDeletableClouddeployDeliveryPipeline)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func isDeletableComputeGlobalForwardingRule(r *compute.ForwardingRule) bool {
+func isDeletableClouddeployDeliveryPipeline(r *clouddeploy.DeliveryPipeline) bool {
 	return isSweepableTestResource(*r.Name)
 }
