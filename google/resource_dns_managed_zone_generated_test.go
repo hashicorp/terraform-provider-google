0e2982e493f22f79c21fe2460288a189bc97e035
Modular Magician <magic-modules@google.com>
2022-07-21 20:22:35 +0000

    Made dns tests that can run in GA run in GA (#6315)
    Resolved https://github.com/hashicorp/terraform-provider-google/issues/12141 by correcting the provider version used in the basic example test
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_dns_managed_zone_generated_test.go
+++ google/resource_dns_managed_zone_generated_test.go
@@ -23,6 +23,90 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+func TestAccDNSManagedZone_dnsManagedZoneQuickstartExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDNSManagedZoneDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDNSManagedZone_dnsManagedZoneQuickstartExample(context),
+			},
+			{
+				ResourceName:            "google_dns_managed_zone.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"force_destroy"},
+			},
+		},
+	})
+}
+
+func testAccDNSManagedZone_dnsManagedZoneQuickstartExample(context map[string]interface{}) string {
+	return Nprintf(`
+# to setup a web-server
+resource "google_compute_instance" "default" {
+  name         = "tf-test-dns-compute-instance%{random_suffix}"
+  machine_type = "g1-small"
+  zone         = "us-central1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = <<-EOF
+  sudo apt-get update && \
+  sudo apt-get install apache2 -y && \
+  echo "<!doctype html><html><body><h1>Hello World!</h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+# to allow http traffic
+resource "google_compute_firewall" "default" {
+  name     = "tf-test-allow-http-traffic%{random_suffix}"
+  network  = "default"
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# to create a DNS zone
+resource "google_dns_managed_zone" "default" {
+  name          = "tf-test-example-zone-googlecloudexample%{random_suffix}"
+  dns_name      = "googlecloudexample.com."
+  description   = "Example DNS zone"
+  force_destroy = "true"
+}
+
+# to register web-server's ip address in DNS
+resource "google_dns_record_set" "default" {
+  name         = google_dns_managed_zone.default.dns_name
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+  rrdatas = [
+    google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  ]
+}
+`, context)
+}
+
 func TestAccDNSManagedZone_dnsRecordSetBasicExample(t *testing.T) {
 	t.Parallel()
 
@@ -50,14 +134,12 @@ func TestAccDNSManagedZone_dnsRecordSetBasicExample(t *testing.T) {
 func testAccDNSManagedZone_dnsRecordSetBasicExample(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_dns_managed_zone" "parent-zone" {
-  provider    = "google-beta"
   name        = "tf-test-sample-zone%{random_suffix}"
   dns_name    = "tf-test-sample-zone%{random_suffix}.hashicorptest.com."
   description = "Test Description"
 }
 
 resource "google_dns_record_set" "default" {
-  provider     = "google-beta"
   managed_zone = google_dns_managed_zone.parent-zone.name
   name         = "test-record.tf-test-sample-zone%{random_suffix}.hashicorptest.com."
   type         = "A"
