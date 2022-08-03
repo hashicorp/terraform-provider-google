0cbda1462f093dd4a9552203f553139c44689c69
Modular Magician <magic-modules@google.com>
2022-08-03 16:42:05 +0000

    Updated some tests from debian-9 to debian-11 (9/?) (#6363)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_instance_iam_test.go
+++ google/resource_compute_instance_iam_test.go
@@ -62,7 +62,7 @@ func testAccComputeInstanceIamPolicy_basic(zone, instanceName, roleId string) st
 
     boot_disk {
       initialize_params {
-        image = "debian-cloud/debian-9"
+        image = "debian-cloud/debian-11"
       }
     }
 
