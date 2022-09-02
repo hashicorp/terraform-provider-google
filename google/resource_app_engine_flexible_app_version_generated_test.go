19a44995399e94472bfc0c1c136626ed4e40dde8
The Magician <magic-modules@google.com>
2022-09-02 11:41:13 -0700

    Support serviceAccount field for AppEngine flex (#6391) (#12463)
    * Support serviceAccount field for AppEngine flex
    
    * Add service account example
    
    * Update account_id
    
    * Use vars instead of test env vars
    
    * Move account id to right tf example
    
    * Update mmv1/templates/terraform/examples/app_engine_flexible_app_version.tf.erb
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Add logs writer and network user role
    
    * Update mmv1/templates/terraform/examples/app_engine_flexible_app_version.tf.erb
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Add objectViewer role
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>

--- google/resource_app_engine_flexible_app_version_generated_test.go
+++ google/resource_app_engine_flexible_app_version_generated_test.go
@@ -71,10 +71,28 @@ resource "google_project_service" "service" {
   disable_dependent_services = false
 }
 
+resource "google_service_account" "custom_service_account" {
+  project      = google_project_service.service.project
+  account_id   = "tf-test-my-account%{random_suffix}"
+  display_name = "Custom Service Account"
+}
+
 resource "google_project_iam_member" "gae_api" {
   project = google_project_service.service.project
   role    = "roles/compute.networkUser"
-  member  = "serviceAccount:service-${google_project.my_project.number}@gae-api-prod.google.com.iam.gserviceaccount.com"
+  member  = "serviceAccount:${google_service_account.custom_service_account.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = google_project_service.service.project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.custom_service_account.email}"
+}
+
+resource "google_project_iam_member" "storage_viewer" {
+  project = google_project_service.service.project
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.custom_service_account.email}"
 }
 
 resource "google_app_engine_flexible_app_version" "myapp_v1" {
@@ -125,6 +143,7 @@ resource "google_app_engine_flexible_app_version" "myapp_v1" {
   }
 
   noop_on_destroy = true
+  service_account = google_service_account.custom_service_account.email
 }
 
 resource "google_storage_bucket" "bucket" {
