68ec524f87764e3ead2a5d0c5be6303841262b14
The Magician <magic-modules@google.com>
2022-09-16 10:46:15 -0700

    Add data source for Serverless VPC Access Connector. (#6533) (#12580)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_vpc_access_connector.go
@@ -0,0 +1,32 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceVPCAccessConnector() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceVPCAccessConnector().Schema)
+	addRequiredFieldsToSchema(dsSchema, "name")
+	addOptionalFieldsToSchema(dsSchema, "project", "region")
+
+	return &schema.Resource{
+		Read:   dataSourceVPCAccessConnectorRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceVPCAccessConnectorRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	id, err := replaceVars(d, config, "projects/{{project}}/locations/{{region}}/connectors/{{name}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+
+	d.SetId(id)
+
+	return resourceVPCAccessConnectorRead(d, meta)
+}
---
+++ google/data_source_vpc_access_connector_test.go
@@ -0,0 +1,48 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccVPCAccessConnectorDatasource_basic(t *testing.T) {
+	t.Parallel()
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCAccessConnectorDatasourceConfig(randString(t, 10)),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceStateWithIgnores(
+						"data.google_vpc_access_connector.connector",
+						"google_vpc_access_connector.connector",
+						map[string]struct{}{
+							// Ignore fields not returned in response
+							"self_link": {},
+							"region":    {},
+						},
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccVPCAccessConnectorDatasourceConfig(suffix string) string {
+	return fmt.Sprintf(`
+resource "google_vpc_access_connector" "connector" {
+  name          = "vpc-con-test-%s"
+  ip_cidr_range = "10.8.0.0/28"
+  network       = "default"
+  region        = "us-central1"
+}
+
+data "google_vpc_access_connector" "connector" {
+  name = google_vpc_access_connector.connector.name
+}
+`, suffix)
+}
