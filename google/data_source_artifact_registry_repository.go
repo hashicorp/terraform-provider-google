5e5d71d825bbb5d39363fe75e6c42e6cb56dc036
Modular Magician <magic-modules@google.com>
2022-09-23 18:11:20 +0000

    Artifact Registry Repository Data Source Added (#6590)
    * Artifact Registry Data Source Added
    
    * Artifact Registry Resource Location Changes Updated
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_artifact_registry_repository.go
@@ -0,0 +1,47 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceArtifactRegistryRepository() *schema.Resource {
+	// Generate datasource schema from resource
+	dsSchema := datasourceSchemaFromResourceSchema(resourceArtifactRegistryRepository().Schema)
+
+	// Set 'Required' schema elements
+	addRequiredFieldsToSchema(dsSchema, "repository_id", "location")
+
+	// Set 'Optional' schema elements
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceArtifactRegistryRepositoryRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceArtifactRegistryRepositoryRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	location, err := getLocation(d, config)
+	if err != nil {
+		return err
+	}
+
+	repository_id := d.Get("repository_id").(string)
+	d.SetId(fmt.Sprintf("projects/%s/locations/%s/repositories/%s", project, location, repository_id))
+
+	err = resourceArtifactRegistryRepositoryRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ google/data_source_artifact_registry_repository_test.go
@@ -0,0 +1,47 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceGoogleArtifactRegistryRepositoryConfig(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+	funcDataName := "data.google_artifact_registry_repository.my-repo"
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckArtifactRegistryRepositoryDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleArtifactRegistryRepositoryConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceState(funcDataName,
+						"google_artifact_registry_repository.my-repo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleArtifactRegistryRepositoryConfig(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "tf-test-my-repository%{random_suffix}"
+  description   = "example docker repository%{random_suffix}"
+  format        = "DOCKER"
+}
+
+data "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = google_artifact_registry_repository.my-repo.repository_id
+}
+`, context)
+}
