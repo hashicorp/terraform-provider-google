a0b330999150e366bf9c98ea0bd12c964a351445
The Magician <magic-modules@google.com>
2022-09-23 11:11:33 -0700

    Artifact Registry Repository Data Source Added (#6590) (#12637)
    * Artifact Registry Data Source Added
    
    * Artifact Registry Resource Location Changes Updated
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -773,6 +773,7 @@ func Provider() *schema.Provider {
 			"google_access_approval_organization_service_account": dataSourceAccessApprovalOrganizationServiceAccount(),
 			"google_access_approval_project_service_account":      dataSourceAccessApprovalProjectServiceAccount(),
 			"google_active_folder":                                dataSourceGoogleActiveFolder(),
+			"google_artifact_registry_repository":                 dataSourceArtifactRegistryRepository(),
 			"google_app_engine_default_service_account":           dataSourceGoogleAppEngineDefaultServiceAccount(),
 			"google_billing_account":                              dataSourceGoogleBillingAccount(),
 			"google_bigquery_default_service_account":             dataSourceGoogleBigqueryDefaultServiceAccount(),
