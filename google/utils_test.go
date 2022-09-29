1002330f73eadc04cff7a13b35f4048f4b206512
The Magician <magic-modules@google.com>
2022-09-29 12:08:40 -0700

    Add retry util func with polling interval (#6581) (#12685)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/utils_test.go
+++ google/utils_test.go
@@ -577,6 +577,101 @@ func TestRetryTimeDuration_URLTimeoutsShouldRetry(t *testing.T) {
 	}
 }
 
+func TestRetryWithPolling_noRetry(t *testing.T) {
+	retryCount := 0
+	retryFunc := func() (interface{}, error) {
+		retryCount++
+		return "", &googleapi.Error{
+			Code: 400,
+		}
+	}
+	result, err := retryWithPolling(retryFunc, time.Duration(1000)*time.Millisecond, time.Duration(100)*time.Millisecond)
+	if err == nil || err.(*googleapi.Error).Code != 400 || result.(string) != "" {
+		t.Errorf("unexpected error %v and result %v", err, result)
+	}
+	if retryCount != 1 {
+		t.Errorf("expected error function to be called exactly once, but was called %d times", retryCount)
+	}
+}
+
+func TestRetryWithPolling_notRetryable(t *testing.T) {
+	retryCount := 0
+	retryFunc := func() (interface{}, error) {
+		retryCount++
+		return "", &googleapi.Error{
+			Code: 400,
+		}
+	}
+	// Retryable if the error code is not 400.
+	isRetryableFunc := func(err error) (bool, string) {
+		return err.(*googleapi.Error).Code != 400, ""
+	}
+	result, err := retryWithPolling(retryFunc, time.Duration(1000)*time.Millisecond, time.Duration(100)*time.Millisecond, isRetryableFunc)
+	if err == nil || err.(*googleapi.Error).Code != 400 || result.(string) != "" {
+		t.Errorf("unexpected error %v and result %v", err, result)
+	}
+	if retryCount != 1 {
+		t.Errorf("expected error function to be called exactly once, but was called %d times", retryCount)
+	}
+}
+
+func TestRetryWithPolling_retriedAndSucceeded(t *testing.T) {
+	retryCount := 0
+	// Retry once and succeeds.
+	retryFunc := func() (interface{}, error) {
+		retryCount++
+		// Error code of 200 is retryable.
+		if retryCount < 2 {
+			return "", &googleapi.Error{
+				Code: 200,
+			}
+		}
+		return "Ok", nil
+	}
+	// Retryable if the error code is not 400.
+	isRetryableFunc := func(err error) (bool, string) {
+		return err.(*googleapi.Error).Code != 400, ""
+	}
+	result, err := retryWithPolling(retryFunc, time.Duration(1000)*time.Millisecond, time.Duration(100)*time.Millisecond, isRetryableFunc)
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+	if result.(string) != "Ok" {
+		t.Errorf("unexpected result %v", result)
+	}
+	if retryCount != 2 {
+		t.Errorf("expected error function to be called exactly twice, but was called %d times", retryCount)
+	}
+}
+
+func TestRetryWithPolling_retriedAndFailed(t *testing.T) {
+	retryCount := 0
+	// Retry once and fails.
+	retryFunc := func() (interface{}, error) {
+		retryCount++
+		// Error code of 200 is retryable.
+		if retryCount < 2 {
+			return "", &googleapi.Error{
+				Code: 200,
+			}
+		}
+		return "", &googleapi.Error{
+			Code: 400,
+		}
+	}
+	// Retryable if the error code is not 400.
+	isRetryableFunc := func(err error) (bool, string) {
+		return err.(*googleapi.Error).Code != 400, ""
+	}
+	result, err := retryWithPolling(retryFunc, time.Duration(1000)*time.Millisecond, time.Duration(100)*time.Millisecond, isRetryableFunc)
+	if err == nil || err.(*googleapi.Error).Code != 400 || result.(string) != "" {
+		t.Errorf("unexpected error %v and result %v", err, result)
+	}
+	if retryCount != 2 {
+		t.Errorf("expected error function to be called exactly twice, but was called %d times", retryCount)
+	}
+}
+
 func TestConflictError(t *testing.T) {
 	confErr := &googleapi.Error{
 		Code: 409,
