0d508ec6970b1b0183ab9936e6984866c174422e
The Magician <magic-modules@google.com>
2022-10-05 15:15:51 -0700

    Revert "Add the vertex endpoint resource. (#6583)" (#6654) (#12728)
    This reverts commit 1750a0751ae82d73ea1c63d32d7a163c25efb227.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider.go
+++ google/provider.go
@@ -892,9 +892,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 236
+// Generated resources: 235
 // Generated IAM resources: 138
-// Total generated resources: 374
+// Total generated resources: 373
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1265,7 +1265,6 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_tags_tag_binding":                                      resourceTagsTagBinding(),
 			"google_tpu_node":                                              resourceTPUNode(),
 			"google_vertex_ai_dataset":                                     resourceVertexAIDataset(),
-			"google_vertex_ai_endpoint":                                    resourceVertexAIEndpoint(),
 			"google_vpc_access_connector":                                  resourceVPCAccessConnector(),
 			"google_workflows_workflow":                                    resourceWorkflowsWorkflow(),
 		},
--- google/resource_vertex_ai_dataset.go
+++ google/resource_vertex_ai_dataset.go
@@ -59,7 +59,7 @@ func resourceVertexAIDataset() *schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"kms_key_name": {
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 							ForceNew: true,
 							Description: `Required. The Cloud KMS resource identifier of the customer managed encryption key used to protect a resource.
 Has the form: projects/my-project/locations/my-region/keyRings/my-kr/cryptoKeys/my-key. The key needs to be in the same region as where the resource is created.`,
