d4e372a2c155618b241b75b21b5474ead90803e7
Modular Magician <magic-modules@google.com>
2022-10-17 16:15:47 +0000

    A topic's schema can't be changed after it's created. (#6697)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_pubsub_topic.go
+++ google/resource_pubsub_topic.go
@@ -112,6 +112,7 @@ and is not a valid configuration.`,
 						"schema": {
 							Type:     schema.TypeString,
 							Required: true,
+							ForceNew: true,
 							Description: `The name of the schema that messages published should be
 validated against. Format is projects/{project}/schemas/{schema}.
 The value of this field will be _deleted-schema_
@@ -120,6 +121,7 @@ if the schema has been deleted.`,
 						"encoding": {
 							Type:         schema.TypeString,
 							Optional:     true,
+							ForceNew:     true,
 							ValidateFunc: validateEnum([]string{"ENCODING_UNSPECIFIED", "JSON", "BINARY", ""}),
 							Description:  `The encoding of messages validated against schema. Default value: "ENCODING_UNSPECIFIED" Possible values: ["ENCODING_UNSPECIFIED", "JSON", "BINARY"]`,
 							Default:      "ENCODING_UNSPECIFIED",
