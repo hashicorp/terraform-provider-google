bdd7769bb6c982101c8adb65336d5646efd53979
The Magician <magic-modules@google.com>
2022-11-02 10:36:53 -0700

    Add PubSub action to DLP Job Trigger (#6757) (#12929)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_data_loss_prevention_job_trigger_test.go
+++ google/resource_data_loss_prevention_job_trigger_test.go
@@ -41,6 +41,31 @@ func TestAccDataLossPreventionJobTrigger_dlpJobTriggerUpdateExample(t *testing.T
 	})
 }
 
+func TestAccDataLossPreventionJobTrigger_dlpJobTriggerPubsub(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project": getTestProjectFromEnv(),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDataLossPreventionJobTriggerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataLossPreventionJobTrigger_publishToPubSub(context),
+			},
+			{
+				ResourceName:            "google_data_loss_prevention_job_trigger.pubsub",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"parent"},
+			},
+		},
+	})
+}
+
 func testAccDataLossPreventionJobTrigger_dlpJobTriggerBasic(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_data_loss_prevention_job_trigger" "basic" {
@@ -101,6 +126,38 @@ resource "google_data_loss_prevention_job_trigger" "basic" {
 						dataset_id = "asdf"
 					}
 				}
+			}
+		}
+		storage_config {
+			cloud_storage_options {
+				file_set {
+					url = "gs://mybucket/directory/"
+				}
+			}
+		}
+	}
+}
+`, context)
+}
+
+func testAccDataLossPreventionJobTrigger_publishToPubSub(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_data_loss_prevention_job_trigger" "pubsub" {
+	parent = "projects/%{project}"
+	description = "Starting description"
+	display_name = "display"
+
+	triggers {
+		schedule {
+			recurrence_period_duration = "86400s"
+		}
+	}
+
+	inspect_job {
+		inspect_template_name = "fake"
+		actions {
+			pub_sub {
+				topic = "projects/%{project}/topics/bar"
 			}
 		}
 		storage_config {
