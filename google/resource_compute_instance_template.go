1ae502231be74fdf0423cb626f2b57ea8f2d9e8f
The Magician <magic-modules@google.com>
2022-11-22 14:12:42 -0800

    Correctly release visible_core_count (#6860) (#13108)
    * Correctly release visible_core_count
    
    * Add test
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_compute_instance_template.go
+++ google/resource_compute_instance_template.go
@@ -642,6 +642,12 @@ func resourceComputeInstanceTemplate() *schema.Resource {
 							ForceNew:    true,
 							Description: `The number of threads per physical core. To disable simultaneous multithreading (SMT) set this to 1. If unset, the maximum number of threads supported per core by the underlying processor is assumed.`,
 						},
+						"visible_core_count": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							ForceNew:    true,
+							Description: `The number of physical cores to expose to an instance. Multiply by the number of threads per core to compute the total number of virtual CPUs to expose to the instance. If unset, the number of cores is inferred from the instance\'s nominal CPU count and the underlying platform\'s SMT width.`,
+						},
 					},
 				},
 			},
--- google/resource_compute_instance_template_test.go
+++ google/resource_compute_instance_template_test.go
@@ -2507,6 +2507,7 @@ resource "google_compute_instance_template" "foobar" {
   advanced_machine_features {
 	threads_per_core = 1
 	enable_nested_virtualization = true
+	visible_core_count = 1
   }
 
   scheduling {
