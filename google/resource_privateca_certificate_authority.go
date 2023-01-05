5695304b63c5c3a78335b93719d81b0c7e99494d
The Magician <magic-modules@google.com>
2023-01-05 12:11:05 -0800

    Configuring a pathlen of 0 requires zero_max_issuer_path_length to be true (#7004) (#13395)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_privateca_certificate_authority.go
+++ google/resource_privateca_certificate_authority.go
@@ -232,7 +232,8 @@ func resourcePrivatecaCertificateAuthority() *schema.Resource {
 													Optional: true,
 													ForceNew: true,
 													Description: `Refers to the "path length constraint" in Basic Constraints extension. For a CA certificate, this value describes the depth of
-subordinate CA certificates that are allowed. If this value is less than 0, the request will fail.`,
+subordinate CA certificates that are allowed. If this value is less than 0, the request will fail. Setting the value to 0
+requires setting 'zero_max_issuer_path_length = true'.`,
 												},
 												"non_ca": {
 													Type:     schema.TypeBool,
@@ -246,7 +247,7 @@ If both 'is_ca' and 'non_ca' are unset, the extension will be omitted from the C
 													Optional: true,
 													ForceNew: true,
 													Description: `When true, the "path length constraint" in Basic Constraints extension will be set to 0.
-if both 'max_issuer_path_length' and 'zero_max_issuer_path_length' are unset,
+If both 'max_issuer_path_length' and 'zero_max_issuer_path_length' are unset,
 the max path length will be omitted from the CA certificate.`,
 												},
 											},
