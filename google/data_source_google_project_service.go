1a07d6f3741f18a4d0bdd63d808213aeebdc5eaf
The Magician <magic-modules@google.com>
2023-01-10 05:53:47 -0800

    Adding google_project_service datasource (#7043) (#13434)
    * Adding google_project_service datasource
    
    * Fixing minor mistake in basic test
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_project_service.go
@@ -0,0 +1,30 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleProjectService() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceGoogleProjectService().Schema)
+	addRequiredFieldsToSchema(dsSchema, "service")
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleProjectServiceRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleProjectServiceRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	id, err := replaceVars(d, config, "{{project}}/{{service}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+	return resourceGoogleProjectServiceRead(d, meta)
+}
