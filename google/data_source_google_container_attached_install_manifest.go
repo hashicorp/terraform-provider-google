cd99ad2c3af6449f59a1c2cc29387b90e8841b68
The Magician <magic-modules@google.com>
2023-01-11 08:27:27 -0800

    Move container_attached_install_manifest to a separate PR. (#7108) (#13455)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_container_attached_install_manifest.go
@@ -0,0 +1,84 @@
+package google
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleContainerAttachedInstallManifest() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceGoogleContainerAttachedInstallManifestRead,
+		Schema: map[string]*schema.Schema{
+			"project": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"location": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"platform_version": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"manifest": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceGoogleContainerAttachedInstallManifestRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.userAgent)
+	if err != nil {
+		return err
+	}
+
+	clusterId := d.Get("cluster_id").(string)
+	platformVersion := d.Get("platform_version").(string)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	location, err := getLocation(d, config)
+	if err != nil {
+		return err
+	}
+	if len(location) == 0 {
+		return fmt.Errorf("Cannot determine location: set location in this data source or at provider-level")
+	}
+
+	url, err := replaceVars(d, config, "{{ContainerAttachedBasePath}}projects/{{project}}/locations/{{location}}:generateAttachedClusterInstallManifest")
+	if err != nil {
+		return err
+	}
+	params := map[string]string{
+		"attached_cluster_id": clusterId,
+		"platform_version":    platformVersion,
+	}
+	url, err = addQueryParams(url, params)
+	if err != nil {
+		return err
+	}
+	res, err := sendRequest(config, "GET", project, url, userAgent, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := d.Set("manifest", res["manifest"]); err != nil {
+		return fmt.Errorf("Error setting manifest: %s", err)
+	}
+
+	d.SetId(time.Now().UTC().String())
+	return nil
+}
---
+++ google/data_source_google_container_attached_install_manifest_test.go
@@ -0,0 +1,63 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDataSourceGoogleContainerAttachedInstallManifest(t *testing.T) {
+	t.Parallel()
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleContainerAttachedInstallManifestConfig(randString(t, 10)),
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceGoogleContainerAttachedInstallManifestCheck("data.google_container_attached_install_manifest.manifest"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleContainerAttachedInstallManifestCheck(data_source_name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		ds, ok := s.RootModule().Resources[data_source_name]
+		if !ok {
+			return fmt.Errorf("root module has no resource called %s", data_source_name)
+		}
+
+		manifest, ok := ds.Primary.Attributes["manifest"]
+		if !ok {
+			return fmt.Errorf("cannot find 'manifest' attribute")
+		}
+		if manifest == "" {
+			return fmt.Errorf("install manifest data is empty")
+		}
+		return nil
+	}
+}
+
+func testAccDataSourceGoogleContainerAttachedInstallManifestConfig(suffix string) string {
+	return fmt.Sprintf(`
+data "google_project" "project" {
+}
+
+data "google_container_attached_versions" "versions" {
+	location       = "us-west1"
+	project        = data.google_project.project.project_id
+}
+
+data "google_container_attached_install_manifest" "manifest" {
+	location         = "us-west1"
+	project          = data.google_project.project.project_id
+	cluster_id       = "test-cluster-%s"
+	platform_version = data.google_container_attached_versions.versions.valid_versions[0]
+}
+`, suffix)
+}
