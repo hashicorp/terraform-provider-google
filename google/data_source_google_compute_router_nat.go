3c1f2df02fe56028dc62a0581f74c1e8475d6747
Modular Magician <magic-modules@google.com>
2023-01-12 19:35:23 +0000

    Added the data_source_compute_router_nat  (#7058)
    * added the data_source_compute_router_nat with test and docs
    
    * changes in test file
    
    * minor fix
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_compute_router_nat.go
@@ -0,0 +1,33 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleComputeRouterNat() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeRouterNat().Schema)
+
+	addRequiredFieldsToSchema(dsSchema, "name", "router")
+	addOptionalFieldsToSchema(dsSchema, "project", "region")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleComputeRouterNatRead,
+		Schema: dsSchema,
+	}
+
+}
+
+func dataSourceGoogleComputeRouterNatRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	id, err := replaceVars(d, config, "{{project}}/{{region}}/{{router}}/{{name}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+
+	return resourceComputeRouterNatRead(d, meta)
+}
