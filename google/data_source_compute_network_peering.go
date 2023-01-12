82fee5efc2d2ec42eca68cdc13a735f3d7d729f4
The Magician <magic-modules@google.com>
2023-01-12 11:40:51 -0800

    Adding google_compute_network_peering datasource (#7051) (#13476)
    * Adding google_compute_network_peering datasource
    
    * Removing unused diffSuppress
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_compute_network_peering.go
@@ -0,0 +1,38 @@
+package google
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const regexGCEName = "^(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)$"
+
+func dataSourceComputeNetworkPeering() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeNetworkPeering().Schema)
+	addRequiredFieldsToSchema(dsSchema, "name", "network")
+
+	dsSchema["name"].ValidateFunc = validateRegexp(regexGCEName)
+	dsSchema["network"].ValidateFunc = validateRegexp(peerNetworkLinkRegex)
+	return &schema.Resource{
+		Read:   dataSourceComputeNetworkPeeringRead,
+		Schema: dsSchema,
+		Timeouts: &schema.ResourceTimeout{
+			Read: schema.DefaultTimeout(4 * time.Minute),
+		},
+	}
+}
+
+func dataSourceComputeNetworkPeeringRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	networkFieldValue, err := ParseNetworkFieldValue(d.Get("network").(string), d, config)
+	if err != nil {
+		return err
+	}
+	d.SetId(fmt.Sprintf("%s/%s", networkFieldValue.Name, d.Get("name").(string)))
+
+	return resourceComputeNetworkPeeringRead(d, meta)
+}
