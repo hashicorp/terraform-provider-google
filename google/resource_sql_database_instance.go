bdff1bc9066d76d05f0d066523b61cab1d0d9d98
Modular Magician <magic-modules@google.com>
2023-01-13 01:19:16 +0000

    Bump sql_database_instance create timeout from 30 to 40 minutes (#7118)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_sql_database_instance.go
+++ google/resource_sql_database_instance.go
@@ -102,7 +102,7 @@ func resourceSqlDatabaseInstance() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(40 * time.Minute),
 			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
