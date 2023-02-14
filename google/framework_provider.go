346fa56aa14fee5c5c3675195f2a3b89d38101b3
Riley Karson <rileykarson@google.com>
2023-02-14 11:26:40 -0800

    Revert "dns managed zone with plugin-framework (#7182) (#13665)"
    This reverts commit 1a810653e34ec6892aa1df95919e022f33029d19.

--- google/framework_provider.go
+++ google/framework_provider.go
@@ -831,7 +831,6 @@ func (p *frameworkProvider) Configure(ctx context.Context, req provider.Configur
 // DataSources defines the data sources implemented in the provider.
 func (p *frameworkProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
-		NewGoogleDnsManagedZoneDataSource,
 		NewGoogleDnsRecordSetDataSource,
 	}
 }
