fc16da7d3c5820d3338994ccfe7612df256155f0
Modular Magician <magic-modules@google.com>
2023-02-14 22:25:56 +0000

    Add Logging Sink data source. (#7250)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_logging_sink.go
@@ -0,0 +1,44 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleLoggingSink() *schema.Resource {
+	dsSchema := datasourceSchemaFromResourceSchema(resourceLoggingSinkSchema())
+	dsSchema["id"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: `Required. An identifier for the resource in format: "projects/[PROJECT_ID]/sinks/[SINK_NAME]", "organizations/[ORGANIZATION_ID]/sinks/[SINK_NAME]", "billingAccounts/[BILLING_ACCOUNT_ID]/sinks/[SINK_NAME]", "folders/[FOLDER_ID]/sinks/[SINK_NAME]"`,
+	}
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleLoggingSinkRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleLoggingSinkRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.userAgent)
+	if err != nil {
+		return err
+	}
+
+	sinkId := d.Get("id").(string)
+
+	sink, err := config.NewLoggingClient(userAgent).Sinks.Get(sinkId).Do()
+	if err != nil {
+		return handleNotFoundError(err, d, fmt.Sprintf("Logging Sink %s", d.Id()))
+	}
+
+	if err := flattenResourceLoggingSink(d, sink); err != nil {
+		return err
+	}
+
+	d.SetId(sinkId)
+
+	return nil
+}
