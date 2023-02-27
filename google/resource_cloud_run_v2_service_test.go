a7eb9894863919edb35806bb115eadebaf711847
The Magician <magic-modules@google.com>
2023-02-27 15:45:55 -0800

    Adds `grpc` field to `liveness_probe` and `startup_probe` to `google_cloud_run_v2_service` resource (#6987) (#13855)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloud_run_v2_service_test.go
+++ google/resource_cloud_run_v2_service_test.go
@@ -1,7 +1,10 @@
 package google
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
@@ -199,7 +202,7 @@ resource "google_compute_network" "custom_test" {
 `, context)
 }
 
-func TestAccCloudRunV2Service_cloudrunv2ServiceProbesUpdate(t *testing.T) {
+func TestAccCloudRunV2Service_cloudrunv2ServiceTCPProbesUpdate(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -229,6 +232,22 @@ func TestAccCloudRunV2Service_cloudrunv2ServiceProbesUpdate(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"name", "location"},
 			},
+		},
+	})
+}
+
+func TestAccCloudRunV2Service_cloudrunv2ServiceHTTPProbesUpdate(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudRunV2ServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
 			{
 				Config: testAccCloudRunV2Service_cloudrunv2ServiceUpdateWithEmptyHTTPStartupProbe(context),
 			},
@@ -251,12 +270,89 @@ func TestAccCloudRunV2Service_cloudrunv2ServiceProbesUpdate(t *testing.T) {
 	})
 }
 
+func TestAccCloudRunV2Service_cloudrunv2ServiceGRPCProbesUpdate(t *testing.T) {
+	t.Parallel()
+
+	serviceName := fmt.Sprintf("tf-test-cloudrun-service%s", randString(t, 10))
+	context := map[string]interface{}{
+		"service_name": serviceName,
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudRunV2ServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunV2Service_cloudRunServiceUpdateWithEmptyGRPCLivenessProbe(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_v2_service.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+			{
+				Config: testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCLivenessProbe(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_v2_service.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+			// The following test steps of gRPC startup probe are expected to fail with startup probe check failures.
+			// This is because, due to the unavailability of ready-to-use container images of a gRPC service that
+			// implements the standard gRPC health check protocol, we compromise and use a container image of an
+			// ordinary HTTP service to deploy the gRPC service, which never passes startup probes.
+			// So we only check that the `startup.grpc {}` block and its properties are accepted by the APIs.
+			{
+				Config:      testAccCloudRunV2Service_cloudRunServiceUpdateWithEmptyGRPCStartupProbe(context),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(`Revision '%s-.*' is not ready and cannot serve traffic\. The user-provided container failed the configured startup probe checks\.`, serviceName)),
+			},
+			{
+				PreConfig:   testAccCheckCloudRunV2ServiceDestroyByNameProducer(t, serviceName),
+				Config:      testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCStartupProbe(context),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(`Revision '%s-.*' is not ready and cannot serve traffic\. The user-provided container failed the configured startup probe checks\.`, serviceName)),
+			},
+			{
+				PreConfig:   testAccCheckCloudRunV2ServiceDestroyByNameProducer(t, serviceName),
+				Config:      testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCLivenessAndStartupProbes(context),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(`Revision '%s-.*' is not ready and cannot serve traffic\. The user-provided container failed the configured startup probe checks\.`, serviceName)),
+			},
+			{
+				PreConfig:          testAccCheckCloudRunV2ServiceDestroyByNameProducer(t, serviceName),
+				Config:             testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCLivenessAndStartupProbes(context),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccCheckCloudRunV2ServiceDestroyByNameProducer(t *testing.T, serviceName string) func() {
+	return func() {
+		config := googleProviderConfig(t)
+		service := config.NewCloudRunV2Client(config.userAgent).Projects.Locations.Services
+		qualifiedServiceName := fmt.Sprintf("projects/%s/locations/%s/services/%s", config.Project, config.Region, serviceName)
+		op, err := service.Delete(qualifiedServiceName).Do()
+		if err != nil {
+			t.Errorf("Error while deleting the Cloud Run service: %s", err)
+			return
+		}
+		err = runAdminV2OperationWaitTime(config, op, config.Project, "Waiting for Cloud Run service to be deleted", config.userAgent, 5*time.Minute)
+		if err != nil {
+			t.Errorf("Error while waiting for Cloud Run service delete operation to complete: %s", err.Error())
+		}
+	}
+}
+
 func testAccCloudRunV2Service_cloudrunv2ServiceWithEmptyTCPStartupProbeAndHTTPLivenessProbe(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_cloud_run_v2_service" "default" {
   name     = "tf-test-cloudrun-service%{random_suffix}"
   location = "us-central1"
-  
+
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -280,7 +376,7 @@ func testAccCloudRunV2Service_cloudrunv2ServiceUpdateWithTCPStartupProbeAndHTTPL
 resource "google_cloud_run_v2_service" "default" {
   name     = "tf-test-cloudrun-service%{random_suffix}"
   location = "us-central1"
-  
+
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -323,7 +419,7 @@ func testAccCloudRunV2Service_cloudrunv2ServiceUpdateWithEmptyHTTPStartupProbe(c
 resource "google_cloud_run_v2_service" "default" {
   name     = "tf-test-cloudrun-service%{random_suffix}"
   location = "us-central1"
-  
+
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -341,7 +437,7 @@ func testAccCloudRunV2Service_cloudrunv2ServiceUpdateWithHTTPStartupProbe(contex
 resource "google_cloud_run_v2_service" "default" {
   name     = "tf-test-cloudrun-service%{random_suffix}"
   location = "us-central1"
-  
+
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -359,6 +455,126 @@ resource "google_cloud_run_v2_service" "default" {
           http_headers {
             name = "Some-Name"
           }
+        }
+      }
+    }
+  }
+}
+`, context)
+}
+
+func testAccCloudRunV2Service_cloudRunServiceUpdateWithEmptyGRPCLivenessProbe(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_v2_service" "default" {
+  name     ="%{service_name}"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 8080
+      }
+      liveness_probe {
+        grpc {}
+      }
+    }
+  }
+}
+`, context)
+}
+
+func testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCLivenessProbe(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_v2_service" "default" {
+  name     = "%{service_name}"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 8080
+      }
+      liveness_probe {
+        grpc {
+          port = 8080
+          service = "grpc.health.v1.Health"
+        }
+      }
+    }
+  }
+}
+`, context)
+}
+
+func testAccCloudRunV2Service_cloudRunServiceUpdateWithEmptyGRPCStartupProbe(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_v2_service" "default" {
+  name     = "%{service_name}"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 8080
+      }
+      startup_probe {
+        grpc {}
+      }
+    }
+  }
+}
+`, context)
+}
+
+func testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCStartupProbe(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_v2_service" "default" {
+  name     = "%{service_name}"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 8080
+      }
+      startup_probe {
+        grpc {
+          port = 8080
+          service = "grpc.health.v1.Health"
+        }
+      }
+    }
+  }
+}
+`, context)
+}
+
+func testAccCloudRunV2Service_cloudRunServiceUpdateWithGRPCLivenessAndStartupProbes(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_v2_service" "default" {
+  name     = "%{service_name}"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 8080
+      }
+      liveness_probe {
+        grpc {
+          port = 8080
+          service = "grpc.health.v1.Health"
+        }
+      }
+      startup_probe {
+        grpc {
+          port = 8080
+          service = "grpc.health.v1.Health"
         }
       }
     }
