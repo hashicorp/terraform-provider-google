17610bf454b0a606e9fb1f4706465595009c09fa
The Magician <magic-modules@google.com>
2023-03-08 07:36:52 -0800

    Add support for Apigee Sharedflow (#7202) (#13938)
    * added sharedflows mmv1 with error
    
    * fix indentation issue for sharedflow
    
    * add semi auto generated resourceApigeeSharedFlow
    
    * remove sharedflow mmv1 and add sharedflow manual provider
    
    * adding files for debugging review, test have issue
    
    * adding files for debugging review, test have issue
    
    * update test, doc, provider for apigee
    
    * Update api.yaml
    
    remove empty new line
    
    * Update api.yaml
    
    * Rename resource_apigee_shared_flow_generated_test.go to resource_apigee_shared_flow_test.go
    
    * Update resource_apigee_shared_flow.go
    
    update import regex, change debug messages to log.Printf
    
    * Update mmv1/third_party/terraform/utils/provider.go.erb
    
    
    
    * update PR to address comments
    
    * update test
    
    * fix sharedflow attribute update issue, add sharedflow deployment
    
    * Update apigee_shared_flow_deployment.html.markdown
    
    minor doc fix
    
    * Update mmv1/third_party/terraform/utils/provider.go.erb
    
    
    
    * Update mmv1/third_party/terraform/website/docs/r/apigee_shared_flow.html.markdown
    
    
    
    * Update resource_apigee_shared_flow_test.go
    
    remove commented out (unused) line of code
    
    * Delete api.yaml
    
    remove api.yaml as it is not needed for handwritten resource. Current repo structure no longer uses aggregated api.yaml
    
    * Rename apigee_shared_flow.html.markdown to apigee_sharedflow.html.markdown
    
    * Rename apigee_shared_flow_deployment.html.markdown to apigee_sharedflow_deployment.html.markdown
    
    * Rename resource_apigee_shared_flow.go to resource_apigee_sharedflow.go
    
    * Rename resource_apigee_shared_flow_deployment_test.go to resource_apigee_sharedflow_deployment_test.go
    
    * Rename resource_apigee_shared_flow_test.go to resource_apigee_sharedflow_test.go
    
    * Rename resource_apigee_shared_flow_sweeper_test.go to resource_apigee_sharedflow_sweeper_test.go
    
    * Update resource_apigee_sharedflow.go
    
    remove comment lines
    
    * Update mmv1/third_party/terraform/resources/resource_apigee_sharedflow.go
    
    
    
    * Update resource_apigee_sharedflow_deployment_test.go
    
    remove unused debugging log
    
    * update function name to capitalize
    
    * Update resource_apigee_sharedflow_deployment_test.go
    
    remove fmt.print
    
    * add flowhook
    
    * move flowhook doc
    
    * Update mmv1/third_party/terraform/tests/resource_apigee_flowhook_test.go
    
    
    
    * Update mmv1/third_party/terraform/website/docs/r/apigee_flowhook.html.markdown
    
    
    
    * Update mmv1/third_party/terraform/website/docs/r/apigee_flowhook.html.markdown
    
    
    
    * Update mmv1/third_party/terraform/tests/resource_apigee_flowhook_test.go
    
    
    
    * Update resource_apigee_flowhook_test.go
    
    remove fmt.print
    
    * update some resource attribute properties
    
    * Update apigee_flowhook.html.markdown
    
    updated `description` and `continue_on_error` as optional
    
    * update referenced function naming case
    
    ---------
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Ray Xu <xrc@google.com>
    Co-authored-by: megan07 <mbang@hashicorp.com>

---
+++ google/resource_apigee_flowhook.go
@@ -0,0 +1,240 @@
+package google
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func ResourceApigeeFlowhook() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceApigeeFlowhookCreate,
+		Read:   resourceApigeeFlowhookRead,
+		Delete: resourceApigeeFlowhookDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: resourceApigeeFlowhookImport,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Delete: schema.DefaultTimeout(20 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: `Description of the flow hook.`,
+			},
+			"environment": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `The resource ID of the environment.`,
+			},
+			"flow_hook_point": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Where in the API call flow the flow hook is invoked. Must be one of PreProxyFlowHook, PostProxyFlowHook, PreTargetFlowHook, or PostTargetFlowHook.`,
+			},
+			"org_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `The Apigee Organization associated with the environment`,
+			},
+			"sharedflow": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Id of the Sharedflow attaching to a flowhook point.`,
+			},
+			"continue_on_error": {
+				Type:        schema.TypeBool,
+				ForceNew:    true,
+				Optional:     true,
+				Default:      true,
+				Description: `Flag that specifies whether execution should continue if the flow hook throws an exception. Set to true to continue execution. Set to false to stop execution if the flow hook throws an exception. Defaults to true.`,
+			},
+		},
+		UseJSONNumber: true,
+	}
+}
+
+func resourceApigeeFlowhookCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	obj := make(map[string]interface{})
+	descriptionProp, err := expandApigeeFlowhookDescription(d.Get("description"), d, config)
+	if err != nil {
+		return err
+	} else if v, ok := d.GetOkExists("description"); !isEmptyValue(reflect.ValueOf(descriptionProp)) && (ok || !reflect.DeepEqual(v, descriptionProp)) {
+		obj["description"] = descriptionProp
+	}
+	sharedflowProp, err := expandApigeeFlowhookSharedflow(d.Get("sharedflow"), d, config)
+	if err != nil {
+		return err
+	} else if v, ok := d.GetOkExists("sharedflow"); !isEmptyValue(reflect.ValueOf(sharedflowProp)) && (ok || !reflect.DeepEqual(v, sharedflowProp)) {
+		obj["sharedFlow"] = sharedflowProp
+	}
+	continue_on_errorProp, err := expandApigeeFlowhookContinueOnError(d.Get("continue_on_error"), d, config)
+	if err != nil {
+		return err
+	} else if v, ok := d.GetOkExists("continue_on_error"); !isEmptyValue(reflect.ValueOf(continue_on_errorProp)) && (ok || !reflect.DeepEqual(v, continue_on_errorProp)) {
+		obj["continueOnError"] = continue_on_errorProp
+	}
+
+	url, err := replaceVars(d, config, "{{ApigeeBasePath}}organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}")
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Creating new Flowhook: %#v", obj)
+	billingProject := ""
+
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+
+	res, err := SendRequestWithTimeout(config, "PUT", billingProject, url, userAgent, obj, d.Timeout(schema.TimeoutCreate))
+	if err != nil {
+		return fmt.Errorf("Error creating Flowhook: %s", err)
+	}
+
+	// Store the ID now
+	id, err := replaceVars(d, config, "organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+
+	log.Printf("[DEBUG] Finished creating Flowhook %q: %#v", d.Id(), res)
+
+	return resourceApigeeFlowhookRead(d, meta)
+}
+
+func resourceApigeeFlowhookRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	url, err := replaceVars(d, config, "{{ApigeeBasePath}}organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}")
+	if err != nil {
+		return err
+	}
+
+	billingProject := ""
+
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+
+	res, err := SendRequest(config, "GET", billingProject, url, userAgent, nil)
+	if err != nil {
+		return handleNotFoundError(err, d, fmt.Sprintf("ApigeeFlowhook %q", d.Id()))
+	}
+	if res["sharedFlow"] == nil || res["sharedFlow"].(string) == "" {
+		//if response does not contain shared_flow field, then nothing is attached to this flowhook, we treat this "binding" resource non-existent
+		d.SetId("")
+		return nil
+	}
+	if err := d.Set("description", flattenApigeeFlowhookDescription(res["description"], d, config)); err != nil {
+		return fmt.Errorf("Error reading Flowhook: %s", err)
+	}
+	if err := d.Set("sharedflow", flattenApigeeFlowhookSharedflow(res["sharedFlow"], d, config)); err != nil {
+		return fmt.Errorf("Error reading Flowhook: %s", err)
+	}
+	if err := d.Set("continue_on_error", flattenApigeeFlowhookContinueOnError(res["continueOnError"], d, config)); err != nil {
+		return fmt.Errorf("Error reading Flowhook: %s", err)
+	}
+
+	return nil
+}
+
+func resourceApigeeFlowhookDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	userAgent, err := generateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	billingProject := ""
+
+	url, err := replaceVars(d, config, "{{ApigeeBasePath}}organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}")
+	if err != nil {
+		return err
+	}
+
+	var obj map[string]interface{}
+	log.Printf("[DEBUG] Deleting Flowhook %q", d.Id())
+
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+
+	res, err := SendRequestWithTimeout(config, "DELETE", billingProject, url, userAgent, obj, d.Timeout(schema.TimeoutDelete))
+	if err != nil {
+		return handleNotFoundError(err, d, "Flowhook")
+	}
+
+	log.Printf("[DEBUG] Finished deleting Flowhook %q: %#v", d.Id(), res)
+	return nil
+}
+
+func resourceApigeeFlowhookImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	config := meta.(*Config)
+	if err := parseImportId([]string{
+		"organizations/(?P<org_id>[^/]+)/environments/(?P<environment>[^/]+)/flowhooks/(?P<flow_hook_point>[^/]+)",
+		"(?P<org_id>[^/]+)/(?P<environment>[^/]+)/(?P<flow_hook_point>[^/]+)",
+	}, d, config); err != nil {
+		return nil, err
+	}
+
+	// Replace import id for the resource id
+	id, err := replaceVars(d, config, "organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}")
+	if err != nil {
+		return nil, fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+
+	return []*schema.ResourceData{d}, nil
+}
+
+func flattenApigeeFlowhookDescription(v interface{}, d *schema.ResourceData, config *Config) interface{} {
+	return v
+}
+
+func flattenApigeeFlowhookSharedflow(v interface{}, d *schema.ResourceData, config *Config) interface{} {
+	return v
+}
+
+func flattenApigeeFlowhookContinueOnError(v interface{}, d *schema.ResourceData, config *Config) interface{} {
+	return v
+}
+
+func expandApigeeFlowhookDescription(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandApigeeFlowhookSharedflow(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandApigeeFlowhookContinueOnError(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
+	return v, nil
+}
---
+++ website/docs/r/apigee_flowhook.html.markdown
@@ -0,0 +1,71 @@
+---
+subcategory: "Apigee"
+description: |-
+  Represents a sharedflow attachment to a flowhook point.
+---
+
+# google\_apigee\_flowhook
+
+Represents a sharedflow attachment to a flowhook point.
+
+
+To get more information about Flowhook, see:
+
+* [API documentation](https://cloud.google.com/apigee/docs/reference/apis/apigee/rest/v1/organizations.environments.flowhooks#FlowHook)
+* How-to Guides
+    * [organizations.environments.flowhooks](https://cloud.google.com/apigee/docs/reference/apis/apigee/rest/v1/organizations.environments.flowhooks#FlowHook)
+
+## Argument Reference
+
+The following arguments are supported:
+
+
+* `org_id` -
+  (Required)
+  The Apigee Organization associated with the environment
+
+* `environment` -
+  (Required)
+  The resource ID of the environment.
+
+* `flow_hook_point` -
+  (Required)
+  Where in the API call flow the flow hook is invoked. Must be one of PreProxyFlowHook, PostProxyFlowHook, PreTargetFlowHook, or PostTargetFlowHook.
+
+* `description` -
+  (Optional)
+  Description of the flow hook.
+
+* `sharedflow` -
+  (Required)
+  Id of the Sharedflow attaching to a flowhook point.
+
+* `continue_on_error` -
+  (Optional)
+  Flag that specifies whether execution should continue if the flow hook throws an exception. Set to true to continue execution. Set to false to stop execution if the flow hook throws an exception. Defaults to true.
+
+
+## Attributes Reference
+
+In addition to the arguments listed above, the following computed attributes are exported:
+
+* `id` - an identifier for the resource with format `organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}`
+
+
+## Timeouts
+
+This resource provides the following
+[Timeouts](https://developer.hashicorp.com/terraform/plugin/sdkv2/resources/retries-and-customizable-timeouts) configuration options:
+
+- `create` - Default is 20 minutes.
+- `delete` - Default is 20 minutes.
+
+## Import
+
+
+Flowhook can be imported using any of these accepted formats:
+
+```
+$ terraform import google_apigee_flowhook.default organizations/{{org_id}}/environments/{{environment}}/flowhooks/{{flow_hook_point}}
+$ terraform import google_apigee_flowhook.default {{org_id}}/{{environment}}/{{flow_hook_point}}
+```
