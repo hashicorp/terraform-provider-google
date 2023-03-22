22a1ce9e2ddad51cd908952e8d0aad412bc607d8
Modular Magician <magic-modules@google.com>
2023-03-22 12:33:50 +0000

    Move various files into correct locations, including moving test files into location so they're used in the downstream (#7505)
    * Move data source file into correct location
    
    * Rename data source test file to match conventions
    
    * Move handwritten tests into correct folder
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/resource_certificate_manager_dns_authorization_test.go
@@ -0,0 +1,67 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccCertificateManagerDnsAuthorization_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckCertificateManagerDnsAuthorizationDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCertificateManagerDnsAuthorization_update0(context),
+			},
+			{
+				ResourceName:            "google_certificate_manager_dns_authorization.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name"},
+			},
+			{
+				Config: testAccCertificateManagerDnsAuthorization_update1(context),
+			},
+			{
+				ResourceName:            "google_certificate_manager_dns_authorization.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name"},
+			},
+		},
+	})
+}
+
+func testAccCertificateManagerDnsAuthorization_update0(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_certificate_manager_dns_authorization" "default" {
+  name        = "tf-test-dns-auth%{random_suffix}"
+  description = "The default dnss"
+	labels = {
+		a = "a"
+	}
+  domain      = "%{random_suffix}.hashicorptest.com"
+}
+`, context)
+}
+
+func testAccCertificateManagerDnsAuthorization_update1(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_certificate_manager_dns_authorization" "default" {
+  name        = "tf-test-dns-auth%{random_suffix}"
+  description = "The default dnss2"
+	labels = {
+		a = "b"
+	}
+  domain      = "%{random_suffix}.hashicorptest.com"
+}
+`, context)
+}
