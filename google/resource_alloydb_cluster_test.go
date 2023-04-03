65868c6699e2d7d96b18653324feae9879a504d8
Modular Magician <magic-modules@google.com>
2023-04-03 22:41:41 +0000

    Deleted immutable attributes for initialUser and weeklySchedule so th… (#7552)
    * Deleted immutable attributes for initialUser and weeklySchedule so that deleting/adding these properties doesn't force new cluster creation
    
    * Automated tests for initialUser and automatedBackupPolicy
    
    * Add ImportStateVerifyIgnore for non-changing fields
    
    * gofmt fixes
    
    ---------
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_alloydb_cluster_test.go
+++ google/resource_alloydb_cluster_test.go
@@ -67,3 +67,153 @@ resource "google_compute_network" "default" {
 }
 `, context)
 }
+
+// Test if adding automatedBackupPolicy AND initialUser re-creates the cluster.
+// Ideally, cluster shouldn't be re-created. This test will only pass if the cluster
+// isn't re-created but updated in-place.
+func TestAccAlloydbCluster_addAutomatedBackupPolicyAndInitialUser(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbCluster_withoutInitialUserAndAutomatedBackupPolicy(context),
+			},
+			{
+				ResourceName:            "google_alloydb_cluster.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"initial_user", "cluster_id", "location"},
+			},
+			{
+				Config: testAccAlloydbCluster_withInitialUserAndAutomatedBackupPolicy(context),
+			},
+			{
+				ResourceName:            "google_alloydb_cluster.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"initial_user", "cluster_id", "location"},
+			},
+			{
+				Config: testAccAlloydbCluster_alloydbClusterBasicExample(context),
+			},
+		},
+	})
+}
+
+// Test if deleting automatedBackupPolicy AND initialUser re-creates the cluster.
+// Ideally, cluster shouldn't be re-created. This test will only pass if the cluster
+// isn't re-created but updated in-place.
+func TestAccAlloydbCluster_deleteAutomatedBackupPolicyAndInitialUser(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbCluster_withInitialUserAndAutomatedBackupPolicy(context),
+			},
+			{
+				ResourceName:            "google_alloydb_cluster.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"initial_user", "cluster_id", "location"},
+			},
+			{
+				Config: testAccAlloydbCluster_withoutInitialUserAndAutomatedBackupPolicy(context),
+			},
+			{
+				ResourceName:            "google_alloydb_cluster.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"initial_user", "cluster_id", "location"},
+			},
+			{
+				Config: testAccAlloydbCluster_alloydbClusterBasicExample(context),
+			},
+		},
+	})
+}
+
+func testAccAlloydbCluster_withInitialUserAndAutomatedBackupPolicy(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_alloydb_cluster" "default" {
+  cluster_id   = "tf-test-alloydb-cluster%{random_suffix}"
+  location     = "us-central1"
+  network      = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+
+  initial_user {
+    user     = "tf-test-alloydb-cluster%{random_suffix}"
+    password = "tf-test-alloydb-cluster%{random_suffix}"
+  }
+
+  automated_backup_policy {
+    location      = "us-central1"
+    backup_window = "1800s"
+    enabled       = true
+
+    weekly_schedule {
+      days_of_week = ["MONDAY"]
+
+      start_times {
+        hours   = 23
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+
+    quantity_based_retention {
+      count = 1
+    }
+
+    labels = {
+      test = "tf-test-alloydb-cluster%{random_suffix}"
+    }
+  }
+  lifecycle {
+    prevent_destroy = true
+  }  
+}
+
+data "google_project" "project" {
+}
+
+resource "google_compute_network" "default" {
+  name = "tf-test-alloydb-cluster%{random_suffix}"
+}
+`, context)
+}
+
+func testAccAlloydbCluster_withoutInitialUserAndAutomatedBackupPolicy(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+  lifecycle {
+    prevent_destroy = true
+  }  
+}
+
+data "google_project" "project" {
+}
+
+resource "google_compute_network" "default" {
+  name = "tf-test-alloydb-cluster%{random_suffix}"
+}
+`, context)
+}
