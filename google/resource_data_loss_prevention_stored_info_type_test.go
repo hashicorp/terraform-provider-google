e0d4c9fb4fba5511f2408c60d078c220213fa1e5
The Magician <magic-modules@google.com>
2023-04-04 14:44:30 -0700

    Added updation capability in google_data_loss_prevention_stored_info_type resource (#7601) (#14207)
    * Added updation capability in data_loss_prevention_stored_info_type resource
    
    * Adjusted the extra whitespaces in the data_loss_prevention_stored_info_type_test file
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_data_loss_prevention_stored_info_type_test.go
+++ google/resource_data_loss_prevention_stored_info_type_test.go
@@ -6,6 +6,209 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+func TestAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeCustomDiffFuncForceNew(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		before   map[string]interface{}
+		after    map[string]interface{}
+		forcenew bool
+	}{
+		"updating_dictionary": {
+			before: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"word", "word2"},
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"wordnew", "word2"},
+					},
+				},
+			},
+			forcenew: false,
+		},
+		"updating_large_custom_dictionary": {
+			before: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/something.json",
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/somethingnew.json",
+					},
+				},
+			},
+			forcenew: false,
+		},
+		"updating_regex": {
+			before: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "patient",
+				},
+			},
+			after: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "newpatient",
+				},
+			},
+			forcenew: false,
+		},
+		"changing_from_dictionary_to_large_custom_dictionary": {
+			before: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"word", "word2"},
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/something.json",
+					},
+				},
+			},
+			forcenew: true,
+		},
+		"changing_from_dictionary_to_regex": {
+			before: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"word", "word2"},
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "patient",
+				},
+			},
+			forcenew: true,
+		},
+		"changing_from_large_custom_dictionary_to_regex": {
+			before: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/something.json",
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "patient",
+				},
+			},
+			forcenew: true,
+		},
+		"changing_from_large_custom_dictionary_to_dictionary": {
+			before: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/something.json",
+					},
+				},
+			},
+			after: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"word", "word2"},
+					},
+				},
+			},
+			forcenew: true,
+		},
+		"changing_from_regex_to_dictionary": {
+			before: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "patient",
+				},
+			},
+			after: map[string]interface{}{
+				"dictionary": map[string]interface{}{
+					"word_list": map[string]interface{}{
+						"word": []string{"word", "word2"},
+					},
+				},
+			},
+			forcenew: true,
+		},
+		"changing_from_regex_to_large_custom_dictionary": {
+			before: map[string]interface{}{
+				"regex": map[string]interface{}{
+					"pattern": "patient",
+				},
+			},
+			after: map[string]interface{}{
+				"large_custom_dictionary": map[string]interface{}{
+					"output_path": map[string]interface{}{
+						"path": "gs://sample-dlp-bucket/something.json",
+					},
+				},
+			},
+			forcenew: true,
+		},
+	}
+
+	for tn, tc := range cases {
+
+		fieldBefore := ""
+		fieldAfter := ""
+		switch tn {
+		case "updating_dictionary":
+			fieldBefore = "dictionary"
+			fieldAfter = fieldBefore
+		case "updating_large_custom_dictionary":
+			fieldBefore = "large_custom_dictionary"
+			fieldAfter = fieldBefore
+		case "updating_regex":
+			fieldBefore = "regex"
+			fieldAfter = fieldBefore
+		case "changing_from_dictionary_to_large_custom_dictionary":
+			fieldBefore = "dictionary"
+			fieldAfter = "large_custom_dictionary"
+		case "changing_from_dictionary_to_regex":
+			fieldBefore = "dictionary"
+			fieldAfter = "regex"
+		case "changing_from_large_custom_dictionary_to_regex":
+			fieldBefore = "large_custom_dictionary"
+			fieldAfter = "regex"
+		case "changing_from_large_custom_dictionary_to_dictionary":
+			fieldBefore = "large_custom_dictionary"
+			fieldAfter = "dictionary"
+		case "changing_from_regex_to_dictionary":
+			fieldBefore = "regex"
+			fieldAfter = "dictionary"
+		case "changing_from_regex_to_large_custom_dictionary":
+			fieldBefore = "regex"
+			fieldAfter = "large_custom_dictionary"
+		}
+
+		d := &ResourceDiffMock{
+			Before: map[string]interface{}{
+				fieldBefore: tc.before[fieldBefore],
+			},
+			After: map[string]interface{}{
+				fieldAfter: tc.after[fieldAfter],
+			},
+		}
+		err := storedInfoTypeCustomizeDiffFunc(d)
+		if err != nil {
+			t.Errorf("failed, expected no error but received - %s for the condition %s", err, tn)
+		}
+		if d.IsForceNew != tc.forcenew {
+			t.Errorf("ForceNew not setup correctly for the condition-'%s', expected:%v; actual:%v", tn, tc.forcenew, d.IsForceNew)
+		}
+	}
+}
+
 func TestAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeUpdate(t *testing.T) {
 	t.Parallel()
 
@@ -65,6 +268,83 @@ resource "google_data_loss_prevention_stored_info_type" "basic" {
 		word_list {
 			words = ["word", "word2"]
 		}
+	}
+}
+`, context)
+}
+
+func TestAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeGroupIndexUpdate(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project": GetTestProjectFromEnv(),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckDataLossPreventionStoredInfoTypeDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeWithoutGroupIndex(context),
+			},
+			{
+				ResourceName:      "google_data_loss_prevention_stored_info_type.basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeStart(context),
+			},
+			{
+				ResourceName:      "google_data_loss_prevention_stored_info_type.basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeGroupIndexUpdate(context),
+			},
+			{
+				ResourceName:      "google_data_loss_prevention_stored_info_type.basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeWithoutGroupIndex(context),
+			},
+			{
+				ResourceName:      "google_data_loss_prevention_stored_info_type.basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeWithoutGroupIndex(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_data_loss_prevention_stored_info_type" "basic" {
+	parent = "projects/%{project}"
+	description = "Description"
+	display_name = "Displayname"
+
+	regex {
+		pattern = "patient"
+	}
+}
+`, context)
+}
+
+func testAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeGroupIndexUpdate(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_data_loss_prevention_stored_info_type" "basic" {
+	parent = "projects/%{project}"
+	description = "Description"
+	display_name = "Displayname"
+
+	regex {
+		pattern = "patient"
+		group_indexes = [3]
 	}
 }
 `, context)
