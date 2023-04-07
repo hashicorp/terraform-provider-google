a3d62576f854d89ad5a4176bb3ca3af6c45b2662
Modular Magician <magic-modules@google.com>
2023-04-07 19:36:08 +0000

    Increase default timeout for google_vertex_ai_index to 180m (#7657)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_vertex_ai_index.go
+++ google/resource_vertex_ai_index.go
@@ -36,9 +36,9 @@ func ResourceVertexAIIndex() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(60 * time.Minute),
-			Update: schema.DefaultTimeout(60 * time.Minute),
-			Delete: schema.DefaultTimeout(60 * time.Minute),
+			Create: schema.DefaultTimeout(180 * time.Minute),
+			Update: schema.DefaultTimeout(180 * time.Minute),
+			Delete: schema.DefaultTimeout(180 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
