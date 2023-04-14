47f0d2028c79af4bd6303b7975a394e98c0e66e5
The Magician <magic-modules@google.com>
2023-04-14 10:48:32 -0700

    compute/backend_bucket: Fix default behavior with `USE_ORIGIN_HEADERS` (#7588) (#14323)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: d10i <hello@hexago.tech>

--- google/resource_compute_backend_bucket_test.go
+++ google/resource_compute_backend_bucket_test.go
@@ -91,6 +91,14 @@ func TestAccComputeBackendBucket_withCdnPolicy(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
+			{
+				Config: testAccComputeBackendBucket_withCdnPolicy4(backendName, storageName, 0, 404, 0),
+			},
+			{
+				ResourceName:      "google_compute_backend_bucket.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
 		},
 	})
 }
@@ -310,4 +318,28 @@ resource "google_storage_bucket" "bucket_one" {
   location = "EU"
 }
 `, backendName, compressionMode, storageName)
+}
+
+func testAccComputeBackendBucket_withCdnPolicy4(backendName, storageName string, age, code, ttl int) string {
+	return fmt.Sprintf(`
+resource "google_compute_backend_bucket" "foobar" {
+  name        = "%s"
+  bucket_name = google_storage_bucket.bucket.name
+  enable_cdn  = true
+  cdn_policy {
+	cache_mode                   = "USE_ORIGIN_HEADERS"
+	signed_url_cache_max_age_sec = %d
+	serve_while_stale            = %d
+	negative_caching_policy {
+		code = %d
+		ttl = %d
+	}
+	negative_caching = true
+  }
+}
+resource "google_storage_bucket" "bucket" {
+  name     = "%s"
+  location = "EU"
+}
+`, backendName, age, ttl, code, ttl, storageName)
 }
