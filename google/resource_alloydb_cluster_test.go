0c2e35edabfdcc6699463e02959ca58f89638481
Modular Magician <magic-modules@google.com>
2023-04-19 13:24:06 +0000

    Automated Tests for AlloyDB cluster (#7746)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_alloydb_cluster_test.go
+++ google/resource_alloydb_cluster_test.go
@@ -57,7 +57,7 @@ resource "google_alloydb_cluster" "default" {
 
   lifecycle {
     prevent_destroy = true
-  }  
+  }
 }
 
 data "google_project" "project" {
@@ -300,6 +300,161 @@ resource "google_alloydb_cluster" "default" {
   }
 }
 data "google_project" "project" {}
+resource "google_compute_network" "default" {
+  name = "tf-test-alloydb-cluster%{random_suffix}"
+}
+`, context)
+}
+
+// The cluster creation should succeed with minimal number of arguments.
+func TestAccAlloydbCluster_mandatoryFields(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbCluster_alloydbClusterBasicExample(context),
+			},
+		},
+	})
+}
+
+// The cluster creation should succeed with maximal number of arguments.
+func TestAccAlloydbCluster_maximumFields(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbCluster_alloydbClusterFullExample(context),
+			},
+		},
+	})
+}
+
+// Deletion of time-based retention policy should be an in-place operation
+func TestAccAlloydbCluster_deleteTimeBasedRetentionPolicy(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbCluster_withTimeBasedRetentionPolicy(context),
+			},
+			{
+				ResourceName:      "google_alloydb_cluster.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbCluster_withoutTimeBasedRetentionPolicy(context),
+			},
+			{
+				ResourceName:      "google_alloydb_cluster.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbCluster_alloydbClusterBasicExample(context),
+			},
+		},
+	})
+}
+
+func testAccAlloydbCluster_withTimeBasedRetentionPolicy(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+  automated_backup_policy {
+    location      = "us-central1"
+    backup_window = "1800s"
+    enabled       = true
+
+    weekly_schedule {
+      days_of_week = ["MONDAY"]
+
+      start_times {
+        hours   = 23
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+    time_based_retention {
+      retention_period = "4.5s"
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      automated_backup_policy[0].time_based_retention
+    ]
+    prevent_destroy = true
+  }
+}
+
+data "google_project" "project" { }
+
+resource "google_compute_network" "default" {
+  name = "tf-test-alloydb-cluster%{random_suffix}"
+}
+`, context)
+}
+
+func testAccAlloydbCluster_withoutTimeBasedRetentionPolicy(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+  automated_backup_policy {
+    location      = "us-central1"
+    backup_window = "1800s"
+    enabled       = true
+
+    weekly_schedule {
+      days_of_week = ["MONDAY"]
+
+      start_times {
+        hours   = 23
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      automated_backup_policy[0].time_based_retention
+    ]
+    prevent_destroy = true
+  }
+}
+
+data "google_project" "project" { }
+
 resource "google_compute_network" "default" {
   name = "tf-test-alloydb-cluster%{random_suffix}"
 }
