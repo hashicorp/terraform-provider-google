7a3932d1a2fe0df884636e80d1b154be906eeadb
The Magician <magic-modules@google.com>
2023-04-20 10:21:37 -0700

    Adding startup_cpu_boost field for cloud run v2 service. (#7732) (#14372)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_cloud_run_v2_service_test.go
+++ google/resource_cloud_run_v2_service_test.go
@@ -89,6 +89,7 @@ resource "google_cloud_run_v2_service" "default" {
       }
       resources {
         cpu_idle = true
+        startup_cpu_boost = true
         limits = {
           cpu = "4"
           memory = "2Gi"
@@ -157,6 +158,7 @@ resource "google_cloud_run_v2_service" "default" {
       }
       resources {
         cpu_idle = true
+        startup_cpu_boost = false
         limits = {
           cpu = "2"
           memory = "8Gi"
