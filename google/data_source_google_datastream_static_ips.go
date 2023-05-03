2b3a71baea547a593257e4d8e07b9cf5960d812b
The Magician <magic-modules@google.com>
2023-05-03 12:37:34 -0700

    Add google_datastream_static_ips data source (#7847) (#14487)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/data_source_google_datastream_static_ips.go
@@ -0,0 +1,77 @@
+package google
+
+import (
+	"fmt"
+
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataSourceGoogleDatastreamStaticIps() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceGoogleDatastreamStaticIpsRead,
+
+		Schema: map[string]*schema.Schema{
+			"project": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"location": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"static_ips": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func dataSourceGoogleDatastreamStaticIpsRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+	userAgent, err := generateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	url, err := ReplaceVars(d, config, "{{DatastreamBasePath}}projects/{{project}}/locations/{{location}}:fetchStaticIps")
+	if err != nil {
+		return err
+	}
+
+	staticIps, err := paginatedListRequest(project, url, userAgent, config, flattenStaticIpsList)
+	if err != nil {
+		return fmt.Errorf("Error retrieving monitoring uptime check ips: %s", err)
+	}
+
+	if err := d.Set("static_ips", staticIps); err != nil {
+		return fmt.Errorf("Error retrieving monitoring uptime check ips: %s", err)
+	}
+
+	// Store the ID now
+	id, err := ReplaceVars(d, config, "projects/{{project}}/locations/{{location}}:fetchStaticIps")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+	return nil
+}
+
+func flattenStaticIpsList(resp map[string]interface{}) []interface{} {
+	ipList := resp["staticIps"].([]interface{})
+	staticIps := make([]interface{}, len(ipList))
+	for i, u := range ipList {
+		staticIps[i] = u
+	}
+	return staticIps
+}
