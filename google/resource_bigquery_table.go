35fe2aafa71f66d7c6ed3a2f5062a1fb4dbb7a47
Modular Magician <magic-modules@google.com>
2023-05-10 20:40:58 +0000

    Add ICEBERG as a supported external data format for bigquery tables (#7858)
    * Add ICEBERG as a supported external data format for bigquery tables
    * updated descriptions to link api reference
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_bigquery_table.go
+++ google/resource_bigquery_table.go
@@ -443,9 +443,9 @@ func ResourceBigQueryTable() *schema.Resource {
 						"source_format": {
 							Type:        schema.TypeString,
 							Required:    true,
-							Description: `The data format. Supported values are: "CSV", "GOOGLE_SHEETS", "NEWLINE_DELIMITED_JSON", "AVRO", "PARQUET", "ORC" and "DATASTORE_BACKUP". To use "GOOGLE_SHEETS" the scopes must include "googleapis.com/auth/drive.readonly".`,
+							Description: ` Please see sourceFormat under ExternalDataConfiguration in Bigquery's public API documentation (https://cloud.google.com/bigquery/docs/reference/rest/v2/tables#externaldataconfiguration) for supported formats. To use "GOOGLE_SHEETS" the scopes must include "googleapis.com/auth/drive.readonly".`,
 							ValidateFunc: validation.StringInSlice([]string{
-								"CSV", "GOOGLE_SHEETS", "NEWLINE_DELIMITED_JSON", "AVRO", "DATASTORE_BACKUP", "PARQUET", "ORC", "BIGTABLE",
+								"CSV", "GOOGLE_SHEETS", "NEWLINE_DELIMITED_JSON", "AVRO", "ICEBERG", "DATASTORE_BACKUP", "PARQUET", "ORC", "BIGTABLE",
 							}, false),
 						},
 						// SourceURIs [Required] The fully-qualified URIs that point to your data in Google Cloud.
@@ -465,7 +465,7 @@ func ResourceBigQueryTable() *schema.Resource {
 						},
 						// Schema: Optional] The schema for the  data.
 						// Schema is required for CSV and JSON formats if autodetect is not on.
-						// Schema is disallowed for Google Cloud Bigtable, Cloud Datastore backups, Avro, ORC and Parquet formats.
+						// Schema is disallowed for Google Cloud Bigtable, Cloud Datastore backups, Avro, Iceberg, ORC, and Parquet formats.
 						"schema": {
 							Type:         schema.TypeString,
 							Optional:     true,
--- website/docs/r/bigquery_table.html.markdown
+++ website/docs/r/bigquery_table.html.markdown
@@ -185,7 +185,7 @@ in Terraform state, a `terraform destroy` or `terraform apply` that would delete
 
 * `schema` - (Optional) A JSON schema for the external table. Schema is required
     for CSV and JSON formats if autodetect is not on. Schema is disallowed
-    for Google Cloud Bigtable, Cloud Datastore backups, Avro, ORC and Parquet formats.
+    for Google Cloud Bigtable, Cloud Datastore backups, Avro, Iceberg, ORC and Parquet formats.
     ~>**NOTE:** Because this field expects a JSON string, any changes to the
     string will create a diff, even if the JSON itself hasn't changed.
     Furthermore drift for this field cannot not be detected because BigQuery
@@ -195,11 +195,10 @@ in Terraform state, a `terraform destroy` or `terraform apply` that would delete
     datasource, after creation the computed schema will be stored in
     `google_bigquery_table.schema`
 
-* `source_format` (Required) - The data format. Supported values are:
-    "CSV", "GOOGLE_SHEETS", "NEWLINE_DELIMITED_JSON", "AVRO", "PARQUET", "ORC",
-    "DATSTORE_BACKUP", and "BIGTABLE". To use "GOOGLE_SHEETS"
-    the `scopes` must include
-    "https://www.googleapis.com/auth/drive.readonly".
+* `source_format` (Required) - The data format. Please see sourceFormat under
+    [ExternalDataConfiguration](https://cloud.google.com/bigquery/docs/reference/rest/v2/tables#externaldataconfiguration)
+    in Bigquery's public API documentation for supported formats. To use "GOOGLE_SHEETS"
+    the `scopes` must include "https://www.googleapis.com/auth/drive.readonly".
 
 * `source_uris` - (Required) A list of the fully-qualified URIs that point to
     your data in Google Cloud.
