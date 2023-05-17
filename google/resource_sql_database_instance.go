42761f4360a57e2e3b5b75838e24ea6237f7cad3
Modular Magician <magic-modules@google.com>
2023-05-17 18:27:18 +0000

    Replaced users.list api with users.get api to increase efficiency. (#7941)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_sql_database_instance.go
+++ google/resource_sql_database_instance.go
@@ -1067,27 +1067,25 @@ func resourceSqlDatabaseInstanceCreate(d *schema.ResourceData, meta interface{})
 	// Users in a replica instance are inherited from the master instance and should be left alone.
 	// This deletion is done immediately after the instance is created, in order to minimize the
 	// risk of it being left on the instance, which would present a security concern.
-	if sqlDatabaseIsMaster(d) {
-		var users *sqladmin.UsersListResponse
+	if sqlDatabaseIsMaster(d) && strings.Contains(strings.ToUpper(databaseVersion), "MYSQL") {
+		var user *sqladmin.User
 		err = transport_tpg.RetryTimeDuration(func() error {
-			users, err = config.NewSqlAdminClient(userAgent).Users.List(project, instance.Name).Do()
+			user, err = config.NewSqlAdminClient(userAgent).Users.Get(project, instance.Name, "root").Host("%").Do()
 			return err
 		}, d.Timeout(schema.TimeoutRead), transport_tpg.IsSqlOperationInProgressError)
 		if err != nil {
-			return fmt.Errorf("Error, attempting to list users associated with instance %s: %s", instance.Name, err)
+			return fmt.Errorf("Error, attempting to fetch root user associated with instance %s: %s", instance.Name, err)
 		}
-		for _, u := range users.Items {
-			if u.Name == "root" && u.Host == "%" {
-				err = transport_tpg.Retry(func() error {
-					op, err = config.NewSqlAdminClient(userAgent).Users.Delete(project, instance.Name).Host(u.Host).Name(u.Name).Do()
-					if err == nil {
-						err = SqlAdminOperationWaitTime(config, op, project, "Delete default root User", userAgent, d.Timeout(schema.TimeoutCreate))
-					}
-					return err
-				})
-				if err != nil {
-					return fmt.Errorf("Error, failed to delete default 'root'@'*' user, but the database was created successfully: %s", err)
+		if user != nil {
+			err = transport_tpg.Retry(func() error {
+				op, err = config.NewSqlAdminClient(userAgent).Users.Delete(project, instance.Name).Host(user.Host).Name(user.Name).Do()
+				if err == nil {
+					err = SqlAdminOperationWaitTime(config, op, project, "Delete default root User", userAgent, d.Timeout(schema.TimeoutCreate))
 				}
+				return err
+			})
+			if err != nil {
+				return fmt.Errorf("Error, failed to delete default 'root'@'*' user, but the database was created successfully: %s", err)
 			}
 		}
 	}
