b5660b698b76594d056255ebe0f96e67d2168134
Modular Magician <magic-modules@google.com>
2023-05-25 17:06:40 +0000

    Add copyright headers to files in the TPG/B downsteam repos that are generated via templates (#7504)
    * Add copyright header to Terraform templates
    
    * Add copyright headers to main.go in the provider
    
    * Add method to template in copyright header from single file
    
    * Add method that templates HC copyright header
    
    * Add ability for templates to know what downstream is being generated
    
    * Make inclusion of HashiCorp copyright headers specific to HC downstreams
    
    * Refactor `is_generating_hashicorp_repo?` with style suggestions
    
    * Rename function following style suggestions
    
    * Address RuboCop style suggestions
    
    * Address RuboCop style suggestions - empty lines in class declaration bodies
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_binary_authorization_attestor_sweeper_test.go
+++ google/resource_binary_authorization_attestor_sweeper_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // ----------------------------------------------------------------------------
 //
 //     ***     AUTO GENERATED CODE    ***    Type: MMv1     ***
