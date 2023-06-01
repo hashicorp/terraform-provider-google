7c286a036810f30dffe1775c8054bebbedf70cfb
The Magician <magic-modules@google.com>
2023-06-01 12:57:50 -0700

    Remove TestAccAlloydb*_missingLocation tests that fail VCR replay (#8052) (#14787)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_alloydb_backup_test.go
+++ google/resource_alloydb_backup_test.go
@@ -3,7 +3,6 @@
 package google
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -91,72 +90,6 @@ resource "google_service_networking_connection" "vpc_connection" {
 
 data "google_compute_network" "default" {
   name = "%{network_name}"
-}
-`, context)
-}
-
-// We expect an error when creating an on-demand backup without location.
-// Location is a `required` field.
-func TestAccAlloydbBackup_missingLocation(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"random_suffix": RandString(t, 10),
-	}
-
-	VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckAlloydbBackupDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config:      testAccAlloydbBackup_missingLocation(context),
-				ExpectError: regexp.MustCompile("Missing required argument"),
-			},
-		},
-	})
-}
-
-func testAccAlloydbBackup_missingLocation(context map[string]interface{}) string {
-	return Nprintf(`
-resource "google_alloydb_backup" "default" {
-  backup_id    = "tf-test-alloydb-backup%{random_suffix}"
-  cluster_name = google_alloydb_cluster.default.name
-  depends_on = [google_alloydb_instance.default]
-}
-
-resource "google_alloydb_cluster" "default" {
-  location = "us-central1"
-  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
-  network    = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
-}
-  
-data "google_project" "project" { }
-
-resource "google_compute_network" "default" {
-  name = "tf-test-alloydb-cluster%{random_suffix}"
-}
-
-resource "google_alloydb_instance" "default" {
-  cluster       = google_alloydb_cluster.default.name
-  instance_id   = "tf-test-alloydb-instance%{random_suffix}"
-  instance_type = "PRIMARY"
-
-  depends_on = [google_service_networking_connection.vpc_connection]
-}
-  
-resource "google_compute_global_address" "private_ip_alloc" {
-  name          =  "tf-test-alloydb-cluster%{random_suffix}"
-  address_type  = "INTERNAL"
-  purpose       = "VPC_PEERING"
-  prefix_length = 16
-  network       = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
-}
-
-resource "google_service_networking_connection" "vpc_connection" {
-  network                 = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
 `, context)
 }
