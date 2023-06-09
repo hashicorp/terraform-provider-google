06c95349e39e2caf6a112f1bf4786d1032c24328
Modular Magician <magic-modules@google.com>
2023-06-09 00:21:09 +0000

    Skip apigee tests for multipart boundary (#8122)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_apigee_keystores_aliases_self_signed_cert_generated_test.go
+++ google/resource_apigee_keystores_aliases_self_signed_cert_generated_test.go
@@ -31,6 +31,7 @@ import (
 )
 
 func TestAccApigeeKeystoresAliasesSelfSignedCert_apigeeEnvKeystoreAliasSelfSignedCertExample(t *testing.T) {
+	acctest.SkipIfVcr(t)
 	t.Parallel()
 
 	context := map[string]interface{}{
