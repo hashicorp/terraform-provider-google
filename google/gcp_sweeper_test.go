cabab213c32983341f9f36fe2bf194536222fdbe
Modular Magician <magic-modules@google.com>
2023-06-12 16:41:34 +0000

    Revert "Move sweeper FirebaseAndroidApp to the service package (#8117)" (#8132)
    This reverts commit 5442ab1d1b605d04b651004bbeb00b35896e059f.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/gcp_sweeper_test.go
+++ google/gcp_sweeper_test.go
@@ -6,8 +6,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
-	_ "github.com/hashicorp/terraform-provider-google/google/services/firebase"
 )
 
 func TestMain(m *testing.M) {
