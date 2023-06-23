3afa28078a49a0c3aa8f8d548725534ad5068d5b
Modular Magician <magic-modules@google.com>
2023-06-23 23:46:39 +0000

    Move sweeper FirebaseAndroidApp to the service package (#8186)
    * Move sweeper FirebaseAndroidApp to the service package
    
    This reverts commit aad16c50e8589dda9162acc72d44c05488a50826.
    
    * Append the current sweeper file name to the sweeper name
    
    * Create a utility function AddTestSweepers
    
    * Hash the file name
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/acctest/gcp_sweeper.go
+++ google/acctest/gcp_sweeper.go
@@ -3,9 +3,13 @@
 package acctest
 
 import (
+	"encoding/hex"
 	"fmt"
+	"hash/crc32"
+	"runtime"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
@@ -54,4 +58,17 @@ func IsSweepableTestResource(resourceName string) bool {
 		}
 	}
 	return false
+}
+
+func AddTestSweepers(name string, sweeper func(region string) error) {
+	_, filename, _, _ := runtime.Caller(0)
+	hash := crc32.NewIEEE()
+	hash.Write([]byte(filename))
+	hashedFilename := hex.EncodeToString(hash.Sum(nil))
+	uniqueName := name + "_" + hashedFilename
+
+	resource.AddTestSweepers(uniqueName, &resource.Sweeper{
+		Name: name,
+		F:    sweeper,
+	})
 }
