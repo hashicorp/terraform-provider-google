5761caee711a1fb79284c639e8e0988e03e5a325
Modular Magician <magic-modules@google.com>
2023-07-27 18:22:59 +0000

    Revert "Add Update Connector Support for VPC Access Connectors (#8205)" (#8456)
    This reverts commit 85ff1c21e6aaff26aafa56c47d0af03ae6aa3726.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/resource_vpc_access_connector_test.go
+++ google/resource_vpc_access_connector_test.go
@@ -33,38 +33,6 @@ func TestAccVPCAccessConnector_vpcAccessConnectorThroughput(t *testing.T) {
 	})
 }
 
-func TestAccVPCAccessConnector_vpcAccessConnectorMachineAndInstancesChanged(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"random_suffix": RandString(t, 10),
-	}
-
-	VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckVPCAccessConnectorDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccVPCAccessConnector_vpcAccessConnectorThroughput(context),
-			},
-			{
-				ResourceName:      "google_vpc_access_connector.connector",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccVPCAccessConnector_vpcAccessConnectorMachineAndInstancesChanged(context),
-			},
-			{
-				ResourceName:      "google_vpc_access_connector.connector",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
 func testAccVPCAccessConnector_vpcAccessConnectorThroughput(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_vpc_access_connector" "connector" {
@@ -75,33 +43,6 @@ resource "google_vpc_access_connector" "connector" {
   machine_type = "e2-standard-4"
   min_instances = 2
   max_instances = 3
-  region        = "us-central1"
-}
-
-resource "google_compute_subnetwork" "custom_test" {
-  name          = "tf-test-vpc-con%{random_suffix}"
-  ip_cidr_range = "10.2.0.0/28"
-  region        = "us-central1"
-  network       = google_compute_network.custom_test.id
-}
-
-resource "google_compute_network" "custom_test" {
-  name                    = "tf-test-vpc-con%{random_suffix}"
-  auto_create_subnetworks = false
-}
-`, context)
-}
-
-func testAccVPCAccessConnector_vpcAccessConnectorMachineAndInstancesChanged(context map[string]interface{}) string {
-	return Nprintf(`
-resource "google_vpc_access_connector" "connector" {
-  name          = "tf-test-vpc-con%{random_suffix}"
-  subnet {
-    name = google_compute_subnetwork.custom_test.name
-  }
-  machine_type  = "e2-micro"
-  min_instances = 3
-  max_instances = 5
   region        = "us-central1"
 }
 
