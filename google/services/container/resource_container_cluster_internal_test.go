e7e9c6839eb2782c150633e7035f765c47affcd4
The Magician <magic-modules@google.com>
2023-07-27 13:33:37 -0700

    feat(google_container_cluster): support enable k8s beta apis (#8355) (#15320)
    * feat(google_container_cluster): support enable k8s beta apis
    
    
    
    * use TypeSet to avoid the re-ordering being treated as a diff
    * add custom diff to recreate cluster when removing enabled apis
    * add unit and integration tests
    
    * fix: use contains instead of sets intersection
    
    * Update mmv1/third_party/terraform/website/docs/r/container_cluster.html.markdown
    
    ---------
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>

--- google/services/container/resource_container_cluster_internal_test.go
+++ google/services/container/resource_container_cluster_internal_test.go
@@ -1,3 +1,104 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 package container
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+)
+
+func TestContainerClusterEnableK8sBetaApisCustomizeDiff(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		before           *schema.Set
+		after            *schema.Set
+		expectedForceNew bool
+	}{
+		"no need to force new from nil to empty apis": {
+			before:           schema.NewSet(schema.HashString, nil),
+			after:            schema.NewSet(schema.HashString, []interface{}{}),
+			expectedForceNew: false,
+		},
+		"no need to force new from empty apis to nil": {
+			before:           schema.NewSet(schema.HashString, []interface{}{}),
+			after:            schema.NewSet(schema.HashString, nil),
+			expectedForceNew: false,
+		},
+		"no need to force new from empty apis to empty apis": {
+			before:           schema.NewSet(schema.HashString, []interface{}{}),
+			after:            schema.NewSet(schema.HashString, []interface{}{}),
+			expectedForceNew: false,
+		},
+		"no need to force new from nil to empty string apis": {
+			before:           schema.NewSet(schema.HashString, nil),
+			after:            schema.NewSet(schema.HashString, []interface{}{""}),
+			expectedForceNew: false,
+		},
+		"no need to force new from empty string apis to empty string apis": {
+			before:           schema.NewSet(schema.HashString, []interface{}{""}),
+			after:            schema.NewSet(schema.HashString, []interface{}{""}),
+			expectedForceNew: false,
+		},
+		"no need to force new for enabling new api from empty apis": {
+			before:           schema.NewSet(schema.HashString, []interface{}{}),
+			after:            schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			expectedForceNew: false,
+		},
+		"no need to force new for enabling new api from nil": {
+			before:           schema.NewSet(schema.HashString, nil),
+			after:            schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			expectedForceNew: false,
+		},
+		"no need to force new for passing same apis": {
+			before:           schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			after:            schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			expectedForceNew: false,
+		},
+		"no need to force new for passing same apis with inconsistent order": {
+			before:           schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo", "dummy.k8s.io/v1beta1/bar"}),
+			after:            schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/bar", "dummy.k8s.io/v1beta1/foo"}),
+			expectedForceNew: false,
+		},
+		"need to force new from empty string apis to nil": {
+			before:           schema.NewSet(schema.HashString, []interface{}{""}),
+			after:            schema.NewSet(schema.HashString, nil),
+			expectedForceNew: true,
+		},
+		"need to force new for disabling existing api": {
+			before:           schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			after:            schema.NewSet(schema.HashString, []interface{}{}),
+			expectedForceNew: true,
+		},
+		"need to force new for disabling existing api with nil": {
+			before:           schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			after:            schema.NewSet(schema.HashString, nil),
+			expectedForceNew: true,
+		},
+		"need to force new for disabling existing apis": {
+			before:           schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo", "dummy.k8s.io/v1beta1/bar", "dummy.k8s.io/v1beta1/baz"}),
+			after:            schema.NewSet(schema.HashString, []interface{}{"dummy.k8s.io/v1beta1/foo"}),
+			expectedForceNew: true,
+		},
+	}
+
+	for tn, tc := range cases {
+		d := &tpgresource.ResourceDiffMock{
+			Before: map[string]interface{}{
+				"enable_k8s_beta_apis.0.enabled_apis": tc.before,
+			},
+			After: map[string]interface{}{
+				"enable_k8s_beta_apis.0.enabled_apis": tc.after,
+			},
+		}
+		err := containerClusterEnableK8sBetaApisCustomizeDiffFunc(d)
+		if err != nil {
+			t.Errorf("%s failed, found unexpected error: %s", tn, err)
+		}
+		if d.IsForceNew != tc.expectedForceNew {
+			t.Errorf("%v: expected d.IsForceNew to be %v, but was %v", tn, tc.expectedForceNew, d.IsForceNew)
+		}
+	}
+}
