19a8a01b3dc2323f493d436e0d387a0889176743
Modular Magician <magic-modules@google.com>
2023-08-23 22:03:18 +0000

    Make mysql_profile.password mutable to avoid recreation of the connection profile. (#8675)
    * Make mysql_profile.password mutable to avoid recreation of the connection profile.
    
    The recreation of connection profile fails the streams permanently. They have to be recreated from the scratch.
    
    * Make fmt
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/datastream/resource_datastream_connection_profile_test.go
+++ google/services/datastream/resource_datastream_connection_profile_test.go
@@ -18,6 +18,9 @@ func TestAccDatastreamConnectionProfile_update(t *testing.T) {
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
+	random_pass_1 := acctest.RandString(t, 10)
+	random_pass_2 := acctest.RandString(t, 10)
+
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
@@ -47,6 +50,29 @@ func TestAccDatastreamConnectionProfile_update(t *testing.T) {
 			{
 				// Disable prevent_destroy
 				Config: testAccDatastreamConnectionProfile_update2(context, false),
+			},
+			{
+				Config: testAccDatastreamConnectionProfile_mySQLUpdate(context, true, random_pass_1),
+			},
+			{
+				ResourceName:            "google_datastream_connection_profile.mysql_con_profile",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"connection_profile_id", "location", "mysql_profile.0.password"},
+			},
+			{
+				// run once more to update the password. it should update it in-place
+				Config: testAccDatastreamConnectionProfile_mySQLUpdate(context, true, random_pass_2),
+			},
+			{
+				ResourceName:            "google_datastream_connection_profile.mysql_con_profile",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"connection_profile_id", "location", "mysql_profile.0.password"},
+			},
+			{
+				// Disable prevent_destroy
+				Config: testAccDatastreamConnectionProfile_mySQLUpdate(context, false, random_pass_2),
 			},
 		},
 	})
@@ -140,6 +166,84 @@ resource "google_datastream_connection_profile" "default" {
 		username = google_sql_user.user.name
 		password = google_sql_user.user.password
 		database = google_sql_database.db.name
+	}
+	%{lifecycle_block}
+}
+`, context)
+}
+
+func testAccDatastreamConnectionProfile_mySQLUpdate(context map[string]interface{}, preventDestroy bool, password string) string {
+	context["lifecycle_block"] = ""
+	if preventDestroy {
+		context["lifecycle_block"] = `
+		lifecycle {
+			prevent_destroy = true
+		}`
+	}
+
+	context["password"] = password
+
+	return acctest.Nprintf(`
+resource "google_sql_database_instance" "mysql_instance" {
+    name             = "tf-test-mysql-database-instance%{random_suffix}"
+    database_version = "MYSQL_8_0"
+    region           = "us-central1"
+    settings {
+      tier = "db-f1-micro"
+        backup_configuration {
+            enabled            = true
+            binary_log_enabled = true
+        }
+
+      ip_configuration {
+
+        // Datastream IPs will vary by region.
+        authorized_networks {
+            value = "34.71.242.81"
+        }
+
+        authorized_networks {
+            value = "34.72.28.29"
+        }
+
+        authorized_networks {
+            value = "34.67.6.157"
+        }
+
+        authorized_networks {
+            value = "34.67.234.134"
+        }
+
+        authorized_networks {
+            value = "34.72.239.218"
+        }
+      }
+    }
+
+    deletion_protection  = "false"
+}
+
+resource "google_sql_database" "mysql_db" {
+    instance = google_sql_database_instance.mysql_instance.name
+    name     = "db"
+}
+
+resource "google_sql_user" "mysql_user" {
+    name = "user"
+    instance = google_sql_database_instance.mysql_instance.name
+    host     = "%"
+    password = "%{password}"
+}
+
+resource "google_datastream_connection_profile" "mysql_con_profile" {
+    display_name          = "Source connection profile"
+	location              = "us-central1"
+	connection_profile_id = "tf-test-mysql-profile%{random_suffix}"
+
+    mysql_profile {
+		hostname = google_sql_database_instance.mysql_instance.public_ip_address
+		username = google_sql_user.mysql_user.name
+		password = google_sql_user.mysql_user.password
 	}
 	%{lifecycle_block}
 }
