0e253e0e2fadca777b8c9710a8e81f2d754f7b4c
The Magician <magic-modules@google.com>
2023-08-24 17:27:52 -0400

    Cleanup deprecated folders (#8720) (#15621)
    * Remove folder resources and data_sources
    
    * Remove utility files
    
    * Move more utility files
    
    * Fix tgc
    
    * Import provider package in tgc
    
    * Remove utils files
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/test-fixtures/cloudfunctions/secret_environment_variables.js
+++
@@ -1,7 +0,0 @@
-/**
- * HTTP Cloud Function for testing environment variable Secrets.
- */
-exports.echoSecret = (req, res) => {
-    let message = req.query.message || req.body.message || "Secret: " + process.env.MY_SECRET;
-    res.status(200).send(message);
-};
