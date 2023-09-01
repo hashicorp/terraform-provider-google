49df58b556208f58542df9ab9c3b940bed93ff1e
Modular Magician <magic-modules@google.com>
2023-09-01 17:14:57 +0000

    Add update support to reconcile_connections in service attachment (#8831)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_service_attachment.go
+++ google/services/compute/resource_compute_service_attachment.go
@@ -139,7 +139,6 @@ supported is 1.`,
 			"reconcile_connections": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				ForceNew: true,
 				Description: `This flag determines whether a consumer accept/reject list change can reconcile the statuses of existing ACCEPTED or REJECTED PSC endpoints.
 
 If false, connection policy update will only affect existing PENDING PSC endpoints. Existing ACCEPTED/REJECTED endpoints will remain untouched regardless how the connection policy is modified .
@@ -470,6 +469,12 @@ func resourceComputeServiceAttachmentUpdate(d *schema.ResourceData, meta interfa
 		return err
 	} else if v, ok := d.GetOkExists("consumer_accept_lists"); ok || !reflect.DeepEqual(v, consumerAcceptListsProp) {
 		obj["consumerAcceptLists"] = consumerAcceptListsProp
+	}
+	reconcileConnectionsProp, err := expandComputeServiceAttachmentReconcileConnections(d.Get("reconcile_connections"), d, config)
+	if err != nil {
+		return err
+	} else if v, ok := d.GetOkExists("reconcile_connections"); ok || !reflect.DeepEqual(v, reconcileConnectionsProp) {
+		obj["reconcileConnections"] = reconcileConnectionsProp
 	}
 
 	obj, err = resourceComputeServiceAttachmentUpdateEncoder(d, meta, obj)
