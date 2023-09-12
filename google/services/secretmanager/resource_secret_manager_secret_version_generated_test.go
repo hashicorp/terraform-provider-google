4e75ffbc6ffd47d6d1bd1e58500828332b7d34df
The Magician <magic-modules@google.com>
2023-09-12 13:10:14 -0400

    Added `deletion_policy` field in `google_secret_manager_secret_version` resource (#8922) (#15818)
    * Added deletion_policy field in google_secret_manager_secret_version resource
    
    * added extra line at the end of secret_version custom import
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/secretmanager/resource_secret_manager_secret_version_generated_test.go
+++ google/services/secretmanager/resource_secret_manager_secret_version_generated_test.go
@@ -78,6 +78,102 @@ resource "google_secret_manager_secret_version" "secret-version-basic" {
 `, context)
 }
 
+func TestAccSecretManagerSecretVersion_secretVersionDeletionPolicyAbandonExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckSecretManagerSecretVersionDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecretManagerSecretVersion_secretVersionDeletionPolicyAbandonExample(context),
+			},
+			{
+				ResourceName:            "google_secret_manager_secret_version.secret-version-deletion-policy",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"secret", "deletion_policy"},
+			},
+		},
+	})
+}
+
+func testAccSecretManagerSecretVersion_secretVersionDeletionPolicyAbandonExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "tf-test-secret-version%{random_suffix}"
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-deletion-policy" {
+  secret = google_secret_manager_secret.secret-basic.id
+
+  secret_data = "tf-test-secret-data%{random_suffix}"
+  deletion_policy = "ABANDON"
+}
+`, context)
+}
+
+func TestAccSecretManagerSecretVersion_secretVersionDeletionPolicyDisableExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckSecretManagerSecretVersionDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecretManagerSecretVersion_secretVersionDeletionPolicyDisableExample(context),
+			},
+			{
+				ResourceName:            "google_secret_manager_secret_version.secret-version-deletion-policy",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"secret", "deletion_policy"},
+			},
+		},
+	})
+}
+
+func testAccSecretManagerSecretVersion_secretVersionDeletionPolicyDisableExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "tf-test-secret-version%{random_suffix}"
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-deletion-policy" {
+  secret = google_secret_manager_secret.secret-basic.id
+
+  secret_data = "tf-test-secret-data%{random_suffix}"
+  deletion_policy = "DISABLE"
+}
+`, context)
+}
+
 func testAccCheckSecretManagerSecretVersionDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
