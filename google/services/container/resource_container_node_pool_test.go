afae6045246e637ac59e47b57fe6f2a3c276d6d5
The Magician <magic-modules@google.com>
2023-10-03 15:54:18 -0400

    feat(container_node_pool): support cgroup mode (#8997) (#16103)
    [upstream:8fa653aed5646ee55483612fa4d1a0706eb326bb]
    
    Signed-off-by: Tsubasa Nagasawa <toversus2357@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/container/resource_container_node_pool_test.go
+++ google/services/container/resource_container_node_pool_test.go
@@ -441,6 +441,38 @@ func TestAccContainerNodePool_withLinuxNodeConfig(t *testing.T) {
 	})
 }
 
+func TestAccContainerNodePool_withCgroupMode(t *testing.T) {
+	t.Parallel()
+
+	cluster := fmt.Sprintf("tf-test-cluster-%s", acctest.RandString(t, 10))
+	np := fmt.Sprintf("tf-test-np-%s", acctest.RandString(t, 10))
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckContainerClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContainerNodePool_withCgroupMode(cluster, np, "CGROUP_MODE_V2"),
+			},
+			{
+				ResourceName:      "google_container_node_pool.np",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Perform an update.
+			{
+				Config: testAccContainerNodePool_withCgroupMode(cluster, np, "CGROUP_MODE_UNSPECIFIED"),
+			},
+			{
+				ResourceName:      "google_container_node_pool.np",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccContainerNodePool_withNetworkConfig(t *testing.T) {
 	t.Parallel()
 
@@ -2450,6 +2482,39 @@ resource "google_container_node_pool" "with_linux_node_config" {
   }
 }
 `, cluster, np, linuxNodeConfig)
+}
+
+func testAccContainerNodePool_withCgroupMode(cluster, np string, mode string) string {
+	return fmt.Sprintf(`
+data "google_container_engine_versions" "central1a" {
+  location = "us-central1-a"
+}
+
+resource "google_container_cluster" "cluster" {
+  name                = "%s"
+  location            = "us-central1-a"
+  initial_node_count  = 1
+  min_master_version  = data.google_container_engine_versions.central1a.latest_master_version
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "np" {
+  name               = "%s"
+  location           = "us-central1-a"
+  cluster            = google_container_cluster.cluster.name
+  initial_node_count = 1
+  node_config {
+    image_type = "COS_CONTAINERD"
+    linux_node_config {
+      cgroup_mode = "%s"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+`, cluster, np, mode)
 }
 
 func testAccContainerNodePool_withNetworkConfig(cluster, np, network string) string {
