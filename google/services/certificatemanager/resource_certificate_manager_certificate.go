98d866a4074f396a355781b1fc25731258460620
The Magician <magic-modules@google.com>
2023-10-03 09:23:02 -0400

    Add more examples for CertificateManagerCertificate resource (#9103) (#16094)
    * Update the documentation of scope field and Add more examples with scope=ALL_REGIONS
    
    * Remove trailing spaces
    
    ---------
    
    
    [upstream:847abdab32628b1d2e62a8cb50e43adf769ab537]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/certificatemanager/resource_certificate_manager_certificate.go
+++ google/services/certificatemanager/resource_certificate_manager_certificate.go
@@ -211,11 +211,11 @@ Not guaranteed to be stable. For programmatic access use 'reason' field.`,
 DEFAULT: Certificates with default scope are served from core Google data centers.
 If unsure, choose this option.
 
-EDGE_CACHE: Certificates with scope EDGE_CACHE are special-purposed certificates,
-served from non-core Google data centers.
+EDGE_CACHE: Certificates with scope EDGE_CACHE are special-purposed certificates, served from Edge Points of Presence.
+See https://cloud.google.com/vpc/docs/edge-locations.
 
 ALL_REGIONS: Certificates with ALL_REGIONS scope are served from all GCP regions (You can only use ALL_REGIONS with global certs).
-see https://cloud.google.com/compute/docs/regions-zones`,
+See https://cloud.google.com/compute/docs/regions-zones`,
 				Default: "DEFAULT",
 			},
 			"self_managed": {
