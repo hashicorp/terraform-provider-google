e45753d0d7bcadf73d21f24d7f218d68457ea35e
Modular Magician <magic-modules@google.com>
2023-12-01 22:21:34 +0000

    Add Logging Settings datasources. (#9526)
    Co-authored-by: Riley Karson <rileykarson@google.com>
    [upstream:1ea0673f5cc562fca1d3517ffc998af062d68442]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/logging/data_source_google_logging_folder_settings_test.go
@@ -0,0 +1,48 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package logging_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccLoggingFolderSettings_datasource(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"folder_name": "tf-test-" + acctest.RandString(t, 10),
+		"org_id":      envvar.GetTestOrgFromEnv(t),
+	}
+	resourceName := "data.google_logging_folder_settings.settings"
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLoggingFolderSettings_datasource(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "kms_service_account_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "logging_service_account_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccLoggingFolderSettings_datasource(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_folder" "default" {
+	display_name = "%{folder_name}"
+	parent       = "organizations/%{org_id}"
+}
+
+data "google_logging_folder_settings" "settings" {
+	folder = google_folder.default.folder_id
+}
+`, context)
+}
---
+++ google/services/logging/data_source_google_logging_organization_settings_test.go
@@ -0,0 +1,42 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package logging_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccLoggingOrganizationSettings_datasource(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id": envvar.GetTestOrgFromEnv(t),
+	}
+	resourceName := "data.google_logging_organization_settings.settings"
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLoggingOrganizationSettings_datasource(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "kms_service_account_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "logging_service_account_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccLoggingOrganizationSettings_datasource(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_logging_organization_settings" "settings" {
+	organization = "%{org_id}"
+}
+`, context)
+}
