1447331528b83c8df45642d3e7be8e1ed6694f60
The Magician <magic-modules@google.com>
2023-12-01 11:37:23 -0800

    Remove usage of resource_definition (#9553) (#16656)
    * Remove usage of resource_definition
    
    * Update mmv1/products/identityplatform/ProjectDefaultConfig.yaml
    
    
    
    ---------
    
    
    [upstream:30d3f116bb169f721ac0b58e772d07c5bce289f0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/identityplatform/resource_identity_platform_project_default_config.go
+++ google/services/identityplatform/resource_identity_platform_project_default_config.go
@@ -52,9 +52,7 @@ func ResourceIdentityPlatformProjectDefaultConfig() *schema.Resource {
 			tpgresource.DefaultProviderProject,
 		),
 
-		DeprecationMessage: "Deprecated. Use the `google_identity_platform_config` resource instead. " +
-			"It contains a more comprehensive list of fields, and was created before " +
-			"`google_identity_platform_project_default_config` was added.",
+		DeprecationMessage: "`google_identity_platform_config` is deprecated and will be removed in the next major release of the provider. Use the `google_identity_platform_config` resource instead. It contains a more comprehensive list of fields, and was created before `google_identity_platform_project_default_config` was added.",
 
 		Schema: map[string]*schema.Schema{
 			"sign_in": {
