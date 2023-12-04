aa8c2b410b81ca0dfe556050a2603e0a374db4d2
Modular Magician <magic-modules@google.com>
2023-12-04 21:53:15 +0000

    Cloud Run v2 limits examples and docs (#9563)
    * Cloud Run v2 limits examples and docs
    
    Improve documentation for limits and provide examples how they can be
    used in Cloud Run Service and Job.
    
    * Fix limits declaration
    
    * Remove primary_resource_name
    [upstream:3005b4c8c20569581358b99ab7b77aefe485fcc3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/cloudrunv2/resource_cloud_run_v2_service.go
+++ google/services/cloudrunv2/resource_cloud_run_v2_service.go
@@ -317,7 +317,7 @@ If omitted, a port number will be chosen and passed to the container through the
 													Type:        schema.TypeMap,
 													Computed:    true,
 													Optional:    true,
-													Description: `Only memory and CPU are supported. Note: The only supported values for CPU are '1', '2', '4', and '8'. Setting 4 CPU requires at least 2Gi of memory. The values of the map is string form of the 'quantity' k8s type: https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go`,
+													Description: `Only memory and CPU are supported. Use key 'cpu' for CPU limit and 'memory' for memory limit. Note: The only supported values for CPU are '1', '2', '4', and '8'. Setting 4 CPU requires at least 2Gi of memory. The values of the map is string form of the 'quantity' k8s type: https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go`,
 													Elem:        &schema.Schema{Type: schema.TypeString},
 												},
 												"startup_cpu_boost": {
