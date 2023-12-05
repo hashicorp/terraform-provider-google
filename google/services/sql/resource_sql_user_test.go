0ecdc7fd5df85d870f0c57bb290660763da7c477
The Magician <magic-modules@google.com>
2023-12-05 11:54:05 -0800

    add support for IAM Group authentication to google_sql_user (#9578) (#16681)
    [upstream:05c4410c0e599f33ab255e3820187855c82c7739]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/sql/resource_sql_user_test.go
+++ google/services/sql/resource_sql_user_test.go
@@ -28,6 +28,7 @@ func TestAccSqlUser_mysql(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user1"),
 					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user2"),
+					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user3"),
 				),
 			},
 			{
@@ -36,6 +37,7 @@ func TestAccSqlUser_mysql(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user1"),
 					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user2"),
+					testAccCheckGoogleSqlUserExists(t, "google_sql_user.user3"),
 				),
 			},
 			{
@@ -313,6 +315,15 @@ resource "google_sql_user" "user2" {
   instance = google_sql_database_instance.instance.name
   host     = "gmail.com"
   password = "hunter2"
+  type = "CLOUD_IAM_USER"
+}
+
+resource "google_sql_user" "user3" {
+  name     = "admin"
+  instance = google_sql_database_instance.instance.name
+  host     = "gmail.com"
+  password = "hunter3"
+  type = "CLOUD_IAM_GROUP"
 }
 `, instance, password)
 }
