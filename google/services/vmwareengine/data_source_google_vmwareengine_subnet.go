d169447a091a0315a26f6183edfffe6f8d74a770
Modular Magician <magic-modules@google.com>
2023-12-06 19:18:43 +0000

    Add Subnet resource to Vmwareengine (#9560) [upstream:3547ac8d589d09f0b66f4c1fe3e1dfd9783c87d0]
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/vmwareengine/data_source_google_vmwareengine_subnet.go
@@ -0,0 +1,40 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package vmwareengine
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceVmwareengineSubnet() *schema.Resource {
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceVmwareengineSubnet().Schema)
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "parent", "name")
+	return &schema.Resource{
+		Read:   dataSourceVmwareengineSubnetRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceVmwareengineSubnetRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+
+	// Store the ID now
+	id, err := tpgresource.ReplaceVars(d, config, "{{parent}}/subnets/{{name}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+	err = resourceVmwareengineSubnetRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	if d.Id() == "" {
+		return fmt.Errorf("%s not found", id)
+	}
+	return nil
+}
