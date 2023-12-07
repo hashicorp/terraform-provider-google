6b31b8cfb17f42718d3877c05c80b6eba69965bf
The Magician <magic-modules@google.com>
2023-12-07 13:29:57 -0800

    Add data source for google_compute_region_disk (#9421) (#16732)
    [upstream:65322ce7395a0a1e39dcfe357dae47c90b954c5a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/compute/data_source_google_compute_region_disk_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package compute_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccDataSourceGoogleComputeRegionDisk_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeDiskDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleComputeRegionDisk_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckDataSourceStateMatchesResourceState("data.google_compute_region_disk.foo", "google_compute_region_disk.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleComputeRegionDisk_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_region_disk" "foo" {
+  name          = "tf-test-compute-disk-%{random_suffix}"
+  type          = "pd-standard"
+  replica_zones = ["us-central1-a", "us-central1-f"]
+
+  labels = {
+    my-label = "my-label-value"
+  }
+}
+
+data "google_compute_region_disk" "foo" {
+  name     = google_compute_region_disk.foo.name
+  project  = google_compute_region_disk.foo.project
+}
+`, context)
+}
