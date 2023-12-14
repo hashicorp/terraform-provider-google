ef26f20191b8b32d9ec22cf7f26e5c1c44878d78
The Magician <magic-modules@google.com>
2023-12-14 12:09:28 -0800

    Revert "The version field should be under configmanagement instead of under oci" (#9645) (#16802)
    [upstream:617cca41c65236cbbb39730359cdcda653fde17a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/gkehub2/resource_gke_hub_feature_test.go
+++ google/services/gkehub2/resource_gke_hub_feature_test.go
@@ -402,7 +402,6 @@ resource "google_gke_hub_feature" "feature" {
   location = "global"
   fleet_default_member_config {
     configmanagement {
-      version = "1.16.0"
       config_sync {
         source_format = "hierarchy"
         git {
@@ -429,7 +428,6 @@ resource "google_gke_hub_feature" "feature" {
   location = "global"
   fleet_default_member_config {
     configmanagement {
-      version = "1.16.1"
       config_sync {
         source_format = "unstructured"
         oci {
