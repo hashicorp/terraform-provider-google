1b947cd455fa78ea3ee6f0e83be6884b69318021
Modular Magician <magic-modules@google.com>
2023-12-18 13:47:08 +0000

    Add Endpoint Attachment support for Integration Connectors (#9616)
    * Add EndpointAttachment support for integrationconnectors
    
    * Update mmv1/third_party/terraform/services/integrationconnectors/resource_integration_connectors_endpoint_attachment_test.go
    
    * Update mmv1/third_party/terraform/services/integrationconnectors/resource_integration_connectors_endpoint_attachment_test.go
    
    ---------
    
    Co-authored-by: Sarah French <15078782+SarahFrench@users.noreply.github.com>
    [upstream:d1c644eba58f37e495a6fb95bdb6566ea26e02d4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/integrationconnectors/resource_integration_connectors_endpoint_attachment_test.go
@@ -0,0 +1,81 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package integrationconnectors_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccIntegrationConnectorsEndpointAttachment_integrationConnectorsEndpointAttachmentExample_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckIntegrationConnectorsEndpointAttachmentDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIntegrationConnectorsEndpointAttachment_integrationConnectorsEndpointAttachmentExample_full(context),
+			},
+			{
+				ResourceName:            "google_integration_connectors_endpoint_attachment.sampleendpointattachment",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
+			},
+			{
+				Config: testAccIntegrationConnectorsEndpointAttachment_integrationConnectorsEndpointAttachmentExample_update(context),
+			},
+			{
+				ResourceName:            "google_integration_connectors_endpoint_attachment.sampleendpointattachment",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccIntegrationConnectorsEndpointAttachment_integrationConnectorsEndpointAttachmentExample_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "test_project" {
+}
+
+resource "google_integration_connectors_endpoint_attachment" "sampleendpointattachment" {
+  name     = "tf-test-test-endpoint-attachment%{random_suffix}"
+  location = "us-central1"
+  description = "tf created description"
+  # Third party dependency, see https://github.com/GoogleCloudPlatform/magic-modules/pull/9616#discussion_r1429029155
+  service_attachment = "projects/connectors-example/regions/us-central1/serviceAttachments/test"
+  labels = {
+    foo = "bar"
+  }
+}
+`, context)
+}
+
+func testAccIntegrationConnectorsEndpointAttachment_integrationConnectorsEndpointAttachmentExample_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "test_project" {
+}
+
+resource "google_integration_connectors_endpoint_attachment" "sampleendpointattachment" {
+  name     = "tf-test-test-endpoint-attachment%{random_suffix}"
+  location = "us-central1"
+  description = "tf updated description"
+  # Third party dependency, see https://github.com/GoogleCloudPlatform/magic-modules/pull/9616#discussion_r1429029155
+  service_attachment = "projects/connectors-example/regions/us-central1/serviceAttachments/test"
+  labels = {
+    bar = "foo"
+  }
+}
+`, context)
+}
