88d096a82f391fe8b633a6ce6c7b475d96dfa202
The Magician <magic-modules@google.com>
2023-12-21 16:31:47 -0800

    Add `google_compute_reservation` datasource (#9339) (#16860)
    * WIP: initial data source go file
    
    * WIP: initial data source creation
    
    * WIP: error shows project is required even though its marked as optional
    
    * WIP: some attributes are not working as expected
    
    * use read function from resource for data source
    
    * fix reservationparsingid test
    
    * add data source to provider_mmv1_resources handwritten list
    
    * remove ParseComputeReservationId function
    
    * remove testAccCheckDataSourceComputeReservationDestroy
    
    * add google_compute_reservation data source docs
    
    * lint fix
    
    * Update mmv1/third_party/terraform/website/docs/d/compute_reservation.html.markdown
    
    
    
    * simplify data source test
    
    * add ReservationIdParsing Test
    
    * remove parsing Test
    
    ---------
    
    
    [upstream:8a8ffc3384a59340f47efe97f18611b6672da9bd]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/compute/data_source_google_compute_reservation_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package compute_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccDataSourceComputeReservation(t *testing.T) {
+	t.Parallel()
+
+	reservationName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+
+	rsName := "foobar"
+	dsName := "my_reservation"
+	rsFullName := fmt.Sprintf("google_compute_reservation.%s", rsName)
+	dsFullName := fmt.Sprintf("data.google_compute_reservation.%s", dsName)
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeReservationDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceComputeReservationConfig(reservationName, rsName, dsName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsFullName, "status", "READY"),
+					acctest.CheckDataSourceStateMatchesResourceState(dsFullName, rsFullName),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceComputeReservationConfig(reservationName, rsName, dsName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_reservation" "%s" {
+  name = "%s"
+  zone = "us-west1-a"
+
+  specific_reservation {
+    count = 1
+    instance_properties {
+      min_cpu_platform = "Intel Cascade Lake"
+      machine_type     = "n2-standard-2"
+    }
+  }
+}
+
+data "google_compute_reservation" "%s" {
+  name = google_compute_reservation.%s.name
+  zone = "us-west1-a"
+}
+`, rsName, reservationName, dsName, rsName)
+}
