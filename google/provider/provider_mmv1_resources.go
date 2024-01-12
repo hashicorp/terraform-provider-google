9216f42ccc2cd9601ca2b13e173b25b538ae19cf
Modular Magician <magic-modules@google.com>
2024-01-12 19:28:51 +0000

    Added volume module for Google Cloud NetApp Volume (#9513)
    * added volume module
    
    * Added updated function and network fix
    
    * updated update test fields.
    
    * updated unixPermissions field.
    
    * updated test file for snapshot_policy
    
    * updated description.
    updated test file with basic example and function name.
    
    * removed the *_UNSPECIFIED values for inputs
    Default values for minutes, hours, days and day_of_week in snapshot block
    Updated tests to incorporate suggestions by shuya
    Fixes some indentation in tests
    Updates some test to get better test coverage
    commented restoreParamters as suggested by Shuya.
    
    * updated tests to use us-west1 region
    
    * updated region to us-west2
    [upstream:bd6b1b34069c41459496f838c32a8014af9a8dc2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -377,9 +377,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 365
+// Generated resources: 366
 // Generated IAM resources: 219
-// Total generated resources: 584
+// Total generated resources: 585
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                         accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                   accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -816,6 +816,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_monitoring_custom_service":                               monitoring.ResourceMonitoringService(),
 	"google_monitoring_slo":                                          monitoring.ResourceMonitoringSlo(),
 	"google_monitoring_uptime_check_config":                          monitoring.ResourceMonitoringUptimeCheckConfig(),
+	"google_netapp_volume":                                           netapp.ResourceNetappVolume(),
 	"google_netapp_active_directory":                                 netapp.ResourceNetappactiveDirectory(),
 	"google_netapp_backup_policy":                                    netapp.ResourceNetappbackupPolicy(),
 	"google_netapp_backup_vault":                                     netapp.ResourceNetappbackupVault(),
