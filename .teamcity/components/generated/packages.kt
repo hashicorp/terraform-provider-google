dda05f0c4801ab05ece3b11b638a53c9808abefa
Modular Magician <magic-modules@google.com>
2024-01-23 19:42:30 +0000

    Update membership.go (#9865) [upstream:dc046ff25dfdf69089bba43397176642e2f5449f]
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- .teamcity/components/generated/packages.kt
+++ .teamcity/components/generated/packages.kt
@@ -12,11 +12,6 @@ var packages = mapOf(
         "displayName" to "Environment Variables",
         "path" to "./google/envvar"
     ),
-    "fwmodels" to mapOf(
-        "name" to "fwmodels",
-        "displayName" to "Framework Models",
-        "path" to "./google/fwmodels"
-    ),
     "fwprovider" to mapOf(
         "name" to "fwprovider",
         "displayName" to "Framework Provider",
