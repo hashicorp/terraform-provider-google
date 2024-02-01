80678ee623a3c68054ab4f3e0f8f86c1d6ffa030
Modular Magician <magic-modules@google.com>
2024-02-01 15:55:29 +0000

    Ignore `update_time` when testing import of `google_vmwareengine_network_policy` (#9903)
    [upstream:423ac5e99839c0926bb0f6ff0f00194d62b2c3e1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/vmwareengine/resource_vmwareengine_network_policy_test.go
+++ google/services/vmwareengine/resource_vmwareengine_network_policy_test.go
@@ -36,7 +36,7 @@ func TestAccVmwareengineNetworkPolicy_update(t *testing.T) {
 				ResourceName:            "google_vmwareengine_network_policy.vmw-engine-network-policy",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "name"},
+				ImportStateVerifyIgnore: []string{"location", "name", "update_time"},
 			},
 			{
 				Config: testAccVmwareengineNetworkPolicy_config(context, "description2", "192.168.1.0/26", true, true),
@@ -45,7 +45,7 @@ func TestAccVmwareengineNetworkPolicy_update(t *testing.T) {
 				ResourceName:            "google_vmwareengine_network_policy.vmw-engine-network-policy",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "name"},
+				ImportStateVerifyIgnore: []string{"location", "name", "update_time"},
 			},
 		},
 	})
