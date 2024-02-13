2cb3a85a381cc843ca60f4878f52d4dff01d922e
The Magician <magic-modules@google.com>
2024-02-13 09:23:39 -0800

    Add headers to address https://github.com/hashicorp/terraform-provider-google/pull/17248 (#9975) (#17261)
    [upstream:1f61383b6c6aa451ae7237e96d099f0de41db214]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- .teamcity/components/projects/reused/vcr_recording.kt
+++ .teamcity/components/projects/reused/vcr_recording.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // This file is controlled by MMv1, any changes made here will be overwritten
 
 package projects.reused
