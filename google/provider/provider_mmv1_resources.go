88b4838071c56389504d311ce53da54292cd9e01
The Magician <magic-modules@google.com>
2024-03-07 08:23:13 -0800

    Create support for KMS Ekmconnection resource (#10094) (#17512)
    [upstream:5c5b415c6c20bb760b511e8589d270dcc3728a50]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -389,9 +389,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 388
+// Generated resources: 389
 // Generated IAM resources: 231
-// Total generated resources: 619
+// Total generated resources: 620
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                         accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                   accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -832,6 +832,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_integration_connectors_endpoint_attachment":              integrationconnectors.ResourceIntegrationConnectorsEndpointAttachment(),
 	"google_kms_crypto_key":                                          kms.ResourceKMSCryptoKey(),
 	"google_kms_crypto_key_version":                                  kms.ResourceKMSCryptoKeyVersion(),
+	"google_kms_ekm_connection":                                      kms.ResourceKMSEkmConnection(),
 	"google_kms_key_ring":                                            kms.ResourceKMSKeyRing(),
 	"google_kms_key_ring_import_job":                                 kms.ResourceKMSKeyRingImportJob(),
 	"google_kms_secret_ciphertext":                                   kms.ResourceKMSSecretCiphertext(),
