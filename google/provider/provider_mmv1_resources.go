0bfb32c9cef3a8bb4ca0c3df5d1977309b5b0418
Modular Magician <magic-modules@google.com>
2024-03-13 01:55:30 +0000

    Add new resource Service for Apphub (#10132)
    * Add new resource for Service Project for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Add new resource for Service Project for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Add new resource for Service Project Attachment for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Add new resource for Service Project Attachment for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Add new resource for Service Project Attachment for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Add new resource for Application for Apphub
    
    * Add new resource for Application for Apphub
    
    * Enable Apphub API in test cases
    
    * Add new resource for Service Project Attachment for Apphub
    
    FIXES https://github.com/hashicorp/terraform-provider-google/issues/17405
    
    * Enable apphub API in test cases
    
    * Fix precheck error
    
    * Fix precheck error
    
    * Fix precheck error
    
    * Fix precheck error
    
    * Resolve comments
    
    * Fix lint error
    
    * Fix errors
    
    * Add new resource for Service for Apphub
    
    * Fix examples
    
    * Add test
    
    * Fix tests
    
    * Add IAM permissions
    
    * Add IAM permission
    
    * Fix IAM issues
    
    * Remove merged changes
    
    * Add billing account
    
    * Remove extra delay from tests
    
    * Add handwritten update test
    
    * Remove merged changes
    
    * Make changes to yaml file incorporating comments from https://github.com/GoogleCloudPlatform/magic-modules/pull/10155
    
    * Fix lint errors in Service YAML definition
    
    * Add a delay between discovered resource fetch and forwarding rule creation
    
    * Add a delay between discovered resource fetch and forwarding rule creation
    
    * Add a delay between discovered resource fetch and forwarding rule creation
    
    * Batch update tests to improve speed
    
    * Use standard diff suppress function
    
    * Update region from us-east1 to us-central1
    
    ---------
    
    Co-authored-by: Krishnan Gopal <krishgopal@google.com>
    Co-authored-by: praseedhaPK <161299686+praseedhaPK@users.noreply.github.com>
    
    [upstream:4fa7f7c5c43d93b9813acd33ecf232f6cd6151ed]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -392,9 +392,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 393
+// Generated resources: 394
 // Generated IAM resources: 234
-// Total generated resources: 627
+// Total generated resources: 628
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                         accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                   accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -446,6 +446,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_app_engine_service_split_traffic":                        appengine.ResourceAppEngineServiceSplitTraffic(),
 	"google_app_engine_standard_app_version":                         appengine.ResourceAppEngineStandardAppVersion(),
 	"google_apphub_application":                                      apphub.ResourceApphubApplication(),
+	"google_apphub_service":                                          apphub.ResourceApphubService(),
 	"google_apphub_service_project_attachment":                       apphub.ResourceApphubServiceProjectAttachment(),
 	"google_apphub_workload":                                         apphub.ResourceApphubWorkload(),
 	"google_artifact_registry_repository":                            artifactregistry.ResourceArtifactRegistryRepository(),
