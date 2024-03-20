70df48baba481067df94b868c202063808196b41
The Magician <magic-modules@google.com>
2024-03-20 12:51:41 -0700

    Fix https://github.com/hashicorp/terraform-provider-google/issues/17389 (#10206) (#17638)
    [upstream:18fd6b4edb626b23e7ee70e761716bd152a15099]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/firebaseappcheck/resource_firebase_app_check_service_config_test.go
+++ google/services/firebaseappcheck/resource_firebase_app_check_service_config_test.go
@@ -1,57 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
 package firebaseappcheck_test
-
-import (
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
-	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/envvar"
-)
-
-func TestAccFirebaseAppCheckServiceConfig_firebaseAppCheckServiceConfigUpdate(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"project_id":    envvar.GetTestProjectFromEnv(),
-		"service_id":    "identitytoolkit.googleapis.com",
-		"random_suffix": acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckFirebaseAppCheckServiceConfigDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccFirebaseAppCheckServiceConfig_firebaseAppCheckServiceConfigUnenforcedExample(context),
-			},
-			{
-				ResourceName:            "google_firebase_app_check_service_config.default",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"service_id"},
-			},
-			{
-				Config: testAccFirebaseAppCheckServiceConfig_firebaseAppCheckServiceConfigOffExample(context),
-			},
-			{
-				ResourceName:            "google_firebase_app_check_service_config.default",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"service_id"},
-			},
-			{
-				Config: testAccFirebaseAppCheckServiceConfig_firebaseAppCheckServiceConfigEnforcedExample(context),
-			},
-			{
-				ResourceName:            "google_firebase_app_check_service_config.default",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"service_id"},
-			},
-		},
-	})
-}
