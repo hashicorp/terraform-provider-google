3f47f2bd612cf0db4cf5355ec4f508a3c06f6131
The Magician <magic-modules@google.com>
2024-05-08 15:45:34 -0700

    Add KMS support for the google_bigquery_connection resource (#10561) (#18057)
    [upstream:807e2f6c28607f989af9741458844a41a4ae8ffa]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigqueryconnection/resource_bigquery_connection_generated_test.go
+++ google/services/bigqueryconnection/resource_bigquery_connection_generated_test.go
@@ -468,6 +468,76 @@ resource "google_dataproc_cluster" "basic" {
 `, context)
 }
 
+func TestAccBigqueryConnectionConnection_bigqueryConnectionKmsExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"deletion_protection": false,
+		"kms_key_name":        acctest.BootstrapKMSKey(t).CryptoKey.Name,
+		"policyChanged":       acctest.BootstrapPSARole(t, "bq-", "bigquery-encryption", "roles/cloudkms.cryptoKeyEncrypterDecrypter"),
+		"random_suffix":       acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBigqueryConnectionConnectionDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigqueryConnectionConnection_bigqueryConnectionKmsExample(context),
+			},
+			{
+				ResourceName:            "google_bigquery_connection.bq-connection-cmek",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "cloud_sql.0.credential"},
+			},
+		},
+	})
+}
+
+func testAccBigqueryConnectionConnection_bigqueryConnectionKmsExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_sql_database_instance" "instance" {
+    name             = "tf-test-my-database-instance%{random_suffix}"
+    database_version = "POSTGRES_11"
+    region           = "us-central1"
+    settings {
+		tier = "db-f1-micro"
+	}
+
+    deletion_protection  = "%{deletion_protection}"
+}
+
+resource "google_sql_database" "db" {
+    instance = google_sql_database_instance.instance.name
+    name     = "db"
+}
+
+resource "google_sql_user" "user" {
+    name = "user%{random_suffix}"
+    instance = google_sql_database_instance.instance.name
+    password = "tf-test-my-password%{random_suffix}"
+}
+
+resource "google_bigquery_connection" "bq-connection-cmek" {
+    friendly_name = "👋"
+    description   = "a riveting description"
+    location      = "US"
+    kms_key_name  = "%{kms_key_name}"
+    cloud_sql {
+        instance_id = google_sql_database_instance.instance.connection_name
+        database    = google_sql_database.db.name
+        type        = "POSTGRES"
+        credential {
+          username = google_sql_user.user.name
+          password = google_sql_user.user.password
+        }
+    }
+}
+`, context)
+}
+
 func testAccCheckBigqueryConnectionConnectionDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
