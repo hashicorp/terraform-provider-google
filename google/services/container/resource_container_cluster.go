9eee696b07043eba1e1ad5234b2e2e9ea67d4f20
Modular Magician <magic-modules@google.com>
2024-05-10 17:21:21 +0000

    Add KUBELET and CADVISOR to monitoring components (#10605)
    [upstream:7159bf76c03f3abded8061e9731b9ac166c5606a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/container/resource_container_cluster.go
+++ google/services/container/resource_container_cluster.go
@@ -1047,7 +1047,7 @@ func ResourceContainerCluster() *schema.Resource {
 							Type:        schema.TypeList,
 							Optional:    true,
 							Computed:    true,
-							Description: `GKE components exposing metrics. Valid values include SYSTEM_COMPONENTS, APISERVER, SCHEDULER, CONTROLLER_MANAGER, STORAGE, HPA, POD, DAEMONSET, DEPLOYMENT and STATEFULSET.`,
+							Description: `GKE components exposing metrics. Valid values include SYSTEM_COMPONENTS, APISERVER, SCHEDULER, CONTROLLER_MANAGER, STORAGE, HPA, POD, DAEMONSET, DEPLOYMENT, STATEFULSET, KUBELET and CADVISOR.`,
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
