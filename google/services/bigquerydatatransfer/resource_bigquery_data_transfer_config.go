def85a5132bb4558ab0708fbd372dbe8ef5d0327
The Magician <magic-modules@google.com>
2024-05-22 08:03:52 -0700

    Update documentation for `google_bigquery_data_transfer_config` (#10766) (#18218)
    [upstream:61e3a741830e4cd37731697c3373cea42f3f05d7]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigquerydatatransfer/resource_bigquery_data_transfer_config.go
+++ google/services/bigquerydatatransfer/resource_bigquery_data_transfer_config.go
@@ -199,7 +199,8 @@ of valid format: 1st,3rd monday of month 15:30, every wed,fri of jan,
 jun 13:15, and first sunday of quarter 00:00. See more explanation
 about the format here:
 https://cloud.google.com/appengine/docs/flexible/python/scheduling-jobs-with-cron-yaml#the_schedule_format
-NOTE: the granularity should be at least 8 hours, or less frequent.`,
+NOTE: The minimum interval time between recurring transfers depends
+on the data source; refer to the documentation for your data source.`,
 			},
 			"schedule_options": {
 				Type:        schema.TypeList,
