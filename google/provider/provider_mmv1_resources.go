b0671995d707dbfc3bd1a17aa41d926da634175e
Modular Magician <magic-modules@google.com>
2024-06-07 22:35:39 +0000

    Add Resource ProjectSecurityHealthAnalyticsCustomModule (#10863)
    [upstream:49e75a0f48d5b9baf0138cbc97cec6ad5f9651c3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -413,9 +413,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 423
+// Generated resources: 424
 // Generated IAM resources: 249
-// Total generated resources: 672
+// Total generated resources: 673
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                  accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                            accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1005,6 +1005,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_scc_source_iam_policy":                                            tpgiamresource.ResourceIamPolicy(securitycenter.SecurityCenterSourceIamSchema, securitycenter.SecurityCenterSourceIamUpdaterProducer, securitycenter.SecurityCenterSourceIdParseFunc),
 	"google_scc_management_folder_security_health_analytics_custom_module":    securitycentermanagement.ResourceSecurityCenterManagementFolderSecurityHealthAnalyticsCustomModule(),
 	"google_scc_management_organization_event_threat_detection_custom_module": securitycentermanagement.ResourceSecurityCenterManagementOrganizationEventThreatDetectionCustomModule(),
+	"google_scc_management_project_security_health_analytics_custom_module":   securitycentermanagement.ResourceSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule(),
 	"google_securityposture_posture":                                          securityposture.ResourceSecurityposturePosture(),
 	"google_securityposture_posture_deployment":                               securityposture.ResourceSecurityposturePostureDeployment(),
 	"google_endpoints_service_iam_binding":                                    tpgiamresource.ResourceIamBinding(servicemanagement.ServiceManagementServiceIamSchema, servicemanagement.ServiceManagementServiceIamUpdaterProducer, servicemanagement.ServiceManagementServiceIdParseFunc),
