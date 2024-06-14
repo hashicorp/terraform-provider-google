a6595c567e826e25a34195a46ecf51094e03f628
BBBmau <alvarez.mauriciotm@gmail.com>
2024-06-14 12:24:56 -0700

    add endline

--- .teamcity/components/builds/build_triggers.kt
+++ .teamcity/components/builds/build_triggers.kt
@@ -27,7 +27,7 @@ fun Triggers.runNightly(config: NightlyTriggerConfiguration) {
 
     schedule{
         enabled = config.nightlyTestsEnabled
-        branchFilter = "+:UTC-*,-:UTC-nightly-*"
+        branchFilter = "+:UTC-*\n-:UTC-nightly-*"
         triggerBuild = always() // Run build even if no new commits/pending changes
         withPendingChangesOnly = false
         enforceCleanCheckout = true
