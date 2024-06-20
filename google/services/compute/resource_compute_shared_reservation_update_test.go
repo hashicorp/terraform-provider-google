f53ee08702982c33b79f7eccb666c50c556786a1
The Magician <magic-modules@google.com>
2024-06-20 10:37:20 -0700

    Fix resource_compute_shared_reservation_update encoder (#11007) (#18498)
    [upstream:12c48c9980c5001d2cae723de54485a324bd990c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_shared_reservation_update_test.go
+++ google/services/compute/resource_compute_shared_reservation_update_test.go
@@ -11,7 +11,6 @@ import (
 )
 
 func TestAccComputeSharedReservation_update(t *testing.T) {
-	acctest.SkipIfVcr(t) // large number of parallel resources.
 	t.Parallel()
 
 	context := map[string]interface{}{
