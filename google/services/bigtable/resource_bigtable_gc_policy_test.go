27fc8ec92ea3ca0fdce300abc506663638733927
The Magician <magic-modules@google.com>
2024-06-20 09:04:48 -0700

    Bigtable: Adds ignore_warning to gc policy resource (#10982) (#18492)
    [upstream:0580617eab696140a6af28763ac2575db33dca9a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigtable/resource_bigtable_gc_policy_test.go
+++ google/services/bigtable/resource_bigtable_gc_policy_test.go
@@ -41,6 +41,43 @@ func TestAccBigtableGCPolicy_basic(t *testing.T) {
 	})
 }
 
+func TestAccBigtableGCPolicy_ignoreWarnings(t *testing.T) {
+	// bigtable instance does not use the shared HTTP client, this test creates an instance
+	acctest.SkipIfVcr(t)
+	t.Parallel()
+
+	instanceName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	tableName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	familyName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	cluster1Name := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	cluster2Name := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+
+	gcRulesOriginal := `{"rules":[{"max_age":"10h"}]}`
+	gcRulesNew := `{"rules":[{"max_age":"12h"}]}`
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBigtableGCPolicyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigtableGCPolicyIgnoreWarning(instanceName, tableName, familyName, cluster1Name, cluster2Name, gcRulesOriginal, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccBigtableGCPolicyExists(t, "google_bigtable_gc_policy.policy", true),
+					resource.TestCheckResourceAttr("google_bigtable_gc_policy.policy", "gc_rules", gcRulesOriginal),
+				),
+			},
+			{
+				Config: testAccBigtableGCPolicyIgnoreWarning(instanceName, tableName, familyName, cluster1Name, cluster2Name, gcRulesNew, true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccBigtableGCPolicyExists(t, "google_bigtable_gc_policy.policy", true),
+					resource.TestCheckResourceAttr("google_bigtable_gc_policy.policy", "gc_rules", gcRulesNew),
+				),
+			},
+		},
+	})
+}
+
 func TestAccBigtableGCPolicy_abandoned(t *testing.T) {
 	// bigtable instance does not use the shared HTTP client, this test creates an instance
 	acctest.SkipIfVcr(t)
@@ -563,6 +600,49 @@ resource "google_bigtable_gc_policy" "policy" {
   }
 }
 `, instanceName, instanceName, tableName, family, family)
+}
+
+func testAccBigtableGCPolicyIgnoreWarning(instanceName, tableName, family string, cluster1 string, cluster2 string, gcRule string, ignoreWarnings bool) string {
+	return fmt.Sprintf(`
+resource "google_bigtable_instance" "instance" {
+  name = "%s"
+
+  cluster {
+    cluster_id = "%s"
+		num_nodes  = 1
+    zone       = "us-central1-b"
+  }
+
+  cluster {
+    cluster_id = "%s"
+		num_nodes  = 1
+    zone       = "us-central1-c"
+  }
+
+  deletion_protection = false
+}
+
+resource "google_bigtable_table" "table" {
+  name          = "%s"
+  instance_name = google_bigtable_instance.instance.id
+
+  column_family {
+    family = "%s"
+  }
+}
+
+resource "google_bigtable_gc_policy" "policy" {
+  instance_name = google_bigtable_instance.instance.id
+  table         = google_bigtable_table.table.name
+  column_family = "%s"
+	gc_rules = <<EOF
+	%s
+	EOF
+
+	ignore_warnings = %t
+	deletion_policy = "ABANDON"
+}
+`, instanceName, cluster1, cluster2, tableName, family, family, gcRule, ignoreWarnings)
 }
 
 func testAccBigtableGCPolicyToBeAbandoned(instanceName, tableName, family string) string {
