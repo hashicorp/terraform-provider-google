e0c336073487e88e4ef4d30806d380a39c459eb9
The Magician <magic-modules@google.com>
2024-06-24 15:39:34 -0700

    Fix https://github.com/hashicorp/terraform-provider-google/issues/18085 (#11031) (#18537)
    [upstream:4abf95bc8d1b3451af255c59267abd98618838b5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/identityplatform/resource_identity_platform_config.go
+++ google/services/identityplatform/resource_identity_platform_config.go
@@ -421,6 +421,7 @@ email/password or email link.`,
 			},
 			"sms_region_config": {
 				Type:        schema.TypeList,
+				Computed:    true,
 				Optional:    true,
 				Description: `Configures the regions where users are allowed to send verification SMS for the project or tenant. This is based on the calling code of the destination phone number.`,
 				MaxItems:    1,
