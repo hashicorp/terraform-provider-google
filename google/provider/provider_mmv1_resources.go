ac58d261bbd5c350d1903fa5f67daf9e9b9983d0
Modular Magician <magic-modules@google.com>
2024-07-12 21:35:35 +0000

    New resource: Managed Instance Group Resize Request (#10139)
    [upstream:d022681bab0fac26dcf9f78e32d8a943afa9131b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -423,9 +423,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 437
+// Generated resources: 438
 // Generated IAM resources: 252
-// Total generated resources: 689
+// Total generated resources: 690
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -655,6 +655,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_region_target_tcp_proxy":                                     compute.ResourceComputeRegionTargetTcpProxy(),
 	"google_compute_region_url_map":                                              compute.ResourceComputeRegionUrlMap(),
 	"google_compute_reservation":                                                 compute.ResourceComputeReservation(),
+	"google_compute_resize_request":                                              compute.ResourceComputeResizeRequest(),
 	"google_compute_resource_policy":                                             compute.ResourceComputeResourcePolicy(),
 	"google_compute_route":                                                       compute.ResourceComputeRoute(),
 	"google_compute_router":                                                      compute.ResourceComputeRouter(),
