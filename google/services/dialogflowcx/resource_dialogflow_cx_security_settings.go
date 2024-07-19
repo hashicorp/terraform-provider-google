8d392d1d1ec297a06ce3da0a5c925bdb63223065
Modular Magician <magic-modules@google.com>
2024-07-19 20:12:52 +0000

    Adding delay after SecuritySettings create to work around API inconsistency (#11165)
    [upstream:b84aed139eeea71b7919887874e8f1a4a6b263cf]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/dialogflowcx/resource_dialogflow_cx_security_settings.go
+++ google/services/dialogflowcx/resource_dialogflow_cx_security_settings.go
@@ -302,6 +302,11 @@ func resourceDialogflowCXSecuritySettingsCreate(d *schema.ResourceData, meta int
 	}
 	d.SetId(id)
 
+	// This is useful if the resource in question doesn't have a perfectly consistent API
+	// That is, the Operation for Create might return before the Get operation shows the
+	// completed state of the resource.
+	time.Sleep(5 * time.Second)
+
 	log.Printf("[DEBUG] Finished creating SecuritySettings %q: %#v", d.Id(), res)
 
 	return resourceDialogflowCXSecuritySettingsRead(d, meta)
