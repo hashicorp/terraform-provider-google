342fa481456a6d807d045afcc0385432d2f9a01a
Modular Magician <magic-modules@google.com>
2024-07-26 18:06:36 +0000

    Permadiff fix: advertised_ip_ranges in cloud router (#11262)
    [upstream:2079ba534361934b883b1d56c162740cf3c2c895]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_router_test.go
+++ google/services/compute/resource_compute_router_test.go
@@ -79,6 +79,29 @@ func TestAccComputeRouter_full(t *testing.T) {
 	})
 }
 
+func TestAccComputeRouter_advertisedIpRangesOrder(t *testing.T) {
+	t.Parallel()
+
+	testId := acctest.RandString(t, 10)
+	routerName := fmt.Sprintf("tf-test-router-%s", testId)
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeRouterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRouterAdvertisedIpRangesOrder(routerName),
+			},
+			{
+				ResourceName:            "google_compute_router.foobar",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"bgp.0.advertised_ip_ranges.0.range", "bgp.0.advertised_ip_ranges.1.range"},
+			},
+		},
+	})
+}
+
 func TestAccComputeRouter_update(t *testing.T) {
 	t.Parallel()
 
@@ -258,6 +281,32 @@ resource "google_compute_router" "foobar" {
     }
     advertised_ip_ranges {
       range = "6.7.0.0/16"
+    }
+    keepalive_interval = 25
+  }
+}
+`, routerName, routerName)
+}
+
+func testAccComputeRouterAdvertisedIpRangesOrder(routerName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_network" "foobar" {
+  name                    = "%s-net"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_router" "foobar" {
+  name    = "%s"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn               = 64514
+    advertise_mode    = "CUSTOM"
+    advertised_groups = ["ALL_SUBNETS"]
+	advertised_ip_ranges {
+      range = "6.7.0.0/16"
+    }
+    advertised_ip_ranges {
+      range = "1.2.3.4"
     }
     keepalive_interval = 25
   }
