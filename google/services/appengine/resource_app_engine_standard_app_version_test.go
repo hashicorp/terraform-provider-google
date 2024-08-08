ff023e9334597305846ff8ef59fb7b53ba099fa8
The Magician <magic-modules@google.com>
2024-08-08 11:40:46 -0700

    Ignore failing tests for AppEngine and IAP (#11396) (#19043)
    [upstream:24de58242760a641cc59a85022b25131b8ffee02]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/appengine/resource_app_engine_standard_app_version_test.go
+++ google/services/appengine/resource_app_engine_standard_app_version_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 func TestAccAppEngineStandardAppVersion_update(t *testing.T) {
+	t.Skip("https://github.com/hashicorp/terraform-provider-google/issues/18936")
 	t.Parallel()
 
 	context := map[string]interface{}{
