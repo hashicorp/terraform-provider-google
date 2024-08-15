104572ed9494b04270c590ca4047b1c9e37ff2b4
The Magician <magic-modules@google.com>
2024-08-15 14:31:52 -0700

    Update reserved_ip_range for testAccFilestoreInstance_reservedIpRange… (#11422) (#19156)
    [upstream:53302465ca4fafd09b0e44f2259b5290297258a5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/filestore/resource_filestore_instance_test.go
+++ google/services/filestore/resource_filestore_instance_test.go
@@ -166,7 +166,7 @@ resource "google_filestore_instance" "instance" {
   networks {
     network           = "default"
     modes             = ["MODE_IPV4"]
-    reserved_ip_range = "172.19.30.0/29"
+    reserved_ip_range = "172.19.31.0/29"
   }
 }
 `, name)
@@ -187,7 +187,7 @@ resource "google_filestore_instance" "instance" {
   networks {
     network           = "default"
     modes             = ["MODE_IPV4"]
-    reserved_ip_range = "172.19.31.0/29"
+    reserved_ip_range = "172.19.31.8/29"
   }
 }
 `, name)
