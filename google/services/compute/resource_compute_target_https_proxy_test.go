3c06d198b2c837c88716ab89ac61f5e2f95e6a73
The Magician <magic-modules@google.com>
2024-08-16 09:14:33 -0700

    Add missing 'global' to serverTlsPolicy update_url on `google_compute_target_https_proxy` (#11443) (#19164)
    [upstream:e7efcc33a8ed2dc580577f588b5098f5da951f40]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_target_https_proxy_test.go
+++ google/services/compute/resource_compute_target_https_proxy_test.go
@@ -49,6 +49,7 @@ func TestAccComputeTargetHttpsProxy_update(t *testing.T) {
 					testAccComputeTargetHttpsProxyDescription("Resource created for Terraform acceptance testing", &proxy),
 					testAccComputeTargetHttpsProxyHasSslCertificate(t, "tf-test-httpsproxy-cert1-"+resourceSuffix, &proxy),
 					testAccComputeTargetHttpsProxyHasSslCertificate(t, "tf-test-httpsproxy-cert2-"+resourceSuffix, &proxy),
+					testAccComputeTargetHttpsProxyHasNullServerTlsPolicy(t, &proxy),
 				),
 			},
 		},
@@ -73,6 +74,37 @@ func TestAccComputeTargetHttpsProxy_certificateMap(t *testing.T) {
 						t, "google_compute_target_https_proxy.foobar", &proxy),
 					testAccComputeTargetHttpsProxyDescription("Resource created for Terraform acceptance testing", &proxy),
 					testAccComputeTargetHttpsProxyHasCertificateMap(t, "tf-test-certmap-"+resourceSuffix, &proxy),
+				),
+			},
+		},
+	})
+}
+
+func TestAccComputeTargetHttpsProxyServerTlsPolicy_update(t *testing.T) {
+	t.Parallel()
+
+	var proxy compute.TargetHttpsProxy
+	resourceSuffix := acctest.RandString(t, 10)
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeTargetHttpsProxyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeTargetHttpsProxyServerTlsPolicy_full(resourceSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeTargetHttpsProxyExists(
+						t, "google_compute_target_https_proxy.foobar", &proxy),
+					testAccComputeTargetHttpsProxyHasNullServerTlsPolicy(t, &proxy),
+				),
+			},
+			{
+				Config: testAccComputeTargetHttpsProxyServerTlsPolicy_update(resourceSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeTargetHttpsProxyExists(
+						t, "google_compute_target_https_proxy.foobar", &proxy),
+					testAccComputeTargetHttpsProxyHasServerTlsPolicy(t, "tf-test-server-tls-policy-"+resourceSuffix, &proxy),
 				),
 			},
 		},
@@ -114,6 +146,7 @@ func testAccComputeTargetHttpsProxyDescription(description string, proxy *comput
 		if proxy.Description != description {
 			return fmt.Errorf("Wrong description: expected '%s' got '%s'", description, proxy.Description)
 		}
+
 		return nil
 	}
 }
@@ -143,6 +176,16 @@ func testAccComputeTargetHttpsProxyHasServerTlsPolicy(t *testing.T, policy strin
 		}
 
 		return fmt.Errorf("Server Tls Policy not found: expected '%s'", serverTlsPolicyUrl)
+	}
+}
+
+func testAccComputeTargetHttpsProxyHasNullServerTlsPolicy(t *testing.T, proxy *compute.TargetHttpsProxy) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if proxy.ServerTlsPolicy != "" {
+			return fmt.Errorf("Server Tls Policy found: expected 'null'")
+		}
+
+		return nil
 	}
 }
 
@@ -257,8 +300,9 @@ resource "google_compute_target_https_proxy" "foobar" {
     google_compute_ssl_certificate.foobar1.self_link,
     google_compute_ssl_certificate.foobar2.self_link,
   ]
-  quic_override  = "ENABLE"
-  tls_early_data = "STRICT"
+  quic_override     = "ENABLE"
+  tls_early_data    = "STRICT"
+  server_tls_policy = null
 }
 
 resource "google_compute_backend_service" "foobar" {
@@ -373,4 +417,116 @@ resource "google_certificate_manager_dns_authorization" "instance" {
   domain = "mysite.com"
 }
 `, id, id, id, id, id, id, id, id)
+}
+
+func testAccComputeTargetHttpsProxyServerTlsPolicy_full(id string) string {
+	return fmt.Sprintf(`
+data "google_project" "project" {}
+
+resource "google_compute_target_https_proxy" "foobar" {
+  description       = "Resource created for Terraform acceptance testing"
+  name              = "tf-test-httpsproxy-%s"
+	url_map           = google_compute_url_map.foobar.self_link
+	ssl_certificates  = [google_compute_ssl_certificate.foobar.self_link]
+  server_tls_policy = null
+}
+
+resource "google_compute_backend_service" "foobar" {
+  name          = "tf-test-httpsproxy-backend-%s"
+  health_checks = [google_compute_http_health_check.zero.self_link]
+}
+
+resource "google_compute_http_health_check" "zero" {
+  name               = "tf-test-httpsproxy-check-%s"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+resource "google_compute_url_map" "foobar" {
+  name            = "tf-test-httpsproxy-urlmap-%s"
+  default_service = google_compute_backend_service.foobar.self_link
+}
+
+resource "google_compute_ssl_certificate" "foobar" {
+  name        = "tf-test-httpsproxy-cert-%s"
+  description = "very descriptive"
+  private_key = file("test-fixtures/test.key")
+  certificate = file("test-fixtures/test.crt")
+}
+
+resource "google_certificate_manager_trust_config" "trust_config" {
+  name     = "tf-test-trust-config-%s"
+  location = "global"
+
+  allowlisted_certificates  {
+    pem_certificate = file("test-fixtures/cert.pem")
+  }
+}
+
+resource "google_network_security_server_tls_policy" "server_tls_policy" {
+  name = "tf-test-server-tls-policy-%s"
+
+  mtls_policy {
+    client_validation_trust_config = "projects/${data.google_project.project.number}/locations/global/trustConfigs/${google_certificate_manager_trust_config.trust_config.name}"
+    client_validation_mode         = "ALLOW_INVALID_OR_MISSING_CLIENT_CERT"
+  }
+}
+`, id, id, id, id, id, id, id)
+}
+
+func testAccComputeTargetHttpsProxyServerTlsPolicy_update(id string) string {
+	return fmt.Sprintf(`
+data "google_project" "project" {}
+
+resource "google_compute_target_https_proxy" "foobar" {
+  description       = "Resource created for Terraform acceptance testing"
+  name              = "tf-test-httpsproxy-%s"
+	url_map           = google_compute_url_map.foobar.self_link
+	ssl_certificates  = [google_compute_ssl_certificate.foobar.self_link]
+  server_tls_policy = google_network_security_server_tls_policy.server_tls_policy.id
+}
+
+resource "google_compute_backend_service" "foobar" {
+  name          = "tf-test-httpsproxy-backend-%s"
+  health_checks = [google_compute_http_health_check.zero.self_link]
+}
+
+resource "google_compute_http_health_check" "zero" {
+  name               = "tf-test-httpsproxy-check-%s"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+resource "google_compute_url_map" "foobar" {
+  name            = "tf-test-httpsproxy-urlmap-%s"
+  default_service = google_compute_backend_service.foobar.self_link
+}
+
+resource "google_compute_ssl_certificate" "foobar" {
+  name        = "tf-test-httpsproxy-cert-%s"
+  description = "very descriptive"
+  private_key = file("test-fixtures/test.key")
+  certificate = file("test-fixtures/test.crt")
+}
+
+resource "google_certificate_manager_trust_config" "trust_config" {
+  name     = "tf-test-trust-config-%s"
+  location = "global"
+
+  allowlisted_certificates  {
+    pem_certificate = file("test-fixtures/cert.pem")
+  }
+}
+
+resource "google_network_security_server_tls_policy" "server_tls_policy" {
+  name = "tf-test-server-tls-policy-%s"
+
+  mtls_policy {
+    client_validation_trust_config = "projects/${data.google_project.project.number}/locations/global/trustConfigs/${google_certificate_manager_trust_config.trust_config.name}"
+    client_validation_mode         = "ALLOW_INVALID_OR_MISSING_CLIENT_CERT"
+  }
+}
+`, id, id, id, id, id, id, id)
 }
