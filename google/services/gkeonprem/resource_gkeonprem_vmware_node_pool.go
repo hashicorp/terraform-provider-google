f0596d90171138ec53e13e1c63f98c2ecf20df7f
The Magician <magic-modules@google.com>
2024-09-05 19:10:26 -0700

    Update docs for new image types support for VmwareNodePool (#11643) (#19392)
    [upstream:b9e00f4e463a233076d2118e3a72b798a9a9b41f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/gkeonprem/resource_gkeonprem_vmware_node_pool.go
+++ google/services/gkeonprem/resource_gkeonprem_vmware_node_pool.go
@@ -67,7 +67,7 @@ func ResourceGkeonpremVmwareNodePool() *schema.Resource {
 							Type:     schema.TypeString,
 							Required: true,
 							Description: `The OS image to be used for each node in a node pool.
-Currently 'cos', 'ubuntu', 'ubuntu_containerd' and 'windows' are supported.`,
+Currently 'cos', 'cos_cgv2', 'ubuntu', 'ubuntu_cgv2', 'ubuntu_containerd' and 'windows' are supported.`,
 						},
 						"boot_disk_size_gb": {
 							Type:        schema.TypeInt,
