3345cc74103d5dccb43e343298f9b23ed53030a1
The Magician <magic-modules@google.com>
2024-09-09 08:45:28 -0700

    continue sweeper run for other resources if no resource found (#11589) (#19417)
    [upstream:924f91440a2b9674686182851772e467252da18e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/netapp/resource_netapp_volume_snapshot_sweeper.go
+++ google/services/netapp/resource_netapp_volume_snapshot_sweeper.go
@@ -57,7 +57,7 @@ func testSweepNetappVolumeSnapshot(region string) error {
 		listUrl, err := tpgresource.ReplaceVars(d, config, listTemplate)
 		if err != nil {
 			log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
-			return nil
+			continue
 		}
 
 		res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
@@ -69,13 +69,13 @@ func testSweepNetappVolumeSnapshot(region string) error {
 		})
 		if err != nil {
 			log.Printf("[INFO][SWEEPER_LOG] Error in response from request %s: %s", listUrl, err)
-			return nil
+			continue
 		}
 
 		resourceList, ok := res["volumeSnapshots"]
 		if !ok {
 			log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
-			return nil
+			continue
 		}
 
 		rl := resourceList.([]interface{})
@@ -87,7 +87,7 @@ func testSweepNetappVolumeSnapshot(region string) error {
 			obj := ri.(map[string]interface{})
 			if obj["name"] == nil {
 				log.Printf("[INFO][SWEEPER_LOG] %s resource name was nil", resourceName)
-				return nil
+				continue
 			}
 
 			name := tpgresource.GetResourceNameFromSelfLink(obj["name"].(string))
@@ -101,7 +101,7 @@ func testSweepNetappVolumeSnapshot(region string) error {
 			deleteUrl, err := tpgresource.ReplaceVars(d, config, deleteTemplate)
 			if err != nil {
 				log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
-				return nil
+				continue
 			}
 			deleteUrl = deleteUrl + name
 
