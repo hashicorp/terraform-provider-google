59f12e74254c2cc493f730f74be5bd1b21030625
Shuya Ma <shuyama@google.com>
2024-09-20 15:56:56 -0700

    remove unintended test changes

--- google/services/container/resource_container_cluster_test.go
+++ google/services/container/resource_container_cluster_test.go
@@ -1363,40 +1363,6 @@ func TestAccContainerCluster_withNodeConfigGcfsConfig(t *testing.T) {
 	})
 }
 
-// Note: Updates for these are currently known to be broken (b/361634104), and
-// so are not tested here.
-// They can probably be made similar to, or consolidated with,
-// TestAccContainerCluster_withInsecureKubeletReadonlyPortEnabledInNodeConfigUpdates
-// after that's resolved.
-func TestAccContainerCluster_withNodeConfigKubeletConfigSettings(t *testing.T) {
-	t.Parallel()
-	clusterName := fmt.Sprintf("tf-test-cluster-%s", acctest.RandString(t, 10))
-	networkName := acctest.BootstrapSharedTestNetwork(t, "gke-cluster")
-	subnetworkName := acctest.BootstrapSubnet(t, "gke-cluster", networkName)
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckContainerClusterDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccContainerCluster_withNodeConfigKubeletConfigSettings(clusterName, networkName, subnetworkName),
-				ConfigPlanChecks: resource.ConfigPlanChecks{
-					PreApply: []plancheck.PlanCheck{
-						acctest.ExpectNoDelete(),
-					},
-				},
-			},
-			{
-				ResourceName:            "google_container_cluster.with_node_config_kubelet_config_settings",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"deletion_protection"},
-			},
-		},
-	})
-}
-
 // This is for node_config.kubelet_config, which affects the default node-pool
 // (default-pool) when created via the google_container_cluster resource
 func TestAccContainerCluster_withInsecureKubeletReadonlyPortEnabledInNodeConfigUpdates(t *testing.T) {
@@ -6020,28 +5986,6 @@ resource "google_container_cluster" "with_node_config_gcfs_config" {
   subnetwork          = "%s"
 }
 `, clusterName, enabled, networkName, subnetworkName)
-}
-
-func testAccContainerCluster_withNodeConfigKubeletConfigSettings(clusterName, networkName, subnetworkName string) string {
-	return fmt.Sprintf(`
-resource "google_container_cluster" "with_node_config_kubelet_config_settings" {
-  name               = "%s"
-  location           = "us-central1-f"
-  initial_node_count = 1
-
-  node_config {
-    kubelet_config {
-      cpu_manager_policy   = "static"
-      cpu_cfs_quota        = true
-      cpu_cfs_quota_period = "100ms"
-      pod_pids_limit       = 2048
-    }
-  }
-  deletion_protection = false
-  network             = "%s"
-  subnetwork          = "%s"
-}
-`, clusterName, networkName, subnetworkName)
 }
 
 func testAccContainerCluster_withInsecureKubeletReadonlyPortEnabledInNodeConfig(clusterName, networkName, subnetworkName, insecureKubeletReadonlyPortEnabled string) string {
