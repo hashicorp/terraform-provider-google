ab9ab338d46e838c2b4d560b26f702756d99e4f4
Modular Magician <magic-modules@google.com>
2024-10-01 20:21:13 +0000

    Add increased length & Unicode tagkey and tagvalue shortname support (#11827)
    Co-authored-by: Nick Elliot <nickelliot@google.com>
    
    [upstream:faf28013d0c42a059c4c63ed89558ce2900369c3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/tags/resource_tags_tag_key.go
+++ google/services/tags/resource_tags_tag_key.go
@@ -62,10 +62,10 @@ func ResourceTagsTagKey() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: validation.StringLenBetween(1, 63),
+				ValidateFunc: validation.StringLenBetween(1, 256),
 				Description: `Input only. The user friendly name for a TagKey. The short name should be unique for TagKeys within the same tag namespace.
 
-The short name must be 1-63 characters, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.`,
+The short name can have a maximum length of 256 characters. The permitted character set for the shortName includes all UTF-8 encoded Unicode characters except single quotes ('), double quotes ("), backslashes (\\), and forward slashes (/).`,
 			},
 			"description": {
 				Type:         schema.TypeString,
--- website/docs/r/tags_tag_value.html.markdown
+++ website/docs/r/tags_tag_value.html.markdown
@@ -57,7 +57,7 @@ The following arguments are supported:
 * `short_name` -
   (Required)
   Input only. User-assigned short name for TagValue. The short name should be unique for TagValues within the same parent TagKey.
-  The short name must be 63 characters or less, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.
+  The short name can have a maximum length of 256 characters. The permitted character set for the shortName includes all UTF-8 encoded Unicode characters except single quotes ('), double quotes ("), backslashes (\\), and forward slashes (/).
 
 
 - - -
