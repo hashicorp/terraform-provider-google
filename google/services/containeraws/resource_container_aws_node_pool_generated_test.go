460e9f06357213234c2c24c0a3352fead17806f8
The Magician <magic-modules@google.com>
2024-10-01 13:59:58 -0700

    Upgrade dcl to 1.74.0 (#11883) (#19714)
    [upstream:1ac0c4874422bd3fd96c05bd8517173de1c3c8fa]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/containeraws/resource_container_aws_node_pool_generated_test.go
+++ google/services/containeraws/resource_container_aws_node_pool_generated_test.go
@@ -276,9 +276,15 @@ resource "google_container_aws_node_pool" "primary" {
     auto_repair = true
   }
 
+  kubelet_config {
+    cpu_manager_policy    = "none"
+    cpu_cfs_quota         = true
+    cpu_cfs_quota_period  = "100ms"
+    pod_pids_limit        = 1024
+  }
+
   project = "%{project_name}"
 }
-
 
 `, context)
 }
@@ -436,9 +442,15 @@ resource "google_container_aws_node_pool" "primary" {
     auto_repair = false
   }
 
+  kubelet_config {
+    cpu_manager_policy    = "none"
+    cpu_cfs_quota         = true
+    cpu_cfs_quota_period  = "100ms"
+    pod_pids_limit        = 1024
+  }
+
   project = "%{project_name}"
 }
-
 
 `, context)
 }
