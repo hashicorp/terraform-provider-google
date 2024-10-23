3c44f223d9d2a9e5e274d9547f3d7e5d6e4df030
The Magician <magic-modules@google.com>
2024-10-23 09:42:35 -0700

    Filestore protocol GA (#12063) (#19982)
    [upstream:7a04aecaaba252d0e5543a1c3f7cb210dd8b6c2c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/filestore/resource_filestore_instance_generated_test.go
+++ google/services/filestore/resource_filestore_instance_generated_test.go
@@ -135,6 +135,53 @@ resource "google_filestore_instance" "instance" {
 `, context)
 }
 
+func TestAccFilestoreInstance_filestoreInstanceProtocolExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckFilestoreInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFilestoreInstance_filestoreInstanceProtocolExample(context),
+			},
+			{
+				ResourceName:            "google_filestore_instance.instance",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels", "zone"},
+			},
+		},
+	})
+}
+
+func testAccFilestoreInstance_filestoreInstanceProtocolExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_filestore_instance" "instance" {
+  name     = "tf-test-test-instance%{random_suffix}"
+  location = "us-central1"
+  tier     = "ENTERPRISE"
+  protocol = "NFS_V4_1"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "share1"
+  }
+
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV4"]
+  }
+
+}
+`, context)
+}
+
 func testAccCheckFilestoreInstanceDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
