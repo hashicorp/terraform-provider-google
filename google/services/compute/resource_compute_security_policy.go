b1b8f77a87d14ab40e8b304961eb8349cc816978
The Magician <magic-modules@google.com>
2024-11-01 09:39:58 -0700

    Increase timeout on `google_compute_security_policy`  (#12212) (#20145)
    [upstream:76054d7f25c56aa98a392c1f1c9ed4fee60044da]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_security_policy.go
+++ google/services/compute/resource_compute_security_policy.go
@@ -37,9 +37,9 @@ func ResourceComputeSecurityPolicy() *schema.Resource {
 		),
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(20 * time.Minute),
-			Update: schema.DefaultTimeout(20 * time.Minute),
-			Delete: schema.DefaultTimeout(20 * time.Minute),
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
