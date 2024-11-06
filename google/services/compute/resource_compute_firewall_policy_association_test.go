ae44078bd9b93e9698c7c7d6c424f9c7448d88ad
Modular Magician <magic-modules@google.com>
2024-11-06 22:05:55 +0000

    Migrate google_compute_firewall_policy_association resource from DCL to MMv1 (#11358)
    Co-authored-by: Zhenhua Li <zhenhuali@google.com>
    
    [upstream:3850a583f7d4839598b5bd5e73bbb3163df398c8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_firewall_policy_association_test.go
+++ google/services/compute/resource_compute_firewall_policy_association_test.go
@@ -64,3 +64,53 @@ resource "google_compute_firewall_policy_association" "default" {
 }
 `, context)
 }
+
+func TestAccComputeFirewallPolicyAssociation_organization(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"org_name":      fmt.Sprintf("organizations/%s", envvar.GetTestOrgFromEnv(t)),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeFirewallPolicyAssociation_organization(context),
+			},
+			{
+				ResourceName:            "google_compute_firewall_policy_association.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"firewall_policy"},
+			},
+		},
+	})
+}
+
+func testAccComputeFirewallPolicyAssociation_organization(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_folder" "folder" {
+  display_name = "tf-test-my-folder-%{random_suffix}"
+  parent       = "%{org_name}"
+  deletion_protection = false
+}
+
+resource "google_compute_firewall_policy" "policy" {
+  parent      = "%{org_name}"
+  short_name  = "tf-test-my-policy-%{random_suffix}"
+  description = "Example Resource"
+}
+
+resource "google_compute_firewall_policy_association" "default" {
+  firewall_policy = google_compute_firewall_policy.policy.id
+  attachment_target = google_folder.folder.name
+  name = "tf-test-my-association-%{random_suffix}"
+}
+`, context)
+}
