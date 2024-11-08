08f6cf9f110e73c285e5c50a32a42e241f932c0f
The Magician <magic-modules@google.com>
2024-11-08 08:51:46 -0800

    Fixes https://github.com/hashicorp/terraform-provider-google/issues/19540 (#12253) (#20234)
    [upstream:3db026ca076c34e112178ab9c5e5a6a7fb251e8b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/accesscontextmanager/resource_access_context_manager_service_perimeter_egress_policy.go
+++ google/services/accesscontextmanager/resource_access_context_manager_service_perimeter_egress_policy.go
@@ -114,9 +114,11 @@ func ResourceAccessContextManagerServicePerimeterEgressPolicy() *schema.Resource
 							Type:     schema.TypeList,
 							Optional: true,
 							ForceNew: true,
-							Description: `A list of identities that are allowed access through this 'EgressPolicy'.
-Should be in the format of email address. The email address should
-represent individual user or service account only.`,
+							Description: `Identities can be an individual user, service account, Google group,
+or third-party identity. For third-party identity, only single identities
+are supported and other identity types are not supported.The v1 identities
+that have the prefix user, group and serviceAccount in
+https://cloud.google.com/iam/docs/principal-identifiers#v1 are supported.`,
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
