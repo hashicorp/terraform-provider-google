77ae6591d6fd91545ade00ddda1d34d3167c72b6
The Magician <magic-modules@google.com>
2024-11-20 09:51:59 -0800

    `ephemeral`: add `google_service_account_id_token` (#12141) (#20409)
    [upstream:89b1a0143545508effaf0040fd04dd56806f8624]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/resourcemanager/ephemeral_google_service_account_id_token.go
@@ -0,0 +1,173 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package resourcemanager
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/option"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-google/google/fwmodels"
+	"github.com/hashicorp/terraform-provider-google/google/fwtransport"
+	"github.com/hashicorp/terraform-provider-google/google/fwutils"
+	"github.com/hashicorp/terraform-provider-google/google/fwvalidators"
+	"google.golang.org/api/iamcredentials/v1"
+)
+
+var _ ephemeral.EphemeralResource = &googleEphemeralServiceAccountIdToken{}
+
+func GoogleEphemeralServiceAccountIdToken() ephemeral.EphemeralResource {
+	return &googleEphemeralServiceAccountIdToken{}
+}
+
+type googleEphemeralServiceAccountIdToken struct {
+	providerConfig *fwtransport.FrameworkProviderConfig
+}
+
+func (p *googleEphemeralServiceAccountIdToken) Metadata(ctx context.Context, req ephemeral.MetadataRequest, resp *ephemeral.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_service_account_id_token"
+}
+
+type ephemeralServiceAccountIdTokenModel struct {
+	TargetAudience       types.String `tfsdk:"target_audience"`
+	TargetServiceAccount types.String `tfsdk:"target_service_account"`
+	Delegates            types.Set    `tfsdk:"delegates"`
+	IncludeEmail         types.Bool   `tfsdk:"include_email"`
+	IdToken              types.String `tfsdk:"id_token"`
+}
+
+func (p *googleEphemeralServiceAccountIdToken) Schema(ctx context.Context, req ephemeral.SchemaRequest, resp *ephemeral.SchemaResponse) {
+	resp.Schema.Description = "This ephemeral resource provides a Google OpenID Connect (oidc) id_token."
+	resp.Schema.MarkdownDescription = "This ephemeral resource provides a Google OpenID Connect (oidc) id_token."
+
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"target_audience": schema.StringAttribute{
+				Description: "The audience claim for the `id_token`.",
+				Required:    true,
+			},
+			"target_service_account": schema.StringAttribute{
+				Description: "The email of the service account being impersonated.  Used only when using impersonation mode.",
+				Optional:    true,
+				Validators: []validator.String{
+					fwvalidators.ServiceAccountEmailValidator{},
+				},
+			},
+			"delegates": schema.SetAttribute{
+				Description: "Delegate chain of approvals needed to perform full impersonation. Specify the fully qualified service account name.  Used only when using impersonation mode.",
+				Optional:    true,
+				ElementType: types.StringType,
+				Validators: []validator.Set{
+					setvalidator.ValueStringsAre(fwvalidators.ServiceAccountEmailValidator{}),
+				},
+			},
+			"include_email": schema.BoolAttribute{
+				Description: "Include the verified email in the claim. Used only when using impersonation mode.",
+				Optional:    true, // Defaults to false when not set (Null / Unknown)
+			},
+			"id_token": schema.StringAttribute{
+				Description: "The `id_token` representing the new generated identity.",
+				Computed:    true,
+				Sensitive:   true,
+			},
+		},
+	}
+}
+
+func (p *googleEphemeralServiceAccountIdToken) Configure(ctx context.Context, req ephemeral.ConfigureRequest, resp *ephemeral.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	pd, ok := req.ProviderData.(*fwtransport.FrameworkProviderConfig)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *fwtransport.FrameworkProviderConfig, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	p.providerConfig = pd
+}
+
+func (p *googleEphemeralServiceAccountIdToken) Open(ctx context.Context, req ephemeral.OpenRequest, resp *ephemeral.OpenResponse) {
+	var data ephemeralServiceAccountIdTokenModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	targetAudience := data.TargetAudience.ValueString()
+
+	// TODO: This is a temporary solution to get the credentials from the provider config.
+	// we'll address this once muxing issues are resolved.
+	model := fwmodels.ProviderModel{
+		Credentials:                        p.providerConfig.Credentials,
+		AccessToken:                        p.providerConfig.AccessToken,
+		ImpersonateServiceAccount:          p.providerConfig.ImpersonateServiceAccount,
+		ImpersonateServiceAccountDelegates: p.providerConfig.ImpersonateServiceAccountDelegates,
+		Project:                            p.providerConfig.Project,
+		BillingProject:                     p.providerConfig.BillingProject,
+		Scopes:                             p.providerConfig.Scopes,
+		UniverseDomain:                     p.providerConfig.UniverseDomain,
+	}
+	creds := fwtransport.GetCredentials(ctx, model, false, &resp.Diagnostics)
+	targetServiceAccount := data.TargetServiceAccount
+	// If a target service account is provided, use the API to generate the idToken
+	if !targetServiceAccount.IsNull() && !targetServiceAccount.IsUnknown() {
+		service := p.providerConfig.NewIamCredentialsClient(p.providerConfig.UserAgent)
+		name := fmt.Sprintf("projects/-/serviceAccounts/%s", targetServiceAccount.ValueString())
+
+		tokenRequest := &iamcredentials.GenerateIdTokenRequest{
+			Audience:     targetAudience,
+			IncludeEmail: data.IncludeEmail.ValueBool(),
+			Delegates:    fwutils.StringSet(data.Delegates),
+		}
+		at, err := service.Projects.ServiceAccounts.GenerateIdToken(name, tokenRequest).Do()
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Error calling iamcredentials.GenerateIdToken",
+				err.Error(),
+			)
+			return
+		}
+
+		data.IdToken = types.StringValue(at.Token)
+		resp.Diagnostics.Append(resp.Result.Set(ctx, data)...)
+		return
+	}
+
+	// If no target service account, use the default credentials
+	ctx = context.Background()
+	co := []option.ClientOption{}
+	if creds.JSON != nil {
+		co = append(co, idtoken.WithCredentialsJSON(creds.JSON))
+	}
+
+	idTokenSource, err := idtoken.NewTokenSource(ctx, targetAudience, co...)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to retrieve TokenSource",
+			err.Error(),
+		)
+		return
+	}
+	idToken, err := idTokenSource.Token()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to retrieve Token",
+			err.Error(),
+		)
+		return
+	}
+
+	data.IdToken = types.StringValue(idToken.AccessToken)
+	resp.Diagnostics.Append(resp.Result.Set(ctx, data)...)
+}
---
+++ website/docs/ephemeral-resources/service_account_id_token.html.markdown
@@ -0,0 +1,89 @@
+---
+subcategory: "Cloud Platform"
+description: |-
+  Produces OpenID Connect token for service accounts
+---
+
+# google_service_account_id_token
+
+This ephemeral resource provides a Google OpenID Connect (`oidc`) `id_token`.  Tokens issued from this ephemeral resource are typically used to call external services that accept OIDC tokens for authentication (e.g. [Google Cloud Run](https://cloud.google.com/run/docs/authenticating/service-to-service)).
+
+For more information see
+[OpenID Connect](https://openid.net/specs/openid-connect-core-1_0.html#IDToken).
+
+## Example Usage - ServiceAccount JSON credential file.
+  `google_service_account_id_token` will use the configured [provider credentials](https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#credentials-1)
+
+  ```hcl
+  ephemeral "google_service_account_id_token" "oidc" {
+    target_audience = "https://foo.bar/"
+  }
+  ```
+
+## Example Usage - Service Account Impersonation.
+  `google_service_account_access_token` will use background impersonated credentials provided by [google_service_account_access_token](https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/service_account_access_token).
+
+  Note: to use the following, you must grant `target_service_account` the
+  `roles/iam.serviceAccountTokenCreator` role on itself.
+
+  ```hcl
+  data "google_service_account_access_token" "impersonated" {
+    provider = google
+    target_service_account = "impersonated-account@project.iam.gserviceaccount.com"
+    delegates = []
+    scopes = ["userinfo-email", "cloud-platform"]
+    lifetime = "300s"
+  }
+
+  provider "google" {
+    alias  = "impersonated"
+    access_token = data.google_service_account_access_token.impersonated.access_token
+  }
+
+  ephemeral "google_service_account_id_token" "oidc" {
+    provider = google.impersonated
+    target_service_account = "impersonated-account@project.iam.gserviceaccount.com"
+    delegates = []
+    include_email = true
+    target_audience = "https://foo.bar/"
+  }
+
+  ```
+
+## Example Usage - Invoking Cloud Run Endpoint
+
+  The following configuration will invoke [Cloud Run](https://cloud.google.com/run/docs/authenticating/service-to-service) endpoint where the service account for Terraform has been granted `roles/run.invoker` role previously.
+
+```hcl
+
+ephemeral "google_service_account_id_token" "oidc" {
+  target_audience = "https://your.cloud.run.app/"
+}
+
+data "http" "cloudrun" {
+  url = "https://your.cloud.run.app/"
+  request_headers  = {
+    Authorization = "Bearer ${ephemeral.google_service_account_id_token.oidc.id_token}"
+  }
+}
+
+
+output "cloud_run_response" {
+  value = data.http.cloudrun.body
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `target_audience` (Required) - The audience claim for the `id_token`.
+* `target_service_account` (Optional) - The email of the service account being impersonated.  Used only when using impersonation mode.
+* `delegates` (Optional) - Delegate chain of approvals needed to perform full impersonation. Specify the fully qualified service account name.   Used only when using impersonation mode.
+* `include_email` (Optional) Include the verified email in the claim. Used only when using impersonation mode.
+
+## Attributes Reference
+
+The following attribute is exported:
+
+* `id_token` - The `id_token` representing the new generated identity.
