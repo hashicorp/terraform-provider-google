bc184bc542d54ba41c1684b718f35ae092071de5
Modular Magician <magic-modules@google.com>
2024-11-21 20:34:08 +0000

    Refactor muxing 2 : Remove unused code, resulting from #11903 (#11578)
    [upstream:665013b5123eb77dc53a70c23045ac8b4a206caa]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/fwtransport/framework_utils.go
+++ google/fwtransport/framework_utils.go
@@ -32,28 +32,6 @@ func CompileUserAgentString(ctx context.Context, name, tfVersion, provVersion st
 	return ua
 }
 
-func GetCurrentUserEmailFramework(p *FrameworkProviderConfig, userAgent string, diags *diag.Diagnostics) string {
-	// When environment variables UserProjectOverride and BillingProject are set for the provider,
-	// the header X-Goog-User-Project is set for the API requests.
-	// But it causes an error when calling GetCurrUserEmail. Set the project to be "NO_BILLING_PROJECT_OVERRIDE".
-	// And then it triggers the header X-Goog-User-Project to be set to empty string.
-
-	// See https://github.com/golang/oauth2/issues/306 for a recommendation to do this from a Go maintainer
-	// URL retrieved from https://accounts.google.com/.well-known/openid-configuration
-	res, d := SendFrameworkRequest(p, "GET", "NO_BILLING_PROJECT_OVERRIDE", "https://openidconnect.googleapis.com/v1/userinfo", userAgent, nil)
-	diags.Append(d...)
-
-	if diags.HasError() {
-		tflog.Info(p.Context, "error retrieving userinfo for your provider credentials. have you enabled the 'https://www.googleapis.com/auth/userinfo.email' scope?")
-		return ""
-	}
-	if res["email"] == nil {
-		diags.AddError("error retrieving email from userinfo.", "email was nil in the response.")
-		return ""
-	}
-	return res["email"].(string)
-}
-
 func GenerateFrameworkUserAgentString(metaData *fwmodels.ProviderMetaModel, currUserAgent string) string {
 	if metaData != nil && !metaData.ModuleName.IsNull() && metaData.ModuleName.ValueString() != "" {
 		return strings.Join([]string{currUserAgent, metaData.ModuleName.ValueString()}, " ")
