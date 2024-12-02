52ee3c6cbf8d799f38a449780c1e82945d2dc81b
The Magician <magic-modules@google.com>
2024-12-02 14:41:34 -0800

    Increase AlloyDB cluster timeout (#12455) (#20547)
    [upstream:6f75b8b6bcf6fd54daa96554f609d97fa9c810e9]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/alloydb/resource_alloydb_cluster.go
+++ google/services/alloydb/resource_alloydb_cluster.go
@@ -45,9 +45,9 @@ func ResourceAlloydbCluster() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(120 * time.Minute),
+			Update: schema.DefaultTimeout(120 * time.Minute),
+			Delete: schema.DefaultTimeout(120 * time.Minute),
 		},
 
 		CustomizeDiff: customdiff.All(
