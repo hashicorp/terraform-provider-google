a3db2a04be745296c6a520e136407c62bdb62eb8
Modular Magician <magic-modules@google.com>
2024-12-18 21:34:38 +0000

    Migrate google_compute_firewall_policy_association resource from DCL to MMv1 (#12466)
    Co-authored-by: Zhenhua Li <zhenhuali@google.com>
    
    [upstream:613bdaee0c0243e40465a7b5497693b9720eeb85]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -463,9 +463,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 499
+// Generated resources: 500
 // Generated IAM resources: 261
-// Total generated resources: 760
+// Total generated resources: 761
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -645,6 +645,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_disk_resource_policy_attachment":                             compute.ResourceComputeDiskResourcePolicyAttachment(),
 	"google_compute_external_vpn_gateway":                                        compute.ResourceComputeExternalVpnGateway(),
 	"google_compute_firewall":                                                    compute.ResourceComputeFirewall(),
+	"google_compute_firewall_policy_association":                                 compute.ResourceComputeFirewallPolicyAssociation(),
 	"google_compute_firewall_policy_rule":                                        compute.ResourceComputeFirewallPolicyRule(),
 	"google_compute_forwarding_rule":                                             compute.ResourceComputeForwardingRule(),
 	"google_compute_global_address":                                              compute.ResourceComputeGlobalAddress(),
