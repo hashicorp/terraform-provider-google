0b67483712b161dbba382bdcc9144fd0b2b66794
The Magician <magic-modules@google.com>
2024-12-26 16:03:09 -0800

    suppressing Project id vs project number diff in forwardingRule.target (#12606) (#20790)
    [upstream:890855bb8c655ea02e0014a68f0f83c69d6d3152]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/tpgresource/self_link_helpers_test.go
+++ google/tpgresource/self_link_helpers_test.go
@@ -68,6 +68,70 @@ func TestCompareSelfLinkOrResourceName(t *testing.T) {
 	}
 }
 
+func TestCompareSelfLinkRelativePathsIgnoreProjectId(t *testing.T) {
+	cases := map[string]struct {
+		Old, New string
+		Expect   bool
+	}{
+		"full path, project number": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/v1/projects/1234/global/networks/a-network",
+			Expect: true,
+		},
+		"partial path, project number": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "projects/1234/global/networks/a-network",
+			Expect: true,
+		},
+		"partial path, same": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "projects/your-project/global/networks/a-network",
+			Expect: true,
+		},
+		"partial path, different name": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "projects/your-project/global/networks/another-network",
+			Expect: false,
+		},
+		"partial path, different project": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "projects/another-project/global/networks/a-network",
+			Expect: false,
+		},
+		"full path, different name": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/another-network",
+			Expect: false,
+		},
+		"full path, different project": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/v1/projects/another-project/global/networks/a-network",
+			Expect: false,
+		},
+		"beta full path, same": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/beta/projects/your-project/global/networks/a-network",
+			Expect: true,
+		},
+		"beta full path, different name": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/beta/projects/your-project/global/networks/another-network",
+			Expect: false,
+		},
+		"beta full path, different project": {
+			Old:    "https://www.googleapis.com/compute/v1/projects/your-project/global/networks/a-network",
+			New:    "https://www.googleapis.com/compute/beta/projects/another-project/global/networks/a-network",
+			Expect: false,
+		},
+	}
+
+	for tn, tc := range cases {
+		if CompareSelfLinkRelativePathsIgnoreProjectId("", tc.Old, tc.New, nil) != tc.Expect {
+			t.Errorf("bad: %s, expected %t for old = %q and new = %q", tn, tc.Expect, tc.Old, tc.New)
+		}
+	}
+}
+
 func TestGetResourceNameFromSelfLink(t *testing.T) {
 	cases := map[string]struct {
 		SelfLink, ExpectedName string
