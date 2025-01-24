7f431f7633ce9e88e32ecee5d08fa3da387301e4
The Magician <magic-modules@google.com>
2025-01-24 08:57:58 -0800

    Fix Firestore field deletion for wildcard fields (#12856) (#21034)
    [upstream:7d08e89ac8686de13d9fdc30dc1f557925c3004d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/firestore/resource_firestore_field_generated_test.go
+++ google/services/firestore/resource_firestore_field_generated_test.go
@@ -204,6 +204,64 @@ resource "google_firestore_field" "match_override" {
 `, context)
 }
 
+func TestAccFirestoreField_firestoreFieldWildcardExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project_id":              envvar.GetTestProjectFromEnv(),
+		"delete_protection_state": "DELETE_PROTECTION_DISABLED",
+		"random_suffix":           acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckFirestoreFieldDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFirestoreField_firestoreFieldWildcardExample(context),
+			},
+			{
+				ResourceName:            "google_firestore_field.wildcard",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"collection", "database", "field"},
+			},
+		},
+	})
+}
+
+func testAccFirestoreField_firestoreFieldWildcardExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_firestore_database" "database" {
+	project     = "%{project_id}"
+	name        = "tf-test-database-id%{random_suffix}"
+	location_id = "nam5"
+	type        = "FIRESTORE_NATIVE"
+
+	delete_protection_state = "%{delete_protection_state}"
+	deletion_policy         = "DELETE"
+  }
+
+  resource "google_firestore_field" "wildcard" {
+	project    = "%{project_id}"
+	database   = google_firestore_database.database.name
+	collection = "chatrooms_%{random_suffix}"
+	field      = "*"
+
+	index_config {
+	  indexes {
+		  order       = "ASCENDING"
+		  query_scope = "COLLECTION_GROUP"
+	  }
+	  indexes {
+		  array_config = "CONTAINS"
+	  }
+	}
+  }
+`, context)
+}
+
 func testAccCheckFirestoreFieldDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
