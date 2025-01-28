9a30aebc1ec146bc824ab7e986861bd0148ea3f2
The Magician <magic-modules@google.com>
2025-01-28 09:48:51 -0800

    upgrade dcl to 1.77 (#12879) (#21077)
    [upstream:a5ea060babe4d01f21acc12ba718c2228badbd0e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/dataproc/resource_dataproc_workflow_template_test.go
+++ google/services/dataproc/resource_dataproc_workflow_template_test.go
@@ -77,6 +77,33 @@ func TestAccDataprocWorkflowTemplate_withShieldedVMs(t *testing.T) {
 	})
 }
 
+func TestAccDataprocWorkflowTemplate_encryptionConfig(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"project":       envvar.GetTestProjectFromEnv(),
+		"version":       "2.0.35-debian10",
+		"kms_key_name":  acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-dataproc-workflow").CryptoKey.Name,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             funcAccTestDataprocWorkflowTemplateCheckDestroy(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataprocWorkflowTemplate_encryptionConfig(context),
+			},
+			{
+				ImportState:       true,
+				ImportStateVerify: true,
+				ResourceName:      "google_dataproc_workflow_template.template",
+			},
+		},
+	})
+}
+
 func testAccDataprocWorkflowTemplate_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_dataproc_workflow_template" "template" {
@@ -195,6 +222,73 @@ resource "google_dataproc_workflow_template" "shielded_vms_template" {
       query_file_uri = "someuri"
     }
   }
+}
+`, context)
+}
+
+func testAccDataprocWorkflowTemplate_encryptionConfig(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_dataproc_workflow_template" "template" {
+  name = "template%{random_suffix}"
+  location = "us-central1"
+  encryption_config {
+    kms_key = "%{kms_key_name}"
+  }
+  placement {
+    managed_cluster {
+      cluster_name = "my-cluster"
+      config {
+        gce_cluster_config {
+          zone = "us-central1-a"
+          tags = ["foo", "bar"]
+        }
+        master_config {
+          num_instances = 1
+          machine_type = "n1-standard-1"
+          disk_config {
+            boot_disk_type = "pd-ssd"
+            boot_disk_size_gb = 15
+          }
+        }
+        worker_config {
+          num_instances = 3
+          machine_type = "n1-standard-2"
+          disk_config {
+            boot_disk_size_gb = 10
+            num_local_ssds = 2
+          }
+        }
+
+        secondary_worker_config {
+          num_instances = 2
+        }
+        software_config {
+          image_version = "%{version}"
+        }
+      }
+    }
+  }
+  jobs {
+    step_id = "someJob"
+    spark_job {
+      main_class = "SomeClass"
+    }
+  }
+  jobs {
+    step_id = "otherJob"
+    prerequisite_step_ids = ["someJob"]
+    presto_job {
+      query_file_uri = "someuri"
+    }
+  }
+  depends_on =[google_kms_crypto_key_iam_member.crypto_key]
+}
+
+data "google_project" "project" {}
+resource "google_kms_crypto_key_iam_member" "crypto_key" {
+  crypto_key_id = "%{kms_key_name}"
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member = "serviceAccount:service-${data.google_project.project.number}@dataproc-accounts.iam.gserviceaccount.com"
 }
 `, context)
 }
