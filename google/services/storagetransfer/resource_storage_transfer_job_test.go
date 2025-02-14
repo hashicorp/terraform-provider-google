75a590338f05a4baa341b3541a59d8d4d721ae25
The Magician <magic-modules@google.com>
2025-02-14 15:31:48 -0800

    Fix TestAccStorageTransferJob_posixSink (#13073) (#21437)
    [upstream:269cd7e8e062d3a0c925a95884859756bfafe116]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/storagetransfer/resource_storage_transfer_job_test.go
+++ google/services/storagetransfer/resource_storage_transfer_job_test.go
@@ -83,6 +83,17 @@ func TestAccStorageTransferJob_basic(t *testing.T) {
 func TestAccStorageTransferReplicationJob_basic(t *testing.T) {
 	t.Parallel()
 
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:service-{project_number}@gs-project-accounts.iam.gserviceaccount.com",
+			Role:   "roles/pubsub.publisher",
+		},
+		{
+			Member: "serviceAccount:project-{project_number}@storage-transfer-service.iam.gserviceaccount.com",
+			Role:   "roles/storagetransfer.serviceAgent",
+		},
+	})
+
 	testDataSourceBucketName := acctest.RandString(t, 10)
 	testDataSinkName := acctest.RandString(t, 10)
 	testTransferReplicationJobDescription := acctest.RandString(t, 10)
@@ -189,6 +200,13 @@ func TestAccStorageTransferJob_omitScheduleEndDate(t *testing.T) {
 func TestAccStorageTransferJob_posixSource(t *testing.T) {
 	t.Parallel()
 
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:project-{project_number}@storage-transfer-service.iam.gserviceaccount.com",
+			Role:   "roles/pubsub.admin",
+		},
+	})
+
 	testDataSinkName := acctest.RandString(t, 10)
 	testTransferJobDescription := acctest.RandString(t, 10)
 	testSourceAgentPoolName := fmt.Sprintf("tf-test-source-agent-pool-%s", acctest.RandString(t, 10))
@@ -211,6 +229,13 @@ func TestAccStorageTransferJob_posixSource(t *testing.T) {
 }
 func TestAccStorageTransferJob_posixSink(t *testing.T) {
 	t.Parallel()
+
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:project-{project_number}@storage-transfer-service.iam.gserviceaccount.com",
+			Role:   "roles/pubsub.admin",
+		},
+	})
 
 	testDataSourceName := acctest.RandString(t, 10)
 	testTransferJobDescription := acctest.RandString(t, 10)
@@ -430,6 +455,13 @@ func TestAccStorageTransferJob_notificationConfig(t *testing.T) {
 
 func TestAccStorageTransferJob_hdfsSource(t *testing.T) {
 	t.Parallel()
+
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:project-{project_number}@storage-transfer-service.iam.gserviceaccount.com",
+			Role:   "roles/pubsub.admin",
+		},
+	})
 
 	testDataSinkName := acctest.RandString(t, 10)
 	otherDataSinkName := acctest.RandString(t, 10)
@@ -985,19 +1017,11 @@ resource "google_storage_bucket_iam_member" "data_sink" {
   member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 }
 
-resource "google_project_iam_member" "pubsub" {
-	project = data.google_storage_transfer_project_service_account.default.project
-  role    = "roles/pubsub.admin"
-  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
 resource "google_storage_transfer_agent_pool" "foo" {
   name         = "%s"
   bandwidth_limit {
     limit_mbps = "120"
   }
-
-  depends_on = [google_project_iam_member.pubsub]
 }
 
 resource "google_storage_transfer_job" "transfer_job" {
@@ -1034,10 +1058,7 @@ resource "google_storage_transfer_job" "transfer_job" {
     }
   }
 
-  depends_on = [
-    google_storage_bucket_iam_member.data_sink,
-    google_project_iam_member.pubsub
-  ]
+  depends_on = [google_storage_bucket_iam_member.data_sink]
 }
 `, project, dataSinkBucketName, project, sourceAgentPoolName, transferJobDescription, project)
 }
@@ -1062,19 +1083,11 @@ resource "google_storage_bucket_iam_member" "data_sink" {
   member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 }
 
-resource "google_project_iam_member" "pubsub" {
-	project = data.google_storage_transfer_project_service_account.default.project
-  role    = "roles/pubsub.admin"
-  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
 resource "google_storage_transfer_agent_pool" "foo" {
   name         = "%s"
   bandwidth_limit {
     limit_mbps = "120"
   }
-
-  depends_on = [google_project_iam_member.pubsub]
 }
 
 resource "google_storage_transfer_job" "transfer_job" {
@@ -1111,10 +1124,7 @@ resource "google_storage_transfer_job" "transfer_job" {
     }
   }
 
-  depends_on = [
-    google_storage_bucket_iam_member.data_sink,
-    google_project_iam_member.pubsub
-  ]
+  depends_on = [google_storage_bucket_iam_member.data_sink]
 }
 `, project, dataSinkBucketName, project, sourceAgentPoolName, transferJobDescription, project, hdfsPath, gcsPath)
 }
@@ -1139,19 +1149,11 @@ resource "google_storage_bucket_iam_member" "data_source" {
   member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 }
 
-resource "google_project_iam_member" "pubsub" {
-	project = data.google_storage_transfer_project_service_account.default.project
-  role    = "roles/pubsub.admin"
-  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
 resource "google_storage_transfer_agent_pool" "foo" {
   name         = "%s"
   bandwidth_limit {
     limit_mbps = "120"
   }
-
-  depends_on = [google_project_iam_member.pubsub]
 }
 
 resource "google_storage_transfer_job" "transfer_job" {
@@ -1187,10 +1189,7 @@ resource "google_storage_transfer_job" "transfer_job" {
     }
   }
 
-  depends_on = [
-    google_storage_bucket_iam_member.data_source,
-    google_project_iam_member.pubsub
-  ]
+  depends_on = [google_storage_bucket_iam_member.data_source]
 }
 `, project, dataSourceBucketName, project, sinkAgentPoolName, transferJobDescription, project)
 }
@@ -1691,26 +1690,6 @@ data "google_storage_transfer_project_service_account" "default" {
   project = "%s"
 }
 
-data "google_project" "my_project" {
-  project_id = "%s"
-}
-
-resource "google_project_iam_binding" "pubsub_publisher" {
-  project = "%s"
-  role    = "roles/pubsub.publisher"
-  members = [
-    "serviceAccount:service-${data.google_project.my_project.number}@gs-project-accounts.iam.gserviceaccount.com",
-  ]
-}
-
-resource "google_project_iam_binding" "service_agent_binding" {
-  project = "%s"
-  role    = "roles/storagetransfer.serviceAgent"
-  members = [
-    "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}",
-  ]
-}
-
 resource "google_storage_bucket" "data_source" {
   name          = "%s"
   project       = "%s"
@@ -1756,37 +1735,16 @@ resource "google_storage_transfer_job" "transfer_job" {
 
   depends_on = [
     google_storage_bucket_iam_member.data_source,
-    google_storage_bucket_iam_member.data_sink,
-    google_project_iam_binding.pubsub_publisher
+    google_storage_bucket_iam_member.data_sink
   ]
 }
-`, project, project, project, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project)
+`, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project)
 }
 
 func testAccStorageTransferReplicationJob_with_transferOptions(project string, dataSourceBucketName string, dataSinkBucketName string, transferJobDescription string, overwriteObjectsAlreadyExistingInSink bool, deleteObjectsUniqueInSink bool, overwriteWhenVal string) string {
 	return fmt.Sprintf(`
 data "google_storage_transfer_project_service_account" "default" {
   project = "%s"
-}
-
-data "google_project" "my_project" {
-  project_id = "%s"
-}
-
-resource "google_project_iam_binding" "pubsub_publisher" {
-  project = "%s"
-  role    = "roles/pubsub.publisher"
-  members = [
-    "serviceAccount:service-${data.google_project.my_project.number}@gs-project-accounts.iam.gserviceaccount.com",
-  ]
-}
-
-resource "google_project_iam_binding" "service_agent_binding" {
-  project = "%s"
-  role    = "roles/storagetransfer.serviceAgent"
-  members = [
-    "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}",
-  ]
 }
 
 resource "google_storage_bucket" "data_source" {
@@ -1852,9 +1810,8 @@ resource "google_storage_transfer_job" "transfer_job" {
 
   depends_on = [
     google_storage_bucket_iam_member.data_source,
-    google_storage_bucket_iam_member.data_sink,
-    google_project_iam_binding.pubsub_publisher
+    google_storage_bucket_iam_member.data_sink
   ]
 }
-`, project, project, project, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project, overwriteObjectsAlreadyExistingInSink, deleteObjectsUniqueInSink, overwriteWhenVal)
+`, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project, overwriteObjectsAlreadyExistingInSink, deleteObjectsUniqueInSink, overwriteWhenVal)
 }
