adad4f9cb3dc801627b4b7d6868c1f778a1ed754
The Magician <magic-modules@google.com>
2025-02-18 14:44:56 -0800

    Promoting BackupDR Resources to GA. (#13091) (#21480)
    [upstream:a87ff8de35f728aac66b828a03195000f374f6b4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/backupdr/data_source_backup_dr_backup_plan.go
+++ google/services/backupdr/data_source_backup_dr_backup_plan.go
@@ -1,3 +1,53 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 package backupdr
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceGoogleCloudBackupDRBackupPlan() *schema.Resource {
+
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceBackupDRBackupPlan().Schema)
+	// Set 'Required' schema elements
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "backup_plan_id", "location")
+
+	// Set 'Optional' schema elements
+	tpgresource.AddOptionalFieldsToSchema(dsSchema, "project")
+	return &schema.Resource{
+		Read:   dataSourceGoogleCloudBackupDRBackupPlanRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleCloudBackupDRBackupPlanRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+
+	project, err := tpgresource.GetProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	location, err := tpgresource.GetLocation(d, config)
+	if err != nil {
+		return err
+	}
+
+	backup_plan_id := d.Get("backup_plan_id").(string)
+
+	id := fmt.Sprintf("projects/%s/locations/%s/backupPlans/%s", project, location, backup_plan_id)
+	d.SetId(id)
+	err = resourceBackupDRBackupPlanRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	if d.Id() == "" {
+		return fmt.Errorf("%s not found", id)
+	}
+
+	return nil
+}
--- google/services/backupdr/data_source_backup_dr_management_server_test.go
+++ google/services/backupdr/data_source_backup_dr_management_server_test.go
@@ -1,3 +1,98 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 package backupdr_test
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+	"strings"
+	"testing"
+)
+
+func TestAccDataSourceGoogleBackupDRManagementServer_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"network_name":  acctest.BootstrapSharedServiceNetworkingConnection(t, "backupdr-managementserver-basic"),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBackupDRManagementServerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleBackupDRManagementServer_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckDataSourceStateMatchesResourceState("data.google_backup_dr_management_server.foo", "google_backup_dr_management_server.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckBackupDRManagementServerDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_backup_dr_management_server" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := acctest.GoogleProviderConfig(t)
+
+			url, err := tpgresource.ReplaceVarsForTest(config, rs, `{{BackupDRBasePath}}projects/{{project}}/locations/{{location}}/managementServers`)
+			if err != nil {
+				return err
+			}
+
+			billingProject := ""
+
+			if config.BillingProject != "" {
+				billingProject = config.BillingProject
+			}
+
+			_, err = transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+				Config:    config,
+				Method:    "GET",
+				Project:   billingProject,
+				RawURL:    url,
+				UserAgent: config.UserAgent,
+			})
+			if err == nil {
+				return fmt.Errorf("BackupDRManagementServer still exists at %s", url)
+			}
+		}
+
+		return nil
+	}
+}
+
+func testAccDataSourceGoogleBackupDRManagementServer_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_compute_network" "default" {
+  name = "%{network_name}"
+}
+resource "google_backup_dr_management_server" "foo" {
+ location = "us-central1"
+  name     = "tf-test-management-server%{random_suffix}"
+  type     = "BACKUP_RESTORE" 
+    networks {
+    network      = data.google_compute_network.default.id
+    peering_mode = "PRIVATE_SERVICE_ACCESS"
+  }
+}
+
+data "google_backup_dr_management_server" "foo" {
+  location =  "us-central1"
+  depends_on = [ google_backup_dr_management_server.foo ]
+}
+`, context)
+}
