7c70d4d345c3921804faac850c9ee6ae74334d9a
The Magician <magic-modules@google.com>
2025-02-28 16:32:11 -0800

    Add new resource IAM OAuth Client for Workforce Identity Federation. (#13209) (#21660)
    [upstream:fed2c989fb252898906b4b2301c74673a4648c92]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/iamworkforcepool/resource_iam_oauth_client_test.go
@@ -0,0 +1,137 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package iamworkforcepool_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccIAMWorkforcePoolOauthClient_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckIAMWorkforcePoolOauthClientDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIAMWorkforcePoolOauthClient_basic(context),
+			},
+			{
+				ResourceName:            "google_iam_oauth_client.example",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "oauth_client_id"},
+			},
+			{
+				Config: testAccIAMWorkforcePoolOauthClient_basic_update(context),
+			},
+			{
+				ResourceName:            "google_iam_oauth_client.example",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "oauth_client_id"},
+			},
+		},
+	})
+}
+
+func testAccIAMWorkforcePoolOauthClient_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_iam_oauth_client" "example" {
+  oauth_client_id = "tf-test-example-client-id%{random_suffix}"
+  location                  = "global"
+  allowed_grant_types       = ["AUTHORIZATION_CODE_GRANT"]
+  allowed_redirect_uris     = ["https://www.example.com"]
+  allowed_scopes            = ["https://www.googleapis.com/auth/cloud-platform"]
+  client_type               = "CONFIDENTIAL_CLIENT"
+}
+`, context)
+}
+
+func testAccIAMWorkforcePoolOauthClient_basic_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_iam_oauth_client" "example" {
+  oauth_client_id = "tf-test-example-client-id%{random_suffix}"
+  location                  = "global"
+  allowed_grant_types       = ["AUTHORIZATION_CODE_GRANT"]
+  allowed_redirect_uris     = ["https://www.update.com"]
+  allowed_scopes            = ["https://www.googleapis.com/auth/cloud-platform", "openid"]
+  client_type               = "CONFIDENTIAL_CLIENT"
+}
+`, context)
+}
+
+func TestAccIAMWorkforcePoolOauthClient_full(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckIAMWorkforcePoolOauthClientDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIAMWorkforcePoolOauthClient_full(context),
+			},
+			{
+				ResourceName:            "google_iam_oauth_client.example",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "oauth_client_id"},
+			},
+			{
+				Config: testAccIAMWorkforcePoolOauthClient_full_update(context),
+			},
+			{
+				ResourceName:            "google_iam_oauth_client.example",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "oauth_client_id"},
+			},
+		},
+	})
+}
+
+func testAccIAMWorkforcePoolOauthClient_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_iam_oauth_client" "example" {
+  oauth_client_id = "tf-test-example-client-id%{random_suffix}"
+  display_name              = "Display Name of OAuth client"
+  description               = "A sample OAuth client"
+  location                  = "global"
+  disabled                  = false
+  allowed_grant_types       = ["AUTHORIZATION_CODE_GRANT"]
+  allowed_redirect_uris     = ["https://www.example.com"]
+  allowed_scopes            = ["https://www.googleapis.com/auth/cloud-platform"]
+  client_type               = "CONFIDENTIAL_CLIENT"
+}
+`, context)
+}
+
+func testAccIAMWorkforcePoolOauthClient_full_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_iam_oauth_client" "example" {
+  oauth_client_id = "tf-test-example-client-id%{random_suffix}"
+  display_name              = "Updated displayName"
+  description               = "Updated description"
+  location                  = "global"
+  disabled                  = true
+  allowed_grant_types       = ["AUTHORIZATION_CODE_GRANT", ]
+  allowed_redirect_uris     = ["https://www.update.com"]
+  allowed_scopes            = ["https://www.googleapis.com/auth/cloud-platform", "openid"]
+  client_type               = "CONFIDENTIAL_CLIENT"
+}
+`, context)
+}
