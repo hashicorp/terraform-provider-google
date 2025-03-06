727eece2a26ba2abedcf7ec8b22b307e204d164f
Modular Magician <magic-modules@google.com>
2025-03-06 17:50:13 +0000

    Add google_eventarc_google_api_source resource (#13186)
    [upstream:bbbf4a3c503ddb94d43e5e43c49ee479180532fe]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/eventarc/resource_eventarc_message_bus_test.go
+++ google/services/eventarc/resource_eventarc_message_bus_test.go
@@ -19,9 +19,10 @@ import (
 // We make sure not to run tests in parallel, since only one MessageBus per project is supported.
 func TestAccEventarcMessageBus(t *testing.T) {
 	testCases := map[string]func(t *testing.T){
-		"basic":     testAccEventarcMessageBus_basic,
-		"cryptoKey": testAccEventarcMessageBus_cryptoKey,
-		"update":    testAccEventarcMessageBus_update,
+		"basic":           testAccEventarcMessageBus_basic,
+		"cryptoKey":       testAccEventarcMessageBus_cryptoKey,
+		"update":          testAccEventarcMessageBus_update,
+		"googleApiSource": testAccEventarcMessageBus_googleApiSource,
 	}
 
 	for name, tc := range testCases {
@@ -228,6 +229,67 @@ resource "google_eventarc_message_bus" "primary" {
   logging_config {
     log_severity = "DEBUG"
   }
+}
+`, context)
+}
+
+// Although this test is defined in resource_eventarc_message_bus_test, it is primarily
+// concerned with testing the GoogleApiSource resource, which depends on a singleton MessageBus.
+func testAccEventarcMessageBus_googleApiSource(t *testing.T) {
+	region := envvar.GetTestRegionFromEnv()
+	context := map[string]interface{}{
+		"project_number": envvar.GetTestProjectNumberFromEnv(),
+		"key1":           acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", region, "tf-bootstrap-eventarc-googleapisource-key1").CryptoKey.Name,
+		"region":         region,
+		"random_suffix":  acctest.RandString(t, 10),
+	}
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:service-{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com",
+			Role:   "roles/cloudkms.cryptoKeyEncrypterDecrypter",
+		},
+	})
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckEventarcGoogleApiSourceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEventarcMessageBus_googleApiSourceCfg(context),
+			},
+			{
+				ResourceName:            "google_eventarc_google_api_source.primary",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels", "annotations"},
+			},
+		},
+	})
+}
+
+func testAccEventarcMessageBus_googleApiSourceCfg(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_eventarc_google_api_source" "primary" {
+  location             = "%{region}"
+  google_api_source_id = "tf-test-googleapisource%{random_suffix}"
+  display_name         = "basic google api source"
+  destination          = google_eventarc_message_bus.message_bus.id
+  crypto_key_name      = "%{key1}"
+  labels = {
+    test_label = "test-eventarc-label"
+  }
+  annotations = {
+    test_annotation = "test-eventarc-annotation"
+  }
+  logging_config {
+    log_severity = "DEBUG"
+  }
+}
+
+resource "google_eventarc_message_bus" "message_bus" {
+  location       = "%{region}"
+  message_bus_id = "tf-test-messagebus%{random_suffix}"
 }
 `, context)
 }
