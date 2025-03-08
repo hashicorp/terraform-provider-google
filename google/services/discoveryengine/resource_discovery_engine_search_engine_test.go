1761c227e9ea4796b99c68a00334e6b981b5053f
The Magician <magic-modules@google.com>
2025-03-07 16:15:47 -0800

    Fix issue#20603 (#13295) (#21759)
    [upstream:184ff95c3ca98aa0cbae943cc9dadd4eb3b6b89e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/discoveryengine/resource_discovery_engine_search_engine_test.go
+++ google/services/discoveryengine/resource_discovery_engine_search_engine_test.go
@@ -3,9 +3,10 @@
 package discoveryengine_test
 
 import (
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"testing"
 )
 
 func TestAccDiscoveryEngineSearchEngine_discoveryengineSearchengineBasicExample_update(t *testing.T) {
@@ -55,12 +56,21 @@ resource "google_discovery_engine_data_store" "basic" {
     solution_types              = ["SOLUTION_TYPE_SEARCH"]
     create_advanced_site_search = false
     }
+resource "google_discovery_engine_data_store" "second" {
+    location                    = "global"
+    data_store_id               = "tf-test-example2-datastore%{random_suffix}"
+    display_name                = "tf-test-structured-datastore2"
+    industry_vertical           = "GENERIC"
+    content_config              = "NO_CONTENT"
+    solution_types              = ["SOLUTION_TYPE_SEARCH"]
+    create_advanced_site_search = false
+    }
 resource "google_discovery_engine_search_engine" "basic" {
   engine_id = "tf-test-example-engine-id%{random_suffix}"
   collection_id = "default_collection"
   location = google_discovery_engine_data_store.basic.location
   display_name = "Example Display Name"
-  data_store_ids = [google_discovery_engine_data_store.basic.data_store_id]
+  data_store_ids = [google_discovery_engine_data_store.basic.data_store_id, google_discovery_engine_data_store.second.data_store_id]
   industry_vertical = google_discovery_engine_data_store.basic.industry_vertical
   common_config {
     company_name = "Example Company Name"
@@ -79,6 +89,15 @@ resource "google_discovery_engine_data_store" "basic" {
     location                    = "global"
     data_store_id               = "tf-test-example-datastore%{random_suffix}"
     display_name                = "tf-test-structured-datastore"
+    industry_vertical           = "GENERIC"
+    content_config              = "NO_CONTENT"
+    solution_types              = ["SOLUTION_TYPE_SEARCH"]
+    create_advanced_site_search = false
+    }
+resource "google_discovery_engine_data_store" "second" {
+    location                    = "global"
+    data_store_id               = "tf-test-example2-datastore%{random_suffix}"
+    display_name                = "tf-test-structured-datastore2"
     industry_vertical           = "GENERIC"
     content_config              = "NO_CONTENT"
     solution_types              = ["SOLUTION_TYPE_SEARCH"]
