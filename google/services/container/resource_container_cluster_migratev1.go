f20fe7cbbc21230ed4e3a27d03582af6301d7846
The Magician <magic-modules@google.com>
2025-03-12 06:39:31 -0700

    Added support for additional values KCP_HPA in `google_container_clus… (#12221) (#21836)
    [upstream:ad2ff30080e3b6b9d3129fdac496694acf9c7120]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/container/resource_container_cluster_migratev1.go
+++ google/services/container/resource_container_cluster_migratev1.go
@@ -666,10 +666,10 @@ func resourceContainerClusterResourceV1() *schema.Resource {
 						"enable_components": {
 							Type:        schema.TypeList,
 							Required:    true,
-							Description: `GKE components exposing logs. Valid values include SYSTEM_COMPONENTS, APISERVER, CONTROLLER_MANAGER, KCP_CONNECTION, KCP_SSHD, SCHEDULER, and WORKLOADS.`,
+							Description: `GKE components exposing logs. Valid values include SYSTEM_COMPONENTS, APISERVER, CONTROLLER_MANAGER, KCP_CONNECTION, KCP_SSHD, KCP_HPA, SCHEDULER, and WORKLOADS.`,
 							Elem: &schema.Schema{
 								Type:         schema.TypeString,
-								ValidateFunc: validation.StringInSlice([]string{"SYSTEM_COMPONENTS", "APISERVER", "CONTROLLER_MANAGER", "KCP_CONNECTION", "KCP_SSHD", "SCHEDULER", "WORKLOADS"}, false),
+								ValidateFunc: validation.StringInSlice([]string{"SYSTEM_COMPONENTS", "APISERVER", "CONTROLLER_MANAGER", "KCP_CONNECTION", "KCP_SSHD", "KCP_HPA", "SCHEDULER", "WORKLOADS"}, false),
 							},
 						},
 					},
