7cf09b5b8ea96bfe53f32ea8d3f4011dcf5612c4
Modular Magician <magic-modules@google.com>
2025-03-21 20:32:05 +0000

    Add storage_control_organization_intelligence_config resource. (#13386)
    [upstream:4a43779c55f7e699a6815983b096b9066fddb655]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/storagecontrol/data_source_storage_control_organizaton_intelligence_config_test.go
@@ -0,0 +1,48 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package storagecontrol_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccDataSourceGoogleStorageControlOrganizationIntelligenceConfig_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"org_id":        envvar.GetTestOrgFromEnv(t),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleStorageControlOrganizationIntelligenceConfig_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckDataSourceStateMatchesResourceState("data.google_storage_control_organization_intelligence_config.organization_storage_intelligence", "google_storage_control_organization_intelligence_config.organization_storage_intelligence"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleStorageControlOrganizationIntelligenceConfig_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_storage_control_organization_intelligence_config" "organization_storage_intelligence" {
+  name = "%{org_id}"
+  edition_config = "STANDARD"
+}
+
+data "google_storage_control_organization_intelligence_config" "organization_storage_intelligence" {
+  name = google_storage_control_organization_intelligence_config.organization_storage_intelligence.name
+}
+`, context)
+}
