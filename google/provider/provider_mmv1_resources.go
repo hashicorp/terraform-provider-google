a167378bc8e54c85559dca1e93933e6b763d0938
The Magician <magic-modules@google.com>
2025-03-24 10:04:49 -0700

    Add google_eventarc_enrollment resource (#13284) (#22028)
    [upstream:c07fc51c63e0f3a2c9b1c36ac0db18cc64222168]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -498,9 +498,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 563
+// Generated resources: 564
 // Generated IAM resources: 279
-// Total generated resources: 842
+// Total generated resources: 843
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -935,6 +935,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_edgenetwork_subnet":                                                  edgenetwork.ResourceEdgenetworkSubnet(),
 	"google_essential_contacts_contact":                                          essentialcontacts.ResourceEssentialContactsContact(),
 	"google_eventarc_channel":                                                    eventarc.ResourceEventarcChannel(),
+	"google_eventarc_enrollment":                                                 eventarc.ResourceEventarcEnrollment(),
 	"google_eventarc_google_api_source":                                          eventarc.ResourceEventarcGoogleApiSource(),
 	"google_eventarc_google_channel_config":                                      eventarc.ResourceEventarcGoogleChannelConfig(),
 	"google_eventarc_message_bus":                                                eventarc.ResourceEventarcMessageBus(),
