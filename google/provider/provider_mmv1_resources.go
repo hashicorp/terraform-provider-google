55c55f59558a5fef34b480ff439eb5ec64afdcd9
Modular Magician <magic-modules@google.com>
2025-03-27 17:54:01 +0000

    Promote google_chronicle_rule resource of chronicle to GA (#13476)
    [upstream:2c915f41d4ff6915ded6ea46f9bc67caaf20c7d2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -501,9 +501,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 567
+// Generated resources: 568
 // Generated IAM resources: 282
-// Total generated resources: 849
+// Total generated resources: 850
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -636,6 +636,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_certificate_manager_trust_config":                                    certificatemanager.ResourceCertificateManagerTrustConfig(),
 	"google_chronicle_data_access_label":                                         chronicle.ResourceChronicleDataAccessLabel(),
 	"google_chronicle_data_access_scope":                                         chronicle.ResourceChronicleDataAccessScope(),
+	"google_chronicle_rule":                                                      chronicle.ResourceChronicleRule(),
 	"google_chronicle_watchlist":                                                 chronicle.ResourceChronicleWatchlist(),
 	"google_cloud_asset_folder_feed":                                             cloudasset.ResourceCloudAssetFolderFeed(),
 	"google_cloud_asset_organization_feed":                                       cloudasset.ResourceCloudAssetOrganizationFeed(),
--- google/services/chronicle/resource_chronicle_rule_test.go
+++ google/services/chronicle/resource_chronicle_rule_test.go
@@ -1,3 +1,80 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 package chronicle_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccChronicleRule_chronicleRuleBasicExample_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"chronicle_id":  envvar.GetTestChronicleInstanceIdFromEnv(t),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckChronicleRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccChronicleRule_chronicleRuleBasicExample_basic(context),
+			},
+			{
+				Config: testAccChronicleRule_chronicleRuleBasicExample_update(context),
+			},
+		},
+	})
+}
+
+func testAccChronicleRule_chronicleRuleBasicExample_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_chronicle_data_access_scope" "data_access_scope_test" {
+ location = "us"
+ instance = "%{chronicle_id}"
+ data_access_scope_id = "tf-test-scope-name%{random_suffix}"
+ description = "scope-description"
+ allowed_data_access_labels {
+   log_type = "GCP_CLOUDAUDIT"
+ }
+}
+
+resource "google_chronicle_rule" "example" {
+ location = "us"
+ instance = "%{chronicle_id}"
+ scope = resource.google_chronicle_data_access_scope.data_access_scope_test.name
+ text = <<-EOT
+             rule test_rule { meta: events:  $userid = $e.principal.user.userid  match: $userid over 10m condition: $e }
+         EOT
+}
+`, context)
+}
+
+func testAccChronicleRule_chronicleRuleBasicExample_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_chronicle_data_access_scope" "data_access_scope_test" {
+ location = "us"
+ instance = "%{chronicle_id}"
+ data_access_scope_id = "tf-test-scope-name%{random_suffix}"
+ description = "scope-description"
+ allowed_data_access_labels {
+   log_type = "GCP_CLOUDAUDIT"
+ }
+}
+
+resource "google_chronicle_rule" "example" {
+ location = "us"
+ instance = "%{chronicle_id}"
+ text = <<-EOT
+             rule test_rule { meta: events:  $updated_userid = $e.principal.user.userid  match: $updated_userid over 10m condition: $e }
+         EOT
+}
+`, context)
+}
