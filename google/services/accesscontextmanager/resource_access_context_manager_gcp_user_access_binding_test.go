5a38b5d5363106905319d4bac93d4238c9a10b93
The Magician <magic-modules@google.com>
2025-04-03 11:59:51 -0700

    Add session settings support to access context (#13523) (#22227)
    [upstream:d0f8dc4dd63e11d1b7cdabe28458f8c66f65140d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/accesscontextmanager/resource_access_context_manager_gcp_user_access_binding_test.go
+++ google/services/accesscontextmanager/resource_access_context_manager_gcp_user_access_binding_test.go
@@ -95,6 +95,13 @@ resource "google_access_context_manager_gcp_user_access_binding" "gcp_user_acces
   access_levels   = [
     google_access_context_manager_access_level.tf_test_access_level_id_for_user_access_binding%{random_suffix}.name,
   ]
+  session_settings {
+	max_inactivity = "300s"
+	session_length = "1800s"
+	session_length_enabled = true
+	session_reauth_method = "LOGIN"
+	use_oidc_max_age = false
+  }
 }
 `, context)
 }
