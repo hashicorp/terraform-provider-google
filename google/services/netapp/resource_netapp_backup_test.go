f27f9e382855d2a25bd8b4d322a7bae78b19b241
The Magician <magic-modules@google.com>
2025-04-15 10:51:21 -0700

    added test for NetApp Flex Backup (#13667) (#22354)
    [upstream:d04989c26ef3d4873a7702c4690ade9a1b0860a1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/netapp/resource_netapp_backup_test.go
+++ google/services/netapp/resource_netapp_backup_test.go
@@ -162,3 +162,88 @@ resource "google_netapp_backup" "test_backup" {
 }
 `, context)
 }
+
+func TestAccNetappBackup_NetappFlexBackup(t *testing.T) {
+	context := map[string]interface{}{
+		"network_name":  acctest.BootstrapSharedServiceNetworkingConnection(t, "gcnv-network-config-1", acctest.ServiceNetworkWithParentService("netapp.servicenetworking.goog")),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetappBackupDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetappBackup_FlexBackup(context),
+			},
+			{
+				ResourceName:            "google_netapp_backup.test_backup",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels", "vault_name"},
+			},
+		},
+	})
+}
+
+func testAccNetappBackup_FlexBackup(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_compute_network" "default" {
+  name = "%{network_name}"
+}
+
+resource "google_netapp_storage_pool" "default" {
+  name = "tf-test-backup-pool%{random_suffix}"
+  location = "us-east4"
+  service_level = "FLEX"
+  capacity_gib = "2048"
+  network = data.google_compute_network.default.id
+  zone = "us-east4-a"
+  replica_zone = "us-east4-b"
+}
+
+resource "google_netapp_volume" "default" {
+  name = "tf-test-backup-volume%{random_suffix}"
+  location = google_netapp_storage_pool.default.location
+  capacity_gib = "100"
+  share_name = "tf-test-backup-volume%{random_suffix}"
+  storage_pool = google_netapp_storage_pool.default.name
+  protocols = ["NFSV3"]
+  deletion_policy = "FORCE"
+  backup_config {
+    backup_vault = google_netapp_backup_vault.default.id
+  }
+}
+
+resource "google_netapp_backup_vault" "default" {
+  name = "tf-test-backup-vault%{random_suffix}"
+  location = google_netapp_storage_pool.default.location
+}
+
+resource "google_netapp_volume_snapshot" "default" {
+    depends_on = [google_netapp_volume.default]
+    location = google_netapp_volume.default.location
+    volume_name = google_netapp_volume.default.name
+    description = "This is a test description"
+    name = "testvolumesnap%{random_suffix}"
+    labels = {
+      key= "test"
+      value= "snapshot"
+    }
+  }
+
+resource "google_netapp_backup" "test_backup" {
+  name = "tf-test-test-backup%{random_suffix}"
+  description = "This is a test backup"
+  source_volume = google_netapp_volume.default.id
+  location = google_netapp_backup_vault.default.location
+  vault_name = google_netapp_backup_vault.default.name
+  source_snapshot = google_netapp_volume_snapshot.default.id
+  labels = {
+    key= "test"
+    value= "backup"
+  }
+}
+`, context)
+}
