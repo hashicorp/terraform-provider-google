2b70395353975217054a23aa77c63e42e106a0c4
Modular Magician <magic-modules@google.com>
2025-04-17 17:54:55 +0000

    Adding terraform support for GKEBackup Backup and Restore channel resources. (#13516)
    [upstream:a47441ecc04833debcf4ac125bd4795152d57a1f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google/services/gkebackup/resource_gke_backup_restore_channel_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package gkebackup_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccGKEBackupRestoreChannel_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":             envvar.GetTestProjectFromEnv(),
+		"destination_project": "projects/331279474308",
+		"random_suffix":       acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckGKEBackupRestoreChannelDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGKEBackupRestoreChannel_basic(context),
+			},
+			{
+				ResourceName:            "google_gke_backup_restore_channel.basic",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccGKEBackupRestoreChannel_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_gke_backup_restore_channel" "basic" {
+  name = "tf-test-basic-channel%{random_suffix}"
+  location = "us-central1"
+  description = ""
+  destination_project = "%{destination_project}"
+  labels = { "key": "some-value" }
+}
+`, context)
+}
