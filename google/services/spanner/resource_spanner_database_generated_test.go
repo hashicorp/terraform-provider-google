fd2cc16f47316cfa19c3862052418512a60c072e
Modular Magician <magic-modules@google.com>
2025-05-07 19:53:57 +0000

    Add default_time_zone to Spanner database resource (#13762)
    [upstream:b4487f55c5e933b0699f5160bc40faf77546ca39]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/spanner/resource_spanner_database_generated_test.go
+++ google/services/spanner/resource_spanner_database_generated_test.go
@@ -68,6 +68,7 @@ resource "google_spanner_database" "database" {
   instance = google_spanner_instance.main.name
   name     = "tf-test-my-database%{random_suffix}"
   version_retention_period = "3d"
+  default_time_zone = "UTC"
   ddl = [
     "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
     "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
