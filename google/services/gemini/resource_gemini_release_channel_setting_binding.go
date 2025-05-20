7bb6598ea50d1ca40bebb1a091a5b4b43695e78f
The Magician <magic-modules@google.com>
2025-05-20 13:47:59 -0700

    Fixed permadiff on `product` in `google_gemini_release_channel_setting_binding` (#13972) (#22929)
    [upstream:bcd9783ed41cc42af6cb5448be0d58aec2c65edb]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/gemini/resource_gemini_release_channel_setting_binding.go
+++ google/services/gemini/resource_gemini_release_channel_setting_binding.go
@@ -92,6 +92,7 @@ Please refer to the field 'effective_labels' for all of the labels present on th
 			},
 			"product": {
 				Type:         schema.TypeString,
+				Computed:     true,
 				Optional:     true,
 				ValidateFunc: verify.ValidateEnum([]string{"GEMINI_CLOUD_ASSIST", "GEMINI_CODE_ASSIST", ""}),
 				Description:  `Product type of the setting binding. Possible values: ["GEMINI_CLOUD_ASSIST", "GEMINI_CODE_ASSIST"]`,
