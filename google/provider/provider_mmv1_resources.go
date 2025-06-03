90cf351290f3596547c4e0090605adcaac85d5b5
Modular Magician <magic-modules@google.com>
2025-06-03 22:50:57 +0000

    feat: added `google_compute_snapshot_settings` resource (#14086)
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    [upstream:ed8579e474b4a5f18c984efd406ee2d92a1fd250]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -540,9 +540,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 614
+// Generated resources: 615
 // Generated IAM resources: 309
-// Total generated resources: 923
+// Total generated resources: 924
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -857,6 +857,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_snapshot_iam_binding":                                        tpgiamresource.ResourceIamBinding(compute.ComputeSnapshotIamSchema, compute.ComputeSnapshotIamUpdaterProducer, compute.ComputeSnapshotIdParseFunc),
 	"google_compute_snapshot_iam_member":                                         tpgiamresource.ResourceIamMember(compute.ComputeSnapshotIamSchema, compute.ComputeSnapshotIamUpdaterProducer, compute.ComputeSnapshotIdParseFunc),
 	"google_compute_snapshot_iam_policy":                                         tpgiamresource.ResourceIamPolicy(compute.ComputeSnapshotIamSchema, compute.ComputeSnapshotIamUpdaterProducer, compute.ComputeSnapshotIdParseFunc),
+	"google_compute_snapshot_settings":                                           compute.ResourceComputeSnapshotSettings(),
 	"google_compute_ssl_certificate":                                             compute.ResourceComputeSslCertificate(),
 	"google_compute_ssl_policy":                                                  compute.ResourceComputeSslPolicy(),
 	"google_compute_storage_pool":                                                compute.ResourceComputeStoragePool(),
