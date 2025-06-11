e495548e8d9dc9c9c92816bae93bdb2eda9490dc
Modular Magician <magic-modules@google.com>
2025-06-11 02:30:04 +0000

    Added Fingerprint to TargetHttpProxy and TartgetHttpsProxy (#14193)
    [upstream:50cb21c344228c799747b74044b5dca798c85ab2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_target_http_proxy_generated_test.go
+++ google/services/compute/resource_compute_target_http_proxy_generated_test.go
@@ -214,6 +214,81 @@ resource "google_compute_url_map" "default" {
 `, context)
 }
 
+func TestAccComputeTargetHttpProxy_targetHttpProxyFingerprintExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeTargetHttpProxyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeTargetHttpProxy_targetHttpProxyFingerprintExample(context),
+			},
+			{
+				ResourceName:            "google_compute_target_http_proxy.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"url_map"},
+			},
+		},
+	})
+}
+
+func testAccComputeTargetHttpProxy_targetHttpProxyFingerprintExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_target_http_proxy" "default" {
+  name    = "tf-test-test-fingerprint-proxy%{random_suffix}"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "tf-test-url-map%{random_suffix}"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "tf-test-backend-service%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "tf-test-http-health-check%{random_suffix}"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+output "target_http_proxy_fingerprint" {
+  value       = google_compute_target_http_proxy.default.fingerprint
+  description = "The fingerprint of the target HTTP proxy for optimistic locking"
+}
+`, context)
+}
+
 func testAccCheckComputeTargetHttpProxyDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
