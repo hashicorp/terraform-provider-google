08b87d442ce86f8be18866de9ff2e241100970e1
The Magician <magic-modules@google.com>
2025-06-11 16:35:19 -0700

    Fix `target_server` validation for service attachment (#14195) (#23243)
    [upstream:a2335a05eb5feae5df981c54e94e9cca82ef0230]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_service_attachment.go
+++ google/services/compute/resource_compute_service_attachment.go
@@ -891,11 +891,12 @@ func expandComputeServiceAttachmentConnectionPreference(v interface{}, d tpgreso
 
 func expandComputeServiceAttachmentTargetService(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	resource := strings.Split(v.(string), "/")
-	resourceKind := resource[len(resource)-2]
-	resourceBound := resource[len(resource)-4]
 	if len(resource) < 4 {
 		return nil, fmt.Errorf("invalid value for target_service")
 	}
+
+	resourceKind := resource[len(resource)-2]
+	resourceBound := resource[len(resource)-4]
 
 	_, err := tpgresource.ParseRegionalFieldValue(resourceKind, v.(string), "project", resourceBound, "zone", d, config, true)
 	if err != nil {
