58443d3124b8499c84d941f55cc685d34063b7e4
The Magician <magic-modules@google.com>
2025-07-09 12:32:42 -0700

    fix apigee instance tests (#14484) (#23558)
    [upstream:db1d26ce50420f8f205249325bc3d65094088ab6]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_instance_update_test.go
+++ google/services/apigee/resource_apigee_instance_update_test.go
@@ -138,7 +138,7 @@ resource "google_apigee_instance" "apigee_instance" {
   ]
 
   access_logging_config {
-    enabled = false,
+    enabled = false
     filter  = "status_code >= 0 && status_code < 600"
   }
 }
@@ -227,7 +227,7 @@ resource "google_apigee_instance" "apigee_instance" {
   ]
 
   access_logging_config {
-    enabled = true,
+    enabled = true
     filter  = "status_code >= 200 && status_code < 300"
   }
 }
