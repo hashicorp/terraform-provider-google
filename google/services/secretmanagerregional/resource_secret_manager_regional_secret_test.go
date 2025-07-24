627d2a710217a6edd664e47529eb95f8e3f2825c
Modular Magician <magic-modules@google.com>
2025-07-24 18:19:48 +0000

    Adding Tags support for Regional Secrets  (#14314)
    [upstream:a191acaa394ecc35a381c4645e01bd8b8a7f26dc]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/secretmanagerregional/resource_secret_manager_regional_secret_test.go
+++ google/services/secretmanagerregional/resource_secret_manager_regional_secret_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
@@ -595,6 +596,38 @@ func TestAccSecretManagerRegionalRegionalSecret_deletionprotection(t *testing.T)
 			},
 			{
 				Config: testAccSecretManagerRegionalSecretDeletionProtectionFalse(context),
+			},
+		},
+	})
+}
+
+func TestAccSecretManagerRegionalRegionalSecret_tags(t *testing.T) {
+	t.Parallel()
+
+	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "secretmanager_regional_regionalsecret-tagkey", map[string]interface{}{})
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"org":           envvar.GetTestOrgFromEnv(t),
+		"tagKey":        tagKey,
+		"tagValue":      acctest.BootstrapSharedTestOrganizationTagValue(t, "secretmanager_regional_regionalsecret-tagvalue", tagKey),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckSecretManagerRegionalRegionalSecretDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecretManagerRegionalSecretTags(context),
+			},
+			{
+				ResourceName:            "google_secret_manager_regional_secret.regional-secret-basic",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"annotations", "labels", "location", "secret_id", "terraform_labels", "deletion_protection", "tags"},
+			},
+			{
+				Config: testAccSecretManagerRegionalSecretTagsDeletionProtection(context),
 			},
 		},
 	})
@@ -1383,6 +1416,31 @@ resource "google_secret_manager_regional_secret" "regional-secret-deletion-prote
   secret_id = "tf-test-reg-secret%{random_suffix}"
   location = "us-central1"
   deletion_protection = false
+}
+`, context)
+}
+
+func testAccSecretManagerRegionalSecretTags(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_secret_manager_regional_secret" "regional-secret-basic" {
+  secret_id = "tf-test-reg-secret-%{random_suffix}"
+  location = "us-central1"
+  tags = {
+	"%{org}/%{tagKey}" = "%{tagValue}"
+  }
+}
+`, context)
+}
+
+func testAccSecretManagerRegionalSecretTagsDeletionProtection(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_secret_manager_regional_secret" "regional-secret-basic" {
+  secret_id = "tf-test-reg-secret-%{random_suffix}"
+  location = "us-central1"
+  tags = {
+	"%{org}/%{tagKey}" = "%{tagValue}"
+  }
+  deletion_protection = false  
 }
 `, context)
 }
