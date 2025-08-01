59f6521af259bb4210871367aeaab5fc8fd69921
Modular Magician <magic-modules@google.com>
2025-08-01 21:23:54 +0000

    Adding log_retention_days field to existing bp resource for backupdr (#14622)
    [upstream:d1d207118dbf770c8a8c7dc820f1954d10219c49]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/backupdr/resource_backup_dr_backup_plan_generated_test.go
+++ google/services/backupdr/resource_backup_dr_backup_plan_generated_test.go
@@ -88,6 +88,65 @@ resource "google_backup_dr_backup_plan" "my-backup-plan-1" {
 `, context)
 }
 
+func TestAccBackupDRBackupPlan_backupDrBackupPlanForCsqlResourceExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBackupDRBackupPlanDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBackupDRBackupPlan_backupDrBackupPlanForCsqlResourceExample(context),
+			},
+			{
+				ResourceName:            "google_backup_dr_backup_plan.my-csql-backup-plan-1",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"backup_plan_id", "location"},
+			},
+		},
+	})
+}
+
+func testAccBackupDRBackupPlan_backupDrBackupPlanForCsqlResourceExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_backup_dr_backup_vault" "my_backup_vault" {
+  location                                      = "us-central1"
+  backup_vault_id                               = "tf-test-backup-vault-csql-test%{random_suffix}"
+  backup_minimum_enforced_retention_duration    = "100000s"
+}
+
+resource "google_backup_dr_backup_plan" "my-csql-backup-plan-1" {
+  location       = "us-central1"
+  backup_plan_id = "tf-test-backup-plan-csql-test%{random_suffix}"
+  resource_type  = "sqladmin.googleapis.com/Instance"
+  backup_vault   = google_backup_dr_backup_vault.my_backup_vault.id
+
+  backup_rules {
+    rule_id                = "rule-1"
+    backup_retention_days  = 5
+
+    standard_schedule {
+      recurrence_type     = "HOURLY"
+      hourly_frequency    = 6
+      time_zone           = "UTC"
+
+      backup_window {
+        start_hour_of_day = 0
+        end_hour_of_day   = 6
+      }
+    }
+  }
+  log_retention_days = 4
+}
+`, context)
+}
+
 func testAccCheckBackupDRBackupPlanDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
