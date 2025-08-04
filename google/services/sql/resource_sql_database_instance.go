9bdb8a718aef06f62f2659e002099d4608bcfd2f
The Magician <magic-modules@google.com>
2025-08-04 14:53:41 -0700

    Fix partial match regexes (#14610) (#23870)
    [upstream:496b240ba31fa8d67907893c5605c9705d4a0471]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/sql/resource_sql_database_instance.go
+++ google/services/sql/resource_sql_database_instance.go
@@ -2332,9 +2332,9 @@ func resourceSqlDatabaseInstanceDelete(d *schema.ResourceData, meta interface{})
 func resourceSqlDatabaseInstanceImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 	config := meta.(*transport_tpg.Config)
 	if err := tpgresource.ParseImportId([]string{
-		"projects/(?P<project>[^/]+)/instances/(?P<name>[^/]+)",
-		"(?P<project>[^/]+)/(?P<name>[^/]+)",
-		"(?P<name>[^/]+)"}, d, config); err != nil {
+		"^projects/(?P<project>[^/]+)/instances/(?P<name>[^/]+)$",
+		"^(?P<project>[^/]+)/(?P<name>[^/]+)$",
+		"^(?P<name>[^/]+)$"}, d, config); err != nil {
 		return nil, err
 	}
 
