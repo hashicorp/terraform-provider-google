566212d03b50bc855d7393cd7525776dd35faba3
The Magician <magic-modules@google.com>
2025-08-11 11:07:42 -0700

    Make metadata required in Vertex AI Index resource (#14382) (#23953)
    [upstream:33623bf52d355a6d4704dd645336575366ee2969]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/vertexai/resource_vertex_ai_index.go
+++ google/services/vertexai/resource_vertex_ai_index.go
@@ -86,10 +86,12 @@ Please refer to the field 'effective_labels' for all of the labels present on th
 				Elem: &schema.Schema{Type: schema.TypeString},
 			},
 			"metadata": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: `An additional information about the Index`,
-				MaxItems:    1,
+				Type:     schema.TypeList,
+				Optional: true,
+				Description: `Additional information about the Index.
+Although this field is not marked as required in the API specification, it is currently required when creating an Index and must be provided.
+Attempts to create an Index without this field will result in an API error.`,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"config": {
