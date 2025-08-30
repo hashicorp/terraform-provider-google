e3516047a29d469c6e7168cb48b48af9abe6b7ad
The Magician <magic-modules@google.com>
2025-08-29 17:52:26 -0700

    Fixed typo in TestAccApigeeSecurityAction_apigeeSecurityActionFull config (#15030) (#24221)
    [upstream:b6cb99b6361868d2885c75566314192ebff32fd6]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_security_action_test.go
+++ google/services/apigee/resource_apigee_security_action_test.go
@@ -211,7 +211,7 @@ resource "time_sleep" "wait_60_seconds" {
 
 resource "google_project_service" "apigee" {
   project = google_project.project.project_id
-  service = "apigee.googleapis.com""
+  service = "apigee.googleapis.com"
   depends_on = [time_sleep.wait_60_seconds]
 }
 
@@ -253,7 +253,7 @@ resource "google_service_networking_connection" "apigee_vpc_connection" {
 
 resource "google_apigee_organization" "apigee_org" {
     analytics_region   = "us-central1"
-    project_id         = data.google_client_config.current.project
+    project_id         = google_project.project.project_id
     authorized_network = google_compute_network.apigee_network.id
     depends_on         = [google_service_networking_connection.apigee_vpc_connection]
 }
