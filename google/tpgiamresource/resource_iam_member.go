5c3bac7830e1383e3b7d4d2960b06e97e699f7c5
The Magician <magic-modules@google.com>
2025-09-15 11:05:08 -0700

    feat: (storage) added timeouts for bucket_iam resources (#15062) (#24376)
    [upstream:ad70e38c215b03f7082f98235988119a8b39040d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/tpgiamresource/resource_iam_member.go
+++ google/tpgiamresource/resource_iam_member.go
@@ -22,6 +22,7 @@ import (
 	"log"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -188,7 +189,9 @@ func iamMemberImport(newUpdaterFunc NewResourceIamUpdaterFunc, resourceIdParser 
 func ResourceIamMember(parentSpecificSchema map[string]*schema.Schema, newUpdaterFunc NewResourceIamUpdaterFunc, resourceIdParser ResourceIdParserFunc, options ...func(*IamSettings)) *schema.Resource {
 	settings := NewIamSettings(options...)
 
-	return &schema.Resource{
+	createTimeOut := time.Duration(settings.CreateTimeOut) * time.Minute
+
+	resourceSchema := &schema.Resource{
 		Create: resourceIamMemberCreate(newUpdaterFunc, settings.EnableBatching),
 		Read:   resourceIamMemberRead(newUpdaterFunc),
 		Delete: resourceIamMemberDelete(newUpdaterFunc, settings.EnableBatching),
@@ -205,6 +208,13 @@ func ResourceIamMember(parentSpecificSchema map[string]*schema.Schema, newUpdate
 		},
 		UseJSONNumber: true,
 	}
+
+	if createTimeOut > 0 {
+		resourceSchema.Timeouts = &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(createTimeOut),
+		}
+	}
+	return resourceSchema
 }
 
 func getResourceIamMember(d *schema.ResourceData) *cloudresourcemanager.Binding {
