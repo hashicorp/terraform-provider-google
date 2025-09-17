1c0f4ab4ab35d058e384b8cf12a13ee78d1cd0e0
Modular Magician <magic-modules@google.com>
2025-09-17 17:20:31 +0000

    workbench: retry instance operations if they error out with `409: unable to queue the operation` (#15158)
    [upstream:06682c45ffc5759b361b49e1eabfb28cdff5a2c0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/workbench/workbench_operation.go
+++ google/services/workbench/workbench_operation.go
@@ -42,11 +42,12 @@ func (w *WorkbenchOperationWaiter) QueryOp() (interface{}, error) {
 	url := fmt.Sprintf("%s%s", w.Config.WorkbenchBasePath, w.CommonOperationWaiter.Op.Name)
 
 	return transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
-		Config:    w.Config,
-		Method:    "GET",
-		Project:   w.Project,
-		RawURL:    url,
-		UserAgent: w.UserAgent,
+		Config:               w.Config,
+		Method:               "GET",
+		Project:              w.Project,
+		RawURL:               url,
+		UserAgent:            w.UserAgent,
+		ErrorRetryPredicates: []transport_tpg.RetryErrorPredicateFunc{transport_tpg.IsWorkbenchQueueError},
 	})
 }
 
