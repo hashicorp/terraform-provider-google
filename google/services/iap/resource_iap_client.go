b45fb3db323532fd72dff0684ab06498cc4cb126
The Magician <magic-modules@google.com>
2025-10-02 15:10:42 -0700

    Update depraction date for IAP OAuth APIs. (#15260) (#24575)
    [upstream:28551d19f615425e5ad4f174f37ef4955667fc74]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/iap/resource_iap_client.go
+++ google/services/iap/resource_iap_client.go
@@ -48,7 +48,7 @@ func ResourceIapClient() *schema.Resource {
 			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
-		DeprecationMessage: "After July 2025, the `google_iap_client` Terraform resource will no longer function as intended due to the deprecation of the IAP OAuth Admin API",
+		DeprecationMessage: "This resource is deprecated on Jan 22, 2025. After Jan 19, 2026 the `google_iap_client` Terraform resource will no longer function as intended due to the deprecation of the IAP OAuth Admin APIs. New projects will not be able to use these APIs. March 19, 2026 The IAP OAuth Admin APIs will be permanently shut down. Access to this feature will no longer be available.",
 
 		Schema: map[string]*schema.Schema{
 			"brand": {
--- website/docs/r/iap_brand.html.markdown
+++ website/docs/r/iap_brand.html.markdown
@@ -20,7 +20,7 @@ description: |-
 ---
 
 # google_iap_brand
-~> **Warning:** after July 2025, the `google_iap_brand` Terraform resource will no longer function as intended due to the deprecation of the IAP OAuth Admin API
+~> **Warning:** This resource is deprecated on Jan 22, 2025. After Jan 19, 2026 the `google_iap_brand` Terraform resource will no longer function as intended due to the deprecation of the IAP OAuth Admin APIs. New projects will not be able to use these APIs. March 19, 2026 The IAP OAuth Admin APIs will be permanently shut down. Access to this feature will no longer be available.
 
 OAuth brand data. Only "Organization Internal" brands can be created
 programmatically via API. To convert it into an external brands
