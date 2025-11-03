94c6190614c625511dce9ac416728ae1dc1106ab
The Magician <magic-modules@google.com>
2025-11-03 13:08:31 -0800

    Add Deployment resource for CES (#15530) (#24945)
    [upstream:1ed141d9fd5bf4c575714c3c52f8e3d5c6cac887]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -576,9 +576,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 669
+// Generated resources: 670
 // Generated IAM resources: 318
-// Total generated resources: 987
+// Total generated resources: 988
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -730,6 +730,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_certificate_manager_dns_authorization":                               certificatemanager.ResourceCertificateManagerDnsAuthorization(),
 	"google_certificate_manager_trust_config":                                    certificatemanager.ResourceCertificateManagerTrustConfig(),
 	"google_ces_app":                                                             ces.ResourceCESApp(),
+	"google_ces_deployment":                                                      ces.ResourceCESDeployment(),
 	"google_ces_toolset":                                                         ces.ResourceCESToolset(),
 	"google_chronicle_data_access_label":                                         chronicle.ResourceChronicleDataAccessLabel(),
 	"google_chronicle_data_access_scope":                                         chronicle.ResourceChronicleDataAccessScope(),
