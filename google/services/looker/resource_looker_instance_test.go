aab9748b76288e9e9031ec5ead435d52a7e700e5
Modular Magician <magic-modules@google.com>
2025-11-18 16:36:38 +0000

    Add Looker egress configs to Instance.yaml (#15749)
    [upstream:4113c98c9d5507eca64b157bb5772899524661fd]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/looker/resource_looker_instance_test.go
+++ google/services/looker/resource_looker_instance_test.go
@@ -17,6 +17,7 @@
 package looker_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -55,4 +56,90 @@ func TestAccLookerInstance_update(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccLookerInstance_updateControlledEgress(t *testing.T) {
+	t.Parallel()
+
+	// Step 1: Create instance WITHOUT controlled egress
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				// Config A: Basic Instance
+				Config: testAccLookerInstance_basic(context),
+			},
+			{
+				ResourceName:            "google_looker_instance.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"oauth_config", "region"},
+			},
+			{
+				// Config B: Update to ENABLE controlled egress
+				// This triggers the PATCH with your update_mask logic
+				Config: testAccLookerInstance_controlledEgress(context),
+			},
+			{
+				ResourceName:            "google_looker_instance.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"oauth_config", "region"},
+			},
+		},
+	})
+}
+
+func testAccLookerInstance_basic(context map[string]interface{}) string {
+	return fmt.Sprintf(`
+resource "google_looker_instance" "test" {
+  name               = "tf-test-looker-%s"
+  platform_edition   = "LOOKER_CORE_ENTERPRISE_ANNUAL"
+  region             = "us-central1"
+  public_ip_enabled  = true
+  psc_enabled  = true
+
+  psc_config {
+    allowed_vpcs = []
+  }
+
+  oauth_config {
+    client_id     = "my-client-id"
+    client_secret = "my-client-secret"
+  }
+}
+`, context["random_suffix"])
+}
+
+func testAccLookerInstance_controlledEgress(context map[string]interface{}) string {
+	return fmt.Sprintf(`
+resource "google_looker_instance" "test" {
+  name               = "tf-test-looker-%s"
+  platform_edition   = "LOOKER_CORE_ENTERPRISE_ANNUAL"
+  region             = "us-central1"
+  public_ip_enabled  = true
+  psc_enabled  = true
+
+  psc_config {
+    allowed_vpcs = []
+  }
+
+  controlled_egress_enabled = true
+
+  controlled_egress_config {
+    marketplace_enabled = true
+    egress_fqdns        = ["google.com", "github.com"]
+  }
+
+  oauth_config {
+    client_id     = "my-client-id"
+    client_secret = "my-client-secret"
+  }
+}
+`, context["random_suffix"])
 }
