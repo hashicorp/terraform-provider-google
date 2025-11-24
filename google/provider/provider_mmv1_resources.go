a6b43afb3db0e59ae60c88bd775b3b257b3ce9d1
The Magician <magic-modules@google.com>
2025-11-24 10:42:14 -0800

    Multicast producer association (#15753) (#25291)
    [upstream:c2b3e87c795d4a04b5ef6e362a0c5248cbb31a02]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -584,9 +584,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 687
+// Generated resources: 688
 // Generated IAM resources: 324
-// Total generated resources: 1011
+// Total generated resources: 1012
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1368,6 +1368,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_network_services_multicast_domain":                                   networkservices.ResourceNetworkServicesMulticastDomain(),
 	"google_network_services_multicast_domain_activation":                        networkservices.ResourceNetworkServicesMulticastDomainActivation(),
 	"google_network_services_multicast_domain_group":                             networkservices.ResourceNetworkServicesMulticastDomainGroup(),
+	"google_network_services_multicast_producer_association":                     networkservices.ResourceNetworkServicesMulticastProducerAssociation(),
 	"google_network_services_service_binding":                                    networkservices.ResourceNetworkServicesServiceBinding(),
 	"google_network_services_tcp_route":                                          networkservices.ResourceNetworkServicesTcpRoute(),
 	"google_network_services_tls_route":                                          networkservices.ResourceNetworkServicesTlsRoute(),
