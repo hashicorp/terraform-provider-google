33dadb82d2f7cc6ae91877c0ef4013ea60c12644
The Magician <magic-modules@google.com>
2025-11-25 14:24:23 -0800

    Handle email casing in google_bigquery_dataset access (#15792) (#25317)
    [upstream:d5a72e8bc4be1d9d998b62c49280c4cdc6e34423]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/bigquery/resource_bigquery_dataset_test.go
+++ google/services/bigquery/resource_bigquery_dataset_test.go
@@ -20,15 +20,14 @@ package bigquery_test
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
-	"testing"
-
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"google.golang.org/api/bigquery/v2"
+	"regexp"
+	"strings"
+	"testing"
 )
 
 func TestAccBigQueryDataset_basic(t *testing.T) {
@@ -324,6 +323,52 @@ func TestAccBigQueryDataset_access(t *testing.T) {
 			},
 			{
 				Config: testAccBigQueryDatasetWithViewAccess(datasetID, otherDatasetID, otherTableID),
+			},
+			{
+				ResourceName:            "google_bigquery_dataset.access_test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func TestAccBigQueryDataset_accessMixedCase_userByEmail(t *testing.T) {
+	t.Parallel()
+
+	datasetID := fmt.Sprintf("tf_test_access_%s", acctest.RandString(t, 10))
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBigQueryDatasetDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigQueryDataset_accessMixedCase(datasetID, "user_by_email", "alicE@google.COM"),
+			},
+			{
+				ResourceName:            "google_bigquery_dataset.access_test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func TestAccBigQueryDataset_accessMixedCase_groupByEmail(t *testing.T) {
+	t.Parallel()
+
+	datasetID := fmt.Sprintf("tf_test_access_%s", acctest.RandString(t, 10))
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBigQueryDatasetDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigQueryDataset_accessMixedCase(datasetID, "group_by_email", "MAGIC-MODULES@gOOgle.com"),
 			},
 			{
 				ResourceName:            "google_bigquery_dataset.access_test",
@@ -853,6 +898,19 @@ resource "google_bigquery_dataset" "access_test" {
   }
 }
 `, otherDatasetID, otherTableID, datasetID)
+}
+
+func testAccBigQueryDataset_accessMixedCase(datasetID, accessType, email string) string {
+	return fmt.Sprintf(`
+resource "google_bigquery_dataset" "access_test" {
+  dataset_id = "%s"
+
+  access {
+    role   = "OWNER"
+    %s = "%s"
+  }
+}
+`, datasetID, accessType, email)
 }
 
 func testAccBigQueryDataset_cmek(pid, datasetID, kmsKey string) string {
