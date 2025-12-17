1e2e4e08231154e2e95131db1a59e96e691d2c6a
The Magician <magic-modules@google.com>
2025-12-17 14:27:52 -0800

    Vertexai feature online store bigtable (#15963) (#25601)
    [upstream:8256b8e76f2704cba792f0da4050e2f88ed627b0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/vertexai/resource_vertex_ai_feature_online_store_test.go
+++ google/services/vertexai/resource_vertex_ai_feature_online_store_test.go
@@ -99,3 +99,97 @@ resource google_vertex_ai_feature_online_store "feature_online_store" {
 }
 `, context)
 }
+
+func TestAccVertexAIFeatureOnlineStore_bigtable_full(t *testing.T) {
+	t.Parallel()
+
+	kms := acctest.BootstrapKMSKeyInLocation(t, "us-central1")
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"kms_key_name":  kms.CryptoKey.Name,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVertexAIFeatureOnlineStoreDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIFeatureOnlineStore_bigtable_full(context),
+			},
+			{
+				ResourceName:            "google_vertex_ai_feature_online_store.feature_online_store",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "etag", "region", "force_destroy", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccVertexAIFeatureOnlineStore_bigtable_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource google_vertex_ai_feature_online_store "feature_online_store" {
+    name = "tf_test_feature_online_store%{random_suffix}"
+    region = "us-central1"
+
+    bigtable {
+        auto_scaling {
+            min_node_count = 1
+            max_node_count = 2
+        }
+        enable_direct_bigtable_access = true
+        zone = "us-central1-a"
+    }
+    encryption_spec {
+        kms_key_name = "%{kms_key_name}"
+    }
+    force_destroy = true
+}
+`, context)
+}
+
+func TestAccVertexAIFeatureOnlineStore_bigtable_with_zone(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVertexAIFeatureOnlineStoreDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIFeatureOnlineStore_bigtable_with_zone(context),
+			},
+			{
+				ResourceName:            "google_vertex_ai_feature_online_store.feature_online_store",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "etag", "region", "force_destroy", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccVertexAIFeatureOnlineStore_bigtable_with_zone(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource google_vertex_ai_feature_online_store "feature_online_store" {
+    name = "tf_test_feature_online_store%{random_suffix}"
+    region = "us-central1"
+
+    bigtable {
+        auto_scaling {
+            min_node_count = 1
+            max_node_count = 2
+            cpu_utilization_target = 60
+        }
+        zone = "us-central1-a"
+    }
+  force_destroy = true
+}
+`, context)
+}
