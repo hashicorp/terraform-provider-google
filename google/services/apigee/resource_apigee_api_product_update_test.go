4f1f64ebe29465e146e1e0304177506a012f3710
The Magician <magic-modules@google.com>
2025-12-23 15:17:39 -0800

    Creating dev environment to be used by product (#16022) (#25644)
    [upstream:fc825ab9b69d792580ed59b9027805d9fe5c8a3d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_api_product_update_test.go
+++ google/services/apigee/resource_apigee_api_product_update_test.go
@@ -134,6 +134,10 @@ resource "google_apigee_instance" "apigee_instance" {
   org_id             = google_apigee_organization.apigee_org.id
   peering_cidr_range = "SLASH_22"
 }
+resource "google_apigee_environment" "env_dev" {
+  name   = "dev"
+  org_id = google_apigee_organization.apigee_org.id
+}
 resource "google_apigee_api_product" "apigee_api_product" {
   org_id        = google_apigee_organization.apigee_org.id
   name              = "tf-test%{random_suffix}"
@@ -148,7 +152,7 @@ resource "google_apigee_api_product" "apigee_api_product" {
   quota_time_unit     = "day"
   quota_counter_scope = "PROXY"
 
-  environments = ["dev", "hom"]
+  environments = ["dev"]
   scopes = [
     "read:weather",
     "write:reports"
@@ -290,7 +294,8 @@ resource "google_apigee_api_product" "apigee_api_product" {
   }
 
   depends_on = [
-    google_apigee_instance.apigee_instance
+    google_apigee_instance.apigee_instance,
+	google_apigee_environment.env_dev
   ]
 }
 `, context)
@@ -372,6 +377,14 @@ resource "google_apigee_developer" "apigee_developer" {
     google_apigee_instance.apigee_instance
   ]
 }
+resource "google_apigee_environment" "env_dev" {
+  name   = "dev"
+  org_id = google_apigee_organization.apigee_org.id
+}
+resource "google_apigee_environment" "env_hom" {
+  name   = "hom"
+  org_id = google_apigee_organization.apigee_org.id
+}
 resource "google_apigee_api_product" "apigee_api_product" {
   org_id        = google_apigee_organization.apigee_org.id
   name              = "tf-test%{random_suffix}"
@@ -386,7 +399,7 @@ resource "google_apigee_api_product" "apigee_api_product" {
   quota_time_unit     = "day"
   quota_counter_scope = "PROXY"
 
-  environments = ["dev"]
+  environments = ["dev", "hom"]
   scopes = [
     "read:weather"
   ]
@@ -527,7 +540,9 @@ resource "google_apigee_api_product" "apigee_api_product" {
   }
 
   depends_on = [
-    google_apigee_instance.apigee_instance
+    google_apigee_instance.apigee_instance,
+	google_apigee_environment.env_dev,
+	google_apigee_environment.env_hom
   ]
 }
 `, context)
