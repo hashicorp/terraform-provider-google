f33b40f37bee9445c0b4409bd38a4e226a247e6a
The Magician <magic-modules@google.com>
2025-12-30 14:17:06 -0800

    Introduce Destination resource (#16002) (#25663)
    [upstream:dac6b8e7f44851f9c830b928b981ea3495fe01b8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -590,9 +590,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 706
+// Generated resources: 707
 // Generated IAM resources: 327
-// Total generated resources: 1033
+// Total generated resources: 1034
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1339,6 +1339,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_netapp_volume_replication":                                           netapp.ResourceNetappVolumeReplication(),
 	"google_netapp_volume_snapshot":                                              netapp.ResourceNetappVolumeSnapshot(),
 	"google_netapp_kmsconfig":                                                    netapp.ResourceNetappkmsconfig(),
+	"google_network_connectivity_destination":                                    networkconnectivity.ResourceNetworkConnectivityDestination(),
 	"google_network_connectivity_group":                                          networkconnectivity.ResourceNetworkConnectivityGroup(),
 	"google_network_connectivity_hub":                                            networkconnectivity.ResourceNetworkConnectivityHub(),
 	"google_network_connectivity_multicloud_data_transfer_config":                networkconnectivity.ResourceNetworkConnectivityMulticloudDataTransferConfig(),
