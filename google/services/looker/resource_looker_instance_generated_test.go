f5ff164a6e939591234c0e494ee7692e9324ff4d
The Magician <magic-modules@google.com>
2025-12-30 09:41:27 -0800

    Update looker_instance PSC example for service_attachments (#15799) (#25658)
    [upstream:19b6842683118a1424c68ae8e288498d17c12074]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/looker/resource_looker_instance_generated_test.go
+++ google/services/looker/resource_looker_instance_generated_test.go
@@ -383,8 +383,24 @@ resource "google_looker_instance" "looker-instance" {
   }
   psc_config {
     allowed_vpcs = ["projects/test-project/global/networks/test"]
-    # update only
-    # service_attachments = [{local_fqdn: "www.local-fqdn.com" target_service_attachment_uri: "projects/my-project/regions/us-east1/serviceAttachments/sa"}]
+    
+    # First Service Attachment
+    # service_attachments {
+    #   local_fqdn                    = "www.example-one.com"
+    #   target_service_attachment_uri = "projects/my-project/regions/us-east1/serviceAttachments/sa-1"
+    # }
+
+    # Second Service Attachment
+    # service_attachments {
+    #   local_fqdn                    = "api.internal-partner.com"
+    #   target_service_attachment_uri = "projects/partner-project/regions/us-central1/serviceAttachments/sa-gateway"
+    # }
+
+    # Third Service Attachment
+    # service_attachments {
+    #   local_fqdn                    = "git.internal-repo.com"
+    #   target_service_attachment_uri = "projects/devops-project/regions/us-west1/serviceAttachments/gitlab-sa"
+    # }
   }
 }
 `, context)
