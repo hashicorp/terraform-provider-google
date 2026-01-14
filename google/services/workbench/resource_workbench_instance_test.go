feb10844885514b0b573a1a12bd469bf0119ac2c
Modular Magician <magic-modules@google.com>
2026-01-14 01:37:52 +0000

    Make 'enable-jupyterlab4' a settable metadata key (#15968)
    [upstream:2a537024765b7b863379709591e61ecf5933df1c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/workbench/resource_workbench_instance_test.go
+++ google/services/workbench/resource_workbench_instance_test.go
@@ -949,3 +949,85 @@ resource "google_workbench_instance" "instance" {
 }
 `, context)
 }
+
+func TestAccWorkbenchInstance_updateJupyterLab(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWorkbenchInstance_jupyterlabTrue(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_workbench_instance.instance", "gce_setup.0.metadata.enable-jupyterlab4", "true"),
+				),
+			},
+			{
+				ResourceName:            "google_workbench_instance.instance",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "instance_owners", "location", "instance_id", "request_id", "labels", "terraform_labels", "desired_state", "update_time", "health_info", "health_state"},
+			},
+			{
+				Config: testAccWorkbenchInstance_jupyterlabFalse(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_workbench_instance.instance", "gce_setup.0.metadata.enable-jupyterlab4", "false"),
+				),
+			},
+			{
+				ResourceName:            "google_workbench_instance.instance",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "instance_owners", "location", "instance_id", "request_id", "labels", "terraform_labels", "desired_state", "update_time", "health_info", "health_state"},
+			},
+			{
+				Config: testAccWorkbenchInstance_jupyterlabTrue(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_workbench_instance.instance", "gce_setup.0.metadata.enable-jupyterlab4", "true"),
+				),
+			},
+			{
+				ResourceName:            "google_workbench_instance.instance",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "instance_owners", "location", "instance_id", "request_id", "labels", "terraform_labels", "desired_state", "update_time", "health_info", "health_state"},
+			},
+		},
+	})
+}
+
+func testAccWorkbenchInstance_jupyterlabTrue(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_workbench_instance" "instance" {
+  name = "tf-test-workbench-instance%{random_suffix}"
+  location = "us-central1-a"
+  gce_setup {
+    metadata = {
+      "enable-jupyterlab4" = "true"
+    }
+  }
+}
+`, context)
+}
+
+func testAccWorkbenchInstance_jupyterlabFalse(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_workbench_instance" "instance" {
+  name = "tf-test-workbench-instance%{random_suffix}"
+  location = "us-central1-a"
+  gce_setup {
+    metadata = {
+      "enable-jupyterlab4" = "false"
+    }
+  }
+}
+`, context)
+}
