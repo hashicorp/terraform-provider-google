39b6f80780ab990bc5c7f8e620351aec3ffe0eb4
Modular Magician <magic-modules@google.com>
2026-01-26 19:05:06 +0000

    Fix update composer v2 image (#16224)
    Co-authored-by: rlapin-pl <rlapin@google.com>
    
    [upstream:c667189050f6edff82162e49483af4e025f3f6cb]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/composer/resource_composer_environment_test.go
+++ google/services/composer/resource_composer_environment_test.go
@@ -1660,7 +1660,7 @@ resource "google_composer_environment" "test" {
     }
 
     software_config {
-      image_version = "composer-2.10.0-airflow-2.10.2"
+      image_version = "composer-2.13.0-airflow-2.10.5"
     }
   }
   depends_on = [google_project_iam_member.composer-worker]
@@ -1709,7 +1709,7 @@ resource "google_composer_environment" "test" {
     }
 
     software_config {
-      image_version = "composer-2.10.1-airflow-2.10.2"
+      image_version = "composer-2.16.2-airflow-2.10.5"
     }
   }
   depends_on = [google_project_iam_member.composer-worker]
