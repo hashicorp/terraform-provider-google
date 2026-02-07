8b93193f70a338989d958f349088e645a2e8eb98
The Magician <magic-modules@google.com>
2026-02-06 17:57:52 -0800

    Add sweeper and URL substitutions to YAML (#16295) (#26032)
    [upstream:0e35ce714da8f317dba954f4fcacb02a5aff5a9c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/networkservices/resource_network_services_multicast_domain_activation_sweeper.go
+++ google/services/networkservices/resource_network_services_multicast_domain_activation_sweeper.go
@@ -57,12 +57,10 @@ func listAndActionNetworkServicesMulticastDomainActivation(action sweeper.Resour
 	var configs []*tpgresource.ResourceDataMock
 	t := &testing.T{}
 	billingId := envvar.GetTestBillingAccountFromEnv(t)
-	// Default single config
-	intermediateValues := []map[string]string{
-		{
-			"region": "us-central1",
-		},
-	}
+	// Build URL substitution maps individually to ensure proper formatting
+	intermediateValues := make([]map[string]string, 1)
+	intermediateValues[0] = map[string]string{}
+	intermediateValues[0]["location"] = "us-central1-b"
 
 	// Create configs from intermediate values
 	for _, values := range intermediateValues {
