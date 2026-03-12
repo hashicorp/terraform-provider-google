46467eaf8d77489fc714d3570871a54ac6dc4c8c
The Magician <magic-modules@google.com>
2026-03-12 11:44:55 -0700

    Promote PSC Endpoint-Based Producer Security to v1 (#16679) (#26434)
    [upstream:cbe032a1195c29f3cfe8750e792c0db2cae23184]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_service_attachment.go
+++ google/services/compute/resource_compute_service_attachment.go
@@ -368,6 +368,13 @@ func computeServiceAttachmentConsumerAcceptListsSchema() *schema.Resource {
 				Description: `The number of consumer forwarding rules the consumer project can
 create.`,
 			},
+			"endpoint_url": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				DiffSuppressFunc: tpgresource.CompareSelfLinkRelativePaths,
+				Description: `The endpoint that is allowed to connect to this service attachment.
+Only one of project_id_or_num, network_url and endpoint_url may be set.`,
+			},
 			"network_url": {
 				Type:             schema.TypeString,
 				Optional:         true,
@@ -980,6 +987,7 @@ func flattenComputeServiceAttachmentConsumerAcceptLists(v interface{}, d *schema
 		transformed.Add(map[string]interface{}{
 			"project_id_or_num": flattenComputeServiceAttachmentConsumerAcceptListsProjectIdOrNum(original["projectIdOrNum"], d, config),
 			"network_url":       flattenComputeServiceAttachmentConsumerAcceptListsNetworkUrl(original["networkUrl"], d, config),
+			"endpoint_url":      flattenComputeServiceAttachmentConsumerAcceptListsEndpointUrl(original["endpointUrl"], d, config),
 			"connection_limit":  flattenComputeServiceAttachmentConsumerAcceptListsConnectionLimit(original["connectionLimit"], d, config),
 		})
 	}
@@ -990,6 +998,10 @@ func flattenComputeServiceAttachmentConsumerAcceptListsProjectIdOrNum(v interfac
 }
 
 func flattenComputeServiceAttachmentConsumerAcceptListsNetworkUrl(v interface{}, d *schema.ResourceData, config *transport_tpg.Config) interface{} {
+	return v
+}
+
+func flattenComputeServiceAttachmentConsumerAcceptListsEndpointUrl(v interface{}, d *schema.ResourceData, config *transport_tpg.Config) interface{} {
 	return v
 }
 
@@ -1112,6 +1124,13 @@ func expandComputeServiceAttachmentConsumerAcceptLists(v interface{}, d tpgresou
 			transformed["networkUrl"] = transformedNetworkUrl
 		}
 
+		transformedEndpointUrl, err := expandComputeServiceAttachmentConsumerAcceptListsEndpointUrl(original["endpoint_url"], d, config)
+		if err != nil {
+			return nil, err
+		} else if val := reflect.ValueOf(transformedEndpointUrl); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+			transformed["endpointUrl"] = transformedEndpointUrl
+		}
+
 		transformedConnectionLimit, err := expandComputeServiceAttachmentConsumerAcceptListsConnectionLimit(original["connection_limit"], d, config)
 		if err != nil {
 			return nil, err
@@ -1129,6 +1148,10 @@ func expandComputeServiceAttachmentConsumerAcceptListsProjectIdOrNum(v interface
 }
 
 func expandComputeServiceAttachmentConsumerAcceptListsNetworkUrl(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandComputeServiceAttachmentConsumerAcceptListsEndpointUrl(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	return v, nil
 }
 
