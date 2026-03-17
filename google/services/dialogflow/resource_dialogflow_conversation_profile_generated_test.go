1b2d008ab2f1f4f286495decb7535e0813043da5
Modular Magician <magic-modules@google.com>
2026-03-17 17:44:55 +0000

    dialogflow: added field `use_bidi_streaming` to resource `google_dialogflow_conversation_profile` (beta) (#16696)
    [upstream:56a8d1b213ddad50d36f3aa10c20adf77fff40a8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/dialogflow/resource_dialogflow_conversation_profile_generated_test.go
+++ google/services/dialogflow/resource_dialogflow_conversation_profile_generated_test.go
@@ -140,6 +140,54 @@ resource "google_pubsub_topic" "recognition_result_notification_profile" {
 `, context)
 }
 
+func TestAccDialogflowConversationProfile_dialogflowConversationProfileBetaBidiExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckDialogflowConversationProfileDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDialogflowConversationProfile_dialogflowConversationProfileBetaBidiExample(context),
+			},
+			{
+				ResourceName:            "google_dialogflow_conversation_profile.bidi_profile",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "logging_config"},
+			},
+		},
+	})
+}
+
+func testAccDialogflowConversationProfile_dialogflowConversationProfileBetaBidiExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_dialogflow_conversation_profile" "bidi_profile" {
+  display_name = "tf-test-dialogflow-profile-bidi-%{random_suffix}"
+  location     = "global"
+  language_code = "en-US"
+  use_bidi_streaming = true
+  automated_agent_config {
+    agent = google_ces_app.ces_app_for_agent.id
+  }
+}
+
+resource "google_ces_app" "ces_app_for_agent" {
+  app_id = "app-id-%{random_suffix}"
+  location = "us"
+  display_name = "my-app"
+  time_zone_settings {
+    time_zone = "America/Los_Angeles"
+  }
+}
+`, context)
+}
+
 func testAccCheckDialogflowConversationProfileDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
