6d39258adf8d4e72e3bdd860e23e04fc85b02ca2
Modular Magician <magic-modules@google.com>
2026-03-18 21:48:38 +0000

    chore: add customHostConfig to instance (#16723)
    [upstream:24996d471e3331534997e3ab2af309ba005c101b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/securesourcemanager/resource_secure_source_manager_instance_generated_test.go
+++ google/services/securesourcemanager/resource_secure_source_manager_instance_generated_test.go
@@ -297,6 +297,126 @@ data "google_project" "project" {}
 `, context)
 }
 
+func TestAccSecureSourceManagerInstance_secureSourceManagerInstancePrivateCustomHostExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"deletion_policy": "DELETE",
+		"random_suffix":   acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		CheckDestroy: testAccCheckSecureSourceManagerInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecureSourceManagerInstance_secureSourceManagerInstancePrivateCustomHostExample(context),
+			},
+			{
+				ResourceName:            "google_secure_source_manager_instance.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"deletion_policy", "instance_id", "labels", "location", "terraform_labels", "update_time"},
+			},
+		},
+	})
+}
+
+func testAccSecureSourceManagerInstance_secureSourceManagerInstancePrivateCustomHostExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "project" {}
+
+resource "google_privateca_ca_pool" "ca_pool" {
+  name     = "tf-test-ca-pool%{random_suffix}"
+  location = "us-central1"
+  tier     = "ENTERPRISE"
+  publishing_options {
+    publish_ca_cert = true
+    publish_crl     = true
+  }
+}
+
+resource "google_privateca_certificate_authority" "root_ca" {
+  pool                     = google_privateca_ca_pool.ca_pool.name
+  certificate_authority_id = "tf-test-root-ca%{random_suffix}"
+  location                 = "us-central1"
+  config {
+    subject_config {
+      subject {
+        organization = "google"
+        common_name = "my-certificate-authority"
+      }
+    }
+    x509_config {
+      ca_options {
+        is_ca = true
+      }
+      key_usage {
+        base_key_usage {
+          cert_sign = true
+          crl_sign = true
+        }
+        extended_key_usage {
+          server_auth = true
+        }
+      }
+    }
+  }
+  key_spec {
+    algorithm = "RSA_PKCS1_4096_SHA256"
+  }
+
+  // Disable deletion protections for easier test cleanup purposes
+  deletion_protection = false
+  ignore_active_certificates_on_deletion = true
+  skip_grace_period = true
+}
+
+resource "google_privateca_ca_pool_iam_binding" "ca_pool_binding" {
+  ca_pool = google_privateca_ca_pool.ca_pool.id
+  role = "roles/privateca.certificateRequester"
+
+  members = [
+    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-sourcemanager.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_secure_source_manager_instance" "default" {
+  instance_id = "tf-test-my-instance%{random_suffix}"
+  location = "us-central1"
+  private_config {
+    is_private = true
+    ca_pool = google_privateca_ca_pool.ca_pool.id
+    custom_host_config {
+      api = "api.example.com"
+      git_http = "git-http.example.com"
+      git_ssh = "git-ssh.example.com"
+      html   = "html.example.com"
+    }
+  }
+
+  # Prevent accidental deletions.
+  deletion_policy = "%{deletion_policy}"
+
+  depends_on = [
+    google_privateca_certificate_authority.root_ca,
+    time_sleep.wait_120_seconds
+  ]
+}
+
+# ca pool IAM permissions can take time to propagate
+resource "time_sleep" "wait_120_seconds" {
+  depends_on = [google_privateca_ca_pool_iam_binding.ca_pool_binding]
+
+  create_duration = "120s"
+}
+`, context)
+}
+
 func TestAccSecureSourceManagerInstance_secureSourceManagerInstancePrivatePscBackendExample(t *testing.T) {
 	t.Parallel()
 
