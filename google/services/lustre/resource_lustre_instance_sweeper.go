70bf6b528dae3a23de9313ee1f467f196f9cdc0d
The Magician <magic-modules@google.com>
2026-04-02 15:54:29 -0700

    Generate terraform changes for Lustre (add dynamic tier option). (#16893) (#26741)
    [upstream:125eb8cebd3480af05dbc2deb5d3af013bbf33d6]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/lustre/resource_lustre_instance_sweeper.go
+++ google/services/lustre/resource_lustre_instance_sweeper.go
@@ -58,9 +58,11 @@ func listAndActionLustreInstance(action sweeper.ResourceAction) error {
 	t := &testing.T{}
 	billingId := envvar.GetTestBillingAccountFromEnv(t)
 	// Build URL substitution maps individually to ensure proper formatting
-	intermediateValues := make([]map[string]string, 1)
+	intermediateValues := make([]map[string]string, 2)
 	intermediateValues[0] = map[string]string{}
 	intermediateValues[0]["location"] = "us-central1-a"
+	intermediateValues[1] = map[string]string{}
+	intermediateValues[1]["location"] = "us-central1-c"
 
 	// Create configs from intermediate values
 	for _, values := range intermediateValues {
