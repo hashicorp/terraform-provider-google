2462bddcc1346e136fd5e94ac8d39dcb40dec357
Modular Magician <magic-modules@google.com>
2026-04-09 20:00:52 +0000

    Adding apigee space (#16328)
    Co-authored-by: Riley Karson <rileykarson@google.com>
    
    [upstream:82d7e80f1835a24af71e46de5992767f5b86cdc8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/provider_mmv1_resources.go
+++ google/provider/provider_mmv1_resources.go
@@ -620,9 +620,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 733
+// Generated resources: 734
 // Generated IAM resources: 342
-// Total generated resources: 1075
+// Total generated resources: 1076
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     registry.Resource("google_folder_access_approval_settings"),
 	"google_organization_access_approval_settings":                               registry.Resource("google_organization_access_approval_settings"),
@@ -682,6 +682,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_apigee_security_feedback":                                            registry.Resource("google_apigee_security_feedback"),
 	"google_apigee_security_monitoring_condition":                                registry.Resource("google_apigee_security_monitoring_condition"),
 	"google_apigee_security_profile_v2":                                          registry.Resource("google_apigee_security_profile_v2"),
+	"google_apigee_space":                                                        registry.Resource("google_apigee_space"),
 	"google_apigee_sync_authorization":                                           registry.Resource("google_apigee_sync_authorization"),
 	"google_apigee_target_server":                                                registry.Resource("google_apigee_target_server"),
 	"google_apihub_api_hub_instance":                                             registry.Resource("google_apihub_api_hub_instance"),
