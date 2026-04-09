bbe51741cfad2cd1f4b67e9ea3fe9507b9b7260b
Modular Magician <magic-modules@google.com>
2026-04-09 15:39:46 +0000

    `apigee`: resolve identity panics not caught by VCR (#17028)
    [upstream:4a20a4d397919ec269dd1fcad493d3a5c0b618e8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_security_monitoring_condition.go
+++ google/services/apigee/resource_apigee_security_monitoring_condition.go
@@ -113,25 +113,6 @@ func ResourceApigeeSecurityMonitoringCondition() *schema.Resource {
 			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
-		Identity: &schema.ResourceIdentity{
-			Version: 1,
-			SchemaFunc: func() map[string]*schema.Schema {
-				return map[string]*schema.Schema{
-					"org_id": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-					},
-					"condition_id": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-					},
-				}
-			},
-		},
-		ResourceBehavior: schema.ResourceBehavior{
-			MutableIdentity: true,
-		},
-
 		Schema: map[string]*schema.Schema{
 			"condition_id": {
 				Type:        schema.TypeString,
@@ -259,22 +240,6 @@ func resourceApigeeSecurityMonitoringConditionCreate(d *schema.ResourceData, met
 	}
 	d.SetId(id)
 
-	identity, err := d.Identity()
-	if err == nil && identity != nil {
-		if orgIdValue, ok := d.GetOk("org_id"); ok && orgIdValue.(string) != "" {
-			if err = identity.Set("org_id", orgIdValue.(string)); err != nil {
-				return fmt.Errorf("Error setting org_id: %s", err)
-			}
-		}
-		if conditionIdValue, ok := d.GetOk("condition_id"); ok && conditionIdValue.(string) != "" {
-			if err = identity.Set("condition_id", conditionIdValue.(string)); err != nil {
-				return fmt.Errorf("Error setting condition_id: %s", err)
-			}
-		}
-	} else {
-		log.Printf("[DEBUG] (Create) identity not set: %s", err)
-	}
-
 	log.Printf("[DEBUG] Finished creating SecurityMonitoringCondition %q: %#v", d.Id(), res)
 
 	return resourceApigeeSecurityMonitoringConditionRead(d, meta)
@@ -339,24 +304,6 @@ func resourceApigeeSecurityMonitoringConditionRead(d *schema.ResourceData, meta 
 		return fmt.Errorf("Error reading SecurityMonitoringCondition: %s", err)
 	}
 
-	identity, err := d.Identity()
-	if err == nil && identity != nil {
-		if v, ok := identity.GetOk("org_id"); !ok && v == "" {
-			err = identity.Set("org_id", d.Get("org_id").(string))
-			if err != nil {
-				return fmt.Errorf("Error setting org_id: %s", err)
-			}
-		}
-		if v, ok := identity.GetOk("condition_id"); !ok && v == "" {
-			err = identity.Set("condition_id", d.Get("condition_id").(string))
-			if err != nil {
-				return fmt.Errorf("Error setting condition_id: %s", err)
-			}
-		}
-	} else {
-		log.Printf("[DEBUG] (Read) identity not set: %s", err)
-	}
-
 	return nil
 }
 
@@ -365,21 +312,6 @@ func resourceApigeeSecurityMonitoringConditionUpdate(d *schema.ResourceData, met
 	userAgent, err := tpgresource.GenerateUserAgentString(d, config.UserAgent)
 	if err != nil {
 		return err
-	}
-	identity, err := d.Identity()
-	if err == nil && identity != nil {
-		if orgIdValue, ok := d.GetOk("org_id"); ok && orgIdValue.(string) != "" {
-			if err = identity.Set("org_id", orgIdValue.(string)); err != nil {
-				return fmt.Errorf("Error setting org_id: %s", err)
-			}
-		}
-		if conditionIdValue, ok := d.GetOk("condition_id"); ok && conditionIdValue.(string) != "" {
-			if err = identity.Set("condition_id", conditionIdValue.(string)); err != nil {
-				return fmt.Errorf("Error setting condition_id: %s", err)
-			}
-		}
-	} else {
-		log.Printf("[DEBUG] (Update) identity not set: %s", err)
 	}
 
 	billingProject := ""
