d6ddcb2f45100365bc9ba0093139775be87cfb8c
The Magician <magic-modules@google.com>
2026-04-15 11:38:17 -0700

    add round-trip marshalling in mapHashId - fixes artifactRegistry tests (#17118) (#26940)
    [upstream:57d6148ed642b4bfb55c4cd6338a856f903f4e25]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/artifactregistry/resource_artifact_registry_repository.go
+++ google/services/artifactregistry/resource_artifact_registry_repository.go
@@ -125,9 +125,22 @@ func durationDiffSuppress(k, oldr, newr string, d *schema.ResourceData) bool {
 }
 
 func mapHashID(v any) int {
-	replaceNestedValue(v, []string{"condition", "older_than"}, expandDuration)
-	replaceNestedValue(v, []string{"condition", "newer_than"}, expandDuration)
-	return schema.HashString(fmt.Sprintf("%v", v))
+	// v's dynamic type can differ between config and state, so we need to marshal and unmarshal to ensure consistent key order.
+	b, err := json.Marshal(v)
+	if err != nil {
+		return schema.HashString(fmt.Sprintf("%v", v))
+	}
+	var c any
+	if err := json.Unmarshal(b, &c); err != nil {
+		return schema.HashString(fmt.Sprintf("%v", v))
+	}
+	m, ok := c.(map[string]any)
+	if !ok {
+		return schema.HashString(fmt.Sprintf("%v", v))
+	}
+	replaceNestedValue(m, []string{"condition", "older_than"}, expandDuration)
+	replaceNestedValue(m, []string{"condition", "newer_than"}, expandDuration)
+	return schema.HashString(fmt.Sprintf("%v", m))
 }
 
 func expandDuration(v any) (any, bool) {
