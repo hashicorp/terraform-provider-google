7d9fe5402fd2dc9b4915b4bbb032275fb764d360
Modular Magician <magic-modules@google.com>
2026-04-17 22:19:32 +0000

    add o+c to multi_tenant in google_identity_platform_config (#17175)
    [upstream:d3e73f9c9eed970764e8bc4e762d1fa7e48f1ba3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/identityplatform/resource_identity_platform_config_test.go
+++ google/services/identityplatform/resource_identity_platform_config_test.go
@@ -27,7 +27,6 @@ import (
 )
 
 func TestAccIdentityPlatformConfig_update(t *testing.T) {
-	acctest.SkipIfVcr(t)
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -43,6 +42,15 @@ func TestAccIdentityPlatformConfig_update(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIdentityPlatformConfig_basic(context),
+			},
+			{
+				ResourceName:            "google_identity_platform_config.basic",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"client.0.api_key", "client.0.firebase_subdomain"},
+			},
+			{
+				Config: testAccIdentityPlatformConfig_disallowTenant(context),
 			},
 			{
 				ResourceName:            "google_identity_platform_config.basic",
@@ -138,6 +146,88 @@ resource "google_identity_platform_config" "basic" {
     allow_tenants           = true
     default_tenant_location = "organizations/%{org_id}"
   }
+
+  depends_on = [google_project_service.identitytoolkit]
+}
+`, context)
+}
+
+func testAccIdentityPlatformConfig_disallowTenant(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_project" "basic" {
+  project_id = "tf-test-my-project%{random_suffix}"
+  name       = "tf-test-my-project%{random_suffix}"
+  org_id     = "%{org_id}"
+  billing_account =  "%{billing_acct}"
+  deletion_policy = "DELETE"
+  labels = {
+    firebase = "enabled"
+  }
+}
+
+resource "google_project_service" "identitytoolkit" {
+  project = google_project.basic.project_id
+  service = "identitytoolkit.googleapis.com"
+}
+
+resource "google_identity_platform_config" "basic" {
+  project = google_project.basic.project_id
+  autodelete_anonymous_users = true
+  sign_in {
+    allow_duplicate_emails = true
+
+    anonymous {
+        enabled = true
+    }
+    email {
+        enabled = true
+        password_required = false
+    }
+    phone_number {
+        enabled = true
+        test_phone_numbers = {
+            "+11231231234" = "000000"
+        }
+    }
+  }
+  sms_region_config {
+    allow_by_default {
+      disallowed_regions = [
+        "CA",
+        "US",
+      ]
+    }
+  }
+
+  client {
+    permissions {
+      disabled_user_deletion = true
+      disabled_user_signup   = true
+    }
+  }
+
+  mfa {
+    enabled_providers = ["PHONE_SMS"]
+    provider_configs {
+      state = "ENABLED"
+      totp_provider_config {
+        adjacent_intervals = 3
+      }
+    }
+    state = "ENABLED"
+  }
+
+  monitoring {
+    request_logging {
+      enabled = true
+    }
+  }
+
+  multi_tenant {
+    allow_tenants           = false
+  }
+
+  depends_on = [google_project_service.identitytoolkit]
 }
 `, context)
 }
@@ -204,6 +294,8 @@ resource "google_identity_platform_config" "basic" {
       enabled = false
     }
   }
+
+  depends_on = [google_project_service.identitytoolkit]
 }
 `, context)
 }
