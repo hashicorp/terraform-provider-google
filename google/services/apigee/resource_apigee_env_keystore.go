88395dcbab9a7cc000eb33928899d4018c93061e
The Magician <magic-modules@google.com>
2026-04-20 10:55:12 -0700

    apigee: make  field required on google_apigee_env_keystore (#16962) (#27006)
    [upstream:2c134215c79bf75e2ce967c6cc52828ac0dbde89]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_env_keystore.go
+++ google/services/apigee/resource_apigee_env_keystore.go
@@ -121,7 +121,7 @@ func ResourceApigeeEnvKeystore() *schema.Resource {
 					},
 					"name": {
 						Type:              schema.TypeString,
-						OptionalForImport: true,
+						RequiredForImport: true,
 					},
 				}
 			},
@@ -140,7 +140,7 @@ in the format 'organizations/{{org_name}}/environments/{{env_name}}'.`,
 			},
 			"name": {
 				Type:        schema.TypeString,
-				Optional:    true,
+				Required:    true,
 				ForceNew:    true,
 				Description: `The name of the newly created keystore.`,
 			},
