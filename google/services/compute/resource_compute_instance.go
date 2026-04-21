06384283efe958665cb537b25e7047280a717aad
The Magician <magic-modules@google.com>
2026-04-21 14:00:48 -0700

    feat: add erase_windows_vss_signature to compute instance (#16859) (#27034)
    [upstream:59e9b08d8bdf14afe89de390bcbabc98e81f5f46]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_instance.go
+++ google/services/compute/resource_compute_instance.go
@@ -3328,6 +3328,7 @@ func resourceComputeInstanceImportState(d *schema.ResourceData, meta interface{}
 	if err != nil {
 		return nil, fmt.Errorf("Error constructing id: %s", err)
 	}
+
 	d.SetId(id)
 
 	return []*schema.ResourceData{d}, nil
