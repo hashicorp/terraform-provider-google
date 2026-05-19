c09193b7f09673ac99a37e973dcdb26e541c20c9
The Magician <magic-modules@google.com>
2026-05-19 10:41:33 -0700

    Add skeleton for RPC support (#17572) (#27462)
    [upstream:7fade1b381a060134541a96c4d8b3d204181f52e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/transport/transport.go
+++ google/transport/transport.go
@@ -43,6 +43,9 @@ type SendRequestOptions struct {
 	Headers              http.Header
 	ErrorRetryPredicates []RetryErrorPredicateFunc
 	ErrorAbortPredicates []RetryErrorPredicateFunc
+	// RPC related opts
+	Product    string
+	RPCService string
 }
 
 func SendRequest(opt SendRequestOptions) (map[string]interface{}, error) {
@@ -129,6 +132,110 @@ func SendRequest(opt SendRequestOptions) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
 		return nil, err
+	}
+
+	return result, nil
+}
+
+func SendRequestRPC(opt SendRequestOptions) (map[string]interface{}, error) {
+	if opt.Config == nil || opt.Config.Client == nil {
+		return nil, fmt.Errorf("http client is nil for request to rpc proxy")
+	}
+	if opt.Product == "" || opt.Config.RPCClients[opt.Product] == nil {
+		return nil, fmt.Errorf("rpc client is nil for request to rpc proxy; product is %q", opt.Product)
+	}
+
+	reqHeaders := opt.Headers
+	if reqHeaders == nil {
+		reqHeaders = make(http.Header)
+	}
+	reqHeaders.Set("User-Agent", opt.UserAgent)
+	reqHeaders.Set("Content-Type", "application/json")
+
+	if opt.Timeout == 0 {
+		opt.Timeout = DefaultRequestTimeout
+	}
+
+	var res *http.Response
+	err := Retry(RetryOptions{
+		RetryFunc: func() error {
+			var reqJsonStr string
+			if opt.Body != nil {
+				b, err := json.Marshal(opt.Body)
+				if err != nil {
+					return err
+				}
+				reqJsonStr = string(b)
+			}
+
+			reqBody := map[string]interface{}{
+				"targetAddress": opt.Config.RPCClients[opt.Product].Address,
+				"packageName":   opt.Config.RPCClients[opt.Product].Package,
+				"serviceName":   opt.RPCService,
+				"methodName":    opt.Method,
+				"requestJson":   reqJsonStr,
+			}
+
+			var buf bytes.Buffer
+			if reqBody != nil {
+				err := json.NewEncoder(&buf).Encode(reqBody)
+				if err != nil {
+					return err
+				}
+			}
+
+			req, err := http.NewRequest("POST", opt.Config.RPCClients[opt.Product].ProxyAddress+"/handleRPC", &buf)
+			if err != nil {
+				return err
+			}
+
+			req.Header = reqHeaders
+			res, err = opt.Config.Client.Do(req)
+			if err != nil {
+				return err
+			}
+
+			if err := googleapi.CheckResponse(res); err != nil {
+				googleapi.CloseBody(res)
+				return err
+			}
+
+			return nil
+		},
+		Timeout:              opt.Timeout,
+		ErrorRetryPredicates: opt.ErrorRetryPredicates,
+		ErrorAbortPredicates: opt.ErrorAbortPredicates,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if res == nil {
+		return nil, fmt.Errorf("Unable to parse server response. This is most likely a terraform problem, please file a bug at https://github.com/hashicorp/terraform-provider-google/issues.")
+	}
+
+	// The defer call must be made outside of the retryFunc otherwise it's closed too soon.
+	defer googleapi.CloseBody(res)
+
+	// 204 responses will have no body, so we're going to error with "EOF" if we
+	// try to parse it. Instead, we can just return nil.
+	if res.StatusCode == 204 {
+		return nil, nil
+	}
+	var proxyResult map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&proxyResult); err != nil {
+		return nil, err
+	}
+
+	if errMsg, ok := proxyResult["errorMessage"].(string); ok && errMsg != "" {
+		return nil, fmt.Errorf("RPC proxy returned error: %s", errMsg)
+	}
+
+	var result map[string]interface{}
+	if respJsonStr, ok := proxyResult["responseJson"].(string); ok && respJsonStr != "" {
+		if err := json.Unmarshal([]byte(respJsonStr), &result); err != nil {
+			return nil, fmt.Errorf("Failed to unmarshal responseJson from proxy: %v", err)
+		}
 	}
 
 	return result, nil
