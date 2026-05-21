2dbad842b41d9342e8788e14042298aee337829a
The Magician <magic-modules@google.com>
2026-05-21 09:44:29 -0700

    `project`: add `ResourceIdentity` to iam_binding (#17587) (#27502)
    [upstream:06cf1c669616ef716db46d3b9faa94c9d9eb2680]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/resourcemanager/resource_google_project_iam_binding_test.go
+++ google/services/resourcemanager/resource_google_project_iam_binding_test.go
@@ -25,6 +25,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func projectIamBindingImportStep(resourceName, pid, role string) resource.TestStep {
@@ -45,6 +46,9 @@ func TestAccProjectIamBinding_basic(t *testing.T) {
 	role := "roles/compute.instanceAdmin"
 	member := "user:admin@hashicorptest.com"
 	acctest.VcrTest(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_12_0),
+		},
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
@@ -60,6 +64,11 @@ func TestAccProjectIamBinding_basic(t *testing.T) {
 				Config: testAccProjectAssociateBindingBasic(pid, org, role, member),
 			},
 			projectIamBindingImportStep("google_project_iam_binding.acceptance", pid, role),
+			{
+				ResourceName:    "google_project_iam_binding.acceptance",
+				ImportState:     true,
+				ImportStateKind: resource.ImportBlockWithResourceIdentity,
+			},
 		},
 	})
 }
