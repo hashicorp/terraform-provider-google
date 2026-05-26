aefc1bbc72282902473821659a819348ed529bd3
Modular Magician <magic-modules@google.com>
2026-05-26 16:56:40 +0000

    Made universe domain test explicitly depend on pubsub product (#17654)
    [upstream:34c6a168372d5a8f90f0a1946b0ebe83528464a2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/provider/universe/universe_domain_pubsub_test.go
+++ google/provider/universe/universe_domain_pubsub_test.go
@@ -26,7 +26,7 @@ import (
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
-	_ "github.com/hashicorp/terraform-provider-google/google/services/pubsub"
+	"github.com/hashicorp/terraform-provider-google/google/services/pubsub"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
@@ -88,7 +88,7 @@ func testAccCheckPubsubSubscriptionDestroyProducer(t *testing.T) func(s *terrafo
 
 			config := acctest.GoogleProviderConfig(t)
 
-			url, err := tpgresource.ReplaceVarsForTest(config, rs, "{{PubsubBasePath}}projects/{{project}}/subscriptions/{{name}}")
+			url, err := tpgresource.ReplaceVarsForTest(config, rs, transport_tpg.BaseUrl(pubsub.Product, config)+"projects/{{project}}/subscriptions/{{name}}")
 			if err != nil {
 				return err
 			}
