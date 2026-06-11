5fc4b48b729f07070e0d7e849a8711c3473aca0d
Modular Magician <magic-modules@google.com>
2026-06-11 20:04:17 +0000

    apigee: fix google_apigee_api detect_md5hash default and sweeper list URL (#17857)
    Signed-off-by: Xuchen Ma <xuchenm@google.com>
    
    [upstream:e9111f58ab5c96c51685e4f88abb97102f363419]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_api_test.go
+++ google/services/apigee/resource_apigee_api_test.go
@@ -144,6 +144,7 @@ resource "google_apigee_api" "test_apigee_api" {
   name            = "tf-test-apigee-api"
   org_id          = google_project.project.project_id
   config_bundle   = "./test-fixtures/apigee_api_bundle.zip"
+  detect_md5hash  = filemd5("./test-fixtures/apigee_api_bundle.zip")
   depends_on      = [google_apigee_organization.apigee_org]
 }
 `, context)
@@ -256,6 +257,7 @@ resource "google_apigee_api" "test_apigee_api" {
   name            = "tf-test-apigee-api"
   org_id          = google_project.project.project_id
   config_bundle   = "./test-fixtures/apigee_api_bundle2.zip"
+  detect_md5hash  = filemd5("./test-fixtures/apigee_api_bundle2.zip")
   depends_on      = [google_apigee_organization.apigee_org]
 }
 `, context)
