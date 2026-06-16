bb1247be4f8822a7f870cfc1609898ef9c6db0e0
Modular Magician <magic-modules@google.com>
2026-06-16 15:53:00 +0000

    `kms`: resource identity support for `crypto_key_version` resource (#17764)
    [upstream:23acecb1a41f0a9f4cefd3c71bb055cf25b71080]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/kms/resource_kms_crypto_key_version.go
+++ google/services/kms/resource_kms_crypto_key_version.go
@@ -571,8 +571,22 @@ func resourceKMSCryptoKeyVersionDelete(d *schema.ResourceData, meta interface{})
 func resourceKMSCryptoKeyVersionImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 
 	config := meta.(*transport_tpg.Config)
+	importId := d.Id()
+	if importId == "" {
+		identity, err := d.Identity()
+		if err != nil {
+			return nil, fmt.Errorf("Error reading import identity: %s", err)
+		}
 
-	cryptoKeyVersionId, err := parseKmsCryptoKeyVersionId(d.Id(), config)
+		identityName, ok := identity.Get("name").(string)
+		if !ok || identityName == "" {
+			return nil, fmt.Errorf("Error reading import identity: missing required identity field \"name\"")
+		}
+
+		importId = identityName
+	}
+
+	cryptoKeyVersionId, err := parseKmsCryptoKeyVersionId(importId, config)
 	if err != nil {
 		return nil, err
 	}
