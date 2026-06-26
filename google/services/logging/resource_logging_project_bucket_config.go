091c9757e10ad46938aede76c41eeb2f978ac73d
Modular Magician <magic-modules@google.com>
2026-06-26 19:05:30 +0000

    Propagate correct error (#18072)
    [upstream:39dba24486b078e6597e5a6f705de7c53c9c729c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/logging/resource_logging_project_bucket_config.go
+++ google/services/logging/resource_logging_project_bucket_config.go
@@ -377,12 +377,15 @@ func resourceLoggingProjectBucketConfigUpdate(d *schema.ResourceData, meta inter
 			Body:      obj,
 			Timeout:   d.Timeout(schema.TimeoutUpdate),
 		})
+		if err != nil {
+			return err
+		}
 
 		var opRes map[string]interface{}
 		// Wait for the operation to complete
 		waitErr := LoggingOperationWaitTimeWithResponse(config, res, &opRes, "Updating Bucket with analytics", userAgent, d.Timeout(schema.TimeoutCreate))
 		if waitErr != nil {
-			return fmt.Errorf("Error updating Logging Bucket Config %q: %s", d.Id(), err)
+			return fmt.Errorf("Error updating Logging Bucket Config %q: %s", d.Id(), waitErr)
 		}
 	}
 
