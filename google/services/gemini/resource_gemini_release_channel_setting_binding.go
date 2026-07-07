dfcd0ea555656cb9ec6fa504d5246bf223e7879d
Modular Magician <magic-modules@google.com>
2026-07-07 17:56:21 +0000

    Fallback to async timeouts if top level timeouts are not specified (#18190)
    [upstream:3a06d8605d56601c8f6e9f4ca5d2988e26c77693]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/gemini/resource_gemini_release_channel_setting_binding.go
+++ google/services/gemini/resource_gemini_release_channel_setting_binding.go
@@ -107,9 +107,9 @@ func ResourceGeminiReleaseChannelSettingBinding() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(20 * time.Minute),
-			Update: schema.DefaultTimeout(20 * time.Minute),
-			Delete: schema.DefaultTimeout(20 * time.Minute),
+			Create: schema.DefaultTimeout(90 * time.Minute),
+			Update: schema.DefaultTimeout(90 * time.Minute),
+			Delete: schema.DefaultTimeout(90 * time.Minute),
 		},
 
 		CustomizeDiff: customdiff.All(
