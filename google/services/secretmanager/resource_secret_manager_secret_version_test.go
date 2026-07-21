bc3354c33d7da81affa64b3d7a6a9bca9453ced3
The Magician <magic-modules@google.com>
2026-07-21 12:27:38 -0700

    secretmanager: fixed secret_data/secret_data_wo constraint allowing neither to be set (#18320) (#28419)
    [upstream:3dc409222b873a58e7cabbe79760bd70afb4708d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/secretmanager/resource_secret_manager_secret_version_test.go
+++ google/services/secretmanager/resource_secret_manager_secret_version_test.go
@@ -17,6 +17,7 @@
 package secretmanager_test
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -92,6 +93,43 @@ func TestAccSecretManagerSecretVersion_byName(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccSecretManagerSecretVersion_neitherSecretDataSet(t *testing.T) {
+	acctest.SkipIfVcr(t)
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccSecretManagerSecretVersion_noSecretData(context),
+				ExpectError: regexp.MustCompile(`Invalid combination of arguments`),
+			},
+		},
+	})
+}
+
+func testAccSecretManagerSecretVersion_noSecretData(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "tf-test-secret-version-%{random_suffix}"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  secret = google_secret_manager_secret.secret-basic.name
+  secret_data_wo_version = 3
+}
+`, context)
 }
 
 func testAccSecretManagerSecretVersion_basic(context map[string]interface{}) string {
