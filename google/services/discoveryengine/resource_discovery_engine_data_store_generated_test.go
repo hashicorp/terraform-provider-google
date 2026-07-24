7c1b537e7215fc9102b94cec4e3cfbc1cad00390
Modular Magician <magic-modules@google.com>
2026-07-24 17:13:19 +0000

    discoveryengine: add acl_enabled to google_discovery_engine_data_store (#18105)
    [upstream:31615eb51a63d54ee8bcb84fcd47007a93075280]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/discoveryengine/resource_discovery_engine_data_store_generated_test.go
+++ google/services/discoveryengine/resource_discovery_engine_data_store_generated_test.go
@@ -459,6 +459,61 @@ resource "google_discovery_engine_data_store" "advanced_site_search_config" {
 `, context)
 }
 
+func TestAccDiscoveryEngineDataStore_discoveryengineDatastoreAclConfigExample(t *testing.T) {
+	t.Parallel()
+
+	randomSuffix := acctest.RandString(t, 10)
+
+	context := map[string]interface{}{
+		"data_store_id": "tf-test-data-store-id" + randomSuffix,
+		"random_suffix": randomSuffix,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckDiscoveryEngineDataStoreDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDiscoveryEngineDataStore_discoveryengineDatastoreAclConfigExample(context),
+			},
+			{
+				ResourceName:            "google_discovery_engine_data_store.acl_config",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"create_advanced_site_search", "data_store_id", "kms_key_name", "location", "skip_default_schema_creation"},
+			},
+			{
+				ResourceName:       "google_discovery_engine_data_store.acl_config",
+				RefreshState:       true,
+				ExpectNonEmptyPlan: true,
+				ImportStateKind:    resource.ImportBlockWithResourceIdentity,
+			},
+		},
+	})
+}
+
+func testAccDiscoveryEngineDataStore_discoveryengineDatastoreAclConfigExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_discovery_engine_data_store" "acl_config" {
+  location                     = "global"
+  data_store_id                = "%{data_store_id}"
+  display_name                 = "tf-test-acl-datastore"
+  industry_vertical            = "GENERIC"
+  content_config               = "CONTENT_REQUIRED"
+  solution_types               = ["SOLUTION_TYPE_SEARCH"]
+  create_advanced_site_search  = false
+  acl_enabled                  = true
+
+  document_processing_config {
+    default_parsing_config {
+      digital_parsing_config {}
+    }
+  }
+}
+`, context)
+}
+
 func testAccCheckDiscoveryEngineDataStoreDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
