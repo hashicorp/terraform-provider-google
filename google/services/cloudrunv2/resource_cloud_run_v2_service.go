6de7c4d41f69d9f371a1a5b1266ad5871e0e24bc
The Magician <magic-modules@google.com>
2026-08-04 12:49:29 -0700

    `cloudrunv2`: permadiff fix - set `template.scaling.maxInstanceCount` to `default_from_api: true` (#18394) (#28644)
    [upstream:e287df708c8d77ed591ba71541fbae10c84890ed]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/cloudrunv2/resource_cloud_run_v2_service.go
+++ google/services/cloudrunv2/resource_cloud_run_v2_service.go
@@ -725,6 +725,7 @@ If not specified or 0, defaults to 80 when requested CPU >= 1 and defaults to 1 
 								Schema: map[string]*schema.Schema{
 									"max_instance_count": {
 										Type:     schema.TypeInt,
+										Computed: true,
 										Optional: true,
 										Description: `Maximum number of serving instances that this resource should have. Must not be less than minimum instance count. If absent, Cloud Run will calculate
 a default value based on the project's available container instances quota in the region and specified instance size.`,
