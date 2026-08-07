93dcace9791a358d2e61b953297249aa49a0ca66
The Magician <magic-modules@google.com>
2026-08-07 15:33:13 -0700

    Add support for scheduleCustomizations in the rule deployment resource. (#18364) (#28715)
    [upstream:562bc8623e4f25e618a3ae336496649ab0d780b1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/chronicle/resource_chronicle_rule_deployment_test.go
+++ google/services/chronicle/resource_chronicle_rule_deployment_test.go
@@ -77,7 +77,7 @@ resource "google_chronicle_rule_deployment" "example" {
  enabled = true
  alerting = true
  archived = false
- run_frequency = "DAILY"
+ run_frequency = "LIVE"
 }
 `, context)
 }
@@ -99,7 +99,7 @@ resource "google_chronicle_rule_deployment" "example" {
  enabled = false
  alerting = false
  archived = false
- run_frequency = "HOURLY"
+ run_frequency = "LIVE"
 }
 `, context)
 }
