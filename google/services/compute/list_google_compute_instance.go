0be9801957d29fdfc5dd907531372070c9784862
The Magician <magic-modules@google.com>
2026-08-17 15:29:33 -0700

    Revert "Migrate resource_compute_instance to new API (#18442)" (#18672) (#28812)
    [upstream:970d2fa3ab9a5795a5205b82449b8281ab0d4f9e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/list_google_compute_instance.go
+++ google/services/compute/list_google_compute_instance.go
@@ -27,6 +27,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
+	"google.golang.org/api/compute/v1"
+
 	"github.com/hashicorp/terraform-provider-google/google/registry"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -102,16 +104,18 @@ func (listR *GoogleComputeInstanceListResource) List(ctx context.Context, listRe
 }
 
 func flattenComputeInstanceListItem(res map[string]interface{}, d *schema.ResourceData, config *transport_tpg.Config, project string) error {
-	instanceName, _ := res["name"].(string)
-	if instanceName == "" {
+	var instance compute.Instance
+	if err := tpgresource.Convert(res, &instance); err != nil {
+		return fmt.Errorf("error converting compute instance list response: %w", err)
+	}
+	if instance.Name == "" {
 		return fmt.Errorf("missing name in compute instance list response")
 	}
 
-	instanceZone, _ := res["zone"].(string)
-	zone := tpgresource.GetResourceNameFromSelfLink(instanceZone)
+	zone := tpgresource.GetResourceNameFromSelfLink(instance.Zone)
 
-	d.SetId(fmt.Sprintf("projects/%s/zones/%s/instances/%s", project, zone, instanceName))
-	return populateComputeInstanceResourceData(d, res, project, zone, config)
+	d.SetId(fmt.Sprintf("projects/%s/zones/%s/instances/%s", project, zone, instance.Name))
+	return populateComputeInstanceResourceData(d, &instance, project, zone, config)
 }
 
 func ListComputeInstances(config *transport_tpg.Config, project, zone string, callback func(rd *schema.ResourceData) error) error {
