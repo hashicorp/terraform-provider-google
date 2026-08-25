9b935fdf04c74e2b712919019052126f90ff9e72
The Magician <magic-modules@google.com>
2026-08-25 12:57:52 -0700

    apigee: add list resource for google_apigee_addons_config (#18478) (#29039)
    [upstream:a7a379a8df51743b9a5ea7f5488be0d2aeb739df]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/apigee/resource_apigee_addons_config_sweeper.go
+++ google/services/apigee/resource_apigee_addons_config_sweeper.go
@@ -142,9 +142,9 @@ func listAndActionApigeeAddonsConfig(action sweeper.ResourceAction) error {
 		}
 
 		// First try the expected resource key
-		resourceList, ok := res["addonsConfigs"]
+		resourceList, ok := res["organizations"]
 		if ok {
-			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'addonsConfigs'")
+			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'organizations'")
 		} else {
 			// Next, try the common "items" pattern
 			resourceList, ok = res["items"]
