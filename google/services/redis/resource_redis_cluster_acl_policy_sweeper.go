d5257bb704669d59672a739fdbe41fed664aae6e
The Magician <magic-modules@google.com>
2026-08-25 12:18:29 -0700

    Fixed API mapping for google_redis_cluster_acl_policy (#18776) (#29032)
    [upstream:11b4c629657f8dc9e5fb8612e5759d0730ceecf8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/redis/resource_redis_cluster_acl_policy_sweeper.go
+++ google/services/redis/resource_redis_cluster_acl_policy_sweeper.go
@@ -142,9 +142,9 @@ func listAndActionRedisClusterAclPolicy(action sweeper.ResourceAction) error {
 		}
 
 		// First try the expected resource key
-		resourceList, ok := res["clusterAclPolicies"]
+		resourceList, ok := res["aclPolicies"]
 		if ok {
-			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'clusterAclPolicies'")
+			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'aclPolicies'")
 		} else {
 			// Next, try the common "items" pattern
 			resourceList, ok = res["items"]
