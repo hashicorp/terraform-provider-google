9ff5df08fddcb21e921c09acb7c25455e854609a
The Magician <magic-modules@google.com>
2026-09-09 11:02:23 -0700

    ces: add transfer_rules to google_ces_agent (#18896) (#29262)
    [upstream:8d02414480b309f7c785365ed8a6130caff00d1a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/ces/ces_agent_test.go
+++ google/services/ces/ces_agent_test.go
@@ -235,6 +235,26 @@ resource "google_ces_agent" "ces_agent_basic" {
 
   child_agents = ["projects/${google_ces_app.ces_app_for_agent.project}/locations/us/apps/${google_ces_app.ces_app_for_agent.app_id}/agents/${google_ces_agent.ces_child_agent.agent_id}"]
 
+  transfer_rules {
+    child_agent = "projects/${google_ces_app.ces_app_for_agent.project}/locations/us/apps/${google_ces_app.ces_app_for_agent.app_id}/agents/${google_ces_agent.ces_child_agent.agent_id}"
+    direction   = "PARENT_TO_CHILD"
+    deterministic_transfer {
+      expression_condition {
+        expression = "true"
+      }
+    }
+  }
+
+  transfer_rules {
+    child_agent = "projects/${google_ces_app.ces_app_for_agent.project}/locations/us/apps/${google_ces_app.ces_app_for_agent.app_id}/agents/${google_ces_agent.ces_child_agent.agent_id}"
+    direction   = "PARENT_TO_CHILD"
+    disable_planner_transfer {
+      expression_condition {
+        expression = "false"
+      }
+    }
+  }
+
   llm_agent {}
 }
 `, context)
@@ -403,6 +423,16 @@ resource "google_ces_agent" "ces_agent_basic" {
   }
 
   child_agents = ["projects/${google_ces_app.ces_app_for_agent.project}/locations/us/apps/${google_ces_app.ces_app_for_agent.app_id}/agents/${google_ces_agent.ces_child_agent.agent_id}"]
+
+  transfer_rules {
+    child_agent = "projects/${google_ces_app.ces_app_for_agent.project}/locations/us/apps/${google_ces_app.ces_app_for_agent.app_id}/agents/${google_ces_agent.ces_child_agent.agent_id}"
+    direction   = "CHILD_TO_PARENT"
+    deterministic_transfer {
+      python_code_condition {
+        python_code = "def should_trigger_transfer_callback(callback_context): return True"
+      }
+    }
+  }
 
   llm_agent {}
 }
