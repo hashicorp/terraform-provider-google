8c7172d0fd97f781a5d6efd7e5f5e08f91762c07
Modular Magician <magic-modules@google.com>
2026-09-11 17:00:34 +0000

    ces: add tool_overrides to google_ces_toolset (#18955)
    [upstream:bef0386957228c6ae19019cf18788ad9ea79b0b5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/ces/ces_toolset_test.go
+++ google/services/ces/ces_toolset_test.go
@@ -1421,6 +1421,11 @@ resource "google_ces_toolset" "ces_toolset_mcp_service_agent_id_token_auth_confi
     api_authentication {
         service_agent_id_token_auth_config {}
     }
+    tool_overrides {
+      tool = "my-tool"
+      name_override = "my_tool_override"
+      description_override = "A tool description override"
+    }
   }
 }
 `, context)
@@ -1464,6 +1469,11 @@ resource "google_ces_toolset" "ces_toolset_mcp_service_agent_id_token_auth_confi
     }
     api_authentication {
         service_agent_id_token_auth_config {}
+    }
+    tool_overrides {
+      tool = "my-tool"
+      name_override = "my_tool_override_updated"
+      description_override = "A tool description override updated"
     }
   }
 }
