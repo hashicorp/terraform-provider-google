4e5ed58764099f4920bc18aa8b705ed95ad0cd0a
The Magician <magic-modules@google.com>
2026-09-15 15:17:59 -0700

    developerconnect: ignore update_time in TestAccDeveloperConnectInsightsConfig (#19019) (#29383)
    [upstream:17fa892e71cccb854f5287019a5e89b15cabcc08]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/developerconnect/resource_developer_connect_insights_config_test.go
+++ google/services/developerconnect/resource_developer_connect_insights_config_test.go
@@ -66,7 +66,7 @@ func TestAccDeveloperConnectInsightsConfig_update(t *testing.T) {
 				ResourceName:            "google_developer_connect_insights_config.insights_config",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"insights_config_id", "labels", "location", "terraform_labels", "workload"},
+				ImportStateVerifyIgnore: []string{"insights_config_id", "labels", "location", "terraform_labels", "update_time", "workload"},
 			},
 			{
 				Config: testAccDeveloperConnectInsightsConfig_update(context),
@@ -80,7 +80,7 @@ func TestAccDeveloperConnectInsightsConfig_update(t *testing.T) {
 				ResourceName:            "google_developer_connect_insights_config.insights_config",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"insights_config_id", "location", "labels", "terraform_labels", "workload"},
+				ImportStateVerifyIgnore: []string{"insights_config_id", "location", "labels", "terraform_labels", "update_time", "workload"},
 			},
 		},
 	})
