08f9b9ecabbdec641090be78c0f5a4801cbd5754
The Magician <magic-modules@google.com>
2026-09-16 15:54:02 -0700

    compute: Fix `TestAccComputeImage_basedondisk` (#19054) (#29402)
    [upstream:2783387d803bd62ce40e5121b97cb2a3261c5ffb]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/compute/resource_compute_image_test.go
+++ google/services/compute/resource_compute_image_test.go
@@ -105,6 +105,8 @@ func TestAccComputeImage_basedondisk(t *testing.T) {
 	t.Parallel()
 
 	var image map[string]interface{}
+	diskName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	imageName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
@@ -112,7 +114,7 @@ func TestAccComputeImage_basedondisk(t *testing.T) {
 		CheckDestroy:             testAccCheckComputeImageDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccComputeImage_basedondisk(acctest.RandString(t, 10), acctest.RandString(t, 10)),
+				Config: testAccComputeImage_basedondisk(diskName, imageName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckComputeImageExists(
 						t, "google_compute_image.foobar", &image),
