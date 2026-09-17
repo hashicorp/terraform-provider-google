4fec4a3eff70c213007dbb87c1e4ed6d8b2a3606
The Magician <magic-modules@google.com>
2026-09-17 16:42:11 -0700

    accesscontextmanager: remove unsupported max_inactivity from GcpUserAccessBinding group test (#19080) (#29434)
    [upstream:30724b061e62221decc10fe8eeff5c24c11a37d2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/accesscontextmanager/resource_access_context_manager_gcp_user_access_binding_test.go
+++ google/services/accesscontextmanager/resource_access_context_manager_gcp_user_access_binding_test.go
@@ -220,7 +220,6 @@ resource "google_access_context_manager_gcp_user_access_binding" "gcp_user_acces
         google_access_context_manager_access_level.tf_test_access_level_id_for_user_access_binding%{random_suffix}.name,
       ]
       session_settings {
-        max_inactivity = "400s"
         session_length = "3600s"
         session_length_enabled = true
         session_reauth_method = "LOGIN"
