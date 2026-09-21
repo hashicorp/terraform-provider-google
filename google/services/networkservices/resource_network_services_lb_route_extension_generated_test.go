96c0a34ead86e76654a56ea9104ebfba97130325
Modular Magician <magic-modules@google.com>
2026-09-21 22:15:33 +0000

    networkservices: make sample and test resources sweepable (#19032)
    [upstream:d522b796d6471748574c3d2f3f456fa10f1dd221]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/services/networkservices/resource_network_services_lb_route_extension_generated_test.go
+++ google/services/networkservices/resource_network_services_lb_route_extension_generated_test.go
@@ -163,7 +163,7 @@ resource "google_compute_region_target_http_proxy" "default" {
 
 # URL map
 resource "google_compute_region_url_map" "default" {
-  name            = "tf-test-l7-ilb-regional-url-map%{random_suffix}"
+  name            = "%{regional_url_map_name}"
   region          = "us-west1"
   default_service = google_compute_region_backend_service.default.id
 
@@ -574,7 +574,7 @@ resource "google_compute_region_target_http_proxy" "default" {
 
 # URL map
 resource "google_compute_region_url_map" "default" {
-  name            = "tf-test-l7-ilb-regional-url-map%{random_suffix}"
+  name            = "%{regional_url_map_name}"
   region          = "us-west1"
   default_service = google_compute_region_backend_service.default.id
 
